23f99fa784a60e007e735f4ca5304f2b59a3d015
Phani Yalamanchili <phani.y@sstech.us>
2012-04-17 12:27:36 +0000

    create employee first cut
    git-svn-id: http://tools.sstech.us/svn/office/trunk@235 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/CreateEmployeePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/CreateEmployeePanel.java
@@ -1,5 +1,6 @@
 package info.yalamanchili.office.client.profile;
 
+import info.yalamanchili.gwt.date.DateUtils;
 import info.yalamanchili.gwt.fields.DateField;
 import info.yalamanchili.gwt.fields.EnumField;
 import info.yalamanchili.gwt.fields.StringField;
@@ -18,8 +19,9 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 
-public class CreateEmp extends Composite implements ClickHandler {
-	private static Logger logger = Logger.getLogger(CreateEmp.class.getName());
+public class CreateEmployeePanel extends Composite implements ClickHandler {
+	private static Logger logger = Logger.getLogger(CreateEmployeePanel.class
+			.getName());
 
 	/*
 	 * Root panel for Create Employee
@@ -35,8 +37,8 @@ public class CreateEmp extends Composite implements ClickHandler {
 			"Employee", false, false);
 	StringField lastNameF = new StringField("Last Name", "lastName",
 			"Employee", false, true);
-	DateField dobF = new DateField("Date of Birth", "dateOfBirth", "Employee",
-			false, false);
+	DateField dateOfBirthF = new DateField("Date of Birth", "dateOfBirth",
+			"Employee", false, false);
 	String[] strs = { "MALE", "FEMALE" };
 	EnumField sexF = new EnumField("Sex", "sex", "Employee", false, false, strs);
 	DateField startDateF = new DateField("Start Date", "startDate", "Employee",
@@ -44,7 +46,7 @@ public class CreateEmp extends Composite implements ClickHandler {
 	StringField ssnF = new StringField("SSN", "ssn", "Employee", false, false);
 	Button createB = new Button("Create Employee");
 
-	public CreateEmp() {
+	public CreateEmployeePanel() {
 		initWidget(panel);
 		addWidgets();
 		addListeners();
@@ -54,7 +56,7 @@ public class CreateEmp extends Composite implements ClickHandler {
 		panel.add(firstNameF);
 		panel.add(middleNameF);
 		panel.add(lastNameF);
-		panel.add(dobF);
+		panel.add(dateOfBirthF);
 		panel.add(sexF);
 		panel.add(startDateF);
 		panel.add(ssnF);
@@ -76,6 +78,22 @@ public class CreateEmp extends Composite implements ClickHandler {
 	protected String getEmployee() {
 		JSONObject jsonValue = new JSONObject();
 		jsonValue.put("firstName", new JSONString(firstNameF.getText()));
+		jsonValue.put("middleName", new JSONString(middleNameF.getText()));
+		jsonValue.put("lastName", new JSONString(lastNameF.getText()));
+		if (dateOfBirthF.getDate() != null) {
+			jsonValue.put(
+					"dateOfBirth",
+					new JSONString(DateUtils.toDateString(dateOfBirthF
+							.getDate())));
+		}
+		jsonValue.put("sex", new JSONString(sexF.getValue()));
+		if (startDateF.getDate() != null) {
+			jsonValue
+					.put("startDate",
+							new JSONString(DateUtils.toDateString(startDateF
+									.getDate())));
+		}
+		jsonValue.put("ssn", new JSONString(ssnF.getText()));
 		logger.info(jsonValue.toString());
 		return jsonValue.toString();
 	}
@@ -102,4 +120,5 @@ public class CreateEmp extends Composite implements ClickHandler {
 
 				});
 	}
+
 }
--- office-web/src/main/java/info/yalamanchili/office/client/profile/ProfileHome.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/ProfileHome.java
@@ -8,6 +8,6 @@ public class ProfileHome extends Composite {
 
 	public ProfileHome() {
 		initWidget(profileHome);
-                profileHome.add(new CreateEmp());
+                profileHome.add(new CreateEmployeePanel());
 	}
 }
