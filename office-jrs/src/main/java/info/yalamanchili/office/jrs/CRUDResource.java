6a568908026e2ba49069c5faa9d56b05899b014f
Phani Yalamanchili <phani.y@sstech.us>
2012-04-21 18:55:38 +0000

    working copy with tableObj
    git-svn-id: http://tools.sstech.us/svn/office/trunk@240 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/CRUDResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/CRUDResource.java
@@ -5,13 +5,20 @@
 package info.yalamanchili.office.jrs;
 
 import info.yalamanchili.office.dao.CRUDDao;
+
 import java.util.List;
-import javax.ws.rs.*;
-import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- *
+ * 
  * @author ayalamanchili
  */
 @Produces("application/json")
@@ -19,22 +26,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public abstract class CRUDResource<T> {
 
-    @GET
-    @Path("/{id}")
-    public T read(@PathParam("id") Long id) {
-        return (T) getDao().findById(id);
-    }
+	@GET
+	@Path("/{id}")
+	public T read(@PathParam("id") Long id) {
+		return (T) getDao().findById(id);
+	}
 
-    @PUT
-    public T save(T entity) {
-        return (T) getDao().save(entity);
-    }
+	@PUT
+	public T save(T entity) {
+		return (T) getDao().save(entity);
+	}
 
-    @GET
-    @Path("/{start}/{limit}")
-    public List<T> readAll(@PathParam("start") int start, @PathParam("limit") int limit) {
-     return    getDao().query(start, limit);
-    }
-    
-    public abstract CRUDDao getDao();
+	@GET
+	@Path("/{start}/{limit}")
+	public List<T> readAll(@PathParam("start") int start,
+			@PathParam("limit") int limit) {
+		return getDao().query(start, limit);
+	}
+
+	@GET
+	@Path("/size")
+	public long size() {
+		return getDao().size();
+	}
+
+	public abstract CRUDDao getDao();
 }
--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeResource.java
@@ -8,23 +8,68 @@ import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.jrs.CRUDResource;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- *
+ * 
  * @author ayalamanchili
  */
 @Path("/employee")
 @Component
 public class EmployeeResource extends CRUDResource<Employee> {
 
-    @Autowired
-    public EmployeeDao employeeDao;
+	@Autowired
+	public EmployeeDao employeeDao;
 
-    @Override
-    public CRUDDao getDao() {
-        return employeeDao;
-    }
+	@GET
+	@Path("/table/{start}/{limit}")
+	public EmployeeTable table(@PathParam("start") int start,
+			@PathParam("limit") int limit) {
+		EmployeeTable tableObj = new EmployeeTable();
+		tableObj.setEntities(getDao().query(start, limit));
+		tableObj.setSize(getDao().size());
+		return tableObj;
+	}
+
+	@Override
+	public CRUDDao getDao() {
+		return employeeDao;
+	}
+
+	@XmlRootElement
+	@XmlType
+	public static class EmployeeTable {
+		protected Long size;
+		protected List<Employee> entities;
+
+		public Long getSize() {
+			return size;
+		}
+
+		public void setSize(Long size) {
+			this.size = size;
+		}
+
+		@XmlElement
+		public List<Employee> getEntities() {
+			return entities;
+		}
+
+		public void setEntities(List<Employee> entities) {
+			this.entities = entities;
+		}
+
+	}
+
 }
