a5babf62ee7587515e9d0d4d8aea36478f7eb1c9
Phani Yalamanchili <phani.y@sstech.us>
2012-05-14 04:35:17 +0000

    logging interceptor.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@299 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/logging/LoggingInterceptor.java
+++ office-jrs/src/main/java/info/yalamanchili/office/logging/LoggingInterceptor.java
@@ -1,6 +1,9 @@
 package info.yalamanchili.office.logging;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -8,10 +11,19 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingInterceptor {
+	private static final Log log = LogFactory.getLog(LoggingInterceptor.class);
+
 	@Before("execution(* info.yalamanchili.office..*.*(..))")
 	public void logBefore(JoinPoint joinPoint) {
-		// TODO add logging/trace related stuff here
-		// System.out.println("****Invoking method : " +
-		// joinPoint.getSignature().getName());
+		if (log.isInfoEnabled()) {
+			log.info("--- invoking ---:" + joinPoint.getSignature());
+		}
+	}
+
+	@AfterReturning(pointcut = "execution(* info.yalamanchili.office..*.*(..))", returning = "result")
+	public void logAfterReturning(JoinPoint joinPoint, Object result) {
+		if (log.isInfoEnabled()) {
+			log.info("--- returning ---:" + result);
+		}
 	}
 }
