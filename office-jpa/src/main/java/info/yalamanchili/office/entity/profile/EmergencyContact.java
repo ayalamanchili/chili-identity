59d1ca3ef8c5bf85be84df29f374cb29ac4a6faf
Phani Yalamanchili <phani.y@sstech.us>
2012-05-23 05:32:46 +0000

    validation for nestest contact
    git-svn-id: http://tools.sstech.us/svn/office/trunk@353 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jpa/src/main/java/info/yalamanchili/office/entity/profile/EmergencyContact.java
+++ office-jpa/src/main/java/info/yalamanchili/office/entity/profile/EmergencyContact.java
@@ -11,6 +11,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -46,10 +47,11 @@ public class EmergencyContact extends AbstractEntity {
 	protected Employee employee;
 
 	/**
-	 * @generated
+	 * @NOT generated
 	 */
 
 	@ManyToOne
+	@Valid
 	protected Contact contact;
 
 	/**
