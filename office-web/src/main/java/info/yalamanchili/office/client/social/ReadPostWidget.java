58c1a5f1da0a8f5376ed61e3ff481e9bd23ceb40
Phani Yalamanchili <phani.y@sstech.us>
2012-06-05 00:24:30 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@383 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
@@ -1,21 +1,20 @@
 package info.yalamanchili.office.client.social;
 
+import com.google.gwt.user.client.ui.CaptionPanel;
 import info.yalamanchili.gwt.composite.ALComposite;
 
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RichTextArea;
 
 public class ReadPostWidget extends ALComposite {
 
-    FlowPanel mainPanel = new FlowPanel();
-    Label header = new Label();
+    CaptionPanel panel = new CaptionPanel();
     RichTextArea body = new RichTextArea();
 
-    public ReadPostWidget(String headerText, String bodyText) {
-        init(mainPanel);
-        header.setText(headerText);
-        body.setHTML(bodyText);
+    public ReadPostWidget(String headerHtml, String bodyHtml) {
+        init(panel);
+        panel.setCaptionText(headerHtml);
+        panel.setContentWidget(body);
+        body.setHTML(bodyHtml);
     }
 
     @Override
@@ -30,7 +29,5 @@ public class ReadPostWidget extends ALComposite {
 
     @Override
     protected void addWidgets() {
-        mainPanel.add(header);
-        mainPanel.add(body);
     }
 }
