5eb57f2c2a119d745659b291736d9e9440c12771
Phani Yalamanchili <phani.y@sstech.us>
2012-06-18 06:04:40 +0000

    logout
    git-svn-id: http://tools.sstech.us/svn/office/trunk@468 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/StatusPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/StatusPanel.java
@@ -1,64 +1,56 @@
 package info.yalamanchili.office.client;
 
+import com.google.gwt.user.client.Window;
 import info.yalamanchili.gwt.widgets.AbstractStatusPanel;
 
 import java.util.logging.Logger;
 
 //TODO convert to UIbinder
 public class StatusPanel extends AbstractStatusPanel {
-	private static Logger logger = Logger.getLogger(StatusPanel.class.getName());
-	private static StatusPanel instance;
 
-	public static StatusPanel instance() {
-		return instance;
-	}
+    private static Logger logger = Logger.getLogger(StatusPanel.class.getName());
+    private static StatusPanel instance;
 
-	public StatusPanel() {
-		instance = this;
-	}
+    public static StatusPanel instance() {
+        return instance;
+    }
 
-	@Override
-	protected void loginClicked() {
-	}
+    public StatusPanel() {
+        instance = this;
+    }
 
-	@Override
-	public void logoutSuccessfull() {
-		statusBar.remove(logoutLink);
-		statusBar.setWidget(1, 2, loginLink);
+    @Override
+    protected void loginClicked() {
+    }
 
-	}
+    @Override
+    public void logoutSuccessfull() {
+        statusBar.remove(logoutLink);
+        statusBar.setWidget(1, 2, loginLink);
 
-	public void loginSuccessfull() {
-		statusBar.remove(loginLink);
-		statusBar.setWidget(1, 2, logoutLink);
+    }
 
-	}
+    public void loginSuccessfull() {
+        statusBar.remove(loginLink);
+        statusBar.setWidget(1, 2, logoutLink);
 
-	@Override
-	protected void logoutClicked() {
-		// AdminServiceAsync.instance().logout(new ALAsyncCallback<Void>() {
-		//
-		// @Override
-		// public void onResponse(Void arg0) {
-		// logoutOfCommunicator();
-		// new ResponseStatusWidget().show("logout successfull");
-		// RootPanel.get().clear();
-		// }
-		//
-		// });
-	}
+    }
 
-	@Override
-	protected void createUserClicked() {
+    @Override
+    protected void logoutClicked() {
+        Window.Location.reload();
+    }
 
-	}
+    @Override
+    protected void createUserClicked() {
+    }
 
-	/**
-	 * @generated
-	 */
-	@Override
-	protected void setUser() {
-		this.userLink.setText("Welcome " + OfficeWelcome.instance().username);
-		loginSuccessfull();
-	}
+    /**
+     * @generated
+     */
+    @Override
+    protected void setUser() {
+        this.userLink.setText("Welcome " + OfficeWelcome.instance().username);
+        loginSuccessfull();
+    }
 }
