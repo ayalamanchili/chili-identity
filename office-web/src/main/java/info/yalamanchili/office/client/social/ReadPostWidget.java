b9276bdd87d99f352981cf4a420193508179211e
p kantu <pkantu@xyz.com>
2012-06-18 19:04:15 +0000

    ReadPostWidget
    git-svn-id: http://tools.sstech.us/svn/office/trunk@472 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
@@ -5,49 +5,71 @@ import info.yalamanchili.office.client.gwt.JSONUtils;
 
 import java.util.logging.Logger;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.CaptionPanel;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RichTextArea;
+import com.google.gwt.user.client.ui.Widget;
+
 import info.yalamanchili.gwt.widgets.ClickableLink;
 
-public class ReadPostWidget extends ALComposite {
+public class ReadPostWidget extends ALComposite implements ClickHandler {
 
-    private static Logger logger = Logger.getLogger(ReadPostWidget.class.getName());
-    JSONObject post;
-    CaptionPanel postRootPanel = new CaptionPanel();
-    FlowPanel postBodyPanel = new FlowPanel();
-    RichTextArea postBodyArea = new RichTextArea();
-  
+	private static Logger logger = Logger.getLogger(ReadPostWidget.class
+			.getName());
+	protected JSONObject post;
+	protected String postId;
+	CaptionPanel postRootPanel = new CaptionPanel();
+	FlowPanel postBodyPanel = new FlowPanel();
+	RichTextArea postBodyArea = new RichTextArea();
+	ClickableLink replyLink = new ClickableLink("Reply");
 
-    public ReadPostWidget(JSONObject post) {
-        init(postRootPanel);
-        this.post = (JSONObject) post.get("post");
-        displayPost();
-    }
+	public ReadPostWidget(JSONObject post) {
+		init(postRootPanel);
+		this.post = (JSONObject) post.get("post");
+		displayPost();
 
-    protected void displayPost() {
-        postRootPanel.setCaptionText(getPostEmployeeNameHtml(post.get("employee").isObject()));
-        postBodyArea.setHTML(JSONUtils.toString(post, "postContent"));
-    }
+	}
 
-    @Override
-    protected void addListeners() {
-        // TODO Auto-generated method 
-    }
+	protected void displayPost() {
+		postRootPanel.setCaptionText(getPostEmployeeNameHtml(post.get(
+				"employee").isObject()));
+		this.postId=JSONUtils.toString(post, "id");
+		postBodyArea.setHTML(JSONUtils.toString(post, "postContent"));
+	}
 
-    @Override
-    protected void configure() {
-        postBodyArea.addStyleName("postRichTextBox");
-    }
+	@Override
+	protected void addListeners() {
+		replyLink.addClickHandler(this);
+	}
 
-    @Override
-    protected void addWidgets() {
-        postRootPanel.setContentWidget(postBodyPanel);
-        postBodyPanel.add(postBodyArea);
-    }
+	@Override
+	protected void configure() {
+		postBodyArea.addStyleName("postRichTextBox");
+	}
 
-    protected String getPostEmployeeNameHtml(JSONObject employee) {
-        return JSONUtils.toString(employee, "firstName") + " " + JSONUtils.toString(employee, "lastName");
-    }
+	@Override
+	protected void addWidgets() {
+		postRootPanel.setContentWidget(postBodyPanel);
+		postBodyPanel.add(postBodyArea);
+		postBodyPanel.add(replyLink);
+	}
+
+	protected String getPostEmployeeNameHtml(JSONObject employee) {
+		return JSONUtils.toString(employee, "firstName") + " "
+				+ JSONUtils.toString(employee, "lastName");
+	}
+
+	@Override
+	public void onClick(ClickEvent arg0) {
+		if(arg0.getSource().equals(replyLink)){
+		ReplyPostWidget replywidget = new ReplyPostWidget(String.valueOf(postId));
+		postBodyPanel.add(replywidget);
+		}
+
+	}
 }
