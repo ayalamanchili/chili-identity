7405fe19af2655a44a853158071cb4d4817f30a3
Phani Yalamanchili <phani.y@sstech.us>
2012-06-21 13:09:56 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@492 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/social/ReplyPostWidget.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/ReplyPostWidget.java
@@ -11,72 +11,67 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RichTextArea;
+import info.yalamanchili.gwt.callback.ALAsyncCallback;
 
 import info.yalamanchili.gwt.composite.ALComposite;
 import info.yalamanchili.gwt.widgets.ClickableLink;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.profile.employee.TreeEmployeePanel;
 import info.yalamanchili.office.client.rpc.HttpService.HttpServiceAsync;
 
 public class ReplyPostWidget extends ALComposite implements ClickHandler {
-
-	private static Logger logger = Logger.getLogger(ReplyPostWidget.class
-			.getName());
-	FlowPanel mainPanel = new FlowPanel();
-	RichTextArea postBodyArea = new RichTextArea();
-	Button replyB = new Button("Post");
-	
-	protected String parentPostId;
-
-	protected ReplyPostWidget(String parentPostId) {
-		init(mainPanel);
-		this.parentPostId=parentPostId;
-	}
-
-	@Override
-	protected void addListeners() {
-
-		replyB.addClickHandler(this);
-	}
-
-	@Override
-	protected void configure() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void addWidgets() {
-		mainPanel.add(postBodyArea);
-		mainPanel.add(replyB);
-	}
-
-	@Override
-	public void onClick(ClickEvent arg0) {
-		JSONObject replyEntity = new JSONObject();
-		replyEntity.put("postContent", new JSONString(postBodyArea.getText()));
-		replyEntity.put("postTimeStamp", new JSONString("2012-06-18T09:38:44.291-04:00"));
-		HttpServiceAsync.instance().doPut(getPostReplyURL(parentPostId), replyEntity.toString(), OfficeWelcome.instance().getHeaders(), true,
-                new AsyncCallback<String>() {
-
-					@Override
-					public void onFailure(Throwable arg0) {
-				Window.alert("error calling service"+arg0.getLocalizedMessage());
-						
-					}
-
-					@Override
-					public void onSuccess(String arg0) {
-						Window.alert("success replied to poest"+arg0);
-						
-					}
-		});
-		
-	}
-	public String getPostReplyURL(String parentPostId) {
-		//update this to be correct url path
-		return OfficeWelcome.constants.root_url() + "social/addreply/"
-				+ parentPostId;
-		
-	}
+    
+    private static Logger logger = Logger.getLogger(ReplyPostWidget.class.getName());
+    FlowPanel mainPanel = new FlowPanel();
+    RichTextArea postBodyArea = new RichTextArea();
+    Button replyB = new Button("Post");
+    protected String parentPostId;
+    
+    protected ReplyPostWidget(String parentPostId) {
+        init(mainPanel);
+        this.parentPostId = parentPostId;
+    }
+    
+    @Override
+    protected void addListeners() {
+        
+        replyB.addClickHandler(this);
+    }
+    
+    @Override
+    protected void configure() {
+        // TODO Auto-generated method stub
+    }
+    
+    @Override
+    protected void addWidgets() {
+        mainPanel.add(postBodyArea);
+        mainPanel.add(replyB);
+    }
+    
+    @Override
+    public void onClick(ClickEvent arg0) {
+        JSONObject replyEntity = new JSONObject();
+        replyEntity.put("postContent", new JSONString(postBodyArea.getText()));
+        replyEntity.put("postTimeStamp", new JSONString("2012-06-18T09:38:44.291-04:00"));
+        HttpServiceAsync.instance().doPut(getPostReplyURL(parentPostId), replyEntity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String arg0) {
+                        new ResponseStatusWidget().show("reply posted");
+                        TabPanel.instance().myOfficePanel.entityPanel.clear();
+                        TabPanel.instance().myOfficePanel.entityPanel.add(new SocialHome());
+                    }
+                });
+        
+    }
+    
+    public String getPostReplyURL(String parentPostId) {
+        //update this to be correct url path
+        return OfficeWelcome.constants.root_url() + "social/addreply/"
+                + parentPostId;
+        
+    }
 }
