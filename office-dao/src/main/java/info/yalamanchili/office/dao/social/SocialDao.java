592901077576546872eb5547e7a1819e5bf05794
bapuji y <bapuji.y@xyz.com>
2012-06-27 11:42:09 +0000

    created dao method for create post.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@519 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/social/SocialDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/social/SocialDao.java
@@ -12,6 +12,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 import javax.persistence.Query;
 import org.hsqldb.User;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,9 +20,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SocialDao {
+    
+    @Autowired
+    protected SecurityService securityService;
 
     @PersistenceContext(type = PersistenceContextType.EXTENDED)
     protected EntityManager em;
+    
+    
 
     public List<Post> getEmployeeFeed(int start, int limit) {
         Query getPostsQuery = em.createQuery("from " + Post.class.getCanonicalName() + " p where p.parentPost is null and p.employee is not null order by p.postTimeStamp",
@@ -46,11 +52,10 @@ public class SocialDao {
 	public Post createPost(Post newPost) {
 		// TODO Auto-generated method stub
            
-            SecurityService SecurityService=new SecurityService();
+           /* SecurityService SecurityService=new SecurityService();*/
             
             
-            newPost.setEmployee(SecurityService.getCurrentUser());
-            
+            newPost.setEmployee(securityService.getCurrentUser());
 	    em.persist(newPost);         
                 return newPost;
 	}
