601b0f83d3db5bd79392284d88abde14035c793d
p kantu <pkantu@xyz.com>
2012-06-28 14:17:38 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@524 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateEmployeePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateEmployeePanel.java
@@ -4,10 +4,13 @@ import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.gwt.UpdateComposite;
 
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.yalamanchili.gwt.fields.DataType;
 import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.profile.emergencycnt.EmergencyContactOptionsPanel;
 import info.yalamanchili.office.client.profile.emergencycnt.ReadAllEmergencyContactsPanel;
+import info.yalamanchili.office.client.rpc.HttpService;
 
 public class UpdateEmployeePanel extends UpdateComposite {
 
@@ -17,18 +20,45 @@ public class UpdateEmployeePanel extends UpdateComposite {
 
     @Override
     protected JSONObject populateEntityFromFields() {
-        // TODO Auto-generated method stub
-        return null;
+        
+        assignFieldValueFromEntity("firstName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("middleInitial", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("lastName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("dateOfBirth", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("sex", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
+        return entity;
     }
 
     @Override
     protected void updateButtonClicked() {
-        // TODO Auto-generated method stub
+        OfficeWelcome.logger.info("dddd"+entity);
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+
     }
 
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
-        // TODO Auto-generated method stub
+        //same here
+        assignFieldValueFromEntity("firstName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("middleInitial", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("lastName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("dateOfBirth", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("sex", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
+
+
     }
 
     @Override
@@ -43,7 +73,13 @@ public class UpdateEmployeePanel extends UpdateComposite {
 
     @Override
     protected void addWidgets() {
-        // TODO Auto-generated method stub
+        // same here update them
+        addField("firstName", false, true, DataType.STRING_FIELD);
+        addField("middleInitial", false, true, DataType.STRING_FIELD);
+        addField("lastName", false, true, DataType.STRING_FIELD);
+        addField("dateOfBirth", false, true, DataType.DATE_FIELD);
+        addField("sex", false, true, DataType.STRING_FIELD);
+        addField("startDate", false, true, DataType.DATE_FIELD);
     }
 
     @Override
@@ -53,12 +89,13 @@ public class UpdateEmployeePanel extends UpdateComposite {
 
     @Override
     protected String getURI() {
-        // TODO Auto-generated method stub
-        return null;
+        return OfficeWelcome.constants.root_url() + "employee";
     }
 
     @Override
     protected void postUpdateSuccess(String result) {
-        //TODO
+        new ResponseStatusWidget().show("successfully updated Employee information");
+        TabPanel.instance().myOfficePanel.entityPanel.clear(); 
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllEmployeesPanel());
     }
 }
