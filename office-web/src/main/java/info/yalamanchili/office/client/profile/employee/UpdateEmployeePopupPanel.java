d541d737bcacd85aa2e5695fc662412ef1e07743
p kantu <pkantu@xyz.com>
2012-06-28 14:17:27 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@523 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateEmployeePopupPanel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.employee;
+
+import com.google.gwt.json.client.JSONObject;
+
+/**
+ *
+ * @author prani
+ */
+public class UpdateEmployeePopupPanel extends UpdateEmployeePanel {
+    public UpdateEmployeePopupPanel(JSONObject entity) {
+        super(entity);
+    }
+    
+}
