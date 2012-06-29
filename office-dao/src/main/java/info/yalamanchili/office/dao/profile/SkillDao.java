807fce54d2c64b1d97e086442c83d13e314fff0a
raghu l <raghu.l@xyz.com>
2012-06-29 13:36:00 +0000

    commit
    git-svn-id: http://tools.sstech.us/svn/office/trunk@533 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/SkillDao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.profile;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.profile.Skill;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author raghu
+ */
+@Component
+public class SkillDao extends CRUDDao<Skill> {
+    
+    public SkillDao()
+    {
+      super(Skill.class);
+    }
+    
+   @PersistenceContext
+	protected EntityManager em;
+
+	@Override
+	public EntityManager getEntityManager() {
+		return em;
+	}
+    
+}
