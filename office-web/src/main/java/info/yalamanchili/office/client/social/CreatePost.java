84b668fa9642ee7f647a3d7f1fabe1e79c475b10
ramana k <ramana.k@xyz.com>
2012-06-29 13:57:21 +0000

    Made changes for adding post
    git-svn-id: http://tools.sstech.us/svn/office/trunk@534 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/social/CreatePost.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/CreatePost.java
@@ -5,6 +5,8 @@
 package info.yalamanchili.office.client.social; 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -15,10 +17,18 @@ import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import info.yalamanchili.gwt.composite.ALComposite;
+import info.yalamanchili.office.client.OfficeWelcome;
+import java.util.Calendar;
+ 
 /**
  *
  * @author ramana
  */
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.profile.employee.TreeEmployeePanel;
+import info.yalamanchili.office.client.rpc.HttpService.HttpServiceAsync;
+import java.util.Date;
 public class CreatePost extends  Composite {
     
      public CreatePost()
@@ -36,11 +46,11 @@ public class CreatePost extends  Composite {
          vp.add(hp1);
          hp1 = new HorizontalPanel();
          Label lblPost = new Label("Body");
-         TextArea taPostBody = new TextArea();
-         taPostBody.setCharacterWidth(80);
-         taPostBody.setVisibleLines(50);
+        final TextArea taContent = new TextArea();
+         taContent.setCharacterWidth(80);
+         taContent.setVisibleLines(50);
          hp1.add(lblPost);
-         hp1.add(taPostBody);
+         hp1.add(taContent);
          vp.add(hp1);
          hp1 = new HorizontalPanel();
          Button btnSubmitPost = new Button("Submit");
@@ -48,7 +58,26 @@ public class CreatePost extends  Composite {
              @Override
              public void onClick(ClickEvent e) {
                  //Code for Adding new Post
-                 
+                JSONObject entity = new JSONObject();
+                
+                 entity.put("postContent", new JSONString(taContent.getText()));
+                 entity.put("postTimeStamp", new JSONString(new Date().toString())  );
+                 entity.put("Employee", OfficeWelcome.instance().employee);
+                 entity.put("company", null );
+                 HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        //handleErrorResponse(arg0);
+
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postCreateSuccess(arg0);
+                    }
+                });
              }
          });
          hp1.add(btnSubmitPost);
@@ -56,6 +85,17 @@ public class CreatePost extends  Composite {
          rp.add(vp);
           
      }
+     
+   
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "social/createPost";
+    }
     
-    
+    protected void postCreateSuccess(String result) {
+        new ResponseStatusWidget().show("successfully  created a post");
+        //TabPanel.instance().myOfficePanel.entityPanel.clear();
+        //TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllPhonesPanel(TreeEmployeePanel.instance().getEntityId()));
+       // TabPanel.instance().myOfficePanel.entityPanel.add(new PhoneOptionsPanel());
+
+    }
 }
