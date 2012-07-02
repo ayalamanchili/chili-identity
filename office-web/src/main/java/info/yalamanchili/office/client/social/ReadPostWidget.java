6b4fcaa317da518f9053e1643886ba9dd97eff1c
Phani Yalamanchili <phani.y@sstech.us>
2012-07-02 14:17:12 +0000

    reverted
    git-svn-id: http://tools.sstech.us/svn/office/trunk@540 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
@@ -16,55 +16,67 @@ import com.google.gwt.user.client.ui.RichTextArea;
 import info.yalamanchili.gwt.widgets.ClickableLink;
 
 public class ReadPostWidget extends ALComposite implements ClickHandler {
-
+    
     private static Logger logger = Logger.getLogger(ReadPostWidget.class.getName());
     protected JSONObject post;
     protected String postId;
     CaptionPanel postRootPanel = new CaptionPanel();
     FlowPanel postBodyPanel = new FlowPanel();
-    HorizontalPanel replyLinksPanel = new HorizontalPanel();
+    HorizontalPanel replyAndCommentLinksPanel = new HorizontalPanel();
     RichTextArea postBodyArea = new RichTextArea();
     ClickableLink replyLink = new ClickableLink("reply");
-
+    ClickableLink commentsLink = new ClickableLink("comments");
+    
     public ReadPostWidget(JSONObject post) {
         init(postRootPanel);
         this.post = post;
         displayPost();
-
+        
     }
-
+    
     protected void displayPost() {
         postRootPanel.setCaptionHTML(JSONUtils.toString(post, "employeeName"));
         this.postId = JSONUtils.toString(post, "id");
         postBodyArea.setHTML(JSONUtils.toString(post, "postContent"));
+        commentsLink.setText(JSONUtils.toString(post, "numberOfReplies") + " comments");
     }
-
+    
     @Override
     protected void addListeners() {
         replyLink.addClickHandler(this);
+        commentsLink.addClickHandler(this);
     }
-
+    
     @Override
     protected void configure() {
+        replyAndCommentLinksPanel.setSpacing(5);
         postBodyArea.addStyleName("postRichTextBox");
         postBodyArea.setEnabled(false);
     }
-
+    
     @Override
     protected void addWidgets() {
         postRootPanel.setContentWidget(postBodyPanel);
-        replyLinksPanel.add(replyLink);
-
+        replyAndCommentLinksPanel.add(replyLink);
+        replyAndCommentLinksPanel.add(commentsLink);
+        
         postBodyPanel.add(postBodyArea);
-        postBodyPanel.add(replyLinksPanel);
+        postBodyPanel.add(replyAndCommentLinksPanel);
     }
-
+    
     @Override
     public void onClick(ClickEvent arg0) {
         if (arg0.getSource().equals(replyLink)) {
             ReplyPostWidget replywidget = new ReplyPostWidget(String.valueOf(postId));
             postBodyPanel.add(replywidget);
         }
-
+        if (arg0.getSource().equals(commentsLink)) {
+            showComments(postId);
+        }
+        
+    }
+    
+    public void showComments(String postId) {
+        //TODO
     }
 }
--- office-web/src/main/java/info/yalamanchili/office/client/social/SocialHome.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/SocialHome.java
@@ -3,9 +3,9 @@ package info.yalamanchili.office.client.social;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 
-public class SocialHome extends Composite { 
-	public   FlowPanel panel = new FlowPanel();
-        
+public class SocialHome extends Composite {
+
+    public FlowPanel panel = new FlowPanel();
 
     public SocialHome() {
         initWidget(panel);
