f25cc4ece756f9cc18423f92d0e1445a48a2251c
p kantu <pkantu@xyz.com>
2012-07-03 16:59:24 +0000

    Skills UI
    git-svn-id: http://tools.sstech.us/svn/office/trunk@549 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/ProfileMenu.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/ProfileMenu.java
@@ -7,28 +7,42 @@ import info.yalamanchili.office.client.profile.employee.ReadAllEmployeesPanel;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.MenuBar;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.profile.skill.ReadAllSkillsPanel;
 
 public class ProfileMenu extends Composite {
-	MenuBar profileMenuBar = new MenuBar(false);
 
-	public ProfileMenu() {
-		initWidget(profileMenuBar);
-		configureAdminMenu();
-	}
+    MenuBar profileMenuBar = new MenuBar(false);
 
-	protected void configureAdminMenu() {
-		MenuBar menu = new MenuBar(true);
-		profileMenuBar.addItem("Menu", menu);
-		profileMenuBar.addStyleName("entityMenuBar");
+    public ProfileMenu() {
+        initWidget(profileMenuBar);
+        configureAdminMenu();
+    }
 
-		menu.addItem("Employees", employeeMaintainenceCmd);
-	}
+    protected void configureAdminMenu() {
+        MenuBar menu = new MenuBar(true);
+        profileMenuBar.addItem("Menu", menu);
+        profileMenuBar.addStyleName("entityMenuBar");
 
-	Command employeeMaintainenceCmd = new Command() {
-		public void execute() {
-			TabPanel.instance().getMyOfficePanel().clear();
-			TabPanel.instance().getMyOfficePanel().entityPanel.add(new ReadAllEmployeesPanel());
-			TabPanel.instance().getMyOfficePanel().sidePanelTop.add(new EmployeeSidePanel());
-		}
-	};
+        menu.addItem("Employees", employeeMaintainenceCmd);
+        if (Auth.isAdmin() || Auth.isHR()) {
+            menu.addItem("skills", skillsMaintainenceCmd);
+        }
+    }
+    Command employeeMaintainenceCmd = new Command() {
+        public void execute() {
+            TabPanel.instance().getMyOfficePanel().clear();
+            TabPanel.instance().getMyOfficePanel().entityPanel.add(new ReadAllEmployeesPanel());
+            TabPanel.instance().getMyOfficePanel().sidePanelTop.add(new EmployeeSidePanel());
+        }
+    };
+    Command skillsMaintainenceCmd = new Command() {
+        public void execute() {
+            TabPanel.instance().getMyOfficePanel().clear();
+            //TabPanel.instance().getMyOfficePanel().entityPanel.add(new ReadAllEmployeesPanel());
+            //TabPanel.instance().getMyOfficePanel().sidePanelTop.add(new EmployeeSidePanel());
+            TabPanel.instance().getMyOfficePanel().entityPanel.add(new ReadAllSkillsPanel());
+            //where is the error?where here?we didnt mention any?
+        }
+    };
 }
