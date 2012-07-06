9766d605cb3acc5f0a1bb02b86f6752b66f821ed
p kantu <pkantu@xyz.com>
2012-07-06 12:34:52 +0000

    Skills UI create and update tasks. Readallskillspanel file. SkillResourcefile
    git-svn-id: http://tools.sstech.us/svn/office/trunk@556 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/SkillResource.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.profile;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.profile.PhoneDao;
+import info.yalamanchili.office.dao.profile.SkillDao;
+import info.yalamanchili.office.entity.profile.Phone;
+import info.yalamanchili.office.entity.profile.Skill;
+import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author prani
+ */
+@Path("skill")
+@Component
+@Transactional
+@Scope("request")
+public class SkillResource extends CRUDResource<Skill> {
+
+    @Autowired
+    public SkillDao skillDao;
+
+    @GET
+    @Path("/{start}/{limit}")
+    public SkillTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        SkillTable tableObj = new SkillTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @Override
+    public CRUDDao getDao() {
+        return skillDao;
+    }//
+    
+
+    @XmlRootElement
+    @XmlType
+    public static class SkillTable{
+
+        protected Long size;
+        protected List<Skill> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<Skill> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<Skill> entities) {
+            this.entities = entities;
+        }
+    }
+}
