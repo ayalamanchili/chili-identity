c1807bedfa916019d1c9e8b9e192e0414cc156d1
bapuji y <bapuji.y@xyz.com>
2012-07-06 13:10:32 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@559 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/AddressResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/AddressResource.java
@@ -2,10 +2,16 @@ package info.yalamanchili.office.jrs.profile;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.profile.AddressDao;
+import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.yalamanchili.office.dao.security.SecurityService;
 import info.yalamanchili.office.entity.profile.Address;
+import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.entity.security.CUser;
 import info.yalamanchili.office.jrs.CRUDResource;
+import info.yalamanchili.office.profile.ProfileNotificationService;
 
 import java.util.List;
+import javax.ws.rs.PUT;
 
 import javax.ws.rs.Path;
 import javax.xml.bind.annotation.XmlElement;
@@ -23,6 +29,19 @@ public class AddressResource extends CRUDResource<Address> {
 
 	@Autowired
 	public AddressDao addressDao;
+        
+        @Autowired
+         public EmployeeDao employeeDao;
+        
+        @Autowired
+        protected SecurityService securityService;
+        
+        @Autowired
+        protected CUser cuser;
+        
+        @Autowired
+         protected ProfileNotificationService profileNotificationservice;
+        
 
 	@Override
 	public CRUDDao getDao() {
@@ -42,6 +61,7 @@ public class AddressResource extends CRUDResource<Address> {
 		public void setSize(Long size) {
 			this.size = size;
 		}
+                
 
 		@XmlElement
 		public List<Address> getEntities() {
@@ -51,6 +71,37 @@ public class AddressResource extends CRUDResource<Address> {
 		public void setEntities(List<Address> entities) {
 			this.entities = entities;
 		}
+               
+                   
 
 	}
+         @PUT
+                public Address save(Address address){
+                    
+                 Employee emp=securityService.getCurrentUser();
+                 
+                
+                    if(address.getId()==null){
+                       
+                        return save(address);
+                        
+                    }
+                    else
+                    {
+                        Address savedAddress= (Address)getDao().save(address);
+                           
+                       
+                        
+                        profileNotificationservice.sendEmployeeAddressUpdatedNotification(emp);
+                        
+                         return savedAddress;
+                        
+                    }
+        
+                    
+                        
+                }
+        
+ 
+       
 }
