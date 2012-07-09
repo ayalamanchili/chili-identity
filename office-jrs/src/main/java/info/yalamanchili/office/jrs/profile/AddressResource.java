3d2759771f7e57e9f9f305b1310fcd53b7deb6eb
Phani Yalamanchili <phani.y@sstech.us>
2012-07-09 20:59:07 +0000

    update address notification working.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@572 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/AddressResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/AddressResource.java
@@ -6,7 +6,6 @@ import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.security.SecurityService;
 import info.yalamanchili.office.entity.profile.Address;
 import info.yalamanchili.office.entity.profile.Employee;
-import info.yalamanchili.office.entity.security.CUser;
 import info.yalamanchili.office.jrs.CRUDResource;
 import info.yalamanchili.office.profile.notification.ProfileNotificationService;
 
@@ -27,81 +26,54 @@ import org.springframework.stereotype.Component;
 @Scope("request")
 public class AddressResource extends CRUDResource<Address> {
 
-	@Autowired
-	public AddressDao addressDao;
-        
-        @Autowired
-         public EmployeeDao employeeDao;
-        
-        @Autowired
-        protected SecurityService securityService;
-        
-        @Autowired
-        protected CUser cuser;
-        
-        @Autowired
-         protected ProfileNotificationService profileNotificationservice;
-        
+    @Autowired
+    public AddressDao addressDao;
+    @Autowired
+    public EmployeeDao employeeDao;
+    @Autowired
+    protected SecurityService securityService;
+    @Autowired
+    protected ProfileNotificationService profileNotificationservice;
 
-	@Override
-	public CRUDDao getDao() {
-		return addressDao;
-	}
+    @Override
+    public CRUDDao getDao() {
+        return addressDao;
+    }
 
-	@XmlRootElement
-	@XmlType
-	public static class AddressTable {
-		protected Long size;
-		protected List<Address> entities;
+    @PUT
+    public Address save(Address address) {
+        Employee emp = securityService.getCurrentUser();
+        if (address.getId() == null) {
+            return save(address);
+        } else {
+            Address savedAddress = (Address) getDao().save(address);
+            profileNotificationservice.sendEmployeeAddressUpdatedNotification(emp);
+            return savedAddress;
+        }
+    }
 
-		public Long getSize() {
-			return size;
-		}
+    @XmlRootElement
+    @XmlType
+    public static class AddressTable {
 
-		public void setSize(Long size) {
-			this.size = size;
-		}
-                
+        protected Long size;
+        protected List<Address> entities;
 
-		@XmlElement
-		public List<Address> getEntities() {
-			return entities;
-		}
+        public Long getSize() {
+            return size;
+        }
 
-		public void setEntities(List<Address> entities) {
-			this.entities = entities;
-		}
-               
-                   
+        public void setSize(Long size) {
+            this.size = size;
+        }
 
-	}
-         @PUT
-                public Address save(Address address){
-                    
-                 Employee emp=securityService.getCurrentUser();
-                 
-                
-                    if(address.getId()==null){
-                       
-                        return save(address);
-                        
-                    }
-                    else
-                    {
-                        Address savedAddress= (Address)getDao().save(address);
-                           
-                       
-                        
-                        profileNotificationservice.sendEmployeeAddressUpdatedNotification(emp);
-                        
-                         return savedAddress;
-                        
-                    }
-        
-                    
-                        
-                }
-        
- 
-       
+        @XmlElement
+        public List<Address> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<Address> entities) {
+            this.entities = entities;
+        }
+    }
 }
