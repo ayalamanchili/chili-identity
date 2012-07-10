c772fa3ea568be06fe5a1e8755f507776e910afe
balakrishna m <balakrishna.m@xyz.com>
2012-07-10 06:49:13 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@577 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/CertificationDao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.profile;
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.profile.Certification;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author bala
+ */
+@Component
+public class CertificationDao extends CRUDDao<Certification> {
+    public CertificationDao()
+    {
+      super(Certification.class);
+    }
+    
+    @PersistenceContext
+	protected EntityManager em;
+
+	@Override
+	public EntityManager getEntityManager() {
+		return em;
+	}
+    
+}
