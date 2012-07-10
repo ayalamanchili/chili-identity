f556a4fd008e59a3666ff6c8c7f63da32f59c5f2
Phani Yalamanchili <phani.y@sstech.us>
2012-07-10 22:49:09 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@582 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/skillset/ReadSkillSetPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/skillset/ReadSkillSetPanel.java
@@ -11,58 +11,64 @@ import info.yalamanchili.office.client.gwt.ReadComposite;
 import info.yalamanchili.office.client.rpc.HttpService.HttpServiceAsync;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
+
 /**
  *
  * @author raghu
  */
-public class ReadSkillSetPanel extends  ReadComposite{
+public class ReadSkillSetPanel extends ReadComposite {
+
     public ReadSkillSetPanel(String id) {
-		initReadComposite(id, "Employee", OfficeWelcome.constants);
-	}
-    
-    @Override
-	public void loadEntity(String entityId) {
-		HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
-				new ALAsyncCallback<String>() {
+        initReadComposite(id, "Employee", OfficeWelcome.constants);
+    }
 
-					@Override
-					public void onResponse(String response) {
-						entity = (JSONObject) JSONParser.parseLenient(response);
-						populateFieldsFromEntity(entity);
-					}
-
-				});
-
-	}
     @Override
-	protected void addWidgets() {
-		addField("lastUpdated", true, true, DataType.DATE_FIELD);
-		addField("resumeUrl", true, true, DataType.STRING_FIELD);
-		
-	}
-    @Override
-	protected void addListeners() {
-		// TODO Auto-generated method stub
+    public void loadEntity(String entityId) {
+        HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response != null && !response.isEmpty()) {
+                            entity = (JSONObject) JSONParser.parseLenient(response);
+                            populateFieldsFromEntity(entity);
+                        }
 
-	}
+                    }
+                });
 
-	@Override
-	protected void configure() {
-		// TODO Auto-generated method stub
-	}
-    @Override
-	protected void addWidgetsBeforeCaptionPanel() {
-		// TODO Auto-generated method stub
+    }
 
-	}
     @Override
-	protected String getURI() {
-		return OfficeWelcome.constants.root_url() + "employee/skillset/" + entityId;
-	}
+    protected void addWidgets() {
+        addField("lastUpdated", true, true, DataType.DATE_FIELD);
+        addField("resumeUrl", true, true, DataType.STRING_FIELD);
+
+    }
+
     @Override
-	public void populateFieldsFromEntity(JSONObject entity) {
-		assignFieldValueFromEntity("lastUpdated", entity, DataType.DATE_FIELD);
-		assignFieldValueFromEntity("resumeUrl", entity, DataType.STRING_FIELD);
-		
-	}
+    protected void addListeners() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void configure() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "employee/skillset/" + entityId;
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("lastUpdated", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("resumeUrl", entity, DataType.STRING_FIELD);
+
+    }
 }
