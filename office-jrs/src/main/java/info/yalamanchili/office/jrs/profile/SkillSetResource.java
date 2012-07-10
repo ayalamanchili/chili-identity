8b3d1105fac28805b5d3efb3ac3b8424245bc8dc
Phani Yalamanchili <phani.y@sstech.us>
2012-07-10 13:43:12 +0000

    formattign
    git-svn-id: http://tools.sstech.us/svn/office/trunk@578 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/SkillSetResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/SkillSetResource.java
@@ -9,18 +9,11 @@ import info.yalamanchili.office.dao.profile.SkillSetDao;
 
 import info.yalamanchili.office.entity.profile.SkillSet;
 import info.yalamanchili.office.jrs.CRUDResource;
-import java.util.List;
 
 import javax.ws.rs.Consumes;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -36,16 +29,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Produces("application/json")
 @Consumes("application/json")
 @Scope("request")
-public class SkillSetResource extends CRUDResource <SkillSet> {
-    
+public class SkillSetResource extends CRUDResource<SkillSet> {
+
     @Autowired
-	public SkillSetDao skillSetDao;
-    
-    
-     
-  @Override
-	public CRUDDao getDao() {
-		return skillSetDao;
-	}
-   
+    public SkillSetDao skillSetDao;
+
+    @Override
+    public CRUDDao getDao() {
+        return skillSetDao;
+    }
 }
