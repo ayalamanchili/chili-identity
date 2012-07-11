4e7854a14d1efcb514778d4e43995127305609a9
Phani Yalamanchili <phani.y@sstech.us>
2012-07-11 22:48:04 +0000

    updated read post to use dock panel.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@598 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/ReadPostWidget.java
@@ -9,29 +9,31 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.ui.CaptionPanel;
+import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RichTextArea;
 
 import info.yalamanchili.gwt.widgets.ClickableLink;
 
 public class ReadPostWidget extends ALComposite implements ClickHandler {
-
+    
     private static Logger logger = Logger.getLogger(ReadPostWidget.class.getName());
     protected JSONObject post;
     protected String postId;
     protected boolean showReplyOption;
+    DockPanel postDockPanel = new DockPanel();
     CaptionPanel postRootPanel = new CaptionPanel();
     FlowPanel postBodyPanel = new FlowPanel();
     RichTextArea postBodyArea = new RichTextArea();
     ClickableLink replyLink = new ClickableLink("reply");
-
+    
     public ReadPostWidget(JSONObject post, boolean showReplyOption) {
         this.post = post;
         this.showReplyOption = showReplyOption;
-        init(postRootPanel);
+        init(postDockPanel);
         displayPost();
     }
-
+    
     protected void displayPost() {
         postRootPanel.setCaptionHTML(JSONUtils.toString(post, "employeeName"));
         this.postId = JSONUtils.toString(post, "id");
@@ -44,25 +46,27 @@ public class ReadPostWidget extends ALComposite implements ClickHandler {
             postBodyPanel.add(replyLink);
         }
     }
-
+    
     @Override
     protected void addListeners() {
         replyLink.addClickHandler(this);
     }
-
+    
     @Override
     protected void configure() {
         postBodyArea.addStyleName("postRichTextBox");
+        postDockPanel.addStyleName("postDockPanel");
         postBodyArea.setHeight("2em");
         postBodyArea.setEnabled(false);
     }
-
+    
     @Override
     protected void addWidgets() {
         postRootPanel.setContentWidget(postBodyPanel);
         postBodyPanel.add(postBodyArea);
+        postDockPanel.add(postRootPanel, DockPanel.CENTER);
     }
-
+    
     @Override
     public void onClick(ClickEvent arg0) {
         if (arg0.getSource().equals(replyLink) && replyLink.isVisible()) {
