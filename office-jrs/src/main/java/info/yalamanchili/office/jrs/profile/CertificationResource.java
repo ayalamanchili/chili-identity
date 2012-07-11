73c8c0ec7d82a8432e12e5fbddb290e01f55c07c
raghu l <raghu.l@xyz.com>
2012-07-11 13:25:32 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@594 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/CertificationResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/CertificationResource.java
@@ -11,8 +11,16 @@ import info.yalamanchili.office.entity.profile.Certification;
 
 import javax.ws.rs.Consumes;
 import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.profile.Email;
+import info.yalamanchili.office.entity.profile.Employee;
+import java.util.List;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -22,6 +30,12 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author Prashanthi
  */
+@Path("certification")
+@Component
+@Transactional
+@Produces("application/json")
+@Consumes("application/json")
+@Scope("request")
 public class CertificationResource extends CRUDResource<Certification> {
      @Autowired
     public CertificationDao certificationDao;
@@ -30,4 +44,38 @@ public class CertificationResource extends CRUDResource<Certification> {
     public CRUDDao getDao() {
         return certificationDao;
     }
+    
+ @GET
+    @Path("/certification/{start}/{limit}")
+    public CertificationResource.CertificationTable getcertifications(@PathParam("start") int start, @PathParam("limit") int limit) {
+        CertificationResource.CertificationTable tableObj = new CertificationResource.CertificationTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+ 
+ @XmlRootElement
+	@XmlType
+	public static class CertificationTable {
+		protected Long size;
+		protected List<Email> entities;
+
+		public Long getSize() {
+			return size;
+		}
+
+		public void setSize(Long size) {
+			this.size = size;
+		}
+
+		@XmlElement
+		public List<Email> getEntities() {
+			return entities;
+		}
+
+		public void setEntities(List<Email> entities) {
+			this.entities = entities;
+		}
+
+	}
 }
