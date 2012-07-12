73db23d6c0e522c9c0326b5ce818bcde33f19104
p kantu <pkantu@xyz.com>
2012-07-12 13:17:27 +0000

    create company post
    git-svn-id: http://tools.sstech.us/svn/office/trunk@610 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/profile/CompanyDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/CompanyDao.java
@@ -7,27 +7,41 @@ package info.yalamanchili.office.dao.profile;
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.profile.Company;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
 
 import org.springframework.stereotype.Component;
 
 /**
- * 
+ *
  * @author ayalamanchili
  */
 @Component
 public class CompanyDao extends CRUDDao<Company> {
 
-	public CompanyDao() {
-		super(Company.class);
-	}
+    @PersistenceContext
+    protected EntityManager em;
 
-	@PersistenceContext
-	protected EntityManager em;
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
 
-	@Override
-	public EntityManager getEntityManager() {
-		return em;
-	}
+    public Company findByCompanyName(String companyName) {
+        Query q = em.createQuery("FROM Company WHERE lower(company) = :CompanyNameParam");
+        q.setParameter("CompanyNameParam", companyName.toLowerCase());
+        if (q.getResultList().size() > 0) {
+            return (Company) q.getResultList().get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public CompanyDao() {
+        super(Company.class);
+    }
 }
