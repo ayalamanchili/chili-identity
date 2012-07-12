52b61219e9fb75d6922ff8cecd89fdf1dc5725ed
Phani Yalamanchili <phani.y@sstech.us>
2012-07-12 21:05:58 +0000

    readpost image completed styling.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@628 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/profile/SkillSetDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/SkillSetDao.java
@@ -6,29 +6,33 @@ package info.yalamanchili.office.dao.profile;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.profile.SkillSet;
+import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+
 /**
  *
  * @author raghu
  */
 @Component
 public class SkillSetDao extends CRUDDao<SkillSet> {
-    
-    public SkillSetDao()
-    {
-      super(SkillSet.class);
-    }
-    
-   @PersistenceContext
-	protected EntityManager em;
 
-	@Override
-	public EntityManager getEntityManager() {
-		return em;
-	}
-    
+    public SkillSetDao() {
+        super(SkillSet.class);
+    }
+    @PersistenceContext
+    protected EntityManager em;
+
+    public SkillSet save(SkillSet entity) {
+        entity.setLastUpdated(new Date());
+        return super.save(entity);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
 }
