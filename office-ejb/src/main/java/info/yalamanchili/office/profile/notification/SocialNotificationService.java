0543c4b3540360e38dd3e7d74310e64b077f0402
bapuji y <bapuji.y@xyz.com>
2012-07-12 11:43:50 +0000

    added function for sending notification message when company feed is posted
    git-svn-id: http://tools.sstech.us/svn/office/trunk@604 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-ejb/src/main/java/info/yalamanchili/office/profile/notification/SocialNotificationService.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.profile.notification;
+
+
+import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.yalamanchili.office.dao.security.SecurityService;
+import info.yalamanchili.office.email.Email;
+import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.entity.security.CUser;
+import info.yalamanchili.office.jms.MessagingService;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author Bapuji
+ */
+@Component
+public class SocialNotificationService {
+   
+    @Autowired
+    protected SecurityService securityService;
+    @Autowired
+    protected MessagingService messagingService;
+    @Autowired
+    public EmployeeDao employeeDao;
+    
+    @Async
+    public void sendNewCompanyPostNotification(Employee emp) {
+        String[] roles = {"USER_ROLE"};
+        Email email = new Email();
+        email.setTos(securityService.getEmailsAddressesForRoles(Arrays.asList(roles)));
+        email.setSubject("New Post in company feed");
+        String messageText = "News is posted in company feed " ;
+        email.setBody(messageText);
+        messagingService.sendEmail(email);
+
+    }
+}
