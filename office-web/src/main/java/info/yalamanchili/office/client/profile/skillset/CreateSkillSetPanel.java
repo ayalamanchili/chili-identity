e609fa89153a775e28de422eb3f1c180a309f660
raghu l <raghu.l@xyz.com>
2012-07-12 13:18:05 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@612 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/skillset/CreateSkillSetPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/skillset/CreateSkillSetPanel.java
@@ -7,15 +7,20 @@ package info.yalamanchili.office.client.profile.skillset;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.gwt.CreateComposite;
 import java.util.logging.Logger;
-
+import info.yalamanchili.office.client.gwt.FileUploadPanel;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import info.yalamanchili.gwt.fields.DataType;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.rpc.HttpService;
 /**
  *
  * @author raghu
  */
 public class CreateSkillSetPanel extends CreateComposite{
+     private static Logger logger = Logger.getLogger(CreateSkillSetPanel.class.getName());
+    FileUploadPanel empresumeUploadPanel = new FileUploadPanel("resume", "Date1");
       public CreateSkillSetPanel(CreateComposite.CreateCompositeType type){
         super(type);
         initCreateComposite("SkillSet", OfficeWelcome.constants);
@@ -23,7 +28,8 @@ public class CreateSkillSetPanel extends CreateComposite{
       @Override
     protected void postCreateSuccess(String result) {
        
-
+        new ResponseStatusWidget().show("successfully created employee");
+        TabPanel.instance().myOfficePanel.clear();
     }
       
        @Override
@@ -41,6 +47,20 @@ public class CreateSkillSetPanel extends CreateComposite{
         @Override
     protected void createButtonClicked() {
         // TODO Auto-generated method stub
+             empresumeUploadPanel.upload();
+            HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        logger.info(arg0.getMessage());
+                        handleErrorResponse(arg0);
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postCreateSuccess(arg0);
+                    }
+                });
     }
          @Override
     protected JSONObject populateEntityFromFields() {
@@ -50,7 +70,9 @@ public class CreateSkillSetPanel extends CreateComposite{
     }
           @Override
     protected void addWidgets() {
-       
+        addField("lastUpdated", true, true, DataType.DATE_FIELD);
+        addField("resumeUrl", true, true, DataType.STRING_FIELD);
+        entityDisplayWidget.add(empresumeUploadPanel);
     }
           @Override
     protected void addWidgetsBeforeCaptionPanel() {
@@ -58,6 +80,6 @@ public class CreateSkillSetPanel extends CreateComposite{
     }
           @Override
     protected String getURI() {
-        return OfficeWelcome.constants.root_url() + "employee/skillset/" ;
+        return OfficeWelcome.constants.root_url() + "employee/skillset" ;
     }
 }
