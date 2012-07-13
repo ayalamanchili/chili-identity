2ed4c33f82051361d80606f1a8d2612692cdfc54
Phani Yalamanchili <phani.y@sstech.us>
2012-07-13 21:36:18 +0000

    refresh employee panel on update
    git-svn-id: http://tools.sstech.us/svn/office/trunk@641 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateEmployeePopupPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateEmployeePopupPanel.java
@@ -5,14 +5,22 @@
 package info.yalamanchili.office.client.profile.employee;
 
 import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.office.client.gwt.GenericPopup;
+import info.yalamanchili.office.client.profile.ProfileHome;
 
 /**
  *
  * @author prani
  */
 public class UpdateEmployeePopupPanel extends UpdateEmployeePanel {
+
     public UpdateEmployeePopupPanel(JSONObject entity) {
         super(entity);
     }
-    
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        GenericPopup.instance().hide();
+        ProfileHome.instance().refreshEmployeePanel();
+    }
 }
