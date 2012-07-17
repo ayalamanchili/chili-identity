54c104d717ae71e42368b8a3573942e8ac2a2bee
ramana k <ramana.k@xyz.com>
2012-07-17 13:19:32 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@655 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-cits-adapter/src/main/java/info/yalamanchili/office/cits/CitsService.java
+++ office-cits-adapter/src/main/java/info/yalamanchili/office/cits/CitsService.java
@@ -3,13 +3,15 @@
  * and open the template in the editor.
  */
 package info.yalamanchili.office.cits;
-
+import java.util.*;
 import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.entity.profile.Address;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  *
@@ -17,7 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Component
 @Transactional
-public class CitsService {
+public class CitsService 
+{
 
     @PersistenceContext
     protected EntityManager em;
@@ -25,6 +28,12 @@ public class CitsService {
     @Async
     public void pushNewEmployeeInformation(Employee employee) {
         //TODO implement logic to push emp data to cits database
+        JdbcTemplate jTemplate=new  JdbcTemplate();
+        jTemplate.update("insert into tbl_Consultant_Master(Consultant_FirstName,Consultant_LastName,Consultant_Email,Consultant_HomeCountryAddress,"
+                + "Consultant_City,Consultant_State,Consultant_Zip,Consultant_Phone,)  "
+                + "values()");
+        List<Address> addr=employee.getAddresss();
+        
     }
 
     @Async
