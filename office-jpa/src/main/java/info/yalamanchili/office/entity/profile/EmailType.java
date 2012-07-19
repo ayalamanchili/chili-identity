e74a98ebdfa22eb91aa00de00fbf7d16cc78b3a4
raghu l <raghu.l@xyz.com>
2012-07-19 13:10:56 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@676 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jpa/src/main/java/info/yalamanchili/office/entity/profile/EmailType.java
+++ office-jpa/src/main/java/info/yalamanchili/office/entity/profile/EmailType.java
@@ -31,7 +31,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Indexed
 @XmlRootElement
 @Entity
-
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"emailType"}))
 public class EmailType extends AbstractEntity {
 	/**
 	 * @generated
@@ -44,7 +44,7 @@ public class EmailType extends AbstractEntity {
 	 */
 	@Field
 	@NotEmpty
-       
+       @Unique(entity = EmailType.class, property = "emailType", message="{EmailType.name.not.unique.msg}")
 	protected String emailType;
 
 	/**
