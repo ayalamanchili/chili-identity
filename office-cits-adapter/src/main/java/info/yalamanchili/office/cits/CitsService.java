fee0db84551ae2000def39cd39262d6e31d809a5
ramana k <ramana.k@xyz.com>
2012-07-19 13:53:40 +0000

    write some code for inserting into local db
    git-svn-id: http://tools.sstech.us/svn/office/trunk@690 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-cits-adapter/src/main/java/info/yalamanchili/office/cits/CitsService.java
+++ office-cits-adapter/src/main/java/info/yalamanchili/office/cits/CitsService.java
@@ -12,6 +12,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
  *
@@ -26,14 +27,31 @@ public class CitsService
     protected EntityManager em;
 
     @Async
-    public void pushNewEmployeeInformation(Employee employee) {
+    public void pushNewEmployeeInformation(Employee employee) 
+    {
         //TODO implement logic to push emp data to cits database
+        try
+        {
         JdbcTemplate jTemplate=new  JdbcTemplate();
-        jTemplate.update("insert into tbl_Consultant_Master(Consultant_FirstName,Consultant_LastName,Consultant_Email,Consultant_HomeCountryAddress,"
-                + "Consultant_City,Consultant_State,Consultant_Zip,Consultant_Phone,)  "
-                + "values()");
-        List<Address> addr=employee.getAddresss();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		dataSource.setUrl("jdbc:sqlserver://SSTECHLAPTOP11;DatabaseName=CITS");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("sstech1!");
+        jTemplate.setDataSource(dataSource);
+        List<Address> addr=employee.getAddresss(); 
         
+        jTemplate.update("insert into tbl_Consultant_Master(Consultant_FirstName,Consultant_LastName,Consultant_Email,Consultant_HomeCountryAddress,"
+                + "Consultant_City,Consultant_State,Consultant_Zip,Consultant_Phone)  "
+                + "values("+employee.getFirstName()+","+employee.getLastName()+","+
+                employee.getPrimaryEmail()+","+"''"+addr.get(0).getCity()+","+addr.get(0).getState()+","+
+                addr.get(0).getZip()+","+addr.get(0).getContact().getPhones().get(0).getPhoneNumber() +")");
+        
+        }
+        catch(Exception ex)
+        {
+             
+        }
     }
 
     @Async
