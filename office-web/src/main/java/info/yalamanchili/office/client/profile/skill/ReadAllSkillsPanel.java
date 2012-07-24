fac2fb923ec83eb41595a2ee5978b45da15e57dc
balakrishna m <balakrishna.m@xyz.com>
2012-07-24 08:54:51 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@705 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/skill/ReadAllSkillsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/skill/ReadAllSkillsPanel.java
@@ -7,15 +7,13 @@ package info.yalamanchili.office.client.profile.skill;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import info.yalamanchili.gwt.callback.ALAsyncCallback;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.Auth;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.gwt.JSONUtils;
 import info.yalamanchili.office.client.gwt.ReadAllComposite;
 import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
-import info.yalamanchili.office.client.internalization.OfficeConstants;
-import info.yalamanchili.office.client.profile.employee.ReadAllEmployeesPanel;
-import info.yalamanchili.office.client.profile.phone.UpdatePhonePanel;
 import info.yalamanchili.office.client.rpc.HttpService;
 import java.util.logging.Logger;
 
@@ -86,10 +84,24 @@ public class ReadAllSkillsPanel extends ReadAllComposite {
 
     @Override
     public void deleteClicked(String entityId) {
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
+    }
+    protected String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "skill/delete/" + entityId;
     }
 
     @Override
     public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("successfully deleted Skill Information");
+        TabPanel.instance().myOfficePanel.entityPanel.clear(); 
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllSkillsPanel());
     }
 
     @Override
