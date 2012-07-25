7fd41103d27c0e00d501f01ceebe405e258aedb1
Phani Yalamanchili <phani.y@sstech.us>
2012-07-25 13:36:37 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@717 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeService.java
+++ office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeService.java
@@ -8,7 +8,6 @@ import info.yalamanchili.office.dto.security.User;
 import info.yalamanchili.office.entity.security.CUser;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import org.springframework.stereotype.Component;
 
 /**
