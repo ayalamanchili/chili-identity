16fd1465cd8f0aa27ad34830712a06530facf087
raghu l <raghu.l@xyz.com>
2012-07-25 11:53:01 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@715 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeService.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dto.profile;
+
+import info.yalamanchili.office.dto.security.User;
+import info.yalamanchili.office.entity.security.CUser;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author raghu
+ */
+@Component
+public class EmployeeService {
+     @PersistenceContext
+    protected EntityManager em;
+     public CUser changePassword(User user)
+     {
+        CUser user1=null;
+        javax.persistence.Query findUserQuery=em.createQuery("from CUser where username=:usernameParam and passwordHash=:passwordHashParam");
+        findUserQuery.setParameter("usernameParam",user.getUserName());
+        findUserQuery.setParameter("passwordHashParam ",user.getoldPassword());
+        if (findUserQuery.getResultList().size()>0){
+        user1=(CUser) findUserQuery.getResultList().get(0);
+        }
+        else{
+        //no user found means incorrect current passwrod
+        return null;
+        }
+        user1.setPasswordHash(user.getnewPassword());
+        return em.merge(user1); 
+         
+     }
+   
+    
+}
