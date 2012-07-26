1e14d9887343a147301cb88b162c3366264ed86c
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-07-26 06:30:34 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@727 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/login/LoginPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/login/LoginPanel.java
@@ -9,6 +9,8 @@ import java.util.logging.Logger;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.json.client.JSONString;
@@ -23,6 +25,7 @@ import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+
 
     public class LoginPanel extends PopupPanel {
 	private static Logger logger = Logger.getLogger(LoginPanel.class.getName());
@@ -54,6 +57,24 @@ import com.google.gwt.user.client.ui.Widget;
 		loginClicked();
 	}
 
+        @UiHandler("usernameTb")
+         void usertextboxkeypress(KeyPressEvent event){
+            if(event.getCharCode()==KeyCodes.KEY_ENTER)
+            {
+                loginClicked();
+                        
+            }
+        }       
+        
+          @UiHandler("passwordTb")
+         void passwordtextboxkeypress(KeyPressEvent event){
+            if(event.getCharCode()==KeyCodes.KEY_ENTER)
+            {
+                loginClicked();
+                        
+            }
+        }   
+                
 	interface LoginPanelUiBinder extends UiBinder<Widget, LoginPanel> {
 	}
 
