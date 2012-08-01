8c3cefb73a0a03cb96a4d01ab50c71b931917ba3
raghu l <raghu.l@xyz.com>
2012-08-01 14:35:59 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@799 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeService.java
+++ office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeService.java
@@ -8,6 +8,7 @@ import info.yalamanchili.office.dto.security.User;
 import info.yalamanchili.office.entity.security.CUser;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.persistence.Query;
 import org.springframework.stereotype.Component;
 
@@ -18,19 +19,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmployeeService {
 
-    @PersistenceContext
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+
+
+  
     protected EntityManager em;
 
-    public CUser changePassword(User user) {
+
+
+
+    public CUser changePassword(Long empId,User user) {
         CUser user1 = null;
-        javax.persistence.Query findUserQuery = em.createQuery("from CUser where username=:usernameParam and passwordHash=:passwordHashParam");
-        findUserQuery.setParameter("usernameParam", user.getUserName());
+        javax.persistence.Query findUserQuery = em.createQuery("from CUser where employee.id=:empIdParam and passwordHash=:passwordHashParam");
+        findUserQuery.setParameter("empIdParam", empId);
         findUserQuery.setParameter("passwordHashParam", user.getOldPassword());
         if (findUserQuery.getResultList().size() > 0) {
             user1 = (CUser) findUserQuery.getResultList().get(0);
         } else {
             //no user found means incorrect current passwrod
             return null;
+
+        }
+        user1.setPasswordHash(user.getNewPassword());
+        return em.merge(user1);
+
+    }
+    
+     public CUser ResetPassword(Long empId,User user) {
+        CUser user1 = null;
+        javax.persistence.Query findUserQuery = em.createQuery("from CUser where employee.id=:empIdParam");
+        findUserQuery.setParameter("empIdParam", empId);
+        if (findUserQuery.getResultList().size() > 0) {
+            user1 = (CUser) findUserQuery.getResultList().get(0);
+        } else {
+            //no user found means incorrect current passwrod
+            return null;
+
         }
         user1.setPasswordHash(user.getNewPassword());
         return em.merge(user1);
