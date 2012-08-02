914ff7d4c0038e426f5c9e2328ff369d6d801a51
balakrishna m <balakrishna.m@xyz.com>
2012-08-02 08:07:15 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@817 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/EmployeeTypeDao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.profile;
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.profile.EmployeeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author bala
+ */
+
+@Component
+public class EmployeeTypeDao extends CRUDDao<EmployeeType> {
+    
+    public EmployeeTypeDao()
+    {
+      super(EmployeeType.class);
+    }
+    
+   @PersistenceContext
+	protected EntityManager em;
+
+	@Override
+	public EntityManager getEntityManager() {
+		return em;
+	}
+    
+}
