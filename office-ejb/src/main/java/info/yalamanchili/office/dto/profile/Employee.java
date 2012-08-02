87bfc67431204efe995071775dd1a744115cd499
Phani Yalamanchili <phani.y@sstech.us>
2012-08-02 13:35:17 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@835 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-ejb/src/main/java/info/yalamanchili/office/dto/profile/Employee.java
+++ office-ejb/src/main/java/info/yalamanchili/office/dto/profile/Employee.java
@@ -120,13 +120,12 @@ public class Employee implements Serializable {
 
     public static Employee map(Mapper mapper, info.yalamanchili.office.entity.profile.Employee entity) {
         Employee dto = mapper.map(entity, Employee.class);
-       if(entity.getPrimaryEmail() !=null) {
-        dto.setEmail(entity.getPrimaryEmail().getEmail());
+        if (entity.getPrimaryEmail() != null) {
+            dto.setEmail(entity.getPrimaryEmail().getEmail());
+        }
         if (entity.getPhones().size() > 0) {
             dto.setPhoneNumber(entity.getPhones().get(0).getPhoneNumber());
         }
-       
-       }
         return dto;
     }
 }
