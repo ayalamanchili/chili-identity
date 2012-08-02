17578c5962eb143b56d81279287816e1810adcab
balakrishna m <balakrishna.m@xyz.com>
2012-08-02 08:31:31 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@818 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeTypeResource.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.profile;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.profile.EmployeeTypeDao;
+import info.yalamanchili.office.entity.profile.EmployeeType;
+import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+/**
+ *
+ * @author bala
+ */
+
+@Path("employeetype")
+@Component
+@Transactional
+@Scope("request")
+public class EmployeeTypeResource extends CRUDResource<EmployeeType> {
+
+    @Autowired
+    public EmployeeTypeDao employeeTypeDao;
+
+    @GET
+    @Path("/{start}/{limit}")
+    public EmployeeTypeTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        EmployeeTypeTable tableObj = new EmployeeTypeTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @Override
+    public CRUDDao getDao() {
+        return employeeTypeDao;
+    }//
+    
+
+    @XmlRootElement
+    @XmlType
+    public static class EmployeeTypeTable{
+
+        protected Long size;
+        protected List<EmployeeType> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<EmployeeType> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<EmployeeType> entities) {
+            this.entities = entities;
+        }
+    }
+}
