2e0592a3e891909e988fcacd04ee23a7afe92120
balakrishna m <balakrishna.m@xyz.com>
2012-08-02 10:25:53 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@822 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/certification/CreateCertificationPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/certification/CreateCertificationPanel.java
@@ -78,8 +78,8 @@ public class CreateCertificationPanel extends CreateComposite {
 
     @Override
     protected void addWidgets() {
-        addField("name", false, true, DataType.STRING_FIELD);
-        addField("description", false, false, DataType.STRING_FIELD);
+        addField("Name", false, true, DataType.STRING_FIELD);
+        addField("Description", false, false, DataType.STRING_FIELD);
     }
 
     @Override
