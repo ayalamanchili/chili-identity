1609a66cebfea1100952bca0eb775d4dc2bc0212
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-08-02 10:37:17 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@825 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Employee.java
+++ office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Employee.java
@@ -217,4 +217,8 @@ public class Employee extends Contact {
     public String toString() {
         return "Contact{" + "firstName=" + firstName + ", lastName=" + lastName + ", middleInitial=" + middleInitial + ", dateOfBirth=" + dateOfBirth + ", sex=" + sex + ", imageURL=" + imageURL + '}';
     }
+
+    public void getprimaremail() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
