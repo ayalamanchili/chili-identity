c02aa83947a9953c0152fdca967a397b11e3a959
balakrishna m <balakrishna.m@xyz.com>
2012-08-02 10:26:14 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@823 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/employeetype/CreateEmployeeTypePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employeetype/CreateEmployeeTypePanel.java
@@ -76,8 +76,8 @@ public class CreateEmployeeTypePanel extends CreateComposite {
 
     @Override
     protected void addWidgets() {
-        addField("name", false, true, DataType.STRING_FIELD);
-        addField("description", false, false, DataType.STRING_FIELD);
+        addField("Name", false, true, DataType.STRING_FIELD);
+        addField("Description", false, false, DataType.STRING_FIELD);
     }
 
     @Override
