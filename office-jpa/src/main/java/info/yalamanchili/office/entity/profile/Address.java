0d9ec7dd2267a40812dd57ac02844dc1bf431b09
Phani Yalamanchili <phani.y@sstech.us>
2012-08-06 16:04:52 +0000

    foreignkey manual naming.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@870 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Address.java
+++ office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Address.java
@@ -1,8 +1,6 @@
-/** 
- * Automanage 
- * Copyright (C) 2011 yalamanchili.info 
+/**
+ * Automanage Copyright (C) 2011 yalamanchili.info
  */
-
 package info.yalamanchili.office.entity.profile;
 
 import info.yalamanchili.jpa.AbstractEntity;
@@ -13,6 +11,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.annotations.ForeignKey;
 
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
@@ -27,209 +26,199 @@ import org.hibernate.validator.constraints.NotEmpty;
 @XmlRootElement
 @Entity
 public class Address extends AbstractEntity {
-	/**
-	 * @generated
-	 */
-	@Transient
-	private static final long serialVersionUID = 4L;
 
-	/**
-	 * @generated
-	 */
-	@Field
-	@NotEmpty(message = "{address.street1.not.empty.msg}")
-	protected String street1;
+    /**
+     * @generated
+     */
+    @Transient
+    private static final long serialVersionUID = 4L;
+    /**
+     * @generated
+     */
+    @Field
+    @NotEmpty(message = "{address.street1.not.empty.msg}")
+    protected String street1;
+    /**
+     * @generated
+     */
+    @Field
+    protected String street2;
+    /**
+     * @generated
+     */
+    @Field
+    @NotEmpty(message = "{address.city.not.empty.msg}")
+    protected String city;
+    /**
+     * @generated
+     */
+    @Field
+    @NotEmpty(message = "{address.state.not.empty.msg}")
+    protected String state;
+    /**
+     * @generated
+     */
+    @Field
+    @NotEmpty(message = "{address.country.not.empty.msg}")
+    protected String country;
+    /**
+     * @generated
+     */
+    @Field
+    protected String zip;
+    /**
+     * @generated
+     */
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @ForeignKey(name = "FK_Contact_Addresses")
+    protected Contact contact;
+    /**
+     * @generated
+     */
+    @ManyToOne
+    protected AddressType addressType;
 
-	/**
-	 * @generated
-	 */
-	@Field
-	protected String street2;
+    /**
+     * @generated
+     */
+    /**
+     * @generated
+     */
+    public Address() {
+        super();
+    }
 
-	/**
-	 * @generated
-	 */
-	@Field
-	@NotEmpty(message = "{address.city.not.empty.msg}")
-	protected String city;
+    /**
+     * @generated
+     */
+    public String getStreet1() {
+        return street1;
+    }
 
-	/**
-	 * @generated
-	 */
-	@Field
-	@NotEmpty(message = "{address.state.not.empty.msg}")
-	protected String state;
+    /**
+     * @generated
+     */
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
 
-	/**
-	 * @generated
-	 */
-	@Field
-	@NotEmpty(message = "{address.country.not.empty.msg}")
-	protected String country;
+    /**
+     * @generated
+     */
+    public String getStreet2() {
+        return street2;
+    }
 
-	/**
-	 * @generated
-	 */
-	@Field
-	protected String zip;
+    /**
+     * @generated
+     */
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
 
-	/**
-	 * @generated
-	 */
+    /**
+     * @generated
+     */
+    public String getCity() {
+        return city;
+    }
 
-	@ManyToOne(cascade = CascadeType.MERGE)
-	protected Contact contact;
+    /**
+     * @generated
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	/**
-	 * @generated
-	 */
+    /**
+     * @generated
+     */
+    public String getState() {
+        return state;
+    }
 
-	@ManyToOne
-	protected AddressType addressType;
+    /**
+     * @generated
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
 
-	/**
-	 * @generated
-	 */
+    /**
+     * @generated
+     */
+    public String getCountry() {
+        return country;
+    }
 
-	/**
-	 * @generated
-	 */
-	public Address() {
-		super();
-	}
+    /**
+     * @generated
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-	/**
-	 * @generated
-	 */
-	public String getStreet1() {
-		return street1;
-	}
+    /**
+     * @generated
+     */
+    public String getZip() {
+        return zip;
+    }
 
-	/**
-	 * @generated
-	 */
-	public void setStreet1(String street1) {
-		this.street1 = street1;
-	}
+    /**
+     * @generated
+     */
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 
-	/**
-	 * @generated
-	 */
-	public String getStreet2() {
-		return street2;
-	}
+    /**
+     * @generated
+     */
+    @XmlElement
+    public Contact getContact() {
+        return this.contact;
+    }
 
-	/**
-	 * @generated
-	 */
-	public void setStreet2(String street2) {
-		this.street2 = street2;
-	}
+    /**
+     * @generated
+     */
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
 
-	/**
-	 * @generated
-	 */
-	public String getCity() {
-		return city;
-	}
+    /**
+     * @generated
+     */
+    @XmlElement
+    public AddressType getAddressType() {
+        return this.addressType;
+    }
 
-	/**
-	 * @generated
-	 */
-	public void setCity(String city) {
-		this.city = city;
-	}
+    /**
+     * @generated
+     */
+    public void setAddressType(AddressType addressType) {
+        this.addressType = addressType;
+    }
 
-	/**
-	 * @generated
-	 */
-	public String getState() {
-		return state;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	/**
-	 * @generated
-	 */
-	public String getCountry() {
-		return country;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	/**
-	 * @generated
-	 */
-	public String getZip() {
-		return zip;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
-	/**
-	 * @generated
-	 */
-	@XmlElement
-	public Contact getContact() {
-		return this.contact;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setContact(Contact contact) {
-		this.contact = contact;
-	}
-
-	/**
-	 * @generated
-	 */
-	@XmlElement
-	public AddressType getAddressType() {
-		return this.addressType;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setAddressType(AddressType addressType) {
-		this.addressType = addressType;
-	}
-
-	/**
-	 * @generated
-	 */
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getStreet1());
-		sb.append(":");
-		sb.append(getStreet2());
-		sb.append(":");
-		sb.append(getCity());
-		sb.append(":");
-		sb.append(getState());
-		sb.append(":");
-		sb.append(getCountry());
-		sb.append(":");
-		sb.append(getZip());
-		sb.append(":");
-		return sb.toString();
-	}
-
+    /**
+     * @generated
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getStreet1());
+        sb.append(":");
+        sb.append(getStreet2());
+        sb.append(":");
+        sb.append(getCity());
+        sb.append(":");
+        sb.append(getState());
+        sb.append(":");
+        sb.append(getCountry());
+        sb.append(":");
+        sb.append(getZip());
+        sb.append(":");
+        return sb.toString();
+    }
 }
