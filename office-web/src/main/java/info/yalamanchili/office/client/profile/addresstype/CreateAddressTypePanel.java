3a7eec187dfa7a9b9fa4a4e74c965ca685fc9a58
raghu l <raghu.l@xyz.com>
2012-08-08 10:17:12 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@900 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/addresstype/CreateAddressTypePanel.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.addresstype;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.yalamanchili.gwt.fields.DataType;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.profile.certification.CertificationSidePanel;
+import info.yalamanchili.office.client.profile.certification.CreateCertificationPanel;
+import info.yalamanchili.office.client.profile.certification.ReadAllCertificationsPanel;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class CreateAddressTypePanel extends CreateComposite {
+
+     private static Logger logger = Logger.getLogger(CreateAddressTypePanel.class.getName());
+     
+      public CreateAddressTypePanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+        initCreateComposite("AddressType", OfficeWelcome.constants);
+    }
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject addressType = new JSONObject();
+        assignEntityValueFromField("addressType", addressType);
+        logger.info(addressType.toString());
+        return addressType;
+    }
+
+    @Override
+    protected void createButtonClicked() {
+          HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        logger.info(arg0.getMessage());
+                        handleErrorResponse(arg0);
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postCreateSuccess(arg0);
+                    }
+                });
+    }
+
+    @Override
+    protected void addButtonClicked() {
+        
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+        new ResponseStatusWidget().show("AddressType creation successful");
+        TabPanel.instance().myOfficePanel.sidePanelTop.clear();
+        TabPanel.instance().myOfficePanel.sidePanelTop.add(new AddressTypeSidePanel());
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllAddressTypePanel());
+    }
+
+    @Override
+    protected void addListeners() {
+        
+    }
+
+    @Override
+    protected void configure() {
+        
+    }
+
+    @Override
+    protected void addWidgets() {
+         addField("addressType", false, true, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+        
+    }
+
+    @Override
+    protected String getURI() {
+         return OfficeWelcome.constants.root_url() + "addresstype";
+    }
+    
+}
