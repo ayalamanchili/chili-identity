d3680e8e59bf282d3821a7c771fbb89f2a7c3658
raghu l <raghu.l@xyz.com>
2012-08-08 11:04:37 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@904 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/AddressTypeResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/AddressTypeResource.java
@@ -2,10 +2,16 @@ package info.yalamanchili.office.jrs.profile;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.profile.AddressTypeDao;
-import info.yalamanchili.office.entity.profile.Address;
+import info.yalamanchili.office.entity.profile.AddressType;
 import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -14,12 +20,44 @@ import org.springframework.stereotype.Component;
 @Path("addresstype")
 @Component
 @Scope("request")
-public class AddressTypeResource extends CRUDResource<Address> {
+public class AddressTypeResource extends CRUDResource<AddressType> {
 	@Autowired
 	public AddressTypeDao addressTypeDao;
 
 	@Override
 	public CRUDDao getDao() {
 		return addressTypeDao;
+	}
+    @GET
+    @Path("/{start}/{limit}")
+    public AddressTypeTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        AddressTypeTable tableObj = new AddressTypeTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+    @XmlRootElement
+	@XmlType
+	public static class AddressTypeTable {
+		protected Long size;
+		protected List<AddressType> entities;
+
+		public Long getSize() {
+			return size;
+		}
+
+		public void setSize(Long size) {
+			this.size = size;
+		}
+
+		@XmlElement
+		public List<AddressType> getEntities() {
+			return entities;
+		}
+
+		public void setEntities(List<AddressType> entities) {
+			this.entities = entities;
+		}
+
 	}
 }
