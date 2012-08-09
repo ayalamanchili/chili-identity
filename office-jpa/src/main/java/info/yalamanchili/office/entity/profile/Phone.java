8ddfa356ba1e0cd2af1f6582bdbe8f17fcad9a95
Phani Yalamanchili <phani.y@sstech.us>
2012-08-09 13:14:59 +0000

    phone number cannot be empty
    git-svn-id: http://tools.sstech.us/svn/office/trunk@929 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Phone.java
+++ office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Phone.java
@@ -1,19 +1,14 @@
-/** 
- * Automanage 
- * Copyright (C) 2011 yalamanchili.info 
+/**
+ * Automanage Copyright (C) 2011 yalamanchili.info
  */
-
 package info.yalamanchili.office.entity.profile;
 
-import info.chili.jpa.validation.Unique;
 import info.yalamanchili.jpa.AbstractEntity;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -21,149 +16,143 @@ import org.hibernate.annotations.ForeignKey;
 
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * @todo add comment for javadoc
  * @author ayalamanchili
  * @generated
-    */
+ */
 @Indexed
 @XmlRootElement
 @Entity
-
 public class Phone extends AbstractEntity {
-	/**
-	 * @generated
-	 */
-	@Transient
-	private static final long serialVersionUID = 6L;
 
-	/**
-	 * @generated
-	 */
-	@Size(min = 10, max = 10)
-	@Field
-	protected String phoneNumber;
+    /**
+     * @generated
+     */
+    @Transient
+    private static final long serialVersionUID = 6L;
+    /**
+     * @generated
+     */
+    @Size(min = 10, max = 10)
+    @NotEmpty(message = "{phone.phonenumber.not.empty.msg}")
+    @Field
+    protected String phoneNumber;
+    /**
+     * @generated
+     */
+    @Field
+    protected String extension;
+    /**
+     * @generated
+     */
+    @Field
+    protected String countryCode;
+    /**
+     * @generated
+     */
+    @ManyToOne
+    @ForeignKey(name = "FK_PhoneType_Phones")
+    protected PhoneType phoneType;
+    /**
+     * @generated
+     */
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @ForeignKey(name = "FK_Contact_Phones")
+    protected Contact contact;
 
-	/**
-	 * @generated
-	 */
-	@Field
-	protected String extension;
+    /**
+     * @generated
+     */
+    public Phone() {
+        super();
+    }
 
-	/**
-	 * @generated
-	 */
-	@Field
-	protected String countryCode;
+    /**
+     * @generated
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	/**
-	 * @generated
-	 */
+    /**
+     * @generated
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-	@ManyToOne
-        @ForeignKey(name = "FK_PhoneType_Phones")
-	protected PhoneType phoneType;
+    /**
+     * @generated
+     */
+    public String getExtension() {
+        return extension;
+    }
 
-	/**
-	 * @generated
-	 */
+    /**
+     * @generated
+     */
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
 
-	@ManyToOne(cascade = CascadeType.MERGE)
-        @ForeignKey(name = "FK_Contact_Phones")
-	protected Contact contact;
+    /**
+     * @generated
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
 
-	/**
-	 * @generated
-	 */
-	public Phone() {
-		super();
-	}
+    /**
+     * @generated
+     */
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
 
-	/**
-	 * @generated
-	 */
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    /**
+     * @generated
+     */
+    @XmlElement
+    public PhoneType getPhoneType() {
+        return this.phoneType;
+    }
 
-	/**
-	 * @generated
-	 */
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    /**
+     * @generated
+     */
+    public void setPhoneType(PhoneType phoneType) {
+        this.phoneType = phoneType;
+    }
 
-	/**
-	 * @generated
-	 */
-	public String getExtension() {
-		return extension;
-	}
+    /**
+     * @generated
+     */
+    @XmlElement
+    public Contact getContact() {
+        return this.contact;
+    }
 
-	/**
-	 * @generated
-	 */
-	public void setExtension(String extension) {
-		this.extension = extension;
-	}
+    /**
+     * @generated
+     */
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
 
-	/**
-	 * @generated
-	 */
-	public String getCountryCode() {
-		return countryCode;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setCountryCode(String countryCode) {
-		this.countryCode = countryCode;
-	}
-
-	/**
-	 * @generated
-	 */
-	@XmlElement
-	public PhoneType getPhoneType() {
-		return this.phoneType;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setPhoneType(PhoneType phoneType) {
-		this.phoneType = phoneType;
-	}
-
-	/**
-	 * @generated
-	 */
-	@XmlElement
-	public Contact getContact() {
-		return this.contact;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void setContact(Contact contact) {
-		this.contact = contact;
-	}
-
-	/**
-	 * @generated
-	 */
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getPhoneNumber());
-		sb.append(":");
-		sb.append(getExtension());
-		sb.append(":");
-		sb.append(getCountryCode());
-		sb.append(":");
-		return sb.toString();
-	}
-
+    /**
+     * @generated
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getPhoneNumber());
+        sb.append(":");
+        sb.append(getExtension());
+        sb.append(":");
+        sb.append(getCountryCode());
+        sb.append(":");
+        return sb.toString();
+    }
 }
