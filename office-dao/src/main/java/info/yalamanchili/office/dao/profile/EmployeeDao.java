cd33af0c21798ccab36ffde11896f69e28b697c8
Phani Yalamanchili <phani.y@sstech.us>
2012-08-09 13:51:57 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@931 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/profile/EmployeeDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/EmployeeDao.java
@@ -7,6 +7,7 @@ package info.yalamanchili.office.dao.profile;
 import info.chili.service.jrs.exception.ServiceException;
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.entity.profile.Phone;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,28 +15,33 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
 
 /**
- * 
+ *
  * @author ayalamanchili
  */
 @Component
 public class EmployeeDao extends CRUDDao<Employee> {
 
-	@PersistenceContext
-	protected EntityManager em;
+    @PersistenceContext
+    protected EntityManager em;
 
-	public EmployeeDao() {
-		super(Employee.class);
-	}
+    public EmployeeDao() {
+        super(Employee.class);
+    }
 
-	@Override
-	public EntityManager getEntityManager() {
-		return em;
-	}
-        @Override
-        public void delete(Long id) {
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    @Override
+    public void delete(Long id) {
         try {
-            //getEntityManager().remove(findById(id));
-            //getEntityManager().flush();
+            Employee emp = findById(id);
+            //Phones
+            for (Phone phone : emp.getPhones()) {
+                em.remove(phone);
+            }
+            //similarly
         } catch (javax.persistence.PersistenceException e) {
             throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "DELETE", "SQLError", "cannot delete due to associated data");
         }
