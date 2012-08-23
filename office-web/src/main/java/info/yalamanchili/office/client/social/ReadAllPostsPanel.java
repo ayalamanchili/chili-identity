7fedaa6e6d5d688458f3a0d568a2d0c6968a4a6a
Phani Yalamanchili <phani.y@sstech.us>
2012-08-23 21:20:36 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1011 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/social/ReadAllPostsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/social/ReadAllPostsPanel.java
@@ -1,5 +1,7 @@
 package info.yalamanchili.office.client.social;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import info.yalamanchili.gwt.callback.ALAsyncCallback;
 import info.yalamanchili.gwt.composite.ALComposite;
 import info.yalamanchili.office.client.OfficeWelcome;
@@ -12,15 +14,20 @@ import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Label;
 import info.yalamanchili.office.client.social.company.ReadCompanyPostPanel;
 import info.yalamanchili.office.client.social.employee.ReadEmployeePostPanel;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ReadAllPostsPanel extends ALComposite {
+public class ReadAllPostsPanel extends ALComposite implements ClickHandler {
 
     private static Logger logger = Logger.getLogger(ReadAllPostsPanel.class.getName());
+    protected int start = 0;
+    protected int limit = 5;
     FlowPanel panel = new FlowPanel();
+    Label showMoreL = new Label("load more");
     Set<String> postIds = new HashSet<String>();
 
     public ReadAllPostsPanel() {
@@ -29,7 +36,7 @@ public class ReadAllPostsPanel extends ALComposite {
     }
 
     public void loadPosts() {
-        HttpService.HttpServiceAsync.instance().doGet(getPostFeedURL(0, 10), OfficeWelcome.instance().getHeaders(),
+        HttpService.HttpServiceAsync.instance().doGet(getPostFeedURL(start, limit), OfficeWelcome.instance().getHeaders(),
                 true, new ALAsyncCallback<String>() {
             @Override
             public void onResponse(String result) {
@@ -40,35 +47,39 @@ public class ReadAllPostsPanel extends ALComposite {
     }
 
     protected void showPosts(String result) {
-        JSONObject postsResp = (JSONObject) JSONParser.parseLenient(result);
-        JSONArray posts = JSONUtils.toJSONArray(postsResp.get("post"));
-        for (int i = 0; i < posts.size(); i++) {
-            JSONObject post = (JSONObject) posts.get(i);
-            String postId = JSONUtils.toString(post, "id");
-            if (!postIds.contains(postId)) {
-                postIds.add(postId);
-                if (SocialSidePanel.isEmployeedFeedSelected()) {
-                    panel.insert(new ReadEmployeePostPanel(post, true), 0);
-                } else if (SocialSidePanel.isCompanyFeedSelected()) {
-                    panel.insert(new ReadCompanyPostPanel(post, true), 0);
-                }
+        JSONObject postsResp = (JSONObject) JSONParser.parseLenient(result).isObject();
+        if (postsResp != null) {
+            JSONArray posts = JSONUtils.toJSONArray(postsResp.get("post"));
+            for (int i = 0; i < posts.size(); i++) {
+                JSONObject post = (JSONObject) posts.get(i);
+                String postId = JSONUtils.toString(post, "id");
+                if (!postIds.contains(postId)) {
+                    postIds.add(postId);
+                    if (SocialSidePanel.isEmployeedFeedSelected()) {
+                        panel.insert(new ReadEmployeePostPanel(post, true), 0);
+                    } else if (SocialSidePanel.isCompanyFeedSelected()) {
+                        panel.insert(new ReadCompanyPostPanel(post, true), 0);
+                    }
 
+                }
             }
         }
     }
 
     @Override
     protected void addListeners() {
-        // TODO Auto-generated method stub
+        showMoreL.addClickHandler(this);
     }
 
     @Override
     protected void configure() {
-        // TODO Auto-generated method stub
+        showMoreL.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+        showMoreL.addStyleName("showMoreL");
     }
 
     @Override
     protected void addWidgets() {
+        panel.add(showMoreL);
     }
 
     public void refresh() {
@@ -81,5 +92,17 @@ public class ReadAllPostsPanel extends ALComposite {
         } else {
             return OfficeWelcome.constants.root_url() + "social/companyfeed/" + start.toString() + "/" + limit.toString();
         }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (showMoreL.equals(event.getSource())) {
+            loadMore();
+        }
+    }
+
+    protected void loadMore() {
+        start = start + limit;
+        loadPosts();
     }
 }
