37bda7d1ae73c9e73707ce61d78c400f24b99336
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-08-27 14:33:16 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1063 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Phone.java
+++ office-jpa/src/main/java/info/yalamanchili/office/entity/profile/Phone.java
@@ -27,7 +27,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Indexed
 @XmlRootElement
 @Entity
-//@Unique(entity = Phone.class, fields = {"phoneNumber"}, message = "{phone.phoneNumber.not.unique.msg}")
+@Unique(entity = Phone.class, fields = {"phoneNumber"}, message = "{phone.phoneNumber.not.unique.msg}")
 public class Phone extends AbstractEntity {
 
     /**
