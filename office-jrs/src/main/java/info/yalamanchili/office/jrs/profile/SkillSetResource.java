98c78cbdf946c95b6f907be8423ec4c20a6f40da
balakrishna m <balakrishna.m@xyz.com>
2012-08-29 12:18:55 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1106 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/SkillSetResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/SkillSetResource.java
@@ -5,16 +5,11 @@
 package info.yalamanchili.office.jrs.profile;
 
 import info.yalamanchili.office.dao.CRUDDao;
-import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.profile.SkillSetDao;
-import info.yalamanchili.office.dao.security.SecurityService;
-import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.entity.profile.SkillSet;
 import info.yalamanchili.office.jrs.CRUDResource;
-import info.yalamanchili.office.profile.notification.ProfileNotificationService;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -37,28 +32,10 @@ public class SkillSetResource extends CRUDResource<SkillSet> {
 
     @Autowired
     public SkillSetDao skillSetDao;
-    @Autowired
-    public EmployeeDao employeeDao;
-    @Autowired
-    protected SecurityService securityService;
-    @Autowired
-    protected ProfileNotificationService profileNotificationservice;
     @Override
     
     
     public CRUDDao getDao() {
         return skillSetDao;
-    }
-    
-    @PUT
-    public SkillSet save(SkillSet skillset) {
-        Employee emp = securityService.getCurrentUser();
-        if (skillset.getId() == null) {
-            return save(skillset);
-        } else {
-            SkillSet savedSkillset = (SkillSet) getDao().save(skillset);
-            profileNotificationservice.ResumeUpdatedNotification(emp);
-            return savedSkillset;
-        }
     }
 }
