fa1cce30ad7536a33470de93bbd653a5a266ba11
raghu l <raghu.l@xyz.com>
2012-08-30 13:58:17 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1140 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmailResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmailResource.java
@@ -2,12 +2,17 @@ package info.yalamanchili.office.jrs.profile;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.profile.EmailDao;
+import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.yalamanchili.office.dto.profile.Employee;
 import info.yalamanchili.office.entity.profile.Email;
+import info.yalamanchili.office.entity.profile.EmailType;
 import info.yalamanchili.office.jrs.CRUDResource;
 
 import java.util.List;
+import javax.ws.rs.PUT;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
@@ -26,11 +31,27 @@ public class EmailResource extends CRUDResource<Email> {
 	@Autowired
 	public EmailDao emailDao;
 
+        @Autowired
+       public EmployeeDao employeeDao;
+        
 	@Override
 	public CRUDDao getDao() {
 		return emailDao;
 	}
 
+    @PUT
+    @Path("/email/{empId}")
+    public void addEmail(@PathParam("empId") Long empId, Email email) {
+        Employee emp = (Employee) employeeDao.findById(empId);
+
+        if (email.getEmailType() != null) {
+            EmailType emailType = getDao().getEntityManager().find(EmailType.class, email.getEmailType().getId());
+            email.setEmailType(emailType);
+        }
+        email = employeeDao.UpdatePrimaryEmail(emp, email);
+        emp.addEmail(email);
+    }
+        
 	@XmlRootElement
 	@XmlType
 	public static class EmailTable {
