760ba971ad325739cb8e94b63d1c45f539d1f0cc
Phani Yalamanchili <phani.y@sstech.us>
2012-09-05 04:33:04 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1186 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employeetype/SelectEmployeeTypeWidget.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.employeetype;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.gwt.callback.ALAsyncCallback;
+import info.yalamanchili.gwt.utils.JSONUtils;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.SelectComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+public class SelectEmployeeTypeWidget extends SelectComposite {
+
+    public SelectEmployeeTypeWidget() {
+        super("Employee Type", "employeeType");
+    }
+
+    protected void fetchDropDownData() {
+        HttpService.HttpServiceAsync.instance().doGet(getDropDownURL(0, 10, null, null, null),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String entityString) {
+                processData(entityString);
+            }
+        });
+    }
+
+    protected String getDropDownURL(Integer start, Integer limit, String param1, String param2, String param3) {
+        return OfficeWelcome.constants.root_url() + "employeetype/dropdown/" + start.toString() + "/" + limit.toString();
+    }
+
+    @Override
+    protected Map<Integer, String> populateValues(JSONArray entities) {
+        Map<Integer, String> values = new HashMap<Integer, String>();
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            Integer id = Integer.valueOf(JSONUtils.toString(entity, "id"));
+            String value = JSONUtils.toString(entity, "name");
+            values.put(id, value);
+        }
+        return values;
+    }
+}
