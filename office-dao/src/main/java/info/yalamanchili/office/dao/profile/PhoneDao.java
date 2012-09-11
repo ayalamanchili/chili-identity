cf84ecf87f06db9e2ffe4ed8b60091576814ccc9
raghu l <raghu.l@xyz.com>
2012-09-11 10:23:57 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1206 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/profile/PhoneDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/PhoneDao.java
@@ -2,6 +2,7 @@ package info.yalamanchili.office.dao.profile;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.profile.Phone;
+import info.yalamanchili.office.entity.profile.PhoneType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,7 +18,21 @@ public class PhoneDao extends CRUDDao<Phone> {
 	public PhoneDao() {
 		super(Phone.class);
 	}
-
+        
+        public Phone save(Phone entity) {
+        if (entity.getId() != null) {
+            Phone updatedPhone = null;
+            updatedPhone = super.save(entity);
+            if (entity.getPhoneType() == null) {
+                updatedPhone.setPhoneType(null);
+            } else {
+                updatedPhone.setPhoneType(em.find(PhoneType.class, entity.getPhoneType().getId()));
+            }
+            return em.merge(updatedPhone);
+        }
+        return super.save(entity);
+         }
+        
 	@Override
 	public EntityManager getEntityManager() {
 		return em;
