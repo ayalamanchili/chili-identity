e503340165c3a7f7a1abef43809417b1d3141687
raghu l <raghu.l@xyz.com>
2012-09-13 14:57:38 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1233 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-ejb/src/main/java/info/yalamanchili/office/task/TaskService.java
+++ office-ejb/src/main/java/info/yalamanchili/office/task/TaskService.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -26,51 +27,56 @@ import org.springframework.stereotype.Service;
  */
 public class TaskService {
 
-    @PersistenceContext
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
     public EntityManager em;
     @Autowired
     protected MessagingService messagingService;
 
-//    public void birthdayNotification() {
-//        System.out.println("----------------RUNNING BIRTHDAY NOTIFICATION---------------");
-//        javax.persistence.Query findUserQuery = em.createQuery("from " + Employee.class.getCanonicalName() + " where  Day_Only(dateOfBirth) = :date and calendar_month(dateOfBirth) = :month ");
-//        findUserQuery.setParameter("date", Calendar.getInstance().DATE);
-//        findUserQuery.setParameter("month", Calendar.getInstance().MONTH);
-//
-//        List lstResult = findUserQuery.getResultList();
-//        for (int i = 0; i < lstResult.size(); i++) {
-//            Employee empres = new Employee();
-//            Set<String> emailto = new HashSet<String>();
-//            Email email = new Email();
-//            emailto.add(empres.getPrimaryEmail().getEmail());
-//            email.setTos(emailto);
-//            email.setSubject("Birthday Wishes");
-//            String messageText = "SystemSoft Technologies Wishes a very Happy Birthday to " + empres.getFirstName() + "," + empres.getLastName();
-//            email.setBody(messageText);
-//            messagingService.sendEmail(email);
-//        }
-//    }
-     public void birthdayNotification() {
+    public void birthdayNotification() {
         System.out.println("----------------RUNNING BIRTHDAY NOTIFICATION---------------");
-        javax.persistence.Query findUserQuery = em.createQuery("from " + Employee.class.getCanonicalName());
-
+        System.out.println("day :" + Calendar.getInstance().DATE);
+        System.out.println("month :" + Calendar.getInstance().MONTH);
         
+        javax.persistence.Query findUserQuery = em.createQuery("from " + Employee.class.getCanonicalName() + " where  day(dateOfBirth) = 13 and month(dateOfBirth) = 9 ");
+        findUserQuery.setParameter("date1", Calendar.getInstance().DATE);
+        findUserQuery.setParameter("month1", Calendar.getInstance().MONTH);
+
         List lstResult = findUserQuery.getResultList();
-        Iterator itr = lstResult.iterator();
+         Iterator itr = lstResult.iterator();
         while(itr.hasNext()) {
             Employee empres = new Employee();
             empres = ((Employee)itr.next());
-            if((empres.getDateOfBirth().getDate() == Calendar.getInstance().DATE)&&(empres.getDateOfBirth().getMonth() == Calendar.getInstance().MONTH))
-            {
-                Set<String> emailto = new HashSet<String>();
-                Email email = new Email();
-                emailto.add(empres.getPrimaryEmail().getEmail());
-                email.setTos(emailto);
-                email.setSubject("Birthday Wishes");
-                String messageText = "SystemSoft Technologies Wishes a very Happy Birthday to " + empres.getFirstName() + "," + empres.getLastName();
-                email.setBody(messageText);
-                messagingService.sendEmail(email);
-            }
+            Set<String> emailto = new HashSet<String>();
+            Email email = new Email();
+            emailto.add(empres.getPrimaryEmail().getEmail());
+            email.setTos(emailto);
+            email.setSubject("Birthday Wishes");
+            String messageText = "SystemSoft Technologies Wishes a very Happy Birthday to " + empres.getFirstName() + "," + empres.getLastName();
+            email.setBody(messageText);
+            messagingService.sendEmail(email);
         }
     }
+//     public void birthdayNotification() {
+//        System.out.println("----------------RUNNING BIRTHDAY NOTIFICATION---------------");
+//        javax.persistence.Query findUserQuery = em.createQuery("from " + Employee.class.getCanonicalName());
+//
+//        
+//        List lstResult = findUserQuery.getResultList();
+//        Iterator itr = lstResult.iterator();
+//        while(itr.hasNext()) {
+//            Employee empres = new Employee();
+//            empres = ((Employee)itr.next());
+//            if((empres.getDateOfBirth().getDate() == Calendar.getInstance().DATE)&&(empres.getDateOfBirth().getMonth() == Calendar.getInstance().MONTH))
+//            {
+//                Set<String> emailto = new HashSet<String>();
+//                Email email = new Email();
+//                emailto.add(empres.getPrimaryEmail().getEmail());
+//                email.setTos(emailto);
+//                email.setSubject("Birthday Wishes");
+//                String messageText = "SystemSoft Technologies Wishes a very Happy Birthday to " + empres.getFirstName() + "," + empres.getLastName();
+//                email.setBody(messageText);
+//                messagingService.sendEmail(email);
+//            }
+//        }
+//    }
 }
