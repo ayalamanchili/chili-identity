7c2ce1f88a64cb7ff5bad8e491daf223152f2198
raghu l <raghu.l@xyz.com>
2012-09-13 12:07:32 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1225 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-ejb/src/main/java/info/yalamanchili/office/task/TaskService.java
+++ office-ejb/src/main/java/info/yalamanchili/office/task/TaskService.java
@@ -4,11 +4,13 @@
  */
 package info.yalamanchili.office.task;
 
+import info.yalamanchili.commons.DateUtils;
 import info.yalamanchili.office.email.Email;
 import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.jms.MessagingService;
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import javax.persistence.EntityManager;
@@ -29,23 +31,46 @@ public class TaskService {
     @Autowired
     protected MessagingService messagingService;
 
-    public void birthdayNotification() {
+//    public void birthdayNotification() {
+//        System.out.println("----------------RUNNING BIRTHDAY NOTIFICATION---------------");
+//        javax.persistence.Query findUserQuery = em.createQuery("from " + Employee.class.getCanonicalName() + " where  Day_Only(dateOfBirth) = :date and calendar_month(dateOfBirth) = :month ");
+//        findUserQuery.setParameter("date", Calendar.getInstance().DATE);
+//        findUserQuery.setParameter("month", Calendar.getInstance().MONTH);
+//
+//        List lstResult = findUserQuery.getResultList();
+//        for (int i = 0; i < lstResult.size(); i++) {
+//            Employee empres = new Employee();
+//            Set<String> emailto = new HashSet<String>();
+//            Email email = new Email();
+//            emailto.add(empres.getPrimaryEmail().getEmail());
+//            email.setTos(emailto);
+//            email.setSubject("Birthday Wishes");
+//            String messageText = "SystemSoft Technologies Wishes a very Happy Birthday to " + empres.getFirstName() + "," + empres.getLastName();
+//            email.setBody(messageText);
+//            messagingService.sendEmail(email);
+//        }
+//    }
+     public void birthdayNotification() {
         System.out.println("----------------RUNNING BIRTHDAY NOTIFICATION---------------");
-        javax.persistence.Query findUserQuery = em.createQuery("from " + Employee.class.getCanonicalName() + "where  day(dateOfBirth.dateTime) = :date and month(dateOfBirth.dateTime) = :month ");
-        findUserQuery.setParameter("date", Calendar.getInstance().DATE);
-        findUserQuery.setParameter("month", Calendar.getInstance().MONTH);
+        javax.persistence.Query findUserQuery = em.createQuery("from " + Employee.class.getCanonicalName());
 
+        
         List lstResult = findUserQuery.getResultList();
-        for (int i = 0; i < lstResult.size(); i++) {
+        Iterator itr = lstResult.iterator();
+        while(itr.hasNext()) {
             Employee empres = new Employee();
-            Set<String> emailto = new HashSet<String>();
-            Email email = new Email();
-            emailto.add(empres.getPrimaryEmail().getEmail());
-            email.setTos(emailto);
-            email.setSubject("Birthday Wishes");
-            String messageText = "SystemSoft Technologies Wishes a very Happy Birthday to " + empres.getFirstName() + "," + empres.getLastName();
-            email.setBody(messageText);
-            messagingService.sendEmail(email);
+            empres = ((Employee)itr.next());
+            if((empres.getDateOfBirth().getDate() == Calendar.getInstance().DATE)&&(empres.getDateOfBirth().getMonth() == Calendar.getInstance().MONTH))
+            {
+                Set<String> emailto = new HashSet<String>();
+                Email email = new Email();
+                emailto.add(empres.getPrimaryEmail().getEmail());
+                email.setTos(emailto);
+                email.setSubject("Birthday Wishes");
+                String messageText = "SystemSoft Technologies Wishes a very Happy Birthday to " + empres.getFirstName() + "," + empres.getLastName();
+                email.setBody(messageText);
+                messagingService.sendEmail(email);
+            }
         }
     }
 }
