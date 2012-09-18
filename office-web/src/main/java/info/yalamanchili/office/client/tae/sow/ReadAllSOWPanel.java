1ed04f9f8194820ec05fbcca76c3cc7af89b6f3d
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-18 13:29:30 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1286 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/sow/ReadAllSOWPanel.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.sow;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.office.client.gwt.ReadAllComposite;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class ReadAllSOWPanel extends ReadAllComposite{
+
+    @Override
+    public void preFetchTable(int start) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void createTableHeader() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
