7f69aa12fddb51e263a90a5c54c2a1d6bcd526f4
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-18 09:24:31 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1272 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-dao/src/main/java/info/yalamanchili/office/dao/Client/ClientEntityDao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.Client;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.client.Client;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class ClientEntityDao extends CRUDDao<Client> {
+
+    @PersistenceContext
+    protected EntityManager em;
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ClientEntityDao() {
+        super(Client.class);
+    }
+}
