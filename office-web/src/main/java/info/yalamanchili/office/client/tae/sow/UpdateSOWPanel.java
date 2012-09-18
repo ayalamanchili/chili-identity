39058ba71792068d7e49ba3c8ea37f29a5d8d882
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-18 13:29:18 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1285 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/sow/UpdateSOWPanel.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.sow;
+
+import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.office.client.gwt.UpdateComposite;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class UpdateSOWPanel extends UpdateComposite{
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void addListeners() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void configure() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void addWidgets() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected String getURI() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
