0155828505f347fba4e159b085b596bd05295f86
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-18 14:30:23 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1287 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/sow/ReadAllSOWPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/sow/ReadAllSOWPanel.java
@@ -6,52 +6,112 @@ package info.yalamanchili.office.client.tae.sow;
 
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.gwt.callback.ALAsyncCallback;
+import info.yalamanchili.gwt.utils.JSONUtils;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.gwt.ReadAllComposite;
+import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Prashanthi
  */
-public class ReadAllSOWPanel extends ReadAllComposite{
+public class ReadAllSOWPanel extends ReadAllComposite {
+
+    private static Logger logger = Logger.getLogger(ReadAllSOWPanel.class.getName());
+    public static ReadAllSOWPanel instance;
+
+    public ReadAllSOWPanel() {
+        instance = this;
+        initTable("Client", OfficeWelcome.constants);
+    }
+
+    public ReadAllSOWPanel(JSONArray array) {
+        instance = this;
+        initTable("StatementOfWork", array, OfficeWelcome.constants);
+    }
 
     @Override
     public void preFetchTable(int start) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HttpService.HttpServiceAsync.instance().doGet(getReadAllSOWPanelURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String result) {
+                        postFetchTable(result);
+                    }
+                });
+
+    }
+
+    public String getReadAllSOWPanelURL(Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "statementofwork/" + start.toString() + "/" + limit.toString();
     }
 
     @Override
     public void createTableHeader() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("Name"));
+        table.setText(0, 2, getKeyValue("Description"));
+        table.setText(0, 3, getKeyValue("SowUrl"));
+        table.setText(0, 4, getKeyValue("StartDate"));
+        table.setText(0, 5, getKeyValue("EndDate"));
+        table.setText(0, 6, getKeyValue("BillRate"));
+        table.setText(0, 7, getKeyValue("TimeSheets"));
+        table.setText(0, 8, getKeyValue("Project"));
     }
 
     @Override
     public void fillData(JSONArray entities) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "name"));
+            table.setText(i, 2, JSONUtils.toString(entity, "description"));
+            table.setText(i, 3, JSONUtils.toString(entity, "sowUrl"));
+            table.setText(i, 4, JSONUtils.toString(entity, "startDate"));
+            table.setText(i, 5, JSONUtils.toString(entity, "endDate"));
+            table.setText(i, 6, JSONUtils.toString(entity, "billDate"));
+            table.setText(i, 7, JSONUtils.toString(entity, "timeSheets"));
+            table.setText(i, 8, JSONUtils.toString(entity, "project"));
+        }
     }
 
     @Override
     protected void addOptionsWidget(int row, JSONObject entity) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
     }
 
     @Override
     public void viewClicked(String entityId) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void deleteClicked(String entityId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
+    }
+
+    protected String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "statementofwork/delete/" + entityId;
     }
 
     @Override
     public void postDeleteSuccess() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        new ResponseStatusWidget().show("Successfully deleted StetementOfWork Information");
+        TabPanel.instance().TimeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().TimeandExpensePanel.entityPanel.add(new ReadAllSOWPanel());
     }
 
     @Override
     public void updateClicked(String entityId) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
 }
