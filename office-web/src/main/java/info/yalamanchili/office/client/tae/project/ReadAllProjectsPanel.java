e9b351043a54e8165120b939c0db03579b3a08a8
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-18 12:44:26 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1282 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/project/ReadAllProjectsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/project/ReadAllProjectsPanel.java
@@ -7,8 +7,12 @@ package info.yalamanchili.office.client.tae.project;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import info.yalamanchili.gwt.callback.ALAsyncCallback;
+import info.yalamanchili.gwt.utils.JSONUtils;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.gwt.ReadAllComposite;
+import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
 import info.yalamanchili.office.client.rpc.HttpService;
 import java.util.logging.Logger;
 
@@ -23,28 +27,56 @@ public class ReadAllProjectsPanel extends ReadAllComposite {
 
     public ReadAllProjectsPanel() {
         instance = this;
-        initTable("Client", OfficeWelcome.constants);
+        initTable("Project", OfficeWelcome.constants);
     }
 
     public ReadAllProjectsPanel(JSONArray array) {
         instance = this;
-        initTable("Client", array, OfficeWelcome.constants);
+        initTable("Project", array, OfficeWelcome.constants);
     }
 
     @Override
     public void preFetchTable(int start) {
+        HttpService.HttpServiceAsync.instance().doGet(getReadAllProjectsPanelURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String result) {
+                        postFetchTable(result);
+                    }
+                });
+
+    }
+
+    public String getReadAllProjectsPanelURL(Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "project/" + start.toString() + "/" + limit.toString();
     }
 
     @Override
     public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("Name"));
+        table.setText(0, 2, getKeyValue("Description"));
+        table.setText(0, 3, getKeyValue("StartDate"));
+        table.setText(0, 4, getKeyValue("EndDate"));
     }
 
     @Override
     public void fillData(JSONArray entities) {
+
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "name"));
+            table.setText(i, 2, JSONUtils.toString(entity, "description"));
+            table.setText(i, 3, JSONUtils.toString(entity, "startDate"));
+            table.setText(i, 4, JSONUtils.toString(entity, "endDate"));
+        }
     }
 
     @Override
     protected void addOptionsWidget(int row, JSONObject entity) {
+
+        createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
     }
 
     @Override
@@ -53,10 +85,25 @@ public class ReadAllProjectsPanel extends ReadAllComposite {
 
     @Override
     public void deleteClicked(String entityId) {
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
+    }
+
+    protected String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "project/delete/" + entityId;
     }
 
     @Override
     public void postDeleteSuccess() {
+
+        new ResponseStatusWidget().show("Successfully deleted Projects Information");
+        TabPanel.instance().TimeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().TimeandExpensePanel.entityPanel.add(new ReadAllProjectsPanel());
     }
 
     @Override
