7152478b046cf6a3b351520fc1f51c5b5997499a
Phani Yalamanchili <phani.y@sstech.us>
2012-09-20 13:39:29 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1324 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/Auth.java
+++ office-web/src/main/java/info/yalamanchili/office/client/Auth.java
@@ -1,38 +1,50 @@
 package info.yalamanchili.office.client;
 
 public class Auth {
-	private enum ROLES {
-		ROLE_USER, ROLE_HR, ROLE_ACCOUNTANT, ROLE_ADMIN,ROLE_PAYROLL
-	}
 
-	public static boolean isAdmin() {
-		if (OfficeWelcome.instance().roles.contains(ROLES.ROLE_ADMIN.name())) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public enum ROLE {
 
-	public static boolean isHR() {
-		if (OfficeWelcome.instance().roles.contains(ROLES.ROLE_HR.name())) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-        public static boolean isAccountant() {
-		if (OfficeWelcome.instance().roles.contains(ROLES.ROLE_ACCOUNTANT.name())) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-        public static boolean isPayroll() {
-		if (OfficeWelcome.instance().roles.contains(ROLES.ROLE_PAYROLL.name())) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-       
+        ROLE_USER, ROLE_HR, ROLE_ACCOUNTANT, ROLE_ADMIN, ROLE_RECRUITER, ROLE_PAYROLL
+    }
+
+    public static boolean isAdmin() {
+        if (OfficeWelcome.instance().roles.contains(ROLE.ROLE_ADMIN.name())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isHR() {
+        if (OfficeWelcome.instance().roles.contains(ROLE.ROLE_HR.name())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean hasAnyOfRoles(ROLE... roles) {
+        for (ROLE role : roles) {
+            if (OfficeWelcome.instance().roles.contains(role.name())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isAccountant() {
+        if (OfficeWelcome.instance().roles.contains(ROLE.ROLE_ACCOUNTANT.name())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isPayroll() {
+        if (OfficeWelcome.instance().roles.contains(ROLE.ROLE_PAYROLL.name())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
