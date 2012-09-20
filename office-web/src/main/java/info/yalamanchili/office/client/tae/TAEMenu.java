e852d7ad21cd8a09d41e6372892a1e045b315cf1
Phani Yalamanchili <phani.y@sstech.us>
2012-09-20 10:20:14 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1307 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/TAEMenu.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/TAEMenu.java
@@ -4,10 +4,42 @@
  */
 package info.yalamanchili.office.client.tae;
 
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.MenuBar;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.tae.client.ClientSidePanel;
+import info.yalamanchili.office.client.tae.client.ReadAllClientsPanel;
+
 /**
  *
  * @author ayalamanchili
  */
-public class TAEMenu {
-    
+public class TAEMenu extends Composite{
+     MenuBar tAEMenuBar = new MenuBar(false);
+     public TAEMenu() {
+        initWidget(tAEMenuBar);
+        configureAdminMenu();
+    }
+       protected void configureAdminMenu() {
+        MenuBar menu = new MenuBar(true);
+        tAEMenuBar.addItem("TAEMenu", menu);
+        tAEMenuBar.addStyleName("entityMenuBar");
+        
+
+        if (Auth.isAdmin() || Auth.isHR()) {
+            menu.addItem("Clients", clientsMaintainenceCmd);
+            
+      
+        }
+    }
+        Command clientsMaintainenceCmd = new Command() {
+        public void execute() {
+            TabPanel.instance().getMyOfficePanel().entityPanel.clear();
+            TabPanel.instance().getMyOfficePanel().sidePanelTop.clear();
+            TabPanel.instance().getMyOfficePanel().entityPanel.add(new ReadAllClientsPanel());
+            TabPanel.instance().getMyOfficePanel().sidePanelTop.add(new ClientSidePanel());
+        }
+    };
 }
