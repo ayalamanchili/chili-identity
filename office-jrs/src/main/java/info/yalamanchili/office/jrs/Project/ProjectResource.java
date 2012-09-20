473792f4fd7f05268c7cc0e0b2322bccfcb33b49
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-20 10:55:37 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1313 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/Project/ProjectResource.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.Project;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.jrs.CRUDResource;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class ProjectResource extends CRUDResource<Object> {
+
+    @Override
+    public CRUDDao getDao() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
