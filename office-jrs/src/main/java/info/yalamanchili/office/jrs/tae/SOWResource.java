75b41b5358239271430687bae5f827c9f12055e3
balakrishna m <balakrishna.m@xyz.com>
2012-09-20 14:09:33 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1329 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/tae/SOWResource.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.tae;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.tae.SOWDao;
+import info.yalamanchili.office.entity.time.TimeSheet;
+import info.yalamanchili.office.jrs.CRUDResource;
+
+import java.util.List;
+import javax.ws.rs.PUT;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+/**
+ *
+ * @author bala
+ */
+public class SOWResource extends CRUDResource<TimeSheet> {
+
+    @Autowired
+    public SOWDao sowDao;
+
+    @Override
+    public CRUDDao getDao() {
+        return sowDao;
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class SOWTable {
+
+        protected Long size;
+        protected List<TimeSheet> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<TimeSheet> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<TimeSheet> entities) {
+            this.entities = entities;
+        }
+    }
+}
