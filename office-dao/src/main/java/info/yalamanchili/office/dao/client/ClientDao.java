152b68ee93ebfa2c04dc977cf8eeff6d905eaa58
Phani Yalamanchili <phani.y@sstech.us>
2012-09-20 12:16:02 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1318 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/client/ClientDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/client/ClientDao.java
@@ -2,17 +2,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package info.yalamanchili.office.dao.Client;
+package info.yalamanchili.office.dao.client;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.client.Client;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author Prashanthi
  */
+@Component
 public class ClientDao extends CRUDDao<Client> {
 
     @PersistenceContext
