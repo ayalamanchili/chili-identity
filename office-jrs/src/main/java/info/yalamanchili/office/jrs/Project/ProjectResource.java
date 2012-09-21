220a6dd5b2526321b0411d27a50a47b62c7086f9
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-21 09:23:59 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1335 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/Project/ProjectResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/Project/ProjectResource.java
@@ -5,17 +5,69 @@
 package info.yalamanchili.office.jrs.Project;
 
 import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.client.ProjectDao;
+import info.yalamanchili.office.entity.client.Client;
+import info.yalamanchili.office.entity.client.Project;
 import info.yalamanchili.office.jrs.CRUDResource;
+import info.yalamanchili.office.jrs.client.ClientResource;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author Prashanthi
  */
+@Path("project")
+@Component
+@Scope("request")
 public class ProjectResource extends CRUDResource<Object> {
+
+    @Autowired
+    public ProjectDao projectDao;
 
     @Override
     public CRUDDao getDao() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return projectDao;
     }
-    
+     @GET
+    @Path("/{start}/{limit}")
+    public ProjectResource.ProjectTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        ProjectResource.ProjectTable tableObj = new ProjectResource.ProjectTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class ProjectTable {
+
+        protected Long size;
+        protected List<Project> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<Project> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<Project> entities) {
+            this.entities = entities;
+        }
+    }
 }
