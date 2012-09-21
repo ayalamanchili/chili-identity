015a32f4a00feb4f4661ae728531ed3945bef8bf
raghu l <raghu.l@xyz.com>
2012-09-21 10:00:40 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1340 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/project/ProjectOptionsPanel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.project;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import info.yalamanchili.gwt.composite.ALComposite;
+import info.yalamanchili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite.CreateCompositeType;
+
+/**
+ *
+ * @author raghu
+ */
+public class ProjectOptionsPanel extends ALComposite implements ClickHandler {
+
+    protected HorizontalPanel panel = new HorizontalPanel();
+
+    protected ClickableLink addProjectLink = new ClickableLink("Add Project");
+    
+    public ProjectOptionsPanel() {
+		init(panel);
+	}
+    @Override
+    protected void addListeners() {
+        addProjectLink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+        panel.setSpacing(5);
+    }
+
+    @Override
+    protected void addWidgets() {
+       if (Auth.isAdmin() || Auth.isHR()) {
+			panel.add(addProjectLink);
+		}
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(addProjectLink)) {
+			TabPanel.instance().myOfficePanel.entityPanel.clear();
+			TabPanel.instance().myOfficePanel.entityPanel.add(new CreateProjectPanel(CreateCompositeType.ADD));
+		}
+    }
+    
+}
