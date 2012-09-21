c78699a29c838fe775ec361214c1b14cd32d7f3c
raghu l <raghu.l@xyz.com>
2012-09-21 14:04:53 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1357 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jpa/src/main/java/info/yalamanchili/office/entity/client/Client.java
+++ office-jpa/src/main/java/info/yalamanchili/office/entity/client/Client.java
@@ -5,8 +5,13 @@
 package info.yalamanchili.office.entity.client;
 
 import info.yalamanchili.jpa.AbstractEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 /**
@@ -23,6 +28,9 @@ public class Client extends AbstractEntity {
     //TODO
 //    protected List<Contact> contacts;
 
+     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    protected List<Project> projects;
+     
     public String getName() {
         return name;
     }
@@ -38,7 +46,32 @@ public class Client extends AbstractEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+    
+     /**
+     * @generated
+     */
+    @XmlTransient
+    public List<Project> getProjects() {
+        if (this.projects == null) {
+            this.projects = new ArrayList<Project>();
+        }
+        return this.projects;
+    }
 
+    /**
+     * @generated
+     */
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+    public void addProject(Project entity) {
+        if (entity == null) {
+            return;
+        }
+        getProjects().add(entity);
+        entity.setClient(this);
+    }
+    
     @Override
     public String toString() {
         return "Client{" + "name=" + name + ", description=" + description + '}';
