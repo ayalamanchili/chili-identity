e15da5ce72e1277a1e97928a7fd1dbba68e9498f
raghu l <raghu.l@xyz.com>
2012-09-21 10:00:00 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1338 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/client/TreeClientPanel.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.client;
+
+import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.TreePanelComposite;
+import info.yalamanchili.office.client.tae.project.ProjectOptionsPanel;
+import info.yalamanchili.office.client.tae.project.ReadAllProjectsPanel;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class TreeClientPanel extends TreePanelComposite {
+
+    private static TreeClientPanel instance;
+
+    public static TreeClientPanel instance() {
+        return instance;
+    }
+    private static Logger logger = Logger.getLogger(TreeClientPanel.class.getName());
+    protected static final String PROJECT_NODE = "project";
+    
+     public TreeClientPanel(String entityId) {
+        super(entityId);
+        instance = this;
+        init("Client", OfficeWelcome.constants);
+    }
+     
+    @Override
+    protected void addListeners() {
+       
+    }
+
+    @Override
+    protected void configure() {
+       
+    }
+
+    @Override
+    protected void addWidgets() {
+        addFirstChildLink("Project", PROJECT_NODE);
+    }
+
+    @Override
+    public void treeNodeSelected(String entityNodeKey) {
+          if (PROJECT_NODE.equals(entityNodeKey)) {
+            TabPanel.instance().myOfficePanel.entityPanel.clear();
+            TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllProjectsPanel(entityId));
+            TabPanel.instance().myOfficePanel.entityPanel.add(new ProjectOptionsPanel());
+        }
+    }
+
+    @Override
+    public JSONObject loadEntity() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void showEntity() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
