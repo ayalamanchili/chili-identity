23365e385f7b4c6364dbfb0853894e7e24ad887e
bapuji y <bapuji.y@xyz.com>
2012-09-24 14:03:20 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1388 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/sow/SOWOptionPanel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.sow;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import info.yalamanchili.gwt.composite.ALComposite;
+import info.yalamanchili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite.CreateCompositeType;
+import info.yalamanchili.office.client.tae.sow.CreateSOWPanel;
+/**
+ *
+ * @author Yogi
+ */
+
+public class SOWOptionsPanel extends ALComposite implements ClickHandler {
+
+    protected HorizontalPanel panel = new HorizontalPanel();
+
+    protected ClickableLink addSOWLink = new ClickableLink("Add SOW");
+    
+    public SOWOptionsPanel() {
+		init(panel);
+	}
+    @Override
+    protected void addListeners() {
+        addSOWLink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+        panel.setSpacing(5);
+    }
+
+    @Override
+    protected void addWidgets() {
+       if (Auth.isAdmin() || Auth.isHR()) {
+			panel.add(addSOWLink);
+		}
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(addSOWLink)) {
+			TabPanel.instance().TimeandExpensePanel.entityPanel.clear();
+			TabPanel.instance().TimeandExpensePanel.entityPanel.add(new CreateSOWPanel(CreateCompositeType.ADD));
+		}
+    }
+    
+}
