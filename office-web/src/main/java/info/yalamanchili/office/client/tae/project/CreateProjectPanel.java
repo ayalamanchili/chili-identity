71537070353cb901d317991aa946ea940eff7b82
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-24 13:21:03 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1381 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/project/CreateProjectPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/project/CreateProjectPanel.java
@@ -81,7 +81,7 @@ public class CreateProjectPanel extends CreateComposite {
     @Override
     protected void addWidgets() {
         addField("name", false, true, DataType.STRING_FIELD);
-        addField("description", false, true, DataType.STRING_FIELD);
+        addField("description", false, false, DataType.STRING_FIELD);
         addField("startDate", false, true, DataType.DATE_FIELD);
         addField("endDate", false, true, DataType.DATE_FIELD);
     }
