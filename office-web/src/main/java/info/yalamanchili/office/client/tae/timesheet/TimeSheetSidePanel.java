00e90de53777fb18c0f53641d5d9e6f99a601d5e
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-25 09:16:41 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1403 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/timesheet/TimeSheetSidePanel.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.timesheet;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import info.yalamanchili.gwt.composite.ALComposite;
+import info.yalamanchili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.tae.client.CreateClientPanel;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class TimeSheetSidePanel extends ALComposite implements ClickHandler {
+
+    private static Logger logger = Logger.getLogger(TimeSheetSidePanel.class.getName());
+    public FlowPanel timeSheetsidepanel = new FlowPanel();
+    ClickableLink createtimeSheetlink = new ClickableLink("Create TimeSheet");
+
+    public TimeSheetSidePanel() {
+        init(timeSheetsidepanel);
+    }
+
+    @Override
+    protected void addListeners() {
+        createtimeSheetlink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            timeSheetsidepanel.add(createtimeSheetlink);
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(createtimeSheetlink)) {
+            TabPanel.instance().TimeandExpensePanel.entityPanel.clear();
+//            TabPanel.instance().TimeandExpensePanel.entityPanel.add(new CreateTimeSheetPanel(CreateComposite.CreateCompositeType.CREATE));
+        }
+    }
+}
