6dbd827fef159c5abcd93f5cb1cfad960019c689
balakrishna m <balakrishna.m@xyz.com>
2012-09-25 07:26:18 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1399 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/timesheetperiod/CreateTimeSheetPeriodPanel.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.timesheetperiod;
+
+import info.yalamanchili.office.client.tae.project.ReadAllProjectsPanel;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.yalamanchili.gwt.fields.DataType;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.profile.emailtype.CreateEmailTypePanel;
+import info.yalamanchili.office.client.profile.skill.SkillSidePanel;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+/**
+ *
+ * @author bala
+ */
+
+public class CreateTimeSheetPeriodPanel extends CreateComposite {
+
+    private static Logger logger = Logger.getLogger(info.yalamanchili.office.client.tae.client.CreateClientPanel.class.getName());
+
+    public CreateTimeSheetPeriodPanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+        initCreateComposite("Time Sheet Period", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject clnt = new JSONObject();
+
+        assignEntityValueFromField("name", clnt);
+        assignEntityValueFromField("description", clnt);
+        logger.info(clnt.toString());
+        return clnt;
+    }
+
+    @Override
+    protected void createButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        logger.info(arg0.getMessage());
+                        handleErrorResponse(arg0);
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postCreateSuccess(arg0);
+                    }
+                });
+    }
+
+    @Override
+    protected void addButtonClicked() {
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+        new ResponseStatusWidget().show("successfully Time Sheet Period created");
+        TabPanel.instance().TimeandExpensePanel.sidePanelTop.clear();
+        TabPanel.instance().TimeandExpensePanel.sidePanelTop.add(new TimeSheetPeriodSidePanel());
+        TabPanel.instance().TimeandExpensePanel.entityPanel.clear();
+        //TabPanel.instance().TimeandExpensePanel.entityPanel.add(new ReadAllTimeSheetPeriodPanel());
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("name", false, true, DataType.STRING_FIELD);
+        addField("description", false, false, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "timesheetperiod" ;
+    }
+}
---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/timesheetperiod/TimeSheetPeriodSidePanel.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.timesheetperiod;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import info.yalamanchili.gwt.composite.ALComposite;
+import info.yalamanchili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import java.util.logging.Logger;
+/**
+ *
+ * @author bala
+ */
+
+public class TimeSheetPeriodSidePanel extends ALComposite implements ClickHandler {
+
+    private static Logger logger=Logger.getLogger(info.yalamanchili.office.client.tae.client.ClientSidePanel.class.getName());
+    public FlowPanel timesheetperiodsidepanel=new FlowPanel();
+    ClickableLink createtimesheetperiodlink= new ClickableLink("Create Time Sheet Period");
+    
+     public TimeSheetPeriodSidePanel(){
+        init(timesheetperiodsidepanel);
+    }
+    @Override
+    protected void addListeners() {
+        createtimesheetperiodlink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+        
+    }
+
+    @Override
+    protected void addWidgets() {
+         if (Auth.isAdmin() || Auth.isHR()) {
+            timesheetperiodsidepanel.add(createtimesheetperiodlink);
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(createtimesheetperiodlink)) {
+            TabPanel.instance().TimeandExpensePanel.entityPanel.clear();
+            TabPanel.instance().TimeandExpensePanel.entityPanel.add(new CreateTimeSheetPeriodPanel(CreateComposite.CreateCompositeType.CREATE));
+        }
+    }
+    
+}
