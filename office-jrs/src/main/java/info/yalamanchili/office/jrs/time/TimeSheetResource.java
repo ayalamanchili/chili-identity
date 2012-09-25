964752e365d3a5f3b75d4aebbdbd769cfcc26c5c
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-09-25 08:51:51 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1402 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/time/TimeSheetResource.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.time;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.time.TimeSheetDao;
+import info.yalamanchili.office.entity.time.TimeSheet;
+import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Prashanthi
+ */
+@Path("timeSheet")
+@Component
+@Scope("request")
+public class TimeSheetResource extends CRUDResource<TimeSheet> {
+
+    @Autowired
+    public TimeSheetDao timeSheetDao;
+
+    @Override
+    public CRUDDao getDao() {
+        return timeSheetDao;
+    }
+
+    @GET
+    @Path("/{start}/{limit}")
+    public TimeSheetResource.TimeSheetTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        TimeSheetResource.TimeSheetTable tableObj = new TimeSheetResource.TimeSheetTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class TimeSheetTable {
+
+        protected Long size;
+        protected List<TimeSheet> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<TimeSheet> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<TimeSheet> entities) {
+            this.entities = entities;
+        }
+    }
+}
