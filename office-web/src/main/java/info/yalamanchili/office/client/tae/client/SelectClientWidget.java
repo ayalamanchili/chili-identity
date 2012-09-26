c5f9e803873547dbc927db7dff07637fc0a1fe14
raghu l <raghu.l@xyz.com>
2012-09-26 06:41:18 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1409 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/client/SelectClientWidget.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.client;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.gwt.callback.ALAsyncCallback;
+import info.yalamanchili.gwt.utils.JSONUtils;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.SelectComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author raghu
+ */
+public class SelectClientWidget extends SelectComposite {
+
+     public SelectClientWidget(Boolean readOnly, Boolean isRequired) {
+        super(OfficeWelcome.constants, "client", "Client", "name", readOnly, isRequired);
+    }
+    @Override
+    protected void fetchDropDownData() {
+         HttpService.HttpServiceAsync.instance().doGet(getDropDownURL(0, 10, null, null, null),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String entityString) {
+                processData(entityString);
+            }
+        });
+    }
+
+    @Override
+    protected Map<Integer, String> populateValues(JSONArray entities) {
+       Map<Integer, String> values = new HashMap<Integer, String>();
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            Integer id = Integer.valueOf(JSONUtils.toString(entity, "id"));
+            String value = JSONUtils.toString(entity, attributeKey);
+            values.put(id, value);
+        }
+        return values;
+    }
+
+    @Override
+    protected String getDropDownURL(Integer start, Integer limit, String param1, String param2, String param3) {
+        return OfficeWelcome.constants.root_url() + "client/dropdown/" + start.toString() + "/" + limit.toString();
+    }
+
+    @Override
+    protected void validate() {
+         clearMessage();
+    }
+    
+}
