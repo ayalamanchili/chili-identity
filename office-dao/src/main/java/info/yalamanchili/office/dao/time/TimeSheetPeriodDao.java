427aec0ccf6dc9237d4ed5a239b6fcedab9d10f3
balakrishna m <balakrishna.m@xyz.com>
2012-10-01 07:59:14 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1444 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetPeriodDao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.time;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.time.TimeSheetPeriod;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author bala
+ */
+
+public class TimeSheetPeriodDao extends CRUDDao<TimeSheetPeriod> {
+
+    @PersistenceContext
+    protected EntityManager em;
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TimeSheetPeriodDao() {
+        super(TimeSheetPeriod.class);
+    }
+}
