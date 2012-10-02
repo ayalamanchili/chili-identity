596ca19d9034f2434ee78a60d391756a27878f9f
Phani Yalamanchili <phani.y@sstech.us>
2012-10-02 20:02:38 +0000

    roles working.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@1491 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-dao/src/main/java/info/yalamanchili/office/dao/security/CroleDao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.security;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.security.CRole;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+@Component
+public class CroleDao extends CRUDDao<CRole> {
+
+    @PersistenceContext
+    protected EntityManager em;
+
+    public CroleDao() {
+        super(CRole.class);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+}
