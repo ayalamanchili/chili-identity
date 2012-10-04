d02a34fa7683e5d14bd4a748074cd6c0e8fa1e3e
Phani Yalamanchili <phani.y@sstech.us>
2012-10-04 12:15:36 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1519 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetDao.java
@@ -4,10 +4,12 @@
  */
 package info.yalamanchili.office.dao.time;
 
+import info.chili.beans.BeanMapper;
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.client.StatementOfWork;
 import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.entity.time.TimeSheet;
+import info.yalamanchili.office.entity.time.TimeSheetPeriod;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
@@ -18,28 +20,36 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TimeSheetDao extends CRUDDao<TimeSheet> {
-    
+
     @PersistenceContext
     protected EntityManager em;
-    
+
     @Override
     public TimeSheet save(TimeSheet entity) {
-        Employee emp = getEntityManager().find(Employee.class, entity.getEmployee().getId());
+        if (entity.getId() != null) {
+            entity = (TimeSheet) BeanMapper.merge(entity, getEntityManager().find(TimeSheet.class, em));
+        }
+        //Statemenet of work
         if (entity.getStatementOfWork() == null) {
             entity.setStatementOfWork(null);
         } else {
             StatementOfWork sow = getEntityManager().find(StatementOfWork.class, entity.getStatementOfWork().getId());
             entity.setStatementOfWork(sow);
         }
+        //TimeSheetPeriod
+        TimeSheetPeriod period = getEntityManager().find(TimeSheetPeriod.class, entity.getTimeSheetPeriod().getId());
+        entity.setTimeSheetPeriod(period);
+        //Employee
+        Employee emp = getEntityManager().find(Employee.class, entity.getEmployee().getId());
         entity.setEmployee(emp);
         return getEntityManager().merge(entity);
     }
-    
+
     @Override
     public EntityManager getEntityManager() {
         return em;
     }
-    
+
     public TimeSheetDao() {
         super(TimeSheet.class);
     }
