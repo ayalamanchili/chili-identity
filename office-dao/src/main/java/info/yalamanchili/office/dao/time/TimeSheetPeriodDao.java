1532e561062b51519f0d919766b91487ef669cf1
balakrishna m <balakrishna.m@xyz.com>
2012-10-04 14:55:59 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1529 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetPeriodDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetPeriodDao.java
@@ -4,11 +4,14 @@
  */
 package info.yalamanchili.office.dao.time;
 
+import info.chili.beans.BeanMapper;
+import info.chili.jpa.AbstractEntity;
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.time.TimeSheetPeriod;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
+import java.util.Calendar;
 /**
  *
  * @author bala
@@ -26,5 +29,22 @@ public class TimeSheetPeriodDao extends CRUDDao<TimeSheetPeriod> {
 
     public TimeSheetPeriodDao() {
         super(TimeSheetPeriod.class);
+    }
+    @Override
+    public TimeSheetPeriod save(TimeSheetPeriod entity) {
+        Calendar c1 = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
+        c1.setTime(entity.getStartDate());
+        c2.setTime(entity.getEndDate());
+        if(c1.get(Calendar.DAY_OF_WEEK) !=1 || c2.get(Calendar.DAY_OF_WEEK) !=5 ){
+            //throw exception
+        }
+
+        if (entity instanceof AbstractEntity) {
+            if (((AbstractEntity) entity).getId() != null) {
+                entity = (TimeSheetPeriod) BeanMapper.merge(entity, findById(((AbstractEntity) entity).getId()));
+            }
+        }
+        return getEntityManager().merge(entity);
     }
 }
