9a2758402c0b94af42cec419db1381f5e16a0fb4
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-10-08 13:26:12 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1563 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/home/todo/UpdateTodoPanel.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.home.todo;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.yalamanchili.gwt.fields.DataType;
+import info.yalamanchili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.UpdateComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class UpdateTodoPanel extends UpdateComposite {
+
+    private DataType DataType;
+
+    public UpdateTodoPanel(JSONObject entity) {
+        initUpdateComposite(entity, "Todo", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        assignEntityValueFromField("name", entity);
+        assignEntityValueFromField("description", entity);
+        assignEntityValueFromField("tododate", entity);
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("name", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("description", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("tododate", entity, DataType.DATE_FIELD);
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        new ResponseStatusWidget().show("Successfully Updated Todo Employee");
+        TabPanel.instance().TimeandExpensePanel.sidePanelTop.clear();
+        // TabPanel.instance().TimeandExpensePanel.sidePanelTop.add(new ProjectSidePanel());
+        TabPanel.instance().TimeandExpensePanel.entityPanel.clear();
+        //TabPanel.instance().TimeandExpensePanel.entityPanel.add(new ReadAllProjectsPanel());
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("name", false, true, DataType.STRING_FIELD);
+        addField("description", false, true, DataType.STRING_FIELD);
+        addField("tododate", false, true, DataType.DATE_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "todo";
+    }
+}
