0a11309d65da70f336aaeccc372deab86e96ea47
balakrishna m <balakrishna.m@xyz.com>
2012-10-09 09:23:15 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1580 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/TodoResource.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.profile;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.profile.TodoDao;
+import info.yalamanchili.office.entity.profile.Todo;
+import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author bala
+ */
+
+@Path("todo")
+@Component
+@Scope("request")
+public class TodoResource extends CRUDResource<Todo> {
+
+    @Autowired
+    public TodoDao todoDao;
+
+    @Override
+    public CRUDDao getDao() {
+        return todoDao;
+    }
+
+    @GET
+    @Path("/{start}/{limit}")
+    public TodoResource.TodoTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        TodoResource.TodoTable tableObj = new TodoResource.TodoTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class TodoTable {
+
+        protected Long size;
+        protected List<Todo> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<Todo> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<Todo> entities) {
+            this.entities = entities;
+        }
+    }
+}
