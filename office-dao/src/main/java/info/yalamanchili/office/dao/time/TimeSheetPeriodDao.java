e29308de9571e1e167a9ddeffbe9504deb16be56
Phani Yalamanchili <phani.y@sstech.us>
2012-10-10 01:03:11 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1587 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetPeriodDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetPeriodDao.java
@@ -6,12 +6,15 @@ package info.yalamanchili.office.dao.time;
 
 import info.chili.beans.BeanMapper;
 import info.chili.jpa.AbstractEntity;
+import info.chili.service.jrs.ServiceMessages;
+import info.chili.service.jrs.exception.ServiceException;
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.time.TimeSheetPeriod;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
 import java.util.Calendar;
+
 /**
  *
  * @author bala
@@ -30,16 +33,22 @@ public class TimeSheetPeriodDao extends CRUDDao<TimeSheetPeriod> {
     public TimeSheetPeriodDao() {
         super(TimeSheetPeriod.class);
     }
+
     @Override
     public TimeSheetPeriod save(TimeSheetPeriod entity) {
         Calendar c1 = Calendar.getInstance();
         Calendar c2 = Calendar.getInstance();
         c1.setTime(entity.getStartDate());
         c2.setTime(entity.getEndDate());
-        if(c1.get(Calendar.DAY_OF_WEEK) !=2 || c2.get(Calendar.DAY_OF_WEEK) !=1 ){
-           throw new RuntimeException("StartDate and EndDate are invalid");
+        //TODO try to process all the errors collect them and throw a service exception at the end
+        if (c1.get(Calendar.DAY_OF_WEEK) != 2) {
+            throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "StartDate", "StartDate", "Start Date must be monday");
         }
-
+        if (c2.get(Calendar.DAY_OF_WEEK) != 1) {
+            throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "EndDate", "EndDate", "End Date Must be Sunday");
+        }
+        
+        //TODO check if the startDate and endDate belong to same week
         if (entity instanceof AbstractEntity) {
             if (((AbstractEntity) entity).getId() != null) {
                 entity = (TimeSheetPeriod) BeanMapper.merge(entity, findById(((AbstractEntity) entity).getId()));
