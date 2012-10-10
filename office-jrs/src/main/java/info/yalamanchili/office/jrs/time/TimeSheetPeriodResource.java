9983d82c78a75974064e68f0207925b736a119e4
raghu l <raghu.l@xyz.com>
2012-10-10 14:19:32 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1596 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-jrs/src/main/java/info/yalamanchili/office/jrs/time/TimeSheetPeriodResource.java
+++ office-jrs/src/main/java/info/yalamanchili/office/jrs/time/TimeSheetPeriodResource.java
@@ -6,10 +6,12 @@ package info.yalamanchili.office.jrs.time;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.time.TimeSheetPeriodDao;
+import info.yalamanchili.office.entity.client.Project;
 import info.yalamanchili.office.entity.time.TimeSheetPeriod;
 import info.yalamanchili.office.jrs.CRUDResource;
 import java.util.List;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.xml.bind.annotation.XmlElement;
@@ -17,6 +19,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 /**
  *
@@ -38,6 +41,7 @@ public class TimeSheetPeriodResource extends CRUDResource<TimeSheetPeriod> {
 
     @GET
     @Path("/{start}/{limit}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
     public TimeSheetPeriodResource.TimeSheetTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
         TimeSheetPeriodResource.TimeSheetTable tableObj = new TimeSheetPeriodResource.TimeSheetTable();
         tableObj.setEntities(getDao().query(start, limit));
@@ -45,6 +49,21 @@ public class TimeSheetPeriodResource extends CRUDResource<TimeSheetPeriod> {
         return tableObj;
     }
 
+    @PUT
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    @Override
+    public TimeSheetPeriod save(TimeSheetPeriod entity) {
+        return super.save(entity);
+    }
+    
+    @PUT
+    @Path("/delete/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    @Override
+    public void delete(@PathParam("id") Long id) {
+        super.delete(id);
+    }
+    
     @XmlRootElement
     @XmlType
     public static class TimeSheetTable {
