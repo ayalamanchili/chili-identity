fa0e7528d4551c2c4c919a977c6fe93967828adc
balakrishna m <balakrishna.m@xyz.com>
2012-10-12 12:36:14 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1609 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-dao/src/main/java/info/yalamanchili/office/dao/profile/SkillSetDao.java
+++ office-dao/src/main/java/info/yalamanchili/office/dao/profile/SkillSetDao.java
@@ -4,12 +4,8 @@
  */
 package info.yalamanchili.office.dao.profile;
 
-import info.chili.beans.BeanMapper;
-import info.chili.jpa.AbstractEntity;
-import info.chili.service.jrs.exception.ServiceException;
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.entity.profile.SkillSet;
-import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -35,18 +31,5 @@ public class SkillSetDao extends CRUDDao<SkillSet> {
     public SkillSetDao() {
         super(SkillSet.class);
     }
-    
-    @Override
-    public SkillSet save(SkillSet entity) {
-        
-        if(entity.getResumeUrl()== null){
-            throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "ResumeUrl", "ResumeUrl", "Resume URL can not be null");
-        }
-        if (entity instanceof AbstractEntity) {
-            if (((AbstractEntity) entity).getId() != null) {
-                entity = (SkillSet) BeanMapper.merge(entity, findById(((AbstractEntity) entity).getId()));
-            }
-        }
-        return getEntityManager().merge(entity);
-    }
+   
 }
