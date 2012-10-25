f79acd72a51c6c9624398cf7952d5fa4b1ad2901
raghu l <raghu.l@xyz.com>
2012-10-25 09:16:06 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1721 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/social/SocialMenu.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.social;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.MenuBar;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.social.company.CompanyFeedHome;
+import info.yalamanchili.office.client.social.employee.EmployeeFeedHome;
+
+/**
+ *
+ * @author raghu
+ */
+public class SocialMenu extends Composite {
+    MenuBar tAEMenuBar = new MenuBar(false);
+    FlowPanel panel = new FlowPanel();
+    private static boolean IsEmployeeFeedSelected = false;
+
+    public SocialMenu() {
+        initWidget(tAEMenuBar);
+        configureSocialMenu();
+    }
+
+    protected void configureSocialMenu() {
+        MenuBar menu = new MenuBar(true);
+//        tAEMenuBar.addItem("Menu", menu);
+       
+        
+        
+//        if (Auth.isAdmin() || Auth.isHR()) {
+            tAEMenuBar.addItem("Employee Feed", employeefeedCmd);
+            tAEMenuBar.addItem("System Soft Feed", systemsoftfeedCmd);
+            
+//        }
+         tAEMenuBar.addStyleName("entityMenuBar");
+    }
+    
+    public static boolean isEmployeedFeedSelected() {
+        return IsEmployeeFeedSelected;
+    }
+
+    public static boolean isCompanyFeedSelected() {
+        return !IsEmployeeFeedSelected;
+    }
+    
+     Command employeefeedCmd = new Command() {
+        public void execute() {
+             IsEmployeeFeedSelected = true;
+            TabPanel.instance().getSocialPanel().entityPanel.clear();
+            TabPanel.instance().getSocialPanel().sidePanelTop.clear();
+            TabPanel.instance().getSocialPanel().entityPanel.add(new EmployeeFeedHome());
+//            TabPanel.instance().getSocialPanel().sidePanelTop.add(new ClientSidePanel());
+        }
+    };
+     
+      Command systemsoftfeedCmd = new Command() {
+        public void execute() {
+            IsEmployeeFeedSelected = false;
+            TabPanel.instance().getSocialPanel().entityPanel.clear();
+            TabPanel.instance().getSocialPanel().sidePanelTop.clear();
+            TabPanel.instance().getSocialPanel().entityPanel.add(new CompanyFeedHome());
+//            TabPanel.instance().getSocialPanel().sidePanelTop.add(new ClientSidePanel());
+        }
+    };
+}
