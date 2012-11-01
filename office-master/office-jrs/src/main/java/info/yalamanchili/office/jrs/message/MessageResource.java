79e544fd7616401d21d3f1d3ab432771f34a0a50
raghu l <raghu.l@xyz.com>
2012-11-01 13:15:49 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1773 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/message/MessageResource.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.message;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.jrs.CRUDResource;
+import info.yalamanchili.office.dto.message.MessageDto;
+import info.yalamanchili.office.dao.message.MessageDao;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.List;
+import javax.ws.rs.Path;
+import javax.xml.bind.annotation.XmlElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ *
+ * @author raghu
+ */
+@Path("message")
+@Component
+@Transactional
+@Scope("request")
+public class MessageResource extends CRUDResource<MessageDto> {
+
+    @Autowired
+    public MessageDao messageDao;
+     
+    @Override
+    public CRUDDao getDao() {
+        return messageDao;
+    }
+    
+    @XmlRootElement
+    @XmlType
+    public static class EmergencyContactTable {
+
+        protected Long size;
+        protected List<MessageDto> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<MessageDto> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<MessageDto> entities) {
+            this.entities = entities;
+        }
+    }
+}
