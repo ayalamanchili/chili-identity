c8a5c0ba02ab58c647e079e002e2b8996ccfa328
raghu l <raghu.l@xyz.com>
2012-11-02 13:22:16 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1788 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/home/message/MessageStackPanelWidget.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.home.message;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.gwt.GenericPopup;
+import info.yalamanchili.office.client.home.message.ReadAllMessagePanel;
+import info.yalamanchili.office.client.home.todo.CreateTodoPanel;
+
+/**
+ *
+ * @author raghu
+ */
+public class MessageStackPanelWidget extends ALComposite implements ClickHandler {
+
+     protected ScrollPanel panel = new ScrollPanel();
+    protected FlowPanel mainPanel = new FlowPanel();
+    protected ClickableLink createMsgL = new ClickableLink("Create");
+    protected ReadAllMessagePanel readAllPanel = new ReadAllMessagePanel();
+    
+    public MessageStackPanelWidget() {
+        init(panel);
+    }
+
+    @Override
+    protected void addListeners() {
+        createMsgL.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+        createMsgL.addStyleName("createMsgL");
+    }
+
+    @Override
+    protected void addWidgets() {
+        mainPanel.add(createMsgL);
+        mainPanel.add(readAllPanel);
+        panel.add(mainPanel);
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+         if (event.getSource().equals(createMsgL)) {
+            GenericPopup.instance().show(new CreateTodoPanel(CreateComposite.CreateCompositeType.CREATE), createMsgL.getAbsoluteLeft(), createMsgL.getAbsoluteTop());
+        }
+    }
+    
+}
