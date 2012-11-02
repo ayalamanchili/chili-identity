1d78f7158942757b0da19025a539dedc9d9d41bd
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-11-02 14:33:13 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1793 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/home/MessageService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.home;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class MessageService {
+    
+}
