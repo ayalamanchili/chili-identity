39f2aafaf6bf2b52c57ba35a19067282068fc750
Phani Yalamanchili <phani.y@sstech.us>
2012-11-03 18:10:52 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1796 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/dto/message/MessageDto.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/dto/message/MessageDto.java
@@ -11,12 +11,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import org.dozer.Mapper;
 
 /**
  *
  * @author raghu
  */
+@XmlRootElement(name = "Message")
+@XmlType
 public class MessageDto implements Serializable {
 
     protected String subject;
