e5ad9e5c2fdc0f6b1844f69cdbec88432446d9fd
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-11-05 07:13:07 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1815 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/home/MessageService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/home/MessageService.java
@@ -4,10 +4,45 @@
  */
 package info.yalamanchili.office.home;
 
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.dao.message.MessageDao;
+import info.yalamanchili.office.dao.security.SecurityService;
+import info.yalamanchili.office.dto.message.MessageDto;
+import info.yalamanchili.office.entity.message.Message;
+import java.util.Map.Entry;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  *
  * @author Prashanthi
  */
 public class MessageService {
-    
+
+    @PersistenceContext
+    protected EntityManager em;
+    @Autowired
+    protected Mapper mapper;
+
+    public void createMessage(MessageDto messageDto) {
+        Message newMessage = new Message();
+        newMessage.setSubject(messageDto.getSubject());
+        newMessage.setMessage(messageDto.getMessage());
+        newMessage.setMessageTs(messageDto.getMessageTs());
+
+        newMessage.setFrom(SecurityService.instance().getCurrentUser());
+
+
+        for (Entry emp : messageDto.getTos()) {
+            newMessage.addTos(MessageDao.instance.getEmployeWithEmpId(emp.getId()));
+        }
+
+        MessageDao.instance().save(newMessage);
+    }
+
+    public static MessageService instance() {
+        return SpringContext.getBean(MessageService.class);
+    }
 }
