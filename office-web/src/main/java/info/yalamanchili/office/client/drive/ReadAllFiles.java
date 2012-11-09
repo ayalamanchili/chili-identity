1352a50a33050220ec8fd5d138e6cb15496945a0
raghu l <raghu.l@xyz.com>
2012-11-09 15:41:15 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1850 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/drive/ReadAllFiles.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.drive;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.utils.JSONUtils;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.ReadAllComposite;
+import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author raghu
+ */
+public class ReadAllFiles extends ReadAllComposite {
+private static Logger logger = Logger.getLogger(ReadAllFiles.class.getName());
+
+ public static ReadAllFiles instance;
+ 
+   public ReadAllFiles(String parentId) {
+        instance = this;
+        this.parentId = parentId;
+        initTable("Files", OfficeWelcome.constants);
+    }
+   
+    @Override
+    public void preFetchTable(int start) {
+         HttpService.HttpServiceAsync.instance().doGet(getreadallfilesURL(parentId, start, OfficeWelcome.constants.tableSize()),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+
+            @Override
+            public void onResponse(String result) {
+                logger.info(result);
+                postFetchTable(result);
+            }
+        });
+    }
+
+     public String getreadallfilesURL(String folderId, Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "drive/files/" + folderId  ;
+               
+    }
+     
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("Name"));
+        table.setText(0, 2, getKeyValue("Created Time Stamp"));
+        table.setText(0, 3, getKeyValue("Updated Time Stamp"));
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+         for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 2, JSONUtils.toString(entity, "name"));
+            table.setText(i, 3, JSONUtils.toString(entity, "createdTs"));
+            table.setText(i, 4, JSONUtils.toString(entity, "updatedTs"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+         createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+        
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+        
+    }
+
+   
+    
+}
