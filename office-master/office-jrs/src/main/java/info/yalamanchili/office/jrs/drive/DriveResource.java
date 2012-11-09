ea1f85df4cee490db09549cccda8dbcddbd12e2f
raghu l <raghu.l@xyz.com>
2012-11-09 15:40:27 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1848 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/drive/DriveResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/drive/DriveResource.java
@@ -7,12 +7,17 @@ package info.yalamanchili.office.jrs.drive;
 import info.yalamanchili.office.drive.DriveService;
 import info.yalamanchili.office.dto.drive.FileDto;
 import info.yalamanchili.office.dto.drive.FolderDto;
+import info.yalamanchili.office.entity.profile.Address;
+import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -51,4 +56,36 @@ public class DriveResource {
     public String addFile(@PathParam("folderId") Long folderId, FileDto file) {
         return driveService.addFile(folderId, file);
     }
+    
+    @GET
+    @Path("/files/{folderId}")
+    @Produces("application/text")
+    public List<FileDto> getFiles(@PathParam("folderId") long id) {
+        return driveService.getFiles(id);
+    }
+    
+//    @XmlRootElement
+//    @XmlType
+//    public static class FileTable {
+//
+//        protected Long size;
+//        protected List<FileDto> entities;
+//
+//        public Long getSize() {
+//            return size;
+//        }
+//
+//        public void setSize(Long size) {
+//            this.size = size;
+//        }
+//
+//        @XmlElement
+//        public List<FileDto> getEntities() {
+//            return entities;
+//        }
+//
+//        public void setEntities(List<FileDto> entities) {
+//            this.entities = entities;
+//        }
+//    }
 }
