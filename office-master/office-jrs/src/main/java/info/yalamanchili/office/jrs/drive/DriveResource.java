2ad3b31ab986f277fb5176b8a04668c2c3043385
raghu l <raghu.l@xyz.com>
2012-11-12 15:24:22 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1866 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/drive/DriveResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/drive/DriveResource.java
@@ -8,6 +8,7 @@ import info.yalamanchili.office.drive.DriveService;
 import info.yalamanchili.office.dto.drive.FileDto;
 import info.yalamanchili.office.dto.drive.FileDto.FileTable;
 import info.yalamanchili.office.dto.drive.FolderDto;
+import info.yalamanchili.office.dao.drive.FileDao;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -17,6 +18,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,6 +36,9 @@ public class DriveResource {
 
     @Autowired
     protected DriveService driveService;
+    
+   
+    
 
     @Path("/tree")
     @GET
@@ -54,6 +59,13 @@ public class DriveResource {
         return driveService.addFile(folderId, file);
     }
 
+    @PUT
+    @Path("/files/delete/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    public void deletefile(@PathParam("id") Long id) {
+        FileDao.instance().delete(id);
+    }
+     
     @GET
     @Path("/files/{folderId}/{start}/{limit}")
     public FileTable getFiles(@PathParam("folderId") long id, @PathParam("start") int start, @PathParam("limit") int limit) {
