2c25f000748d0fb304171ae6ee0d0e4217a54fae
Phani Yalamanchili <phani.y@sstech.us>
2012-11-15 13:04:55 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1888 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/notification/CreateNotificationGroupPanel.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.notification;
+
+import info.chili.gwt.composite.ALComposite;
+
+/**
+ *
+ * @author anuyalamanchili
+ */
+public class CreateNotificationGroupPanel extends ALComposite {
+
+    public CreateNotificationGroupPanel() {
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+    }
+}
