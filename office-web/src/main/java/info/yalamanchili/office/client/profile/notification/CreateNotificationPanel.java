ff9950283dc44197bf907a518ac654a3f9cf2c79
raghu l <raghu.l@xyz.com>
2012-11-15 09:44:32 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1886 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/notification/CreateNotificationPanel.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.notification;
+
+import com.google.gwt.json.client.JSONObject;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+
+/**
+ *
+ * @author raghu
+ */
+public class CreateNotificationPanel extends CreateComposite{
+
+    public CreateNotificationPanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+        initCreateComposite("Notification", OfficeWelcome.constants);
+    }
+    
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void createButtonClicked() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void addButtonClicked() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void addListeners() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void configure() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void addWidgets() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String getURI() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/notification/NotificationSidePanel.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.notification;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class NotificationSidePanel extends ALComposite implements ClickHandler {
+
+    private static Logger logger=Logger.getLogger(NotificationSidePanel.class.getName());
+    public FlowPanel notificationsidepanel=new FlowPanel();
+    ClickableLink createnotificationlink= new ClickableLink("Create Notification");
+    
+     public NotificationSidePanel(){
+        init(notificationsidepanel);
+    }
+     
+    @Override
+    protected void addListeners() {
+       createnotificationlink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+        
+    }
+
+    @Override
+    protected void addWidgets() {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            notificationsidepanel.add(createnotificationlink);
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+         if (event.getSource().equals(createnotificationlink)) {
+            TabPanel.instance().myOfficePanel.entityPanel.clear();
+            TabPanel.instance().myOfficePanel.entityPanel.add(new CreateNotificationPanel(CreateComposite.CreateCompositeType.CREATE));
+        }
+    }
+    
+}
