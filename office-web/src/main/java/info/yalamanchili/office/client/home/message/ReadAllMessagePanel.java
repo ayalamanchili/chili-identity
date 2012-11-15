04802116932fe3d70755990b299ef659b6316154
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-11-15 09:07:56 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1885 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/home/message/ReadAllMessagePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/home/message/ReadAllMessagePanel.java
@@ -65,7 +65,7 @@ public class ReadAllMessagePanel extends ReadAllComposite {
             addOptionsWidget(i, entity);
             table.setText(i, 1, JSONUtils.toString(entity, "subject"));
             table.setText(i, 2, JSONUtils.toString(entity, "message"));
-            table.setText(i, 3, DateUtils.getFormatedDate(JSONUtils.toString(entity, "messageTs"), DateTimeFormat.PredefinedFormat.DATE_LONG));
+            table.setText(i, 3, DateUtils.getFormatedDate(JSONUtils.toString(entity, "messageTs"), DateTimeFormat.PredefinedFormat.DATE_TIME_FULL));
             table.setText(i, 4, JSONUtils.toString(entity, "from"));
         }
     }
