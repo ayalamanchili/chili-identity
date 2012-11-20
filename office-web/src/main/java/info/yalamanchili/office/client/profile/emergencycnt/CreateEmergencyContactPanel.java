596dbdda0dc35be0e70a1f3da11e5c44c6866332
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-11-20 14:03:58 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@1928 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/emergencycnt/CreateEmergencyContactPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/emergencycnt/CreateEmergencyContactPanel.java
@@ -89,7 +89,7 @@ public class CreateEmergencyContactPanel extends CreateComposite {
         addEnumField("sex", false, false, strs);
         addField("relation", false, true, DataType.STRING_FIELD);
         addField("ecPrimary", false, false, DataType.BOOLEAN_FIELD);
-        addField("email", false, true, DataType.STRING_FIELD);
+        addField("email", false, false, DataType.STRING_FIELD);
         addField("phoneNumber", false, true, DataType.LONG_FIELD);
     }
 
