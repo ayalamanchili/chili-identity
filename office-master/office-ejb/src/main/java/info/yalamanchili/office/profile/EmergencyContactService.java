ce176e7760ee063150c559b3c015931592e7d660
Phani Yalamanchili <phani.y@sstech.us>
2012-11-22 19:49:20 +0000

    fixed issue with emergency contact update.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@1946 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/EmergencyContactService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/EmergencyContactService.java
@@ -12,6 +12,7 @@ import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.entity.profile.Phone;
 import info.yalamanchili.office.profile.notification.ProfileNotificationService;
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,14 +24,15 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class EmergencyContactService {
-    
+
     @PersistenceContext
     protected EntityManager em;
     @Autowired
     protected Mapper mapper;
     @Autowired
-    protected ProfileNotificationService ProfileNotificationService;
-    
+    protected ProfileNotificationService profileNotificationService;
+
+    //TODO try to consolidate add and update
     public void addEmergencyContact(Long empId, info.yalamanchili.office.dto.profile.EmergencyContact ec) {
         Employee emp = (Employee) em.find(Employee.class, empId);
         Contact contact = new Contact();
@@ -39,7 +41,7 @@ public class EmergencyContactService {
         contact.setSex(ec.getSex());
         //Email
 
-        if ((ec.getEmail() != null) && (!ec.getEmail().isEmpty()) ){
+        if ((ec.getEmail() != null) && (!ec.getEmail().isEmpty())) {
             Email email = new Email();
             email.setEmail(ec.getEmail());
             email.setPrimaryEmail(Boolean.TRUE);
@@ -60,9 +62,9 @@ public class EmergencyContactService {
         emergencyCnt.setContact(contact);
         emergencyCnt.setEmployee(emp);
         em.merge(emergencyCnt);
-        ProfileNotificationService.sendEmergencyContactUpdateNotification(emp);
+        profileNotificationService.sendEmergencyContactUpdateNotification(emp);
     }
-    
+
     public info.yalamanchili.office.dto.profile.EmergencyContact update(info.yalamanchili.office.dto.profile.EmergencyContact ec) {
         //TODO user dozer mapping?
         EmergencyContact ecEntity = em.find(EmergencyContact.class, ec.getId());
@@ -73,22 +75,28 @@ public class EmergencyContactService {
         ecEntity.getContact().setMiddleInitial(ec.getMiddleInitial());
         ecEntity.getContact().setSex(ec.getSex());
         //Email
-        if((ec.getEmail() != null)&&(!ec.getEmail().isEmpty()))
-        {
+        if ((ec.getEmail() != null) && (!ec.getEmail().isEmpty())) {
             if (ecEntity.getContact().getEmails().size() > 0) {
-            ecEntity.getContact().getEmails().get(0).setEmail(ec.getEmail());
-             }
-        }
-        else if((ec.getEmail() == null)&&(ec.getEmail().isEmpty()))
-        {
-          ecEntity.getContact().getEmails().remove(ecEntity.getContact().getEmails().get(0));
+                ecEntity.getContact().getEmails().get(0).setEmail(ec.getEmail());
+            } else {
+                Email email = new Email();
+                email.setEmail(ec.getEmail());
+                email.setPrimaryEmail(Boolean.TRUE);
+                ecEntity.getContact().addEmail(email);
+            }
+        } else if ((ec.getEmail() == null) || (ec.getEmail().isEmpty())) {
+            if (ecEntity.getContact().getEmails().size() > 0) {
+                Email email = ecEntity.getContact().getEmails().get(0);
+                ecEntity.getContact().getEmails().remove(email);
+                em.remove(email);
+            }
         }
         //Phone
         if (ecEntity.getContact().getPhones().size() > 0) {
             ecEntity.getContact().getPhones().get(0).setPhoneNumber(ec.getPhoneNumber());
         }
         em.merge(ecEntity);
-        ProfileNotificationService.sendEmergencyContactUpdateNotification(ecEntity.getEmployee());
+        profileNotificationService.sendEmergencyContactUpdateNotification(ecEntity.getEmployee());
         return ec;
     }
 }
