25e742eee58333dd9ece598fb5ca37d333f39174
Phani Yalamanchili <phani.y@sstech.us>
2012-12-07 21:55:15 +0000

    http://yalamanchili.info:8080/jira/browse/SSTO-650
    git-svn-id: http://tools.sstech.us/svn/office/trunk@1993 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/gwt/TreeEntityItem.java
+++ office-web/src/main/java/info/yalamanchili/office/client/gwt/TreeEntityItem.java
@@ -17,7 +17,7 @@ import info.yalamanchili.office.client.drive.DriveFolderOptionsWidget;
  * @author yphanikumar
  */
 public class TreeEntityItem extends TreeItem {
-
+    
     protected JSONObject entity;
     protected String entityId;
     protected String key;
@@ -27,7 +27,7 @@ public class TreeEntityItem extends TreeItem {
             rightClick(event);
         }
     };
-
+    
     public TreeEntityItem(ConstantsWithLookup constants, String key, JSONObject entity) {
         super();
         super.setWidget(itemWIdget);
@@ -36,7 +36,7 @@ public class TreeEntityItem extends TreeItem {
         this.entity = entity;
         
     }
-
+    
     public TreeEntityItem(ConstantsWithLookup constants, String key, String entityId) {
         super();
         super.setWidget(itemWIdget);
@@ -45,24 +45,25 @@ public class TreeEntityItem extends TreeItem {
         this.entityId = entityId;
         
     }
-
+    
     public JSONObject getEntity() {
         return entity;
     }
-
+    
     public String getEntityId() {
         if (entityId == null) {
             return JSONUtils.toString(entity, "id");
         }
         return entityId;
     }
-
+    
     public String getKey() {
         return key;
     }
 //TODO this does not belong here
 
     public void rightClick(ContextMenuEvent event) {
+        this.getTree().setSelectedItem(this);
         DriveFolderOptionsWidget menu = new DriveFolderOptionsWidget();
         new GenericPopup(menu, "driveMenu", event.getNativeEvent().getClientX(), event.getNativeEvent().getClientY()).show();
     }
