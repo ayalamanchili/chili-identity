61af57b0893796f02460bdfab7ed3cd2909dbc13
raghu l <raghu.l@xyz.com>
2012-12-11 13:02:59 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2012 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmergencyContactResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmergencyContactResource.java
@@ -2,14 +2,16 @@ package info.yalamanchili.office.jrs.profile;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.profile.EmergencyContactDao;
-import info.yalamanchili.office.dto.profile.EmergencyContact;
+import info.yalamanchili.office.dto.profile.EmergencyContactDto;
 import info.yalamanchili.office.jrs.CRUDResource;
 import info.yalamanchili.office.profile.EmergencyContactService;
 
 import java.util.List;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
@@ -23,7 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @Transactional
 @Scope("request")
-public class EmergencyContactResource extends CRUDResource<EmergencyContact> {
+public class EmergencyContactResource extends CRUDResource<EmergencyContactDto> {
 
     @Autowired
     public EmergencyContactDao emergencyContactDao;
@@ -36,16 +38,23 @@ public class EmergencyContactResource extends CRUDResource<EmergencyContact> {
     }
 
     @PUT
-    public EmergencyContact save(EmergencyContact entity) {
+    public EmergencyContactDto save(EmergencyContactDto entity) {
         return emergencyContactService.update(entity);
     }
 
+    @GET
+    @Override
+    @Path("/{id}")
+    public EmergencyContactDto read(@PathParam("id") Long id) {
+        return emergencyContactService.read(id);
+    }
+     
     @XmlRootElement
     @XmlType
     public static class EmergencyContactTable {
 
         protected Long size;
-        protected List<EmergencyContact> entities;
+        protected List<EmergencyContactDto> entities;
 
         public Long getSize() {
             return size;
@@ -56,11 +65,11 @@ public class EmergencyContactResource extends CRUDResource<EmergencyContact> {
         }
 
         @XmlElement
-        public List<EmergencyContact> getEntities() {
+        public List<EmergencyContactDto> getEntities() {
             return entities;
         }
 
-        public void setEntities(List<EmergencyContact> entities) {
+        public void setEntities(List<EmergencyContactDto> entities) {
             this.entities = entities;
         }
     }
