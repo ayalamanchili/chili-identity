d064b214b8991a15099bd55ff5c0475ed35dd0c8
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-12-13 08:05:35 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2028 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/client/TreeClientPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/client/TreeClientPanel.java
@@ -29,52 +29,52 @@ public class TreeClientPanel extends TreePanelComposite {
     private static Logger logger = Logger.getLogger(TreeClientPanel.class.getName());
     protected static final String PROJECT_NODE = "project";
     protected static final String CLIENTLOCATION_NODE = "clientlocation";
-    
-     public TreeClientPanel(String entityId) {
+    protected static final String CONTACTS_NODE = "contacts";
+
+    public TreeClientPanel(String entityId) {
         super(entityId);
         instance = this;
         init("Client", OfficeWelcome.constants);
     }
-     
+
     @Override
     protected void addListeners() {
-       
     }
 
     @Override
     protected void configure() {
-       
     }
 
     @Override
     protected void addWidgets() {
         addFirstChildLink("Project", PROJECT_NODE);
         addFirstChildLink("Client Location", CLIENTLOCATION_NODE);
+        addFirstChildLink("Contacts", CONTACTS_NODE);
     }
 
     @Override
     public void treeNodeSelected(String entityNodeKey) {
-          if (PROJECT_NODE.equals(entityNodeKey)) {
+        if (PROJECT_NODE.equals(entityNodeKey)) {
             TabPanel.instance().timeandExpensePanel.entityPanel.clear();
             TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllProjectsPanel(entityId));
             TabPanel.instance().timeandExpensePanel.entityPanel.add(new ProjectOptionsPanel());
         }
-          if(CLIENTLOCATION_NODE.equals(entityNodeKey))
-          {
+        if (CLIENTLOCATION_NODE.equals(entityNodeKey)) {
             TabPanel.instance().timeandExpensePanel.entityPanel.clear();
             TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllClientLocationsPanel(entityId));
             TabPanel.instance().timeandExpensePanel.entityPanel.add(new ClientLocationOptionsPanel());
-          }
+        }
+        if (CONTACTS_NODE.equals(entityNodeKey)) {
+            TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+            TabPanel.instance().timeandExpensePanel.entityPanel.add(new ClientContactOptionsPanel());
+        }
     }
 
     @Override
     public void loadEntity() {
-        
     }
 
     @Override
     public void showEntity() {
-        
     }
-    
 }
