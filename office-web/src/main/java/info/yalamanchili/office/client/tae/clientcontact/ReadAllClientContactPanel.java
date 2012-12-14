93e1cecbab0e17fa182a8d66d3fb255118638c46
raghu l <raghu.l@xyz.com>
2012-12-14 14:36:16 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2057 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/clientcontact/ReadAllClientContactPanel.java
@@ -0,0 +1,123 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.clientcontact;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.utils.JSONUtils;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.ImageField;
+import info.yalamanchili.office.client.gwt.ReadAllComposite;
+import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class ReadAllClientContactPanel extends ReadAllComposite{
+
+      private static Logger logger = Logger.getLogger(ReadAllClientContactPanel.class.getName());
+    public static ReadAllClientContactPanel instance;
+
+    public ReadAllClientContactPanel(String parentId) {
+        instance = this;
+        this.parentId = parentId;
+        initTable("ClientLocation", OfficeWelcome.constants);
+    }
+    
+    @Override
+    protected void addListeners() {
+        
+    }
+
+    @Override
+    protected void configure() {
+        
+    }
+
+    @Override
+    protected void addWidgets() {
+       
+    }
+
+    @Override
+    public void preFetchTable(int start) {
+         HttpService.HttpServiceAsync.instance().doGet(getClientContactURL(start, OfficeWelcome.constants.tableSize()),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+
+            @Override
+            public void onResponse(String result) {
+                logger.info(result);
+                postFetchTable(result);
+            }
+        });
+    }
+    
+      public String getClientContactURL(Integer start, String limit) {
+        if (parentId != null) {
+            return OfficeWelcome.constants.root_url() + "client/clientcontact/" + parentId + "/" + start.toString() + "/" + limit.toString();
+        } else {
+            return OfficeWelcome.constants.root_url() + "clientcontact/" + start.toString() + "/" + limit.toString();
+        }
+    }
+
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("First Name"));
+        table.setText(0, 2, getKeyValue("Last Name"));
+        table.setText(0, 4, getKeyValue("Email"));
+        table.setText(0, 5, getKeyValue("Phone"));
+        table.setText(0, 6, getKeyValue("Sex"));
+        
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+         for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "firstName"));
+            table.setText(i, 2, JSONUtils.toString(entity, "lastName"));
+            table.setText(i, 3, JSONUtils.toString(entity, "email"));
+            table.setText(i, 4, JSONUtils.toString(entity, "phoneNumber"));
+            table.setText(i, 5, JSONUtils.toString(entity, "sex"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+        } else {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ, row, JSONUtils.toString(entity, "id"));
+        }
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+        
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+        
+    }
+    
+}
