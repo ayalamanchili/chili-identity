63e559af8a2852a551ebd4e6acbff92ff2595ff5
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-12-14 12:48:06 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2051 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/clientcontact/CreateClientContactPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/clientcontact/CreateClientContactPanel.java
@@ -5,63 +5,105 @@
 package info.yalamanchili.office.client.tae.clientcontact;
 
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+import info.yalamanchili.office.client.tae.client.TreeClientPanel;
+import java.util.logging.Logger;
 
 /**
  *
  * @author raghu
  */
-public class CreateClientContactPanel extends CreateComposite{
+public class CreateClientContactPanel extends CreateComposite {
 
-   public CreateClientContactPanel(CreateCompositeType type) {
+    private static Logger logger = Logger.getLogger(CreateClientContactPanel.class.getName());
+
+    public CreateClientContactPanel(CreateCompositeType type) {
         super(type);
-        initCreateComposite("Client Contact", OfficeWelcome.constants);
+        initCreateComposite("ClientContact", OfficeWelcome.constants);
     }
-    
+
     @Override
     protected JSONObject populateEntityFromFields() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        JSONObject entity = new JSONObject();
+        assignEntityValueFromField("firstName", entity);
+        assignEntityValueFromField("middleInitial", entity);
+        assignEntityValueFromField("lastName", entity);
+        assignEntityValueFromField("sex", entity);
+        assignEntityValueFromField("relation", entity);
+        assignEntityValueFromField("ecPrimary", entity);
+        assignEntityValueFromField("email", entity);
+        assignEntityValueFromField("countryCode", entity);
+        assignEntityValueFromField("phoneNumber", entity);
+        assignEntityValueFromField("extension", entity);
+        logger.info(entity.toString());
+        return entity;
     }
 
     @Override
     protected void createButtonClicked() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     protected void addButtonClicked() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        handleErrorResponse(arg0);
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postCreateSuccess(arg0);
+                    }
+                });
     }
 
     @Override
     protected void postCreateSuccess(String result) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        new ResponseStatusWidget().show("successfully added Client Location");
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+//        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllClientContactsPanel(TreeClientPanel.instance().getEntityId()));
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ClientContactOptionsPanel());
+
     }
 
     @Override
     protected void addListeners() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     protected void configure() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     protected void addWidgets() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        addField("firstName", false, true, DataType.STRING_FIELD);
+        addField("middleInitial", false, false, DataType.STRING_FIELD);
+        addField("lastName", false, true, DataType.STRING_FIELD);
+        String[] strs = {"MALE", "FEMALE"};
+        addEnumField("sex", false, false, strs);
+        addField("relation", false, true, DataType.STRING_FIELD);
+        addField("ecPrimary", false, false, DataType.BOOLEAN_FIELD);
+        addField("email", false, false, DataType.STRING_FIELD);
+        addField("countryCode", false, true, DataType.STRING_FIELD);
+        addField("phoneNumber", false, true, DataType.LONG_FIELD);
+        addField("extension", false, true, DataType.STRING_FIELD);
     }
 
     @Override
     protected void addWidgetsBeforeCaptionPanel() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     protected String getURI() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return OfficeWelcome.constants.root_url() + "client/clientcontact/" + TreeClientPanel.instance().getEntityId();
     }
-    
 }
