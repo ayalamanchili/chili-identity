6aa3d285d66193ccacc5d8415ce914a71e914032
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-12-17 15:00:08 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2084 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/clientlocation/UpdateClientLocationPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/clientlocation/UpdateClientLocationPanel.java
@@ -11,6 +11,8 @@ import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.gwt.UpdateComposite;
+import info.yalamanchili.office.client.profile.address.CreateAddressPanel;
+
 import info.yalamanchili.office.client.rpc.HttpService;
 
 /**
@@ -25,7 +27,13 @@ public class UpdateClientLocationPanel extends UpdateComposite {
 
     @Override
     protected JSONObject populateEntityFromFields() {
-       
+        assignEntityValueFromField("street1", entity);
+        assignEntityValueFromField("street2", entity);
+        assignEntityValueFromField("city", entity);
+        assignEntityValueFromField("state", entity);
+        assignEntityValueFromField("country", entity);
+        assignEntityValueFromField("zip", entity);
+
         return entity;
     }
 
@@ -47,7 +55,14 @@ public class UpdateClientLocationPanel extends UpdateComposite {
 
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
-       
+        // TODO Auto-generated method stub
+        assignFieldValueFromEntity("street1", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("street2", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("city", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("state", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("country", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("zip", entity, DataType.LONG_FIELD);
+
     }
 
     @Override
@@ -61,22 +76,25 @@ public class UpdateClientLocationPanel extends UpdateComposite {
 
     @Override
     protected void addListeners() {
-       
     }
 
     @Override
     protected void configure() {
-       
     }
 
     @Override
     protected void addWidgets() {
-       
+        addField("street1", false, true, DataType.STRING_FIELD);
+        addField("street2", false, false, DataType.STRING_FIELD);
+        addField("city", false, true, DataType.STRING_FIELD);
+        addField("state", false, true, DataType.ENUM_FIELD);
+        addEnumField("state", false, true, CreateAddressPanel.getStates().toArray(new String[0]));
+        addEnumField("country", false, true, CreateAddressPanel.getCountries().toArray(new String[0]));
+
     }
 
     @Override
     protected void addWidgetsBeforeCaptionPanel() {
-        
     }
 
     @Override
