80530472c2ab5d3581358a09cbf4e5e67f2a354f
Phani Yalamanchili <phani.y@sstech.us>
2012-12-19 13:24:17 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2114 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/dto/time/TimeSummary.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dto.time;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *
+ * @author anuyalamanchili
+ */
+@XmlRootElement
+@XmlType
+public class TimeSummary {
+
+    protected BigDecimal quickBooksHours;
+    protected BigDecimal adpHours;
+    protected BigDecimal balanceHours;
+
+    public BigDecimal getQuickBooksHours() {
+        return quickBooksHours;
+    }
+
+    public void setQuickBooksHours(BigDecimal quickBooksHours) {
+        this.quickBooksHours = quickBooksHours;
+    }
+
+    public BigDecimal getAdpHours() {
+        return adpHours;
+    }
+
+    public void setAdpHours(BigDecimal adpHours) {
+        this.adpHours = adpHours;
+    }
+
+    public BigDecimal getBalanceHours() {
+        return balanceHours;
+    }
+
+    public void setBalanceHours(BigDecimal balanceHours) {
+        this.balanceHours = balanceHours;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeSummary{" + "quickBooksHours=" + quickBooksHours + ", adpHours=" + adpHours + ", balanceHours=" + balanceHours + '}';
+    }
+}
