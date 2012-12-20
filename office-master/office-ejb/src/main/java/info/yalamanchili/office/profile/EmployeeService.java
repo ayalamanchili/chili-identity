2bdf401b9284b9f3eaa3a61effcedc961b3c0787
raghu l <raghu.l@xyz.com>
2012-12-20 11:41:43 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2120 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeService.java
@@ -48,6 +48,17 @@ public class EmployeeService {
         em.merge(user1);
 
     }
+    
+     public String generatepassword()
+    {
+    final int PASSWORD_LENGTH = 6;  
+    StringBuilder sb = new StringBuilder();  
+    for (int x = 0; x < PASSWORD_LENGTH; x++)  
+    {  
+      sb.append((char)((int)(Math.random()*26)+97));  
+    }  
+     return sb.toString();
+    }
 
     private Employee getEmployee(Long empId) {
         Employee employee = em.find(Employee.class, empId);
