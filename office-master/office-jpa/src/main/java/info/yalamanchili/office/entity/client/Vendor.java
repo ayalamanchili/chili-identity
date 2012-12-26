c5f50e953720b4d837895218eae2164d0e3646ba
raghu l <raghu.l@xyz.com>
2012-12-26 14:35:55 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2139 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/client/Vendor.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.client;
+
+import info.chili.jpa.AbstractEntity;
+import info.chili.jpa.validation.Unique;
+import info.yalamanchili.office.entity.profile.Address;
+import info.yalamanchili.office.entity.profile.Contact;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.hibernate.envers.Audited;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author raghu
+ */
+@Entity
+@Audited
+@XmlRootElement
+@XmlType
+@Table(uniqueConstraints =
+@UniqueConstraint(columnNames = {"name"}))
+@Unique(entity = Vendor.class, fields = {"name"}, message = "{vendor.name.not.unique.msg}")
+public class Vendor extends AbstractEntity {
+    
+    @NotEmpty(message = "{vendor.not.empty.msg}")
+    protected String name;
+
+    protected String description;
+    
+    @ManyToMany(cascade = CascadeType.ALL)
+    protected List<Address> locations;
+    
+    @ManyToMany(cascade = CascadeType.ALL)
+    protected List<Contact> contacts;
+    
+     public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLocations(List<Address> locations) {
+        this.locations = locations;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Address> getLocations() {
+        return locations;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+    
+    @Override
+    public String toString() {
+        return "Vendor{" + "name=" + name + ", description=" + description + '}';
+    }
+}
