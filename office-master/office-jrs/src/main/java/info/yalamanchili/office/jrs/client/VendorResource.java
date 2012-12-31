2c12abbe8dfc51c1e99dda313c13f1adcefaef1a
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2012-12-31 12:40:23 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2172 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/client/VendorResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/client/VendorResource.java
@@ -6,12 +6,20 @@ package info.yalamanchili.office.jrs.client;
 
 import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.client.VendorDao;
+import info.yalamanchili.office.dto.profile.ContactDto;
+import info.yalamanchili.office.dto.profile.ContactDto.ContactDtoTable;
 import info.yalamanchili.office.entity.client.Vendor;
+import info.yalamanchili.office.entity.profile.Address;
+import info.yalamanchili.office.entity.profile.Contact;
 import info.yalamanchili.office.jrs.CRUDResource;
+import info.yalamanchili.office.jrs.profile.AddressResource.AddressTable;
+import info.yalamanchili.office.mapper.profile.ContactMapper;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.xml.bind.annotation.XmlElement;
@@ -19,6 +27,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 /**
@@ -39,13 +48,89 @@ public class VendorResource extends CRUDResource<Vendor> {
     public CRUDDao getDao() {
         return vendorDao;
     }
-    
+
     @GET
     @Path("/{start}/{limit}")
     public VendorTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
         VendorTable tableObj = new VendorTable();
         tableObj.setEntities(getDao().query(start, limit));
         tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    /**
+     * Add Vendor Contact
+     *
+     * @param vendorId
+     * @param dto
+     *
+     */
+    @PUT
+    @Path("/vendorcontact/{clientId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    public void addvendorContact(@PathParam("vId") Long vendorId, ContactDto dto) {
+        Vendor vendor = (Vendor) getDao().findById(vendorId);
+        vendor.addContact(ContactMapper.map(dto, null));
+    }
+
+    /**
+     * Get Vendor Contact
+     *
+     * @param id
+     * @param start
+     * @param limit
+     * @return
+     *
+     */
+    @GET
+    @Path("/vendorcontact/{id}/{start}/{limit}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    public ContactDtoTable getVendorContacts(@PathParam("id") long id, @PathParam("start") int start,
+            @PathParam("limit") int limit) {
+        ContactDtoTable tableObj = new ContactDtoTable();
+        Vendor vendors = (Vendor) getDao().findById(id);
+        List<ContactDto> dtos = new ArrayList<ContactDto>();
+        for (Contact entity : vendors.getContacts()) {
+            dtos.add(ContactMapper.map(entity));
+        }
+        tableObj.setEntities(dtos);
+        tableObj.setSize((long) vendors.getContacts().size());
+        return tableObj;
+    }
+
+    /**
+     * Add Vendor locations
+     *
+     * @param vendorId
+     * @param address
+     *
+     */
+    @PUT
+    @Path("/vendorlocation/{clientId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    public void addvendorlocation(@PathParam("vendorId") Long vendorId, Address address) {
+        Vendor vend = (Vendor) getDao().findById(vendorId);
+        vend.addLocations(address);
+    }
+
+    /**
+     * Get Vendor Locations
+     *
+     * @param id
+     * @param start
+     * @param limit
+     * @return
+     *
+     */
+    @GET
+    @Path("/vendorlocation/{id}/{start}/{limit}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    public AddressTable getClientLocations(@PathParam("id") long id, @PathParam("start") int start,
+            @PathParam("limit") int limit) {
+        AddressTable tableObj = new AddressTable();
+        Vendor evendor = (Vendor) getDao().findById(id);
+        tableObj.setEntities(evendor.getLocations());
+        tableObj.setSize((long) evendor.getLocations().size());
         return tableObj;
     }
 
