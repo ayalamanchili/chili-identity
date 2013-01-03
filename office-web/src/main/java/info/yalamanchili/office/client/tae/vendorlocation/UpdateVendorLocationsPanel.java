ff48d0a5ca8d785a3e9252a36b643e8cde91a237
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-03 11:59:31 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2200 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/vendorlocation/UpdateVendorLocationsPanel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.vendorlocation;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class UpdateVendorLocationsPanel {
+    
+}
