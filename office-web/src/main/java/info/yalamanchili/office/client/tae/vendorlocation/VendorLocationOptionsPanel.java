0ac74d8ee4a74c151a79ec48a670620b94646dc8
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-03 10:07:21 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2198 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/vendorlocation/VendorLocationOptionsPanel.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.vendorlocation;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TabPanel;
+import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class VendorLocationOptionsPanel extends ALComposite implements ClickHandler {
+
+    protected HorizontalPanel panel = new HorizontalPanel();
+    protected ClickableLink addvendorLocLink = new ClickableLink("Add Vendor Location");
+
+    public VendorLocationOptionsPanel() {
+        init(panel);
+    }
+
+    @Override
+    protected void addListeners() {
+        addvendorLocLink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+        panel.setSpacing(5);
+    }
+
+    @Override
+    protected void addWidgets() {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            panel.add(addvendorLocLink);
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(addvendorLocLink)) {
+//            TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+//            TabPanel.instance().timeandExpensePanel.entityPanel.add(new CreateClientLocationPanel(CreateComposite.CreateCompositeType.ADD));
+        }
+    }
+}
