c07331edf618d8a89d141908cb86125f376204a7
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-03 12:17:17 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2204 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/vendorlocation/UpdateVendorLocationsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/vendorlocation/UpdateVendorLocationsPanel.java
@@ -4,10 +4,105 @@
  */
 package info.yalamanchili.office.client.tae.vendorlocation;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.UpdateComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+import info.yalamanchili.office.client.tae.vendor.TreeVendorsPanel;
+
 /**
  *
  * @author Prashanthi
  */
-public class UpdateVendorLocationsPanel {
-    
+public class UpdateVendorLocationsPanel extends UpdateComposite {
+
+    public UpdateVendorLocationsPanel(JSONObject entity) {
+        initUpdateComposite(entity, "VendorContact", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        assignEntityValueFromField("firstName", entity);
+        assignEntityValueFromField("middleInitial", entity);
+        assignEntityValueFromField("lastName", entity);
+        assignEntityValueFromField("sex", entity);
+        assignEntityValueFromField("email", entity);
+        assignEntityValueFromField("countryCode", entity);
+        assignEntityValueFromField("phoneNumber", entity);
+        assignEntityValueFromField("extension", entity);
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("firstName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("middleInitial", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("lastName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("sex", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("email", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("countryCode", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("phoneNumber", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("extension", entity, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        new ResponseStatusWidget().show("Successfully Updated Vendor location");
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllVendorLocationPanel(TreeVendorsPanel.instance().getEntityId()));
+    }
+
+    @Override
+    protected void addListeners() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void configure() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("firstName", false, true, DataType.STRING_FIELD);
+        addField("middleInitial", false, false, DataType.STRING_FIELD);
+        addField("lastName", false, true, DataType.STRING_FIELD);
+        String[] strs = {"MALE", "FEMALE"};
+        addEnumField("sex", false, false, strs);
+        addField("email", false, false, DataType.STRING_FIELD);
+        addField("countryCode", false, true, DataType.STRING_FIELD);
+        addField("phoneNumber", false, true, DataType.STRING_FIELD);
+        addField("extension", false, true, DataType.STRING_FIELD);
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "location";
+    }
 }
