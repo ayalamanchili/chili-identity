0f13e9f5fb49934bb08e7eccf5e62333509d1f64
Phani Yalamanchili <phani.y@sstech.us>
2013-01-06 06:43:21 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2230 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/timesheet/EmpTimeSummaryPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/timesheet/EmpTimeSummaryPanel.java
@@ -4,8 +4,18 @@
  */
 package info.yalamanchili.office.client.tae.timesheet;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.user.client.ui.CaptionPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.utils.JSONUtils;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,11 +23,47 @@ import info.chili.gwt.composite.ALComposite;
  */
 public class EmpTimeSummaryPanel extends ALComposite {
 
+    private static Logger logger = Logger.getLogger(EmpTimeSummaryPanel.class.getName());
     protected FlowPanel panel = new FlowPanel();
     protected ReadAllTimesheetPanel readAllTimeSheetsPanel = new ReadAllTimesheetPanel();
+    //Time Summary Panel
+    protected CaptionPanel summaryCP = new CaptionPanel("Time Summary");
+    protected FlowPanel summaryPanel = new FlowPanel();
+    protected Label adpHoursL = new Label("ADP Hours");
+    protected TextBox adpHoursTB = new TextBox();
+    protected Label qbHoursL = new Label("QuickBooks Hours");
+    protected TextBox qbHoursTB = new TextBox();
 
     public EmpTimeSummaryPanel() {
         init(panel);
+        getTimeSummary();
+    }
+
+    protected void configureSummaryPanel() {
+        summaryPanel.add(adpHoursL);
+        summaryPanel.add(adpHoursTB);
+        summaryPanel.add(qbHoursL);
+        summaryPanel.add(qbHoursTB);
+        summaryCP.setContentWidget(summaryPanel);
+    }
+
+    protected void getTimeSummary() {
+        HttpService.HttpServiceAsync.instance().doGet(getTimeSummaryURL(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String result) {
+                        logger.info(result);
+                        if (result != null && !result.isEmpty()) {
+                            JSONObject summary = JSONParser.parseLenient(result).isObject();
+                            adpHoursTB.setText(JSONUtils.toString(summary, "adpHours"));
+                            qbHoursTB.setText(JSONUtils.toString(summary, "quickBooksHours"));
+                        }
+                    }
+                });
+    }
+
+    public String getTimeSummaryURL() {
+        return OfficeWelcome.constants.root_url() + "timesheet/summary/currentuser";
     }
 
     @Override
@@ -30,6 +76,8 @@ public class EmpTimeSummaryPanel extends ALComposite {
 
     @Override
     protected void addWidgets() {
+        panel.add(summaryCP);
         panel.add(readAllTimeSheetsPanel);
+        configureSummaryPanel();
     }
 }
