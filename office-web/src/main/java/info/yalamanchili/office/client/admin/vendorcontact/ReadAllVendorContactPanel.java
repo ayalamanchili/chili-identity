b0cc3a2311c139210683851a181b240e55ebe1ae
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-07 09:22:57 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2242 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/vendorcontact/ReadAllVendorContactPanel.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.vendorcontact;
+
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.ReadAllComposite;
+import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class ReadAllVendorContactPanel extends ReadAllComposite {
+
+    private static Logger logger = Logger.getLogger(ReadAllVendorContactPanel.class.getName());
+    public static ReadAllVendorContactPanel instance;
+
+    public ReadAllVendorContactPanel(String parentId) {
+        instance = this;
+        this.parentId = parentId;
+        initTable("VendorContact", OfficeWelcome.constants);
+    }
+
+    private ReadAllVendorContactPanel() {
+        instance = this;
+        initTable("Vendors", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void preFetchTable(int start) {
+        HttpService.HttpServiceAsync.instance().doGet(getVendorContactURL(start, OfficeWelcome.constants.tableSize()),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String result) {
+                logger.info(result);
+                postFetchTable(result);
+            }
+        });
+    }
+
+    private String getVendorContactURL(Integer start, String limit) {
+        if (parentId != null) {
+            return OfficeWelcome.constants.root_url() + "vendor/vendorcontact/" + parentId + "/" + start.toString() + "/" + limit.toString();
+        } else {
+            return OfficeWelcome.constants.root_url() + "vendorcontact/" + start.toString() + "/" + limit.toString();
+        }
+    }
+
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("First Name"));
+        table.setText(0, 2, getKeyValue("Middle Initial"));
+        table.setText(0, 3, getKeyValue("Last Name"));
+        table.setText(0, 4, getKeyValue("Email"));
+        table.setText(0, 5, getKeyValue("Phone"));
+        table.setText(0, 6, getKeyValue("Sex"));
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "firstName"));
+            table.setText(i, 2, JSONUtils.toString(entity, "middleInitial"));
+            table.setText(i, 3, JSONUtils.toString(entity, "lastName"));
+            table.setText(i, 4, JSONUtils.toString(entity, "email"));
+            table.setText(i, 5, JSONUtils.toString(entity, "phoneNumber"));
+            table.setText(i, 6, JSONUtils.toString(entity, "sex"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+        } else {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ, row, JSONUtils.toString(entity, "id"));
+        }
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
+    }
+
+    private String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "employee/delete/" + entityId;
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully deleted vendor contact information");
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllVendorContactPanel());
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new UpdateVendorContactPanel(getEntity(entityId)));
+    }
+}
