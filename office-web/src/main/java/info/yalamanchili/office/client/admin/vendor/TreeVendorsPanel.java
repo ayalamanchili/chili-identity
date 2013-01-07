11b483b557b11df550462143dee8494e61aa8dca
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-07 09:22:40 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2241 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/vendor/TreeVendorsPanel.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.vendor;
+
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.TreePanelComposite;
+import info.yalamanchili.office.client.admin.vendorcontact.ReadAllVendorContactPanel;
+import info.yalamanchili.office.client.admin.vendorcontact.VendorContactOptionsPanel;
+import info.yalamanchili.office.client.admin.vendorlocation.ReadAllVendorLocationPanel;
+import info.yalamanchili.office.client.admin.vendorlocation.VendorLocationOptionsPanel;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class TreeVendorsPanel extends TreePanelComposite {
+
+    private static TreeVendorsPanel instance;
+
+    public static TreeVendorsPanel instance() {
+        return instance;
+    }
+    private static Logger logger = Logger.getLogger(TreeVendorsPanel.class.getName());
+    protected static final String VENDORLOCATION_NODE = "vendorlocation";
+    protected static final String VENDORCONTACTS_NODE = "vendorcontacts";
+
+    public TreeVendorsPanel(String entityId) {
+        super(entityId);
+        instance = this;
+        init("Vendors", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addFirstChildLink("Locations", VENDORLOCATION_NODE);
+        addFirstChildLink("Contacts", VENDORCONTACTS_NODE);
+    }
+
+    @Override
+    public void treeNodeSelected(String entityNodeKey) {
+        if (VENDORLOCATION_NODE.equals(entityNodeKey)) {
+            TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+            TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllVendorLocationPanel(entityId));
+            TabPanel.instance().timeandExpensePanel.entityPanel.add(new VendorLocationOptionsPanel());
+        }
+        if (VENDORCONTACTS_NODE.equals(entityNodeKey)) {
+            TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+            TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllVendorContactPanel(entityId));
+            TabPanel.instance().timeandExpensePanel.entityPanel.add(new VendorContactOptionsPanel());
+        }
+    }
+
+    @Override
+    public void loadEntity() {
+    }
+
+    @Override
+    public void showEntity() {
+    }
+}
