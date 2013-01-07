8dcf054da0260eb2942e4e84bd400f6dc363e210
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-07 09:23:15 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2243 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/vendorlocation/CreateVendorLocationsPanel.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.vendorlocation;
+
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.data.CountryFactory;
+import info.chili.gwt.data.USAStatesFactory;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.profile.address.CreateAddressPanel;
+import info.yalamanchili.office.client.admin.vendor.TreeVendorsPanel;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class CreateVendorLocationsPanel extends CreateAddressPanel {
+
+    private static Logger logger = Logger.getLogger(CreateVendorLocationsPanel.class.getName());
+
+    public CreateVendorLocationsPanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+//         initCreateComposite("VendorLocation", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        //TODO is thid needed
+        JSONObject entity = new JSONObject();
+        assignEntityValueFromField("street1", entity);
+        assignEntityValueFromField("street2", entity);
+        assignEntityValueFromField("city", entity);
+        assignEntityValueFromField("state", entity);
+        assignEntityValueFromField("country", entity);
+        assignEntityValueFromField("zip", entity);
+        logger.info(entity.toString());
+        return entity;
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+        new ResponseStatusWidget().show("successfully added Vendor Locatrion");
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllVendorLocationPanel(TreeVendorsPanel.instance().getEntityId()));
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new VendorLocationOptionsPanel());
+
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("street1", false, true, DataType.STRING_FIELD);
+        addField("street2", false, false, DataType.STRING_FIELD);
+        addField("city", false, true, DataType.STRING_FIELD);
+        addField("state", false, true, DataType.ENUM_FIELD);
+        addEnumField("state", false, true, USAStatesFactory.getStates().toArray(new String[0]));
+        addEnumField("country", false, true, CountryFactory.getCountries().toArray(new String[0]));
+        addField("zip", false, false, DataType.LONG_FIELD);
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "vendor/vendorlocation/" + TreeVendorsPanel.instance().getEntityId();
+    }
+}
---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/vendorlocation/SelectVendorLocationsWidget.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.vendorlocation;
+
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.GenericListener;
+import info.yalamanchili.office.client.gwt.SelectComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+import info.yalamanchili.office.client.admin.client.SelectClientWidget;
+import info.yalamanchili.office.client.admin.vendor.SelectVendorWidget;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class SelectVendorLocationsWidget extends SelectComposite implements GenericListener {
+
+    public SelectVendorLocationsWidget(Boolean readOnly, Boolean isRequired) {
+        super(OfficeWelcome.constants, "VendorLocation", readOnly, isRequired);
+        SelectVendorWidget.instance().addListner(this);
+    }
+
+    @Override
+    protected void fetchDropDownData() {
+    }
+
+    @Override
+    protected String getDropDownURL(Integer start, Integer limit, String... columns) {
+        return super.generateDropdownUrl(OfficeWelcome.constants.root_url() + "vendor/locations/dropdown/" + SelectVendorWidget.instance().getSelectedObjectId(), start, limit, columns);
+    }
+
+    @Override
+    protected void validate() {
+        clearMessage();
+    }
+
+    @Override
+    public void onChange() {
+        if (SelectVendorWidget.instance().getSelectedObjectId() == null || SelectVendorWidget.instance().getSelectedObjectId().isEmpty()) {
+            processData(null);
+            return;
+        }
+        HttpService.HttpServiceAsync.instance().doGet(getDropDownURL(0, 10, "id", "firstName", "lastName"),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String entityString) {
+                processData(entityString);
+            }
+        });
+    }
+}
