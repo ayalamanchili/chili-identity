62b125916869e4805742c88612476656432b0943
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-07 09:21:29 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2238 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/clientlocation/CreateClientLocationPanel.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.clientlocation;
+
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.data.CountryFactory;
+import info.chili.gwt.data.USAStatesFactory;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.profile.address.CreateAddressPanel;
+import info.yalamanchili.office.client.admin.client.TreeClientPanel;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class CreateClientLocationPanel extends CreateAddressPanel {
+
+    private static Logger logger = Logger.getLogger(CreateClientLocationPanel.class.getName());
+
+    public CreateClientLocationPanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+//         initCreateComposite("ClientLocation", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        //TODO is thid needed
+        JSONObject entity = new JSONObject();
+        assignEntityValueFromField("street1", entity);
+        assignEntityValueFromField("street2", entity);
+        assignEntityValueFromField("city", entity);
+        assignEntityValueFromField("state", entity);
+        assignEntityValueFromField("country", entity);
+        assignEntityValueFromField("zip", entity);
+//        assignEntityValueFromField("addressType", entity);
+        logger.info(entity.toString());
+        return entity;
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+        new ResponseStatusWidget().show("successfully added Client Locatrion");
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllClientLocationsPanel(TreeClientPanel.instance().getEntityId()));
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ClientLocationOptionsPanel());
+
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("street1", false, true, DataType.STRING_FIELD);
+        addField("street2", false, false, DataType.STRING_FIELD);
+        addField("city", false, true, DataType.STRING_FIELD);
+        addField("state", false, true, DataType.ENUM_FIELD);
+        addEnumField("state", false, true, USAStatesFactory.getStates().toArray(new String[0]));
+        addEnumField("country", false, true, CountryFactory.getCountries().toArray(new String[0]));
+        addField("zip", false, false, DataType.LONG_FIELD);
+//        addDropDown("addressType", new SelectAddressTypeWidget(false, false));
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "client/clientlocation/" + TreeClientPanel.instance().getEntityId();
+    }
+}
---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/clientlocation/UpdateClientLocationPanel.java
@@ -0,0 +1,107 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.clientlocation;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.data.CountryFactory;
+import info.chili.gwt.data.USAStatesFactory;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.UpdateComposite;
+import info.yalamanchili.office.client.profile.address.CreateAddressPanel;
+
+import info.yalamanchili.office.client.rpc.HttpService;
+import info.yalamanchili.office.client.admin.client.TreeClientPanel;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class UpdateClientLocationPanel extends UpdateComposite {
+
+    public UpdateClientLocationPanel(JSONObject entity) {
+        initUpdateComposite(entity, "ClientLocation", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        assignEntityValueFromField("street1", entity);
+        assignEntityValueFromField("street2", entity);
+        assignEntityValueFromField("city", entity);
+        assignEntityValueFromField("state", entity);
+        assignEntityValueFromField("country", entity);
+        assignEntityValueFromField("zip", entity);
+
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        // TODO Auto-generated method stub
+        assignFieldValueFromEntity("street1", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("street2", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("city", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("state", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("country", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("zip", entity, DataType.LONG_FIELD);
+
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        new ResponseStatusWidget().show("successfully updated clientlocation");
+//        TabPanel.instance().timeandExpensePanel.sidePanelTop.clear();
+//        TabPanel.instance().timeandExpensePanel.sidePanelTop.add(new ClientLocationOptionsPanel());
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllClientLocationsPanel(TreeClientPanel.instance().getEntityId()));
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("street1", false, true, DataType.STRING_FIELD);
+        addField("street2", false, false, DataType.STRING_FIELD);
+        addField("city", false, true, DataType.STRING_FIELD);
+        addField("state", false, true, DataType.ENUM_FIELD);
+        addEnumField("state", false, true, USAStatesFactory.getStates().toArray(new String[0]));
+        addEnumField("country", false, true, CountryFactory.getCountries().toArray(new String[0]));
+        addField("zip", false, false, DataType.LONG_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "address";
+    }
+}
