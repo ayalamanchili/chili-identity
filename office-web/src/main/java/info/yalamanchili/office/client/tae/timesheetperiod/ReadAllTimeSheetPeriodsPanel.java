35a4f0ee9eb20226db21d491f0c29b83c932d125
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-08 09:33:34 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2308 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/timesheetperiod/ReadAllTimeSheetPeriodsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/timesheetperiod/ReadAllTimeSheetPeriodsPanel.java
@@ -66,8 +66,8 @@ public class ReadAllTimeSheetPeriodsPanel extends ReadAllComposite {
             JSONObject entity = (JSONObject) entities.get(i - 1);
             addOptionsWidget(i, entity);
             table.setText(i, 1, JSONUtils.toString(entity, "name"));
-            table.setText(i, 2, DateUtils.getFormatedDate(JSONUtils.toString(entity, "startDate"), DateTimeFormat.PredefinedFormat.DATE_LONG));
-            table.setText(i, 3, DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_LONG));
+            table.setText(i, 2, JSONUtils.toString(entity, "startDate"));
+            table.setText(i, 3, JSONUtils.toString(entity, "endDate"));
         }
     }
 
