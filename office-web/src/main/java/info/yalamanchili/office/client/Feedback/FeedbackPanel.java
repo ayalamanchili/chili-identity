587234e55289d0785fba87a93e404849b2423b45
raghu l <raghu.l@xyz.com>
2013-01-09 14:09:04 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2333 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/Feedback/FeedbackPanel.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.Feedback;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class FeedbackPanel extends CreateComposite {
+
+    private static Logger logger = Logger.getLogger(FeedbackPanel.class.getName());
+
+    public FeedbackPanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+        initCreateComposite("Feedback", OfficeWelcome.constants);
+    }
+    
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject feedbck = new JSONObject();
+        assignEntityValueFromField("feedbackmsg", feedbck);
+        logger.info(feedbck.toString());
+        return feedbck;
+    }
+
+    @Override
+    protected void createButtonClicked() {
+         HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        logger.info(arg0.getMessage());
+                        handleErrorResponse(arg0);
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postCreateSuccess(arg0);
+                    }
+                });
+    }
+
+    @Override
+    protected void addButtonClicked() {
+       
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+       new ResponseStatusWidget().show("Feedback has been sent"); 
+    }
+
+    @Override
+    protected void addListeners() {
+        
+    }
+
+    @Override
+    protected void configure() {
+        
+    }
+
+    @Override
+    protected void addWidgets() {
+         addField("feedbackmsg", false, true, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+        
+    }
+
+    @Override
+    protected String getURI() {
+       return OfficeWelcome.constants.root_url() + "feedback";
+    }
+    
+}
