c6c5180b22998622bc85aec529f5de851653738d
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-10 09:52:57 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2336 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/message/Message.java
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/message/Message.java
@@ -14,6 +14,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlElement;
 import org.hibernate.annotations.ForeignKey;
 import org.hibernate.envers.Audited;
@@ -39,6 +40,7 @@ public class Message extends AbstractEntity {
     @Temporal(javax.persistence.TemporalType.TIMESTAMP)
     protected Date messageTs;
     @ManyToMany
+//    @NotNull(message = "{tos.not.empty.msg}")
     protected List<Employee> tos;
     @OneToMany
     protected List<Message> replies;
