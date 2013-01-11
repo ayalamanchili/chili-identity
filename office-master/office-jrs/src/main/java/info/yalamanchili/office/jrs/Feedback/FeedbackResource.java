c3b366fd0f1fdfe0b2b98e968893e1e55923ccb3
raghu l <raghu.l@xyz.com>
2013-01-11 14:45:42 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2355 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/Feedback/FeedbackResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/Feedback/FeedbackResource.java
@@ -8,6 +8,8 @@ import info.yalamanchili.office.dao.CRUDDao;
 import info.yalamanchili.office.dao.feedback.FeedbackDao;
 import info.yalamanchili.office.entity.Feedback.Feedback;
 import info.yalamanchili.office.jrs.CRUDResource;
+import info.yalamanchili.office.profile.notification.ProfileNotificationService;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -28,6 +30,13 @@ public class FeedbackResource extends CRUDResource<Feedback> {
     @Override
     public CRUDDao getDao() {
         return feedbackDao;
+    }
+    
+    @PUT
+    public Feedback save(Feedback entity) {
+       Feedback fb=  (Feedback) getDao().save(entity);
+       ProfileNotificationService.instance().feedBackNotification(entity);
+       return fb;
     }
     
 }
