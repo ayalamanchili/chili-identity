c4fc991e3a5be534da6a9fdbf408cd59209ee094
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-29 15:12:59 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2486 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/bonuspayment/BonusPaymentsSidePanel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.bonuspayment;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class BonusPaymentsSidePanel extends ALComposite implements ClickHandler {
+
+    private static Logger logger = Logger.getLogger(BonusPaymentsSidePanel.class.getName());
+    public FlowPanel bonuspaymentsidepanel = new FlowPanel();
+    ClickableLink createbonuspaymentslink = new ClickableLink("Create Bonus Payments");
+
+    public BonusPaymentsSidePanel() {
+        init(bonuspaymentsidepanel);
+    }
+
+    @Override
+    protected void addListeners() {
+        createbonuspaymentslink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            bonuspaymentsidepanel.add(createbonuspaymentslink);
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(createbonuspaymentslink)) {
+            TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+//            TabPanel.instance().timeandExpensePanel.entityPanel.add(new CreateTimeSheetPeriodPanel(CreateComposite.CreateCompositeType.CREATE));
+        }
+    }
+}
