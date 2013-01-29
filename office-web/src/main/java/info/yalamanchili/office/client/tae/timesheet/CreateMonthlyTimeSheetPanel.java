216010d513995eef6cbfc1877db0c678ce65d43c
Phani Yalamanchili <phani.y@sstech.us>
2013-01-29 03:16:30 +0000

    added logic to prepopulate start date and end date.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2472 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/timesheet/CreateMonthlyTimeSheetPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/timesheet/CreateMonthlyTimeSheetPanel.java
@@ -5,11 +5,18 @@
 package info.yalamanchili.office.client.tae.timesheet;
 
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.date.DateUtils;
 import info.chili.gwt.fields.DataType;
+import info.chili.gwt.fields.DateField;
+import info.chili.gwt.utils.JSONUtils;
 import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.gwt.CreateComposite;
 import info.yalamanchili.office.client.profile.employee.SelectEmployeeWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
 import info.yalamanchili.office.client.tae.timesheetperiod.SelectTimesheetPeriodWidget;
 import java.util.logging.Logger;
 
@@ -57,5 +64,24 @@ public class CreateMonthlyTimeSheetPanel extends CreateTimeSheetPanel {
         addField("startDate", false, false, DataType.DATE_FIELD);
         addField("endDate", false, false, DataType.DATE_FIELD);
         addField("notes", false, false, DataType.TEXT_AREA_FIELD);
+        //prepopulate start and end dates
+        HttpService.HttpServiceAsync.instance().doGet(getTimeSheetPeriodUrl(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response != null && !response.isEmpty()) {
+                            JSONObject timePeriod = (JSONObject) JSONParser.parseLenient(response);
+                            DateField startDateF = (DateField) fields.get("startDate");
+                            startDateF.setDate(DateUtils.toDate(JSONUtils.toString(timePeriod, "startDate")));
+                            DateField endDateF = (DateField) fields.get("endDate");
+                            endDateF.setDate(DateUtils.toDate(JSONUtils.toString(timePeriod, "endDate")));
+                        }
+                    }
+                });
+    }
+
+    protected String getTimeSheetPeriodUrl() {
+        JSONObject timePeriod = SelectTimesheetPeriodWidget.instance().getSelectedObject();
+        return OfficeWelcome.constants.root_url() + "timesheetperiod/" + JSONUtils.toString(timePeriod, "id");
     }
 }
