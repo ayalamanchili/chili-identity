4f5cc635785f54d30a03d5e538d890f6282a04a1
raghu l <raghu.l@xyz.com>
2013-01-29 10:41:52 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2476 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/time/BonusPaymentDao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.time;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.time.BonusPayment;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author raghu
+ */
+@Component
+@Scope("request")
+public class BonusPaymentDao extends CRUDDao<BonusPayment> {
+
+    @PersistenceContext
+    protected EntityManager em;
+    
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+    
+       public BonusPaymentDao() {
+        super(BonusPayment.class);
+    }
+
+}
