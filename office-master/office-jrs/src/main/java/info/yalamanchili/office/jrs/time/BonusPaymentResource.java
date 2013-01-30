d3dee63503be235ff8e931e3def71975942fb094
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-30 15:12:41 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2499 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/time/BonusPaymentResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/time/BonusPaymentResource.java
@@ -9,10 +9,17 @@ import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.time.BonusPaymentDao;
 import info.yalamanchili.office.entity.time.BonusPayment;
 import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 /**
@@ -38,5 +45,40 @@ public class BonusPaymentResource extends CRUDResource<BonusPayment> {
         info.yalamanchili.office.entity.profile.Employee emp = EmployeeDao.instance().findById(entity.getEmployee().getId());
         entity.setEmployee(emp);
         return (BonusPayment) getDao().save(entity);
+    }
+
+    @GET
+    @Path("/{start}/{limit}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    public BonusPaymentResource.BopnusPaymentTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        BonusPaymentResource.BopnusPaymentTable tableObj = new BonusPaymentResource.BopnusPaymentTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class BopnusPaymentTable {
+
+        protected Long size;
+        protected List<BonusPayment> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<BonusPayment> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<BonusPayment> entities) {
+            this.entities = entities;
+        }
     }
 }
