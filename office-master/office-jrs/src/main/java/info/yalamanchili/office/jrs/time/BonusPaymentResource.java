9c00a1fae18e42d59876b6c27eb3d1a2f54c523b
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-30 14:32:22 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2496 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/time/BonusPaymentResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/time/BonusPaymentResource.java
@@ -5,9 +5,11 @@
 package info.yalamanchili.office.jrs.time;
 
 import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.time.BonusPaymentDao;
 import info.yalamanchili.office.entity.time.BonusPayment;
 import info.yalamanchili.office.jrs.CRUDResource;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -22,12 +24,19 @@ import org.springframework.stereotype.Component;
 @Scope("request")
 public class BonusPaymentResource extends CRUDResource<BonusPayment> {
 
-     @Autowired
+    @Autowired
     public BonusPaymentDao bonusPaymentDao;
-     
+
     @Override
     public CRUDDao getDao() {
         return bonusPaymentDao;
     }
-    
+
+    @PUT
+    @Override
+    public BonusPayment save(BonusPayment entity) {
+        info.yalamanchili.office.entity.profile.Employee emp = EmployeeDao.instance().findById(entity.getEmployee().getId());
+        entity.setEmployee(emp);
+        return (BonusPayment) getDao().save(entity);
+    }
 }
