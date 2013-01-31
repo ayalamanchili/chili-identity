2883d960b65870fa9ea2ea83d53f506c0e4df302
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-01-31 14:17:41 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2544 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/bonuspayment/UpdateBonusPaymentPanel.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.bonuspayment;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.UpdateComposite;
+import info.yalamanchili.office.client.profile.employee.SelectEmployeeWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class UpdateBonusPaymentPanel extends UpdateComposite {
+
+    private static Logger logger = Logger.getLogger(UpdateBonusPaymentPanel.class.getName());
+    protected String bonuspaymentsId;
+
+    public UpdateBonusPaymentPanel(String bonuspaymentId, JSONObject entity) {
+        this.bonuspaymentsId = bonuspaymentId;
+        initUpdateComposite(entity, "BonusPayments", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        assignEntityValueFromField("employee", entity);
+        assignEntityValueFromField("paidRate", entity);
+        assignEntityValueFromField("paidHours", entity);
+        assignEntityValueFromField("paidDate", entity);
+        assignEntityValueFromField("paymentInfo", entity);
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("employee", entity, null);
+        assignFieldValueFromEntity("paidRate", entity, DataType.CURRENCY_FIELD);
+        assignFieldValueFromEntity("paidHours", entity, DataType.FLOAT_FIELD);
+        assignFieldValueFromEntity("paidDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("paymentInfo", entity, DataType.RICH_TEXT_AREA);
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        new ResponseStatusWidget().show("Successfully Updated Bonus Payments Information");
+        TabPanel.instance().timeandExpensePanel.entityPanel.clear();
+        TabPanel.instance().timeandExpensePanel.entityPanel.add(new ReadAllBonusPaymentsPanel(bonuspaymentsId));
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addDropDown("employee", new SelectEmployeeWidget(false, true));
+        addField("paidRate", false, true, DataType.CURRENCY_FIELD);
+        addField("paidHours", false, true, DataType.FLOAT_FIELD);
+        addField("paidDate", false, true, DataType.DATE_FIELD);
+        addField("paymentInfo", false, false, DataType.RICH_TEXT_AREA);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "bonuspayment";
+    }
+}
