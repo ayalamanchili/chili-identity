44881889e4874828d6f240e0f30308d448b0279a
Phani Yalamanchili <phani.y@sstech.us>
2013-02-04 16:09:14 +0000

    added duplicate timesheet validations
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2565 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/time/TimeSheetDao.java
@@ -69,55 +69,32 @@ public class TimeSheetDao extends CRUDDao<TimeSheet> {
         entity.setEmployee(emp);
         return getEntityManager().merge(entity);
     }
-    
-    protected void validatetimesheet(TimeSheet newTS)
-    {
-        //TypedQuery<Long> sizeQuery = getEntityManager().createQuery("select count(*) from " + entityCls.getCanonicalName() + " where employee.id=:currentEmpid and timeSheetPeriod.id=:payperiodid", Long.class);
-        //TypedQuery<TimeSheet> sizeQuery = getEntityManager().createQuery("select oldts from timesheet oldts  where oldts.employee.id=:currentEmpid and oldts.timeSheetPeriod.id=:payperiodid", TimeSheet.class);
-       // Query sizeQuery = getEntityManager().createQuery("select startDate,endDate from " + entityCls.getCanonicalName() + " where employee.id=:currentEmpid and timeSheetPeriod.id=:payperiodid");
-        
-      //  sizeQuery.setParameter("currentEmpid", ts.getEmployee().getId());
-      //  sizeQuery.setParameter("payperiodid",ts.getTimeSheetPeriod().getId());
-        
-        //List lstresults = sizeQuery.getResultList();
-        
-       //if (lstresults.size() > 0) 
-     //  {
-       //  if ((ts.getStartDate() == null) && (ts.getEndDate() == null))
-       //  {
-       //     throw new RuntimeException("Enter Timesheet Start date and End date");
-       //  }
-       //  else if(lstresults.get(0)!= null)
-       //  {
-         //   Date dtstart = (Date)lstresults.get(0)   ;
-         //   if(dtstart.compareTo(ts.getEndDate()) < 0)
-         //   {
-         //   throw new RuntimeException("New Timesheet Start Date should be greater than old Timesheet end date");
-        //    }
-            
-        // }
-         
-      // }
-        
-         //validate if new timesheet belongs to existing period
-        //TODO
+
+    protected void validatetimesheet(TimeSheet newTS) {
         //validate new timesheet start date less than end date
         if (!newTS.getStartDate().before(newTS.getEndDate())) {
             throw new RuntimeException("start date must be less than end date");
         }
-        
-        Calendar c1 = Calendar.getInstance();
-        Calendar c2 = Calendar.getInstance();
-       // Calendar c3 = Calendar.getInstance();
-        
-        c1.setTime(newTS.getStartDate());
-        c2.setTime(newTS.getEndDate());
-        //c3.setTime(newTS.getTimeSheetPeriod().getStartDate());
-        
-        //TODO try to process all the errors collect them and throw a service exception at the end
-        if ((c1.get(Calendar.MONTH) == c2.get(Calendar.MONTH)))
-        {
-        //validate if there is a existing timesheet and compare the start date of newTS is greater than end date of existingTS
+        //validate if new timesheet belongs to existing period
+        TimeSheetPeriod payPeriod = TimeSheetPeriodDao.instance().findById(newTS.getTimeSheetPeriod().getId());
+
+        Calendar timesheetPeriodCalender = Calendar.getInstance();
+        Calendar newTSStartDateCalnder = Calendar.getInstance();
+        Calendar newTSEndDateCalender = Calendar.getInstance();
+
+        timesheetPeriodCalender.setTime(payPeriod.getStartDate());
+        newTSStartDateCalnder.setTime(newTS.getStartDate());
+        newTSEndDateCalender.setTime(newTS.getEndDate());
+
+        if (newTSStartDateCalnder.get(Calendar.MONTH) != timesheetPeriodCalender.get(Calendar.MONTH)) {
+            throw new RuntimeException("start date does not belog to currnt payperiod");
+        }
+
+        if (newTSEndDateCalender.get(Calendar.MONTH) != timesheetPeriodCalender.get(Calendar.MONTH)) {
+            throw new RuntimeException("end date does not belog to currnt payperiod");
+        }
+        //Validate is new and existing date do not overlap
+
         TypedQuery<TimeSheet> sizeQuery = getEntityManager().createQuery("from " + entityCls.getCanonicalName() + " where employee.id=:currentEmpid and timeSheetPeriod.id=:payperiodid", TimeSheet.class);
         sizeQuery.setParameter("currentEmpid", newTS.getEmployee().getId());
         sizeQuery.setParameter("payperiodid", newTS.getTimeSheetPeriod().getId());
@@ -127,11 +104,6 @@ public class TimeSheetDao extends CRUDDao<TimeSheet> {
             if (newTS.getStartDate().before(existingTS.getEndDate())) {
                 throw new RuntimeException("New Timesheet Start date should be after Old Timesheet Enddate");
             }
-        }
-        }
-        else
-        {
-          throw new RuntimeException("Start date, End date,Timesheet Period should be in same month");
         }
     }
 
