4927a9844682b1990b8e73031094b6bb7a75793f
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-04 12:47:29 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2561 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/timesheet/ReadTimeSheetPanel.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.timesheet;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.ReadComposite;
+import info.yalamanchili.office.client.profile.employee.SelectEmployeeWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import info.yalamanchili.office.client.tae.timesheetperiod.SelectTimesheetPeriodWidget;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class ReadTimeSheetPanel extends ReadComposite {
+
+    private static ReadTimeSheetPanel instance;
+
+    public static ReadTimeSheetPanel instance() {
+        return instance;
+    }
+
+    public ReadTimeSheetPanel(JSONObject entity) {
+        instance = this;
+        initReadComposite(entity, "TimeSheet", OfficeWelcome.constants);
+    }
+
+    public ReadTimeSheetPanel(String id) {
+        initReadComposite(id, "TimeSheet", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        entity = (JSONObject) JSONParser.parseLenient(response);
+                        populateFieldsFromEntity(entity);
+                    }
+                });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("adpRate", entity, DataType.CURRENCY_FIELD);
+        assignFieldValueFromEntity("quickBooksRate", entity, DataType.CURRENCY_FIELD);
+        assignFieldValueFromEntity("adpHours", entity, DataType.FLOAT_FIELD);
+        assignFieldValueFromEntity("quickBooksHours", entity, DataType.FLOAT_FIELD);
+        assignFieldValueFromEntity("employee", entity, null);
+        assignFieldValueFromEntity("timeSheetPeriod", entity, null);
+        assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("endDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("notes", entity, DataType.TEXT_AREA_FIELD);
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addDropDown("employee", new SelectEmployeeWidget(false, true));
+        addDropDown("timeSheetPeriod", new SelectTimesheetPeriodWidget(false, true));
+        addField("adpHours", false, true, DataType.FLOAT_FIELD);
+        addField("quickBooksHours", false, true, DataType.FLOAT_FIELD);
+        addField("adpRate", false, false, DataType.CURRENCY_FIELD);
+        addField("quickBooksRate", false, false, DataType.CURRENCY_FIELD);
+        addField("startDate", false, false, DataType.DATE_FIELD);
+        addField("endDate", false, false, DataType.DATE_FIELD);
+        addField("notes", false, false, DataType.TEXT_AREA_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "timesheet/" + entityId;
+    }
+}
