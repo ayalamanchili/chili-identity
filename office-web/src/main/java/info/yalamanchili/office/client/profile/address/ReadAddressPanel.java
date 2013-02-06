c2ae6022ea5e1911658ef2d14fac722f0739fffe
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-06 14:28:26 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2579 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/address/ReadAddressPanel.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.address;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.data.CountryFactory;
+import info.chili.gwt.data.USAStatesFactory;
+import info.chili.gwt.fields.DataType;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.ReadComposite;
+import info.yalamanchili.office.client.profile.addresstype.SelectAddressTypeWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class ReadAddressPanel extends ReadComposite {
+
+    private static ReadAddressPanel instance;
+    private static Logger logger = Logger.getLogger(ReadAddressPanel.class.getName());
+
+    public static ReadAddressPanel instance() {
+        return instance;
+    }
+
+    public ReadAddressPanel(JSONObject entity) {
+        instance = this;
+        initReadComposite(entity, "Address", OfficeWelcome.constants);
+    }
+
+    public ReadAddressPanel(String id) {
+        initReadComposite(id, "Address", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        logger.info("read ec6 response" + response);
+                        entity = (JSONObject) JSONParser.parseLenient(response);
+                        populateFieldsFromEntity(entity);
+                    }
+                });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("street1", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("street2", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("city", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("state", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("country", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("zip", entity, DataType.LONG_FIELD);
+        assignFieldValueFromEntity("addressType", entity, null);
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("street1", false, false, DataType.STRING_FIELD);
+        addField("street2", false, false, DataType.STRING_FIELD);
+        addField("city", false, false, DataType.STRING_FIELD);
+        addEnumField("state", false, false, USAStatesFactory.getStates().toArray(new String[0]));
+        addEnumField("country", false, false, CountryFactory.getCountries().toArray(new String[0]));
+        addField("zip", false, false, DataType.LONG_FIELD);
+        addDropDown("addressType", new SelectAddressTypeWidget(false, false));
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "employee/addresses/" + entityId;
+    }
+}
