c2fd5a12ce12691ed3fcfd9c1f5d5c5950ff3cd1
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-07 14:31:19 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2594 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/admin/notificationgroup/ReadNotificationGroupPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/notificationgroup/ReadNotificationGroupPanel.java
@@ -4,16 +4,10 @@
  */
 package info.yalamanchili.office.client.admin.notificationgroup;
 
-import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.json.client.JSONString;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import info.chili.gwt.fields.DataType;
-import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
-import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.gwt.ReadComposite;
-import info.yalamanchili.office.client.rpc.HttpService;
 import java.util.logging.Logger;
 
 /**
