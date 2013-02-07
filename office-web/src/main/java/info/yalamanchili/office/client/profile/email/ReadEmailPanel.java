695c8b1b61c6c9f1eacfab5b0e3e0484266860aa
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-07 15:07:30 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2596 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/email/ReadEmailPanel.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.email;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.ReadComposite;
+import info.yalamanchili.office.client.profile.emailtype.SelectEmailTypeWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class ReadEmailPanel extends ReadComposite {
+
+    private static ReadEmailPanel instance;
+    private static Logger logger = Logger.getLogger(ReadEmailPanel.class.getName());
+
+    public static ReadEmailPanel instance() {
+        return instance;
+    }
+
+    public ReadEmailPanel(JSONObject entity) {
+        instance = this;
+        initReadComposite(entity, "Email", OfficeWelcome.constants);
+    }
+
+    public ReadEmailPanel(String id) {
+        initReadComposite(id, "Email", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        logger.info("read ec6 response" + response);
+                        entity = (JSONObject) JSONParser.parseLenient(response);
+                        populateFieldsFromEntity(entity);
+                    }
+                });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("email", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("primaryEmail", entity, DataType.BOOLEAN_FIELD);
+        assignFieldValueFromEntity("emailType", entity, null);
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("email", true, false, DataType.STRING_FIELD);
+        addField("primaryEmail", true, false, DataType.BOOLEAN_FIELD);
+        addDropDown("emailType", new SelectEmailTypeWidget(true, false));
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "email";
+    }
+}
