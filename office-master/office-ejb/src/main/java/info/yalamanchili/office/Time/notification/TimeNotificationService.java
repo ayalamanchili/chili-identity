852863e9584cf8fd5e14b81721094d7ae29e8d1a
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-12 11:11:39 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2623 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/Time/notification/TimeNotificationService.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.Time.notification;
+
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.dto.profile.Employee;
+import info.yalamanchili.office.email.Email;
+import info.yalamanchili.office.email.MailUtils;
+import info.yalamanchili.office.jms.MessagingService;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Prashanthi
+ */
+@Component
+@Scope("prototype")
+public class TimeNotificationService {
+    
+    @Autowired
+    protected MailUtils mailUtils;
+    @Autowired
+    protected MessagingService messagingService;
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    public EntityManager em;
+    
+    public static TimeNotificationService instance() {
+        return SpringContext.getBean(TimeNotificationService.class);
+    }
+}
