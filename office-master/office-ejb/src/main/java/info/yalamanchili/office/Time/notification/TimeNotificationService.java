01512cec7c577308ba2475ee31c5f8988d439fb4
Phani Yalamanchili <phani.y@sstech.us>
2013-02-12 13:29:16 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2629 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/Time/notification/TimeNotificationService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/Time/notification/TimeNotificationService.java
@@ -10,9 +10,7 @@ import info.yalamanchili.office.email.Email;
 import info.yalamanchili.office.email.MailUtils;
 import info.yalamanchili.office.jms.MessagingService;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
@@ -36,14 +34,28 @@ public class TimeNotificationService {
     @PersistenceContext(type = PersistenceContextType.EXTENDED)
     public EntityManager em;
 
-    public void sendAdjustmenthoursNotification(Employee emp) {
-        Email email = new Email();
-        Map<String, String> tos = new HashMap<String, String>();
-//        tos.add(info.yalamanchili.office.config.OfficeServiceConfiguration.instance().getAdminEmail());
-//        email.setTos(tos);
-        email.setSubject("Employee adjustment hours from ");
-        email.setBody(emp.getUsername());
-        messagingService.sendEmail(email);
+    @Async
+    public void sendOvertimeRequestSubmitedNotification(Employee emp) {
+        //send email to roles ROLE_TIME,ROLE_EXPENSE,ROLE_ADMIN
+        //subject "Overtime Pay request submited for [add employee name]"
+        //body "overtime Pay request submited for empleyee name  please go to my tasks to complete the task to approve or deny.
+    }
+
+    @Async
+    public void sendOvertimeRequestApprovedNotification(Employee emp, String reason) {
+        //send email to roles ROLE_TIME,ROLE_EXPENSE,ROLE_ADMIN
+        //subject "Overtime Pay request was approved]"
+        //new line
+        //reason is: reason
+    }
+
+    @Async
+    public void sendOvertimeRequestDeniedNotification(Employee emp, String reason) {
+        //send email to just employee email
+        //subject "Your Overtime Pay request was denied"
+        //body "overtime Pay request submited was denied
+        //ne line
+        //reason is: reason
     }
 
     public static TimeNotificationService instance() {
