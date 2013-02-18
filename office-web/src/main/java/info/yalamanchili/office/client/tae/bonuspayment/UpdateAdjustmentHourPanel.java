ca0621787948bd4d4153bc7a1aa29424d84b86c9
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-18 13:33:13 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2651 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/bonuspayment/UpdateAdjustmentHourPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/bonuspayment/UpdateAdjustmentHourPanel.java
@@ -82,9 +82,9 @@ public class UpdateAdjustmentHourPanel extends UpdateComposite {
     @Override
     protected void addWidgets() {
         addDropDown("employee", new SelectEmployeeWidget(false, true));
-        addField("paidRate", false, true, DataType.CURRENCY_FIELD);
+        addField("paidRate", false, false, DataType.CURRENCY_FIELD);
         addField("paidHours", false, true, DataType.FLOAT_FIELD);
-        addField("paidDate", false, true, DataType.DATE_FIELD);
+        addField("paidDate", false, false, DataType.DATE_FIELD);
         addField("paymentInfo", false, false, DataType.RICH_TEXT_AREA);
     }
 
