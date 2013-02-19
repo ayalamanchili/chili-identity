b24a6f505dd054928c215e51bd185d4d9b02dd25
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-19 09:48:41 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2659 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/adp/ADPMonthyReportImporter.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/adp/ADPMonthyReportImporter.java
@@ -4,6 +4,17 @@
  */
 package info.yalamanchili.office.adp;
 
+import info.yalamanchili.office.qb.QuickBooksRecord;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+
 /**
  * This class is responsible for reading the export file and converting it
  *
@@ -13,6 +24,31 @@ public class ADPMonthyReportImporter {
 
     String path = "";
 
-    public static void main(String... str) {
+    public static void main(String... str) throws Exception {
+        importEmployeeHoursData();
+    }
+
+    protected static List<AdpHoursRecords> importEmployeeHoursData() throws Exception {
+        List<AdpHoursRecords> records = new ArrayList<AdpHoursRecords>();
+        InputStream inp = new FileInputStream(getFilePath());
+        HSSFWorkbook workbook = new HSSFWorkbook(inp);
+        HSSFSheet sheet = workbook.getSheetAt(0);
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row record = rowIterator.next();
+            if (record.getCell(1) != null && record.getCell(1).toString().contains("Total")) {
+                AdpHoursRecords qbRecord = new AdpHoursRecords();
+                String empid = "";
+                empid = record.getCell(1).getStringCellValue().substring(0, 1) + record.getCell(2).getStringCellValue();
+                qbRecord.setEmployeeId(empid);
+                System.out.println("name----" + empid);
+                records.add(qbRecord);
+            }
+        }
+        return records;
+    }
+
+    protected static String getFilePath() {
+        return "c://Jan2013_Hrs_Ran.xls";
     }
 }
