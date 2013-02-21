3a22e4b418cbedf585514add78fd07bf88fb3047
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-02-21 13:26:14 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2661 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/adp/ADPMonthyReportImporter.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/adp/ADPMonthyReportImporter.java
@@ -13,6 +13,7 @@ import java.util.Iterator;
 import java.util.List;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 /**
@@ -21,33 +22,36 @@ import org.apache.poi.ss.usermodel.Row;
  * @author ayalamanchili
  */
 public class ADPMonthyReportImporter {
-
+    
     String path = "";
-
+    
     public static void main(String... str) throws Exception {
         importEmployeeHoursData();
     }
-
+    
     protected static List<AdpHoursRecords> importEmployeeHoursData() throws Exception {
         List<AdpHoursRecords> records = new ArrayList<AdpHoursRecords>();
         InputStream inp = new FileInputStream(getFilePath());
         HSSFWorkbook workbook = new HSSFWorkbook(inp);
         HSSFSheet sheet = workbook.getSheetAt(0);
-        Iterator<Row> rowIterator = sheet.iterator();
-        while (rowIterator.hasNext()) {
-            Row record = rowIterator.next();
-            if (record.getCell(1) != null && record.getCell(1).toString().contains("Total")) {
-                AdpHoursRecords qbRecord = new AdpHoursRecords();
-                String empid = "";
-                empid = record.getCell(1).getStringCellValue().substring(0, 1) + record.getCell(2).getStringCellValue();
-                qbRecord.setEmployeeId(empid);
-                System.out.println("name----" + empid);
-                records.add(qbRecord);
+        for (Row record : sheet) {
+            for (Cell cell : record) {
+                // Do something here
+                if (cell != null) {
+                    AdpHoursRecords qbRecord = new AdpHoursRecords();
+                    String empid = "";
+                    empid = record.getCell(1).getStringCellValue().substring(0, 1) + record.getCell(2).getStringCellValue();
+                    qbRecord.setEmployeeId(empid);
+                    System.out.println("name----" + empid);
+                    qbRecord.setHours(new BigDecimal(record.getCell(4).getNumericCellValue()));
+                    System.out.println("hours----" + record.getCell(4).getNumericCellValue());
+                    records.add(qbRecord);
+                }
             }
         }
         return records;
     }
-
+    
     protected static String getFilePath() {
         return "c://Jan2013_Hrs_Ran.xls";
     }
