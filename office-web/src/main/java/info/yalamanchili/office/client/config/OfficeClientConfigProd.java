46bf072873e41f885689284d5de31d75541f438f
Phani Yalamanchili <phani.y@sstech.us>
2013-03-01 13:00:34 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2700 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/config/OfficeClientConfigProd.java
+++ office-web/src/main/java/info/yalamanchili/office/client/config/OfficeClientConfigProd.java
@@ -13,10 +13,10 @@ import info.yalamanchili.office.client.config.OfficeClientConfig;
 public class OfficeClientConfigProd implements OfficeClientConfig {
 
     public String getFileUploadUrl() {
-        return "https://apps.sstech.us/office-web/office/rpc/fileService";
+        return "http://yalamanchili.info:8080/office-web/office/rpc/fileService";
     }
 
     public String getFileDownloadUrl() {
-        return "https://apps.sstech.us/office-web/office/rpc/fileService?path=";
+        return "http://yalamanchili.info:8080/office-web/office/rpc/fileService?path=";
     }
 }
