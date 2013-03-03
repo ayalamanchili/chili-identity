f9bfb82402af90061bb8e1315d8874d2b1f12fe4
Phani Yalamanchili <phani.y@sstech.us>
2013-03-03 06:01:15 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2705 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/OfficeServiceJobConfiguration.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs;
+
+import info.yalamanchili.office.Time.TimeJobService;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+@Component
+public class OfficeServiceJobConfiguration {
+
+    @ManagedOperation
+    public void syncTimeSheetPeriods() {
+        TimeJobService.instance().syncTimeSheetPeriods();
+    }
+}
