bbd254c999689567abd48ce9aaaec4e59b0269d3
Phani Yalamanchili <phani.y@sstech.us>
2013-03-20 12:12:22 +0000

    check task status before send escalation email.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2746 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/EmailEscalation.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/EmailEscalation.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.activiti.engine.task.DelegationState;
 import org.activiti.engine.task.IdentityLink;
 import org.activiti.engine.task.TaskQuery;
 
@@ -31,24 +32,26 @@ public class EmailEscalation implements JavaDelegate {
         //
         TaskQuery query = bpmTaskService.createTaskQuery().processInstanceId(execution.getProcessInstanceId());
         for (org.activiti.engine.task.Task task : query.list()) {
-            List<String> roles = new ArrayList<String>();
-            List<IdentityLink> identityLinks = bpmTaskService.getIdentityLinksForTask(task.getId());
-            Email email = new Email();
-            for (IdentityLink identityLink : identityLinks) {
-                if (identityLink.getGroupId() != null && !identityLink.getGroupId().isEmpty()) {
-                    roles.add(identityLink.getGroupId());
+            if (!task.getDelegationState().equals(DelegationState.RESOLVED)) {
+                List<String> roles = new ArrayList<String>();
+                List<IdentityLink> identityLinks = bpmTaskService.getIdentityLinksForTask(task.getId());
+                Email email = new Email();
+                for (IdentityLink identityLink : identityLinks) {
+                    if (identityLink.getGroupId() != null && !identityLink.getGroupId().isEmpty()) {
+                        roles.add(identityLink.getGroupId());
+                    }
+                    if (identityLink.getUserId() != null && !identityLink.getUserId().isEmpty()) {
+                        email.addTo(identityLink.getUserId());
+                    }
                 }
-                if (identityLink.getUserId() != null && !identityLink.getUserId().isEmpty()) {
-                    email.addTo(identityLink.getUserId());
+                if (roles.size() > 0) {
+                    email.setTos(mailUtils.getEmailsAddressesForRoles(roles));
                 }
+                email.setSubject("Task Escalation Email");
+                String messageText = "Task is Escalated. Please complete.\n Details: \n Name" + task.getName() + " \n Description:" + task.getDescription();
+                email.setBody(messageText);
+                messagingService.sendEmail(email);
             }
-            if (roles.size() > 0) {
-                email.setTos(mailUtils.getEmailsAddressesForRoles(roles));
-            }
-            email.setSubject("Task Escalation Email");
-            String messageText = "Task is Escalated. Please complete.\n Details: \n Name" + task.getName() + " \n Description:" + task.getDescription();
-            email.setBody(messageText);
-            messagingService.sendEmail(email);
         }
     }
 }
