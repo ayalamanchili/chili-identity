42f9a4ce182935dcb77cbf4b09df7ee4706da488
Phani Yalamanchili <phani.y@sstech.us>
2013-03-25 05:42:29 +0000

    add new client request. -2
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2769 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/email/GenericTaskApprovalEmail.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/email/GenericTaskApprovalEmail.java
@@ -4,6 +4,14 @@
  */
 package info.yalamanchili.office.bpm.email;
 
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.email.Email;
+import info.yalamanchili.office.email.MailUtils;
+import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.jms.MessagingService;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 
@@ -16,6 +24,16 @@ public class GenericTaskApprovalEmail implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        System.out.println("approval");
+        MessagingService messagingService = (MessagingService) SpringContext.getBean("messagingService");
+        Employee emp = (Employee) execution.getVariable("currentEmployee");
+        Email email = new Email();
+        email.addTo(emp.getPrimaryEmail().getEmail());
+        Set<String> tos = new HashSet<String>();
+        tos.add(emp.getPrimaryEmail().getEmail());
+        email.setTos(tos);
+        //TODO add details
+        email.setSubject("Request Approved ");
+        email.setBody("Request Approved");
+        messagingService.sendEmail(email);
     }
 }
