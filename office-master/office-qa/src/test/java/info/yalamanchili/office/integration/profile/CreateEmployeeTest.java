713d2b8ec288647b80524a6658288079336e977b
Phani Yalamanchili <phani.y@sstech.us>
2013-04-01 13:47:01 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2788 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-qa/src/test/java/info/yalamanchili/office/integration/profile/CreateEmployeeTest.java
+++ office-master/office-qa/src/test/java/info/yalamanchili/office/integration/profile/CreateEmployeeTest.java
@@ -5,12 +5,14 @@
 package info.yalamanchili.office.integration.profile;
 
 import info.yalamanchili.office.integration.AbstractOfficeTest;
-import static info.yalamanchili.office.integration.profile.LoginTest.driver;
+import java.util.Date;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  *
@@ -18,20 +20,42 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class CreateEmployeeTest extends AbstractOfficeTest {
 
-    protected static FirefoxDriver driver;
+    protected static ChromeDriver driver;
 
     @BeforeClass
     public static void init() {
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
     }
 
     @Test
-    public void testCreateEmployee() {
+    public void testCreateEmployee() throws InterruptedException {
         assertTrue(login(driver, ADMIN, ADMIN));
+        //click link
+        WebElement createEmployeeL = driver.findElement(By.id("gwt-debug-Create EmployeeCL"));
+        createEmployeeL.click();
+        System.out.println("ddddd" + new Date());
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(3000);
+        System.out.println("fggfff" + new Date());
+//        WebDriverWait wait = new WebDriverWait(driver, 2);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gwt-debug-Employee_firstName_TB")));
+//        wait.until(ExpectedConditions.elementToBeClickable(By.id("gwt-debug-Employee_firstName_TB")));
+        ///populate data
+//        WebElement employeeTypeDD = driver.findElement(By.id("gwt-debug-EmployeeType_employeeType_LB"));
+
+        WebElement employeefirstNameF = driver.findElement(By.id("gwt-debug-Employee_firstName_TB"));
+        System.out.println("ddddd" + employeefirstNameF.toString());
+        System.out.println("ddddd" + employeefirstNameF.isDisplayed());
+        System.out.println("ddddd" + employeefirstNameF.isEnabled());
+        System.out.println("d"+driver.getPageSource());
+        employeefirstNameF.sendKeys("testemp");
+
+        WebElement createB = driver.findElement(By.linkText("create"));
+        createB.click();
     }
 
     @AfterClass
     public static void destroy() {
-        driver.kill();
+//        driver.kill();
     }
 }
