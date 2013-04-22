17a58c9dce2da9f220385316e8d15c14ba329abe
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-04-22 14:19:56 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2862 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/expense/ExpenseMenu.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.expense;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.MenuBar;
+import info.yalamanchili.office.client.TabPanel;
+
+/**
+ *
+ * @author Prashanthi
+ */
+//To do add expense menu to tab panel 
+// jira issue no ssto 954
+public class ExpenseMenu extends Composite {
+
+    MenuBar expenseMenuBar = new MenuBar(false);
+
+    public ExpenseMenu() {
+        initWidget(expenseMenuBar);
+        configureExpenseMenu();
+    }
+
+    protected void configureExpenseMenu() {
+        MenuBar menu = new MenuBar(true);
+        expenseMenuBar.addItem("Expense", ecpenseMaintainenceCmd);
+        expenseMenuBar.addItem("ExpenseCategories", ecpenseCategoriesMaintainenceCmd);
+
+        expenseMenuBar.addStyleName("entityMenuBar");
+    }
+    Command ecpenseMaintainenceCmd = new Command() {
+        public void execute() {
+            TabPanel.instance().getExpensePanel().entityPanel.clear();
+            TabPanel.instance().getExpensePanel().sidePanelTop.clear();
+//            TabPanel.instance().getExpensePanel().entityPanel.add(new ());
+//            TabPanel.instance().getExpensePanel().sidePanelTop.add(new ());
+        }
+    };
+    Command ecpenseCategoriesMaintainenceCmd = new Command() {
+        public void execute() {
+            TabPanel.instance().getExpensePanel().entityPanel.clear();
+            TabPanel.instance().getExpensePanel().sidePanelTop.clear();
+//            TabPanel.instance().getExpensePanel().entityPanel.add(new ());
+//            TabPanel.instance().getExpensePanel().sidePanelTop.add(new ());
+        }
+    };
+}
