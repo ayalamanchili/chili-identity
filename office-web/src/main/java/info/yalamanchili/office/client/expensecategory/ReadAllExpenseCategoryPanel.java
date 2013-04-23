43d43ed4a342131acd8d2418ca0f520a46763db1
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-04-23 14:59:57 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2879 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/expensecategory/ReadAllExpenseCategoryPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/expensecategory/ReadAllExpenseCategoryPanel.java
@@ -4,10 +4,108 @@
  */
 package info.yalamanchili.office.client.expensecategory;
 
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CRUDReadAllComposite;
+import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Prashanthi
  */
-public class ReadAllExpenseCategoryPanel {
-    
+public class ReadAllExpenseCategoryPanel extends CRUDReadAllComposite {
+
+    private static Logger logger = Logger.getLogger(ReadAllExpenseCategoryPanel.class.getName());
+    public static ReadAllExpenseCategoryPanel instance;
+
+    public ReadAllExpenseCategoryPanel() {
+        instance = this;
+        initTable("ExpenseCategory", OfficeWelcome.constants);
+    }
+
+    public ReadAllExpenseCategoryPanel(String parentId) {
+        instance = this;
+        this.parentId = parentId;
+        initTable("ExpenseCategory", OfficeWelcome.constants);
+    }
+
+    public ReadAllExpenseCategoryPanel(JSONArray array) {
+        instance = this;
+        initTable("ExpenseCategory", array, OfficeWelcome.constants);
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully Deleted ExpenseCategory Information");
+        TabPanel.instance().expensePanel.entityPanel.clear();
+        TabPanel.instance().expensePanel.entityPanel.add(new ReadAllExpenseCategoryPanel());
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+    }
+
+    @Override
+    public void preFetchTable(int start) {
+        HttpService.HttpServiceAsync.instance().doGet(getReadAllExpenseCategoryURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String result) {
+                        logger.info("rrr:" + result);
+                        postFetchTable(result);
+                    }
+                });
+    }
+
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("Name"));
+        table.setText(0, 2, getKeyValue("Description"));
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "name"));
+            table.setText(i, 2, JSONUtils.toString(entity, "description"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+    }
+
+    private String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "expensecategory/delete/" + entityId;
+    }
+
+    private String getReadAllExpenseCategoryURL(Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "expensecategory/" + start.toString() + "/" + limit.toString();
+    }
 }
