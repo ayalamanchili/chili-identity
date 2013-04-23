2e816b7cb653a399cfaa06135c9647120f95fa47
raghu l <raghu.l@xyz.com>
2013-04-23 12:12:03 +0000

    added expenses DAO - ssto(953)
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2870 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/expense/ExpenseDao.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.expense;
+
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.entity.expense.Expense;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author raghu
+ */
+@Component
+@Scope("prototype")
+public class ExpenseDao extends CRUDDao<Expense> {
+
+    @PersistenceContext
+    protected EntityManager em;
+    
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+   
+     public ExpenseDao() {
+        super(Expense.class);
+    }
+
+    public static ExpenseDao instance() {
+        return SpringContext.getBean(ExpenseDao.class);
+    }
+}
