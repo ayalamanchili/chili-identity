7348b8c8b93d6c4cfb13824497b4f43582b9ad21
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-04-24 12:21:44 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@2883 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/expensecategory/ExpenseCategorySidePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/expensecategory/ExpenseCategorySidePanel.java
@@ -4,10 +4,51 @@
  */
 package info.yalamanchili.office.client.expensecategory;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Prashanthi
  */
-public class ExpenseCategorySidePanel {
-    
+public class ExpenseCategorySidePanel extends ALComposite implements ClickHandler {
+
+    private static Logger logger = Logger.getLogger(ExpenseCategorySidePanel.class.getName());
+    public FlowPanel expenseCategorySidePanel = new FlowPanel();
+    ClickableLink createExpenseCategoryLink = new ClickableLink("Create ExpenseCategory");
+
+    public ExpenseCategorySidePanel() {
+        init(expenseCategorySidePanel);
+    }
+
+    @Override
+    protected void addListeners() {
+        createExpenseCategoryLink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            expenseCategorySidePanel.add(createExpenseCategoryLink);
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent clickEvent) {
+        if (clickEvent.getSource().equals(createExpenseCategoryLink)) {
+            TabPanel.instance().expensePanel.entityPanel.clear();
+            TabPanel.instance().expensePanel.entityPanel.add(new CreateExpenseCategoryPanel(CreateComposite.CreateCompositeType.CREATE));
+        }
+    }
 }
