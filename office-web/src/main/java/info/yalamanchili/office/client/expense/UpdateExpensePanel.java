111ba110827b0b591cf8d0dfc3bc39247a0ad528
raghu l <raghu.l@xyz.com>
2013-04-24 13:39:57 +0000

    Update Expense Panel ssto - 963 (949 sub task) .look at  employee and category updations
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2894 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/expense/UpdateExpensePanel.java
@@ -0,0 +1,107 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.expense;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.expensecategory.SelectExpenseCategoryWidget;
+import info.yalamanchili.office.client.gwt.UpdateComposite;
+import info.yalamanchili.office.client.profile.employee.SelectEmployeeWidget;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class UpdateExpensePanel extends UpdateComposite{
+
+     private static Logger logger = Logger.getLogger(UpdateExpensePanel.class.getName());
+
+    public UpdateExpensePanel(JSONObject entity) {
+        initUpdateComposite(entity, "Expenses", OfficeWelcome.constants);
+    }
+    
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        assignEntityValueFromField("name", entity);
+        assignEntityValueFromField("amount", entity);
+        assignEntityValueFromField("expenseDate", entity);
+        assignEntityValueFromField("description", entity);
+        assignEntityValueFromField("employee", entity);
+        assignEntityValueFromField("category", entity);
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+         HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+       assignFieldValueFromEntity("name", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("amount", entity, DataType.CURRENCY_FIELD);
+        assignFieldValueFromEntity("expenseDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("description", entity, DataType.TEXT_AREA_FIELD);
+        assignFieldValueFromEntity("employee", entity, null);
+        assignFieldValueFromEntity("category", entity, null);
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+       new ResponseStatusWidget().show("Successfully  Updated Email Information");
+        TabPanel.instance().getExpensePanel().entityPanel.clear();
+        TabPanel.instance().getExpensePanel().entityPanel.add(new ReadAllExpensesPanel());
+    }
+
+    @Override
+    protected void addListeners() {
+        
+    }
+
+    @Override
+    protected void configure() {
+        
+    }
+
+    @Override
+    protected void addWidgets() {
+        addDropDown("employee", new SelectEmployeeWidget(false, false));
+        addField("name", false, true, DataType.STRING_FIELD);
+        addField("amount", false, true, DataType.CURRENCY_FIELD);
+        addDropDown("category", new SelectExpenseCategoryWidget(false, false));
+        addField("expenseDate", false, true, DataType.DATE_FIELD);
+        addField("description", false, true, DataType.TEXT_AREA_FIELD);
+        
+        
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+        
+    }
+
+    @Override
+    protected String getURI() {
+         return OfficeWelcome.constants.root_url() + "expense";
+    }
+    
+}
