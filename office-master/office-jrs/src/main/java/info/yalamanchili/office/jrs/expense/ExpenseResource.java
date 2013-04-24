59b2d514e12e54308b8154e5c220082d1bae1268
raghu l <raghu.l@xyz.com>
2013-04-24 13:43:20 +0000

    added ExpenseResource ssto-953 (949 sub task)
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2896 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/expense/ExpenseResource.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.expense;
+
+import info.yalamanchili.office.dao.CRUDDao;
+import info.yalamanchili.office.dao.expense.ExpenseDao;
+import info.yalamanchili.office.jrs.CRUDResource;
+import info.yalamanchili.office.entity.expense.Expense;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author raghu
+ */
+@Path("secured/expense")
+@Component
+@Transactional
+@Scope("request")
+public class ExpenseResource extends CRUDResource<Expense> {
+
+    @Autowired
+    public ExpenseDao expenseDao;
+     
+    @Override
+    public CRUDDao getDao() {
+        return expenseDao;
+    }
+    
+    @GET
+    @Path("/{start}/{limit}")
+     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_HR')")
+    public ExpenseTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        ExpenseTable tableObj = new ExpenseTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+       
+    @XmlRootElement
+    @XmlType
+    public static class ExpenseTable {
+
+        protected Long size;
+        protected List<Expense> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<Expense> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<Expense> entities) {
+            this.entities = entities;
+        }
+    }
+}
