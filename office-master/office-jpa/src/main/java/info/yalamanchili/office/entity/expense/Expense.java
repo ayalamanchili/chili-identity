d915d4cc098df291d84a6d41399716218e1631c2
raghu l <raghu.l@xyz.com>
2013-04-24 14:38:05 +0000

    added xml element for employee in expense entity
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2898 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/expense/Expense.java
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/expense/Expense.java
@@ -71,7 +71,7 @@ public class Expense extends AbstractEntity{
     public Date getExpenseDate() {
         return expenseDate;
     }
-
+     @XmlElement
     public Employee getEmployee() {
         return employee;
     }
