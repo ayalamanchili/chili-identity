d2c9206b6b44baa6fe7776e76e48216383b06761
raghu l <raghu.l@xyz.com>
2013-04-30 10:16:09 +0000

    Coding for Searching Client added
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2924 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/client/SearchClientpanel.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.client;
+
+import com.google.gwt.http.client.URL;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.utils.JSONUtils;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.SearchComposite;
+import info.yalamanchili.office.client.rpc.HttpService;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author raghu
+ */
+public class SearchClientpanel extends SearchComposite{
+
+    private static Logger logger = Logger.getLogger(SearchClientpanel.class.getName());
+   
+     public SearchClientpanel() {
+        init("Client Search", "Client", OfficeWelcome.constants);
+    }
+     
+    @Override
+    protected void populateSearchSuggestBox() {
+        HttpService.HttpServiceAsync.instance().doGet(getnameDropDownUrl(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String entityString) {
+                Map<Integer, String> values = JSONUtils.convertKeyValuePairs(entityString);
+                loadSearchSuggestions(values.values());
+            }
+        });
+    }
+
+       protected String getnameDropDownUrl() {
+        //TODO think about the limit
+        return OfficeWelcome.constants.root_url() + "client/dropdown/0/500?column=id&column=name";
+    }
+       
+    @Override
+    protected void populateAdvancedSuggestBoxes() {
+        
+    }
+
+    @Override
+    protected void addListeners() {
+        
+    }
+
+    @Override
+    protected void configure() {
+        
+    }
+
+    @Override
+    protected void addWidgets() {
+          addField("name", DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject entity = new JSONObject();
+        assignEntityValueFromField("name", entity);
+        logger.info(entity.toString());
+        return entity;
+    }
+
+    @Override
+    protected void search(String searchText) {
+         if (getSearchText() != null) {
+            HttpService.HttpServiceAsync.instance().doGet(getSearchURI(getSearchText(), 0, 10),
+                    OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+                @Override
+                public void onResponse(String result) {
+                    processSearchResult(result);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void search(JSONObject entity) {
+          logger.info("ggggg" + entity.toString());
+        HttpService.HttpServiceAsync.instance().doPut(getSearchURI(0, 10), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String result) {
+                processSearchResult(result);
+            }
+        });
+    }
+
+    @Override
+    protected void postSearchSuccess(JSONArray result) {
+       TabPanel.instance().adminPanel.entityPanel.clear();
+       TabPanel.instance().adminPanel.entityPanel.add(new ReadAllClientsPanel(result));
+    }
+
+    @Override
+    protected String getSearchURI(String searchText, Integer start, Integer limit) {
+         return URL.encode(OfficeWelcome.constants.root_url() + "client/searchclient/" + start.toString() + "/"
+                + limit.toString() + "/?text=" + searchText);
+    }
+
+    @Override
+    protected String getSearchURI(Integer start, Integer limit) {
+         return OfficeWelcome.constants.root_url() + "client/searchclient/" + start.toString() + "/"
+                + limit.toString();
+    }
+    
+}
