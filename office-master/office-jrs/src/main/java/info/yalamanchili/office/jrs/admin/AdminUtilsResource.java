36bce95854b5848bb602354ebeff418261ce54fd
Phani Yalamanchili <phani.y@sstech.us>
2013-04-30 15:51:32 +0000

    office toolbox. email hash utils
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2938 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/admin/AdminUtilsResource.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.admin;
+
+import info.yalamanchili.office.toolbox.DataTools;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+@Path("secured/adminutils")
+@Produces("application/json")
+@Consumes("application/json")
+@Component
+@Transactional
+@Scope("request")
+public class AdminUtilsResource {
+
+    /*
+     * hashes  all the emails in the databases
+     */
+    @Path("hash_emails")
+    @PUT
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void hashEmails() {
+        DataTools.instance().hashEmails();
+    }
+}
