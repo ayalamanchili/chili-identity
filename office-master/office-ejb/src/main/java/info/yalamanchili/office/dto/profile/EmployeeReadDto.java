7be4522d1ea0f22a0f8b678a6a185dba0f6694c9
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-05-16 15:38:44 +0000

    in read /view employee panel show is the employee is active or inactive. ssto 998
    git-svn-id: http://tools.sstech.us/svn/office/trunk@2992 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/dto/profile/EmployeeReadDto.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/dto/profile/EmployeeReadDto.java
@@ -4,7 +4,12 @@
  */
 package info.yalamanchili.office.dto.profile;
 
+import info.yalamanchili.office.entity.VersionStatus;
 import info.yalamanchili.office.entity.profile.Employee;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -14,8 +19,28 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class EmployeeReadDto extends Employee {
 
+    @Enumerated(EnumType.STRING)
+    protected VersionStatus status;
+
+    public VersionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(VersionStatus status) {
+        this.status = status;
+    }
+
     @Override
     public String getSsn() {
         return "*********";
+    }
+
+    @PrePersist
+    @PreUpdate
+    protected void preSave() {
+        //TODO invoke validator also
+        if (this.status == null) {
+            this.status = VersionStatus.ACTIVE;
+        }
     }
 }
