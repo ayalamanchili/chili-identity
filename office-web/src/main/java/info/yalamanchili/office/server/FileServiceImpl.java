c86a106c458036d02eafe59a65ea2f8b40333fa8
Phani Yalamanchili <phani.y@sstech.us>
2013-05-29 22:05:09 +0000

    oi
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3042 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/server/FileServiceImpl.java
+++ office-web/src/main/java/info/yalamanchili/office/server/FileServiceImpl.java
@@ -14,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
