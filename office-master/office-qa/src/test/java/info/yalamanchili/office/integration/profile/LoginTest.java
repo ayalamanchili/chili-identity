bda2a627f875d60add40d202fbf1e43e28cd3eb0
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-05 13:10:33 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@3071 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-qa/src/test/java/info/yalamanchili/office/integration/profile/LoginTest.java
+++ office-master/office-qa/src/test/java/info/yalamanchili/office/integration/profile/LoginTest.java
@@ -1,3 +1,6 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
 package info.yalamanchili.office.integration.profile;
 
 import info.yalamanchili.office.integration.AbstractOfficeTest;
