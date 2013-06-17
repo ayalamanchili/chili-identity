f8fa034e4c2318c4f9df803453fc60ece25f0ca8
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-17 13:14:47 +0000

    http://yalamanchili.info:8080/jira/browse/SSTO-1091 create resource/practice
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3132 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/practice/PracticeResource.java
@@ -0,0 +1,75 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.practice;
+
+import info.chili.dao.CRUDDao;
+import info.yalamanchili.office.dao.practice.PracticeDao;
+import info.yalamanchili.office.entity.Practice.Practice;
+import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Prashanthi
+ */
+@Path("secured/practice")
+@Component
+@Scope("request")
+public class PracticeResource extends CRUDResource<Practice> {
+
+    @Autowired
+    public PracticeDao pracitceDao;
+
+    @Override
+    public CRUDDao getDao() {
+        return pracitceDao;
+    }
+
+    @GET
+    @Path("/{start}/{limit}")
+    public PracticeTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        PracticeTable tableObj = new PracticeTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class PracticeTable {
+
+        protected Long size;
+        protected List<Practice> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<Practice> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<Practice> entities) {
+            this.entities = entities;
+        }
+    }
+}
