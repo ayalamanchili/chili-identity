c8ecb05471b8ae551522a214862ac82b188e627c
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-18 12:21:34 +0000

    added create update and read all panels to practice
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3145 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/practice/CreatePracticePanel.java
@@ -0,0 +1,94 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.practice;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class CreatePracticePanel extends CreateComposite {
+
+    private static Logger logger = Logger.getLogger(CreatePracticePanel.class.getName());
+
+    public CreatePracticePanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+        initCreateComposite("Practice", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject practice = new JSONObject();
+        assignEntityValueFromField("name", practice);
+        assignEntityValueFromField("description", practice);
+        logger.info(practice.toString());
+        return practice;
+    }
+
+    @Override
+    protected void createButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        logger.info(arg0.getMessage());
+                        handleErrorResponse(arg0);
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postCreateSuccess(arg0);
+                    }
+                });
+    }
+
+    @Override
+    protected void addButtonClicked() {
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+        new ResponseStatusWidget().show("Successfully created practice data");
+        TabPanel.instance().myOfficePanel.sidePanelTop.clear();
+        TabPanel.instance().myOfficePanel.sidePanelTop.add(new PracticeSidePanel());
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllPracticePanel());
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("name", false, true, DataType.STRING_FIELD);
+        addField("description", false, false, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "practice";
+    }
+}
---
+++ office-web/src/main/java/info/yalamanchili/office/client/practice/UpdatePracticePanel.java
@@ -0,0 +1,91 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.practice;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.UpdateComposite;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class UpdatePracticePanel extends UpdateComposite {
+
+    private static Logger logger = Logger.getLogger(UpdatePracticePanel.class.getName());
+
+    public UpdatePracticePanel(JSONObject entity) {
+        logger.info("ddddd" + entity);
+        initUpdateComposite(entity, "Practice", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        assignEntityValueFromField("name", entity);
+        assignEntityValueFromField("description", entity);
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("name", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("description", entity, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        new ResponseStatusWidget().show("Successfully Updated Practice Data");
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllPracticePanel());
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("name", false, true, DataType.STRING_FIELD);
+        addField("description", false, false, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "practice";
+    }
+}
