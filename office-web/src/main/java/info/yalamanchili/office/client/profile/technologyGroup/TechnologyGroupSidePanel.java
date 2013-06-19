cb2d67e37c13e2f5f98f4bc149a0b66dd21b1144
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-19 10:56:13 +0000

    created update create and read all technology group panels in ui http://yalamanchili.info:8080/jira/browse/SSTO-1102
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3160 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/technologyGroup/TechnologyGroupSidePanel.java
@@ -0,0 +1,57 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.technologyGroup;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.widgets.ClickableLink;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class TechnologyGroupSidePanel extends ALComposite implements ClickHandler {
+
+    private static Logger logger = Logger.getLogger(TechnologyGroupSidePanel.class.getName());
+    public FlowPanel technologyGroupSidePanel = new FlowPanel();
+    ClickableLink createTechnologyGroupLink = new ClickableLink("Create Technology Group");
+
+    public TechnologyGroupSidePanel() {
+        init(technologyGroupSidePanel);
+    }
+
+    @Override
+    protected void addListeners() {
+        createTechnologyGroupLink.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        if (Auth.hasAnyOfRoles(Auth.ROLE.ROLE_ADMIN, Auth.ROLE.ROLE_RECRUITER)) {
+            technologyGroupSidePanel.add(createTechnologyGroupLink);
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(createTechnologyGroupLink)) {
+            TabPanel.instance().myOfficePanel.entityPanel.clear();
+            TabPanel.instance().myOfficePanel.entityPanel.add(new CreateTechnologyGroupPanel(CreateComposite.CreateCompositeType.CREATE));
+        }
+    }
+}
