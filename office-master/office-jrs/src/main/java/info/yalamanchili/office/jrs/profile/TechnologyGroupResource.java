a540bf9b0a33d380ab7170d144c5bf70aa428109
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-19 08:53:03 +0000

    added the code save and delete methods t o TechnologyGroupResource
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3156 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/TechnologyGroupResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/TechnologyGroupResource.java
@@ -11,9 +11,17 @@ import info.chili.dao.CRUDDao;
 import info.yalamanchili.office.dao.profile.TechnologyGroupDao;
 import info.yalamanchili.office.entity.profile.TechnologyGroup;
 import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 /**
@@ -31,5 +39,54 @@ public class TechnologyGroupResource extends CRUDResource<TechnologyGroup> {
     @Override
     public CRUDDao getDao() {
         return technologyGroupDao;
+    }
+
+    @GET
+    @Path("/{start}/{limit}")
+    public TechnologyGroupTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
+        TechnologyGroupTable tableObj = new TechnologyGroupTable();
+        tableObj.setEntities(getDao().query(start, limit));
+        tableObj.setSize(getDao().size());
+        return tableObj;
+    }
+
+    @PUT
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_RECRUITER')")
+    @Override
+    public TechnologyGroup save(TechnologyGroup entity) {
+        return super.save(entity);
+    }
+
+    @PUT
+    @Path("/delete/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_RECRUITER')")
+    @Override
+    public void delete(@PathParam("id") Long id) {
+        super.delete(id);
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class TechnologyGroupTable {
+
+        protected Long size;
+        protected List<TechnologyGroup> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<TechnologyGroup> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<TechnologyGroup> entities) {
+            this.entities = entities;
+        }
     }
 }
