cf0eee3fc74fe31a465814e75393c891996a9dff
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-19 13:09:59 +0000

    CREATE NEW SELCT WIDGET IN PRACTICE
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3164 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/practice/SelectPracticeWidget.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.practice;
+
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.rpc.HttpService;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.gwt.SelectComposite;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public class SelectPracticeWidget extends SelectComposite {
+
+    public SelectPracticeWidget(Boolean readOnly, Boolean isRequired) {
+        super(OfficeWelcome.constants, "Practice", readOnly, isRequired);
+    }
+
+    @Override
+    protected void fetchDropDownData() {
+        HttpService.HttpServiceAsync.instance().doGet(getDropDownURL(0, 10, "id", "name"),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String entityString) {
+                processData(entityString);
+            }
+        });
+    }
+
+    @Override
+    protected String getDropDownURL(Integer start, Integer limit, String... columns) {
+        return super.generateDropdownUrl(OfficeWelcome.constants.root_url() + "practice/dropdown", start, limit, columns);
+    }
+
+    @Override
+    protected void validate() {
+    }
+}
