adcfd20c5d31bad5de4d67a65ec78b5479d1b58a
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-25 11:48:45 +0000

    added new  package activity under ejb
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3185 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/activity/IssueType.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.activity;
+
+import info.chili.jpa.AbstractEntity;
+import info.chili.jpa.validation.Unique;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.envers.Audited;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author Prashanthi
+ */
+@Indexed
+@XmlRootElement
+@Entity
+@Audited
+@Table(uniqueConstraints =
+@UniqueConstraint(columnNames = {"name"}))
+@Unique(entity = IssueType.class, fields = {"name"}, message = "{issuetype.name.not.unique.msg}")
+public class IssueType extends AbstractEntity {
+
+    @Field
+    @NotEmpty(message = "{issuetype.name.not.empty.msg}")
+    @org.hibernate.annotations.Index(name = "ISSUETYPE_NM_IDX")
+    protected String name;
+    protected String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "IssueType{" + "name=" + name + ", desctiption=" + description + '}';
+    }
+}
