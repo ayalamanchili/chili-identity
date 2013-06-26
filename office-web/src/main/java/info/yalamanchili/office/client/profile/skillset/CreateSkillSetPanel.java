031a64aa12818f4b65ae46e7ae824ae0ba3dd37d
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-26 14:25:50 +0000

    added new bpm skill and certification request in create skill set panel.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3202 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/skillset/CreateSkillSetPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/skillset/CreateSkillSetPanel.java
@@ -7,13 +7,17 @@
  */
 package info.yalamanchili.office.client.profile.skillset;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
 import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.gwt.CreateComposite;
 import info.yalamanchili.office.client.gwt.FileuploadField;
 import info.chili.gwt.rpc.HttpService;
+import info.yalamanchili.office.client.gwt.GenericPopup;
+import info.yalamanchili.office.client.home.tasks.GenericBPMStartFormPanel;
 import info.yalamanchili.office.client.practice.SelectPracticeWidget;
 import info.yalamanchili.office.client.profile.technologyGroup.SelectTechnologyGroupWidget;
 import java.util.logging.Logger;
@@ -28,6 +32,8 @@ public class CreateSkillSetPanel extends CreateComposite {
     private static Logger logger = Logger.getLogger(CreateSkillSetPanel.class.getName());
     SelectPracticeWidget practiceF = new SelectPracticeWidget(false, false);
     SelectTechnologyGroupWidget technologyGroupF = new SelectTechnologyGroupWidget(false, false);
+    protected Anchor addSkilltL = new Anchor("Skills not present? submit request");
+    protected Anchor addCertificationL = new Anchor("Certifications not present? submit request");
     FileuploadField resumeUploadPanel = new FileuploadField(OfficeWelcome.constants, "SkillSet", "resumeUrl", "SkillSet/resumeUrl", true) {
         @Override
         public void onUploadComplete() {
@@ -45,12 +51,16 @@ public class CreateSkillSetPanel extends CreateComposite {
     protected void addWidgets() {
         addDropDown("practice", practiceF);
         addDropDown("technologyGroup", technologyGroupF);
+        entityFieldsPanel.add(addSkilltL);
+        entityFieldsPanel.add(addCertificationL);
         entityFieldsPanel.add(resumeUploadPanel);
     }
 
     @Override
     protected void addListeners() {
         // TODO Auto-generated method stub
+        addSkilltL.addClickHandler(this);
+        addCertificationL.addClickHandler(this);
     }
 
     @Override
@@ -106,6 +116,21 @@ public class CreateSkillSetPanel extends CreateComposite {
     @Override
     protected String getURI() {
         return OfficeWelcome.constants.root_url() + "employee/skillset/" + employeeId;
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(addSkilltL)) {
+            new GenericPopup(new GenericBPMStartFormPanel("add_new_skill_request", "add_new_skill_request")).show();
+        }
+        if (event.getSource().equals(addCertificationL)) {
+            new GenericPopup(new GenericBPMStartFormPanel("add_new_certification_request", "add_new_certificetion_request")).show();
+        }
+        super.onClick(event);
+    }
+
+    protected String getAddSkillRequestUrl() {
+        return OfficeWelcome.constants.root_url() + "bpm/startprocess/add_new_skill_request";
     }
 
     @Override
