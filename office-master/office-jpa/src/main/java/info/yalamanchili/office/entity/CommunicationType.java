6e134db292f672205c78f2de5a2160ea8e421e28
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-06-27 14:13:54 +0000

    added enum for CommunicationMode and CommunicationType http://yalamanchili.info:8080/jira/browse/SSTO-1122
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3221 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/CommunicationType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity;
+
+/**
+ *
+ * @author Prashanthi
+ */
+public enum CommunicationType {
+
+    INBOUND, OUTBOUND, OTHER
+}
