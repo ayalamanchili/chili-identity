89153457ec5d30dc226453ea1736150219bb9831
Phani Yalamanchili <phani.y@sstech.us>
2013-08-10 23:58:48 +0000

    privacy interceptor first cut. http://yalamanchili.info:8080/jira/browse/SSTO-1077
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3318 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/privacy/PrivacyAware.java
@@ -0,0 +1,22 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.privacy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to trigger  customer interceptor to filter the return results based on privacy settings
+ * @author anuyalamanchili
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrivacyAware {
+}
---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/privacy/PrivacyInterceptor.java
@@ -0,0 +1,65 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.privacy;
+
+import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.yalamanchili.office.dao.security.SecurityService;
+import info.yalamanchili.office.entity.profile.Employee;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author anuyalamanchili
+ */
+@Aspect
+@Component
+@Scope("request")
+public class PrivacyInterceptor {
+
+    @Pointcut(value = "execution(public * *(..))")
+    public void anyPublicMethod() {
+    }
+
+    @Around("anyPublicMethod() && @annotation(info.yalamanchili.office.privacy.PrivacyAware)")
+    public Object privacyCheck(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result = null;
+        try {
+            if (performPrivacyCheck(joinPoint)) {
+                result = joinPoint.proceed();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    protected boolean performPrivacyCheck(ProceedingJoinPoint joinPoint) {
+        Employee employee = getEmployee(joinPoint);
+        if (employee != null) {
+            Employee currentUser = SecurityService.instance().getCurrentUser();
+
+
+
+            //TODO check here
+        }
+
+        return true;
+    }
+
+    protected Employee getEmployee(ProceedingJoinPoint joinPoint) {
+        if (joinPoint.getArgs().length > 0 && joinPoint.getArgs()[0] instanceof Long) {
+            return EmployeeDao.instance().findById((Long) joinPoint.getArgs()[0]);
+        }
+        return null;
+    }
+}
