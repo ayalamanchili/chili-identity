6ed77e7bde3c51fed06a3da0a2c909540a98652e
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-08-12 14:54:43 +0000

    added read all privacy setting data
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3326 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/privacy/ReadAllPrivacySettngsPanel.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.privacy;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.date.DateUtils;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CRUDReadAllComposite;
+import info.yalamanchili.office.client.gwt.TableRowOptionsWidget;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi.P
+ */
+public class ReadAllPrivacySettngsPanel extends CRUDReadAllComposite {
+
+    private static Logger logger = Logger.getLogger(ReadAllPrivacySettngsPanel.class.getName());
+    public static ReadAllPrivacySettngsPanel instance;
+
+    public ReadAllPrivacySettngsPanel(String parentId) {
+        instance = this;
+        this.parentId = parentId;
+        initTable("Privacy", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String arg0) {
+                postDeleteSuccess();
+            }
+        });
+    }
+
+    private String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "privacy/delete/" + entityId;
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully Deleted Privacy Data");
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllPrivacySettngsPanel(parentId));
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+    }
+
+    @Override
+    public void preFetchTable(int start) {
+        HttpService.HttpServiceAsync.instance().doGet(getEmployeePruvacyURL(parentId, start, OfficeWelcome.constants.tableSize()),
+                OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String result) {
+                logger.info(result);
+                postFetchTable(result);
+            }
+        });
+    }
+
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("PrivacyData"));
+        table.setText(0, 2, getKeyValue("PrivacyMode"));
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "privacyData"));
+            table.setText(i, 2, JSONUtils.toString(entity, "privacyMode"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+    }
+
+    private String getEmployeePruvacyURL(String parentId, Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "employee/privacy/" + parentId + "/" + start.toString() + "/"
+                + limit.toString();
+    }
+}
