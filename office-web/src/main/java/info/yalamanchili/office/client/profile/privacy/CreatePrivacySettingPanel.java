aacaac0f32cd20ca841918638efe24a5b317d0d1
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-08-13 12:06:03 +0000

    added privacy potion panel and create privcay panel
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3333 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/privacy/CreatePrivacySettingPanel.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.privacy;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.gwt.CreateComposite;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Prashanthi.P
+ */
+public class CreatePrivacySettingPanel extends CreateComposite {
+
+    private static Logger logger = Logger.getLogger(CreatePrivacySettingPanel.class.getName());
+
+    public CreatePrivacySettingPanel(CreateComposite.CreateCompositeType type) {
+        super(type);
+        initCreateComposite("Privacy", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject privacy = new JSONObject();
+        assignEntityValueFromField("privacyData", privacy);
+        assignEntityValueFromField("privacyMode", privacy);
+        logger.info(privacy.toString());
+        return privacy;
+    }
+
+    @Override
+    protected void createButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                logger.info(arg0.getMessage());
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postCreateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    protected void addButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
+                new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                logger.info(arg0.getMessage());
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postCreateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    protected void postCreateSuccess(String result) {
+        new ResponseStatusWidget().show("Successfully created privacy data");
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllPrivacySettngsPanel(result));
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        String[] dataStrs = {"EMAILS", "PHONES", "ADDRESSES", "EMERGENCY_CONTACTS", "SKILL_SET", "COMPANY_CONTACTS"};
+        addEnumField("privacyData", false, true, dataStrs);
+        String[] modeStrs = {"PUBLIC", "INTERNAL", "PRIVATE"};
+        addEnumField("privacyMode", false, true, modeStrs);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "privacy";
+    }
+}
