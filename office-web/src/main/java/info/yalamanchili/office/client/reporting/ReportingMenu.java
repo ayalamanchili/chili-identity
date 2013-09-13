71ea450bfcea06495a23eb61cefb7d75db6b375c
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-09-13 14:34:24 +0000

    added reportingmenu package
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3408 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/reporting/ReportingMenu.java
@@ -0,0 +1,41 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.reporting;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.MenuBar;
+import info.yalamanchili.office.client.TabPanel;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class ReportingMenu extends Composite {
+
+    MenuBar reportingMenuBar = new MenuBar(false);
+
+    public ReportingMenu() {
+        initWidget(reportingMenuBar);
+        configureReportingMenu();
+    }
+
+    protected void configureReportingMenu() {
+        reportingMenuBar.addItem("Contracting", ContractingMaintainenceCmd);
+
+        reportingMenuBar.addStyleName("entityMenuBar");
+    }
+    Command ContractingMaintainenceCmd = new Command() {
+        public void execute() {
+//            TabPanel.instance().getAdminPanel().entityPanel.clear();
+//            TabPanel.instance().getAdminPanel().sidePanelTop.clear();
+////            TabPanel.instance().getAdminPanel().entityPanel.add(new ReadAllNotificationGroupsPanel());
+////            TabPanel.instance().getAdminPanel().sidePanelTop.add(new NotificationGroupSidePanel());
+        }
+    };
+}
