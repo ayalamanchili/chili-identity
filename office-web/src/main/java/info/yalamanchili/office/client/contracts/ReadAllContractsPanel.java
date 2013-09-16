9c2de630c438d40c9556ba67db0eacef665393b6
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-09-16 14:08:01 +0000

    added properties to read all client contracts panel
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3417 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/contracts/ReadAllContractsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/contracts/ReadAllContractsPanel.java
@@ -1,13 +1,138 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package info.yalamanchili.office.client.contracts;
 
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.Window;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.CRUDReadAllComposite;
+import info.chili.gwt.crud.TableRowOptionsWidget;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import java.util.logging.Logger;
+
 /**
  *
  * @author anuyalamanchili
  */
-public class ReadAllContractsPanel {
-    
+public class ReadAllContractsPanel extends CRUDReadAllComposite {
+
+    private static Logger logger = Logger.getLogger(ReadAllContractsPanel.class.getName());
+    public static ReadAllContractsPanel instance;
+
+    public ReadAllContractsPanel() {
+        instance = this;
+        initTable("Contract", OfficeWelcome.constants);
+    }
+
+    public ReadAllContractsPanel(JSONArray array) {
+        instance = this;
+        initTable("Contract", array, OfficeWelcome.constants);
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        if (Window.confirm("Are you sure? All Employee details will be deleted")) {
+            HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                    new ALAsyncCallback<String>() {
+                @Override
+                public void onResponse(String arg0) {
+                    postDeleteSuccess();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully Deleted Client Information");
+        TabPanel.instance().reportingPanel.entityPanel.clear();
+        TabPanel.instance().reportingPanel.entityPanel.add(new ReadAllContractsPanel());
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+    }
+
+    @Override
+    public void preFetchTable(int start) {
+        // TODO externalize the limit size for read all
+        HttpService.HttpServiceAsync.instance().doGet(getReadAllContractsURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String result) {
+                logger.info(result);
+                postFetchTable(result);
+            }
+        });
+    }
+
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("Primary"));
+        table.setText(0, 2, getKeyValue("Job Title"));
+        table.setText(0, 3, getKeyValue("Client"));
+        table.setText(0, 4, getKeyValue("Vendor"));
+        table.setText(0, 5, getKeyValue("ItemNumber"));
+        table.setText(0, 6, getKeyValue("BillingRate"));
+        table.setText(0, 7, getKeyValue("InvoiceFrequency"));
+        table.setText(0, 8, getKeyValue("Employee"));
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            OfficeWelcome.instance().logger.info(entity.toString());
+            table.setText(i, 1, JSONUtils.toString(entity, "ciPrimary"));
+            table.setText(i, 2, JSONUtils.toString(entity, "consultantJobTitle"));
+            if (entity.get("client") != null) {
+                JSONObject client = entity.get("client").isObject();
+                table.setText(i, 3, JSONUtils.toString(client, "name"));
+            }
+            if (entity.get("vendor") != null) {
+                JSONObject vendor = entity.get("vendor").isObject();
+                table.setText(i, 4, JSONUtils.toString(vendor, "name"));
+            }
+            table.setText(i, 5, JSONUtils.toString(entity, "itemNumber"));
+            table.setText(i, 6, JSONUtils.toString(entity, "billingRate"));
+            table.setText(i, 7, JSONUtils.toString(entity, "invoiceFrequency"));
+            table.setText(i, 8, JSONUtils.toString(entity, "Employee"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        if (Auth.isAdmin()) {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+        } else if (Auth.hasAnyOfRoles(Auth.ROLE.ROLE_ADMIN, Auth.ROLE.ROLE_HR, Auth.ROLE.ROLE_RELATIONSHIP)) {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE, row, JSONUtils.toString(entity, "id"));
+        } else {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ, row, JSONUtils.toString(entity, "id"));
+        }
+    }
+
+    private String getReadAllContractsURL(Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "contract/" + start.toString() + "/" + limit.toString();
+    }
+
+    private String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "contract/delete/" + entityId;
+    }
 }
