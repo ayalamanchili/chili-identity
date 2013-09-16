1e2aa1d962f12fa5d95ab877a5f182954469713d
raghu l <raghu.l@xyz.com>
2013-09-16 14:25:49 +0000

    created billing duration Enum
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3418 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/profile/BillingDuration.java
@@ -0,0 +1,18 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.profile;
+
+/**
+ *
+ * @author raghu.l
+ */
+public enum BillingDuration {
+    HOUR,
+    DAY,
+    MONTH
+}
