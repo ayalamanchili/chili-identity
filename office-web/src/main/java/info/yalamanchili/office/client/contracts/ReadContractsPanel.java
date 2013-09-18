208e888e80eb435181d0b4e39d41486527083447
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-09-18 08:11:04 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@3426 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/contracts/ReadContractsPanel.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.contracts;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.ReadComposite;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.admin.client.SelectClientWidget;
+import info.yalamanchili.office.client.admin.vendor.SelectVendorWidget;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class ReadContractsPanel extends ReadComposite {
+
+    private static ReadContractsPanel instance;
+    private static Logger logger = Logger.getLogger(ReadContractsPanel.class.getName());
+
+    public ReadContractsPanel(JSONObject entity) {
+        instance = this;
+        initReadComposite(entity, "Contract", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String response) {
+                logger.info("read ec6 response" + response);
+                entity = (JSONObject) JSONParser.parseLenient(response);
+                populateFieldsFromEntity(entity);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        addField("consultantJobTitle", true, false, DataType.STRING_FIELD);
+        addDropDown("client", new SelectClientWidget(true, false));
+        addDropDown("vendor", new SelectVendorWidget(true, false));
+        addField("itemNumber", true, false, DataType.STRING_FIELD);
+        addField("billingRate", true, false, DataType.CURRENCY_FIELD);
+        String[] invoiceFrequencies = {"WEEKLY", "BI_WEEKLY", "MONTHLY", "SEMI_MONTHLY", "NOT_REQUIRED"};
+        addEnumField("invoiceFrequency", true, false, invoiceFrequencies);
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("consultantJobTitle", true, false, DataType.STRING_FIELD);
+        addDropDown("client", new SelectClientWidget(true, false));
+        addDropDown("vendor", new SelectVendorWidget(true, false));
+        addField("itemNumber", true, false, DataType.STRING_FIELD);
+        addField("billingRate", true, false, DataType.CURRENCY_FIELD);
+        String[] invoiceFrequencies = {"WEEKLY", "BI_WEEKLY", "MONTHLY", "SEMI_MONTHLY", "NOT_REQUIRED"};
+        addEnumField("invoiceFrequency", true, false, invoiceFrequencies);
+        assignFieldValueFromEntity("employee", entity, DataType.STRING_FIELD);
+        addField("employee", true, false, DataType.STRING_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "contract/" + entityId;
+    }
+}
