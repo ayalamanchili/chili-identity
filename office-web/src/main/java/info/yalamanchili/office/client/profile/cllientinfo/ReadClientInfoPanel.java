a314564786b17fd8dc272aecb50d8214b917fa80
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-09-26 11:42:09 +0000

    added new hr fields to client read panel
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3469 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/cllientinfo/ReadClientInfoPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/cllientinfo/ReadClientInfoPanel.java
@@ -64,6 +64,13 @@ public class ReadClientInfoPanel extends ReadComposite {
         assignFieldValueFromEntity("invoiceDeliveryMethod", entity, DataType.ENUM_FIELD);
         assignFieldValueFromEntity("recruiter", entity, null);
         assignFieldValueFromEntity("visaStatus", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("joiningReport", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("accountVerificationDocs", entity, DataType.BOOLEAN_FIELD);
+        assignFieldValueFromEntity("signedCopyOfWorkOrder", entity, DataType.BOOLEAN_FIELD);
+        assignFieldValueFromEntity("i9Filled", entity, DataType.BOOLEAN_FIELD);
+        assignFieldValueFromEntity("w4Filled", entity, DataType.BOOLEAN_FIELD);
+        assignFieldValueFromEntity("logisticsPreparation", entity, DataType.BOOLEAN_FIELD);
+        assignFieldValueFromEntity("hrOrientation", entity, DataType.BOOLEAN_FIELD);
         if (Auth.isSubContractor(TreeEmployeePanel.instance().getEntity())) {
             assignFieldValueFromEntity("subcontractor", entity, null);
             assignFieldValueFromEntity("subcontractorContact", entity, null);
@@ -106,6 +113,13 @@ public class ReadClientInfoPanel extends ReadComposite {
         addEnumField("invoiceDeliveryMethod", true, false, invoiceDeliveryMethods);
         addDropDown("recruiter", selectRecruiterWidget);
         addField("visaStatus", true, false, DataType.STRING_FIELD);
+        addField("joiningReport", false, true, DataType.STRING_FIELD);
+        addField("accountVerificationDocs", false, false, DataType.BOOLEAN_FIELD);
+        addField("signedCopyOfWorkOrder", false, false, DataType.BOOLEAN_FIELD);
+        addField("i9Filled", false, false, DataType.BOOLEAN_FIELD);
+        addField("w4Filled", false, false, DataType.BOOLEAN_FIELD);
+        addField("logisticsPreparation", false, true, DataType.BOOLEAN_FIELD);
+        addField("hrOrientation", false, false, DataType.BOOLEAN_FIELD);
         if (Auth.isSubContractor(TreeEmployeePanel.instance().getEntity())) {
             addDropDown("subcontractor", new SelectSubcontractorWidget(false, false));
             addDropDown("subcontractorContact", new SelectSubcontractorContactWidget(false, false));
