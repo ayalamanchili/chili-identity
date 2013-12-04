82e465f8a2bd951e4116b0837001cb3a2ad111c3
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-12-04 06:48:54 +0000

    added billing rate dao to profile.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3687 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/BillingRateDao.java
@@ -0,0 +1,41 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.profile;
+
+import info.chili.dao.CRUDDao;
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.entity.profile.BillingRate;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class BillingRateDao extends CRUDDao<BillingRate> {
+
+    @PersistenceContext
+    protected EntityManager em;
+
+    public BillingRateDao() {
+        super(BillingRateDao.class);
+    }
+
+    public BillingRate save(BillingRate entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public static BillingRateDao instance() {
+        return SpringContext.getBean(BillingRateDao.class);
+    }
+}
