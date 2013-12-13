c85461cfe709c1751f1e1c88fe981527a0b812c7
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-12-13 06:11:32 +0000

    added alignment horizontal to read employee
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3759 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/employee/ReadEmployeePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employee/ReadEmployeePanel.java
@@ -11,50 +11,51 @@ import info.chili.gwt.rpc.HttpService.HttpServiceAsync;
 
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.utils.Alignment;
 import info.yalamanchili.office.client.Auth;
 import info.yalamanchili.office.client.Auth.ROLE;
 import info.yalamanchili.office.client.profile.employeetype.SelectEmployeeTypeWidget;
 import java.util.logging.Logger;
 
 public class ReadEmployeePanel extends ReadComposite {
-    
+
     private static Logger logger = Logger.getLogger(ReadEmployeePanel.class.getName());
     private static ReadEmployeePanel instance;
     protected SelectEmployeeTypeWidget employeeSelectWidget = new SelectEmployeeTypeWidget(true, false);
-    
+
     public static ReadEmployeePanel instance() {
         return instance;
     }
-    
+
     public ReadEmployeePanel(JSONObject entity) {
         instance = this;
         initReadComposite(entity, "Employee", OfficeWelcome.constants);
     }
-    
+
     public ReadEmployeePanel(String id) {
         initReadComposite(id, "Employee", OfficeWelcome.constants);
     }
-    
+
     @Override
     public void loadEntity(String entityId) {
         HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        logger.info("this is the response from the server" + response);
-                        entity = (JSONObject) JSONParser.parseLenient(response);
-                        populateFieldsFromEntity(entity);
-                    }
-                });
-        
+            @Override
+            public void onResponse(String response) {
+                logger.info("this is the response from the server" + response);
+                entity = (JSONObject) JSONParser.parseLenient(response);
+                populateFieldsFromEntity(entity);
+            }
+        });
+
     }
-    
+
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
         assignFieldValueFromEntity("firstName", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("middleInitial", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("lastName", entity, DataType.STRING_FIELD);
-         if (canViewDOBField()) {
+        if (canViewDOBField()) {
             assignFieldValueFromEntity("dateOfBirth", entity, DataType.DATE_FIELD);
         }
         assignFieldValueFromEntity("sex", entity, DataType.ENUM_FIELD);
@@ -69,58 +70,59 @@ public class ReadEmployeePanel extends ReadComposite {
             assignFieldValueFromEntity("status", entity, DataType.BOOLEAN_FIELD);
         }
     }
-    
+
     @Override
     protected void addListeners() {
         // TODO Auto-generated method stub
     }
-    
+
     @Override
     protected void configure() {
         // TODO Auto-generated method stub
     }
-    
+
     @Override
     protected void addWidgets() {
         addDropDown("employeeType", employeeSelectWidget);
-        addField("firstName", true, false, DataType.STRING_FIELD);
-        addField("middleInitial", true, false, DataType.STRING_FIELD);
-        addField("lastName", true, false, DataType.STRING_FIELD);
-        addField("employeeId", true, false, DataType.STRING_FIELD);
+        addField("firstName", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("middleInitial", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("lastName", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("employeeId", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         if (canViewDOBField()) {
-            addField("dateOfBirth", true, false, DataType.DATE_FIELD);
+            addField("dateOfBirth", true, false, DataType.DATE_FIELD, Alignment.HORIZONTAL);
         }
         String[] strs = {"MALE", "FEMALE"};
-        addEnumField("sex", true, false, strs);
-        addField("startDate", true, false, DataType.DATE_FIELD);
-        addField("jobTitle", true, false, DataType.STRING_FIELD);
+        addEnumField("sex", true, false, strs, Alignment.HORIZONTAL);
+        addField("startDate", true, false, DataType.DATE_FIELD, Alignment.HORIZONTAL);
+        addField("jobTitle", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         if (Auth.isAdmin()) {
-            addField("ssn", true, false, DataType.STRING_FIELD);
+            addField("ssn", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         }
         if (Auth.isAdmin()) {
-            addField("status", true, false, DataType.BOOLEAN_FIELD);
+            addField("status", true, false, DataType.BOOLEAN_FIELD, Alignment.HORIZONTAL);
         }
+        alignFields();
     }
-    
+
     protected boolean canViewDOBField() {
         return Auth.hasAnyOfRoles(ROLE.ROLE_ADMIN, ROLE.ROLE_HR, ROLE.ROLE_RELATIONSHIP);
     }
-    
+
     @Override
     protected void addWidgetsBeforeCaptionPanel() {
         // TODO Auto-generated method stub
     }
-    
+
     @Override
     protected String getURI() {
         return OfficeWelcome.constants.root_url() + "employee/" + entityId;
     }
-    
+
     @Override
     protected boolean enableAudit() {
         return Auth.hasAnyOfRoles(ROLE.ROLE_ADMIN, ROLE.ROLE_HR, ROLE.ROLE_RELATIONSHIP);
     }
-    
+
     @Override
     protected String getAuditUrl() {
         return OfficeWelcome.instance().constants.root_url() + "audit/changes/" + "info.yalamanchili.office.entity.profile.Employee" + "/" + getEntityId();
