8e9a2d827b89d4c62c26d01ee148c5f49f0386ae
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2013-12-31 11:51:27 +0000

    added create,update ,read all and read panels for create time sheet panel. http://yalamanchili.info/jira/browse/SSTO-1352
    git-svn-id: http://tools.sstech.us/svn/office/trunk@3862 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/corporatetimesheet/ReadCorporateTimeSheetPanel.java
@@ -0,0 +1,91 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.corporatetimesheet;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.ReadComposite;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.profile.employee.SelectEmployeeWidget;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class ReadCorporateTimeSheetPanel extends ReadComposite {
+
+    private static ReadCorporateTimeSheetPanel instance;
+
+    public static ReadCorporateTimeSheetPanel instance() {
+        return instance;
+    }
+
+    public ReadCorporateTimeSheetPanel(JSONObject entity) {
+        instance = this;
+        initReadComposite(entity, "CorporateTimeSheet", OfficeWelcome.constants);
+    }
+
+    public ReadCorporateTimeSheetPanel(String id) {
+        initReadComposite(id, "CorporateTimeSheet", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String response) {
+                entity = (JSONObject) JSONParser.parseLenient(response);
+                populateFieldsFromEntity(entity);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("employee", entity, null);
+        assignFieldValueFromEntity("category", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("endDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("status", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("hours", entity, DataType.FLOAT_FIELD);
+        assignFieldValueFromEntity("notes", entity, DataType.TEXT_AREA_FIELD);
+
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addDropDown("employee", new SelectEmployeeWidget("Employee", true, false));
+        addField("category", true, false, DataType.ENUM_FIELD);
+        addField("startDate", true, false, DataType.DATE_FIELD);
+        addField("endDate", true, false, DataType.DATE_FIELD);
+        addField("status", true, false, DataType.STRING_FIELD);
+        addField("hours", true, false, DataType.FLOAT_FIELD);
+        addField("notes", true, false, DataType.TEXT_AREA_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "corporate-timesheet/" + entityId;
+    }
+}
---
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/corporatetimesheet/UpdateCorporateTimeSheetPanel.java
@@ -0,0 +1,108 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.tae.corporatetimesheet;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import info.chili.gwt.crud.UpdateComposite;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.profile.employee.SelectEmployeeWidget;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class UpdateCorporateTimeSheetPanel extends UpdateComposite {
+
+    private static Logger logger = Logger.getLogger(UpdateCorporateTimeSheetPanel.class.getName());
+    SelectEmployeeWidget selectEmployeeWidget = new SelectEmployeeWidget("Employee", true, false);
+
+    public UpdateCorporateTimeSheetPanel(JSONObject entity) {
+        initUpdateComposite(entity, "CorporateTimeSheet", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject entity = new JSONObject();
+        assignEntityValueFromField("employee", entity);
+        assignEntityValueFromField("category", entity);
+        assignEntityValueFromField("startDate", entity);
+        assignEntityValueFromField("endDate", entity);
+        assignEntityValueFromField("status", entity);
+        assignEntityValueFromField("hours", entity);
+        assignEntityValueFromField("notes", entity);
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
+
+            @Override
+            public void onSuccess(String arg0) {
+                postUpdateSuccess(arg0);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("employee", entity, null);
+        assignFieldValueFromEntity("category", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("endDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("status", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("hours", entity, DataType.FLOAT_FIELD);
+        assignFieldValueFromEntity("notes", entity, DataType.TEXT_AREA_FIELD);
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        new ResponseStatusWidget().show("Successfully Updated Corporate Time Sheet Information");
+        TabPanel.instance().timePanel.entityPanel.clear();
+        TabPanel.instance().timePanel.entityPanel.add(new ReadAllCorporateTimeSheetPanel());
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addDropDown("employee", new SelectEmployeeWidget("Employee", true, false));
+        addField("category", true, false, DataType.ENUM_FIELD);
+        addField("startDate", true, false, DataType.DATE_FIELD);
+        addField("endDate", true, false, DataType.DATE_FIELD);
+        addField("status", true, false, DataType.STRING_FIELD);
+        addField("hours", true, false, DataType.FLOAT_FIELD);
+        addField("notes", true, false, DataType.TEXT_AREA_FIELD);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "corporate-timesheet";
+    }
+}
