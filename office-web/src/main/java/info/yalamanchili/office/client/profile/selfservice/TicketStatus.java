202171622522f88efc6316fa979dc69d7d8939b3
raghu l <raghu.l@xyz.com>
2014-02-04 11:12:55 +0000

    Ticket Status Enum values
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4046 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/selfservice/TicketStatus.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.selfservice;
+
+/**
+ *
+ * @author raghu.l
+ */
+public enum TicketStatus {
+    Open,
+    ReOpened,
+    InProgres,
+    Rejected,
+    Resolved;
+    
+     public static String[] names() {
+        TicketStatus[] values = values();
+        String[] names = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            names[i] = values[i].name();
+        }
+        return names;
+    }
+    
+}
