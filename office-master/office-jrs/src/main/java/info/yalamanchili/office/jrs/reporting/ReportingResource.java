9e7494231616a46ea3d1c96adb4a0f68591d07f3
Phani Yalamanchili <phani.y@sstech.us>
2014-03-17 01:26:19 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@4173 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/reporting/ReportingResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/reporting/ReportingResource.java
@@ -8,6 +8,7 @@
 package info.yalamanchili.office.jrs.reporting;
 
 import info.yalamanchili.office.reporting.AdhocReportingService;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -20,6 +21,8 @@ import org.springframework.stereotype.Component;
  * @author anuyalamanchili
  */
 @Path("secured/reporting")
+@Produces("application/json")
+@Consumes("application/json")
 @Component
 @Scope("request")
 public class ReportingResource {
--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/reports/RecruitingReportsResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/reports/RecruitingReportsResource.java
@@ -12,9 +12,11 @@ import info.yalamanchili.office.dto.profile.EmployeeDto;
 import info.yalamanchili.office.reports.recruiting.RecruitingReportsService;
 import info.yalamanchili.office.reports.recruiting.SkillSetSearchDto;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -26,6 +28,8 @@ import org.springframework.transaction.annotation.Transactional;
  * @author ayalamanchili
  */
 @Path("secured/recruiting-reports")
+@Produces("application/json")
+@Consumes("application/json")
 @Component
 @Scope("request")
 @Transactional
