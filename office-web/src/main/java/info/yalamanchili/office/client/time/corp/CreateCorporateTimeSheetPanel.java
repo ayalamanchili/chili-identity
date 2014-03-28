4301ec77863f613f684d9f2355ba6de33ac33b8c
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-03-28 10:06:22 +0000

    added validation for end date.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4207 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/time/corp/CreateCorporateTimeSheetPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/time/corp/CreateCorporateTimeSheetPanel.java
@@ -13,6 +13,7 @@ import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import info.chili.gwt.crud.CreateComposite;
 import info.chili.gwt.fields.DataType;
+import info.chili.gwt.fields.DateField;
 import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
@@ -51,16 +52,16 @@ public class CreateCorporateTimeSheetPanel extends CreateComposite {
     protected void createButtonClicked() {
         HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(), OfficeWelcome.instance().getHeaders(), true,
                 new AsyncCallback<String>() {
-                    @Override
-                    public void onFailure(Throwable arg0) {
-                        handleErrorResponse(arg0);
-                    }
+            @Override
+            public void onFailure(Throwable arg0) {
+                handleErrorResponse(arg0);
+            }
 
-                    @Override
-                    public void onSuccess(String arg0) {
-                        postCreateSuccess(arg0);
-                    }
-                });
+            @Override
+            public void onSuccess(String arg0) {
+                postCreateSuccess(arg0);
+            }
+        });
     }
 
     @Override
@@ -93,6 +94,17 @@ public class CreateCorporateTimeSheetPanel extends CreateComposite {
         addField("endDate", false, true, DataType.DATE_FIELD);
         addField("hours", false, true, DataType.FLOAT_FIELD);
         addField("notes", false, false, DataType.TEXT_AREA_FIELD);
+    }
+
+    @Override
+    protected boolean processClientSideValidations(JSONObject entity) {
+        DateField startDateF = (DateField) fields.get("startDate");
+        DateField endDateF = (DateField) fields.get("endDate");
+        if (startDateF.getDate() != null && endDateF.getDate() != null && startDateF.getDate().after(endDateF.getDate())) {
+            endDateF.setMessage("End Date must be equal to or after Start Date");
+            return false;
+        }
+        return true;
     }
 
     @Override
