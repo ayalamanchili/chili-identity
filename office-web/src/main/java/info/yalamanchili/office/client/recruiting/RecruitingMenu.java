e3383f8df9e06e7b00ffc3bad7fc773587080f2a
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-04-03 12:18:09 +0000

    added recruiting menu to recruiting tab .
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4221 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/recruiting/RecruitingMenu.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.recruiting;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.MenuBar;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.TabPanel;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class RecruitingMenu extends Composite {
+
+    MenuBar recruitingMenuBar = new MenuBar(false);
+
+    public RecruitingMenu() {
+        initWidget(recruitingMenuBar);
+        configureRecruitingMenu();
+    }
+
+    protected void configureRecruitingMenu() {
+        if (Auth.hasAnyOfRoles(Auth.ROLE.ROLE_RECRUITER)) {
+            recruitingMenuBar.addItem("SkillSet Tags", skillSetTagsMaintainenceCmd);
+        }
+        recruitingMenuBar.addStyleName("entityMenuBar");
+    }
+    Command skillSetTagsMaintainenceCmd = new Command() {
+        public void execute() {
+            TabPanel.instance().getRecruitingPanel().entityPanel.clear();
+            TabPanel.instance().getRecruitingPanel().sidePanelTop.clear();
+//            TabPanel.instance().getSocialPanel().entityPanel.add(new EmployeeFeedHome());
+        }
+    };
+}
