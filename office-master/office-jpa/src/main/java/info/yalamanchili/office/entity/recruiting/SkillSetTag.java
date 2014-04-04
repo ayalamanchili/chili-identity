fa898e22f942cae11112b7b0173eb3dcb09e96a8
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-04-04 12:56:36 +0000

    added skill set tag to jpa
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4223 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/recruiting/SkillSetTag.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.recruiting;
+
+import info.chili.jpa.AbstractEntity;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.envers.Audited;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+@Indexed
+@XmlRootElement
+@Entity
+@Audited
+public class SkillSetTag extends AbstractEntity {
+
+    @Field
+    @NotEmpty(message = "{skillsettag.name.not.empty.msg}")
+    @ManyToMany
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "SkillSetTag {" + "name=" + name + '}';
+    }
+}
