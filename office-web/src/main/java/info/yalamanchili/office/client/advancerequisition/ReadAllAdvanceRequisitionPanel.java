b6aa75f1a5cad9b514f3e9d662112c3ce5124344
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-04-14 09:57:17 +0000

    added read all advacne form in web
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4249 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/advancerequisition/ReadAllAdvanceRequisitionPanel.java
@@ -0,0 +1,112 @@
+/**
+ * System Soft Technolgies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.advancerequisition;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.CRUDReadAllComposite;
+import info.chili.gwt.crud.TableRowOptionsWidget;
+import info.chili.gwt.date.DateUtils;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class ReadAllAdvanceRequisitionPanel extends CRUDReadAllComposite {
+
+    private static Logger logger = Logger.getLogger(ReadAllAdvanceRequisitionPanel.class.getName());
+    public static ReadAllAdvanceRequisitionPanel instance;
+
+    public ReadAllAdvanceRequisitionPanel() {
+        instance = this;
+        initTable("Expenses", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String arg0) {
+                postDeleteSuccess();
+            }
+        });
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully Deleted Expenses Information");
+        TabPanel.instance().expensePanel.entityPanel.clear();
+        TabPanel.instance().expensePanel.entityPanel.add(new ReadAllAdvanceRequisitionPanel());
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+    }
+
+    @Override
+    public void preFetchTable(int start) {
+        HttpService.HttpServiceAsync.instance().doGet(getadvanceURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(),
+                false, new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String result) {
+                logger.info(result);
+                postFetchTable(result);
+            }
+        });
+    }
+
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("Amount"));
+        table.setText(0, 2, getKeyValue("NeededBy"));
+        table.setText(0, 3, getKeyValue("DateRequested"));
+        table.setText(0, 4, getKeyValue("PayrollFileNumber"));
+        table.setText(0, 5, getKeyValue("transaction"));
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "amount"));
+            table.setText(i, 2, JSONUtils.toString(entity, "neededBy"));
+            table.setText(i, 3, JSONUtils.toString(entity, "dateRequested"));
+            table.setText(i, 4, JSONUtils.toString(entity, "payrollFileNumber"));
+            table.setText(i, 5, JSONUtils.toString(entity, "transaction"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+    }
+
+    private String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "advancerequisition/delete/" + entityId;
+    }
+
+    private String getadvanceURL(Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "advancerequisition/" + start.toString() + "/"
+                + limit.toString();
+    }
+}
