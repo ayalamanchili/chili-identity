766697546bd4ea9921677a1a18329c246f7369fb
Phani Yalamanchili <phani.y@sstech.us>
2014-05-07 19:36:34 +0000

    http://tools.yalamanchili.info:8080/browse/SSTO-1541 account reset modifications
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4346 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/email/AccountResetCompleteNotification.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/email/AccountResetCompleteNotification.java
@@ -8,10 +8,8 @@
 package info.yalamanchili.office.bpm.email;
 
 import info.chili.spring.SpringContext;
-import info.yalamanchili.office.OfficeRoles.OfficeRole;
 import info.yalamanchili.office.bpm.types.AccountReset;
 import info.yalamanchili.office.email.Email;
-import info.yalamanchili.office.email.MailUtils;
 import info.yalamanchili.office.jms.MessagingService;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
@@ -24,11 +22,9 @@ public class AccountResetCompleteNotification implements TaskListener {
 
     @Override
     public void notify(DelegateTask delegateTask) {
-        MailUtils mailUtils = (MailUtils) SpringContext.getBean("mailUtils");
         MessagingService messagingService = (MessagingService) SpringContext.getBean("messagingService");
         AccountReset account = (AccountReset) delegateTask.getExecution().getVariable("account");
         Email email = new Email();
-        email.setTos(mailUtils.getEmailsAddressesForRoles(OfficeRole.ROLE_ADMIN.name(), OfficeRole.ROLE_HR.name(), OfficeRole.ROLE_RELATIONSHIP.name()));
         email.addTo(account.getEmail());
         String status = (String) delegateTask.getExecution().getVariable("status");
         email.setSubject("Account Reset Task Completed: Status:" + status);
@@ -37,18 +33,15 @@ public class AccountResetCompleteNotification implements TaskListener {
         //username
         String username = (String) delegateTask.getExecution().getVariable("username");
         if (username != null) {
-            message.append(" Username:").append(username).append("\n");;
-        }
-        //password
-        String password = (String) delegateTask.getExecution().getVariable("password");
-        if (password != null) {
-            message.append(" Password:").append(password).append("\n");;
+            message.append(" Username / EmployeeId:").append(username).append("\n");;
         }
         //task notes
         String notes = (String) delegateTask.getExecution().getVariable("notes");
         if (notes != null) {
             message.append(" Notes:").append(notes).append("\n");
         }
+        message.append("Account was reset. Please submit forgot password request with the Username / EmployeeId above to get a temporary password \n");
+        message.append("https://apps.sstech.us/site/office/forgot-password.html");
         email.setHtml(Boolean.TRUE);
         email.setBody(message.toString());
         messagingService.sendEmail(email);
