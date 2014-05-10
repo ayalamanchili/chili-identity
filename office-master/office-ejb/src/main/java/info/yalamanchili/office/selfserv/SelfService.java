c12ebb7e32ebc413772cdbeafeeb1bf6ec7fcb95
Phani Yalamanchili <phani.y@sstech.us>
2014-05-10 15:32:35 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@4363 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/selfserv/SelfService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/selfserv/SelfService.java
@@ -218,7 +218,9 @@ public class SelfService {
         Set<String> notificationGroup = new HashSet<String>();
         //employee who created the ticket;
         Employee emp = comment.getTicket().getEmployee();
-        notificationGroup.add(emp.getPrimaryEmail().getEmail());
+        if (TicketStatus.Resolved.equals(comment.getTicket().getStatus()) || TicketStatus.Rejected.equals(comment.getTicket().getStatus())) {
+            notificationGroup.add(emp.getPrimaryEmail().getEmail());
+        }
         // Role to which the ticket is assigned to 
         String role = comment.getTicket().getDepartmentAssigned().getRolename();
         notificationGroup.addAll(MailUtils.instance().getEmailsAddressesForRoles(role));
@@ -230,7 +232,7 @@ public class SelfService {
         return notificationGroup;
     }
 
-//TODO externalize
+    //TODO externalize
     protected OfficeRole getDepartmentToAssign(ServiceTicket ticket) {
         switch (ticket.getType()) {
             case Immigration:
