51c38b1f818e28a88105853cb372d5d73c314c90
Phani Yalamanchili <phani.y@sstech.us>
2014-06-04 23:18:51 +0000

    refactored create message with new widget,
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4433 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/gwt/MultiSelectSuggestBox.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.gwt;
+
+import com.google.common.base.Splitter;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import info.chili.gwt.composite.ALComposite;
+import info.chili.gwt.fields.TextAreaField;
+import info.chili.gwt.widgets.SuggestBox;
+import info.yalamanchili.office.client.OfficeWelcome;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+public abstract class MultiSelectSuggestBox extends ALComposite implements KeyPressHandler, ClickHandler {
+
+    private static Logger logger = Logger.getLogger(MultiSelectSuggestBox.class.getName());
+    protected FlowPanel panel = new FlowPanel();
+    protected SuggestBox suggestionsBox = new SuggestBox(OfficeWelcome.constants, "search", "Employee", false, false);
+    protected Button selectB = new Button("Add");
+    protected TextAreaField valuesTA = new TextAreaField(OfficeWelcome.constants, "selected", "Employee", false, false);
+
+    public MultiSelectSuggestBox() {
+        init(panel);
+        initTosSuggesBox();
+    }
+
+    public abstract void initTosSuggesBox();
+
+    @Override
+    protected void addListeners() {
+        suggestionsBox.getSuggestBox().addKeyPressHandler(this);
+        selectB.addClickHandler(this);
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        panel.add(suggestionsBox);
+        panel.add(selectB);
+        panel.add(valuesTA);
+    }
+
+    public JSONArray getValues() {
+        JSONArray array = new JSONArray();
+        int i = 0;
+        for (String toStr : Splitter.on(" ").split(valuesTA.getValue())) {
+            if (!toStr.trim().isEmpty()) {
+                JSONObject to = new JSONObject();
+                to.put("id", new JSONString(toStr.trim()));
+                to.put("value", new JSONString(toStr.trim()));
+                array.set(i, to);
+                i++;
+            }
+        }
+        return array;
+    }
+
+    protected void addTo() {
+        if (suggestionsBox.getValue() != null && !suggestionsBox.getValue().trim().isEmpty()) {
+            valuesTA.setValue(valuesTA.getValue() + " " + suggestionsBox.getValue());
+            suggestionsBox.setValue("");
+        }
+    }
+
+    @Override
+    public void onKeyPress(KeyPressEvent event) {
+        int keyCode = event.getUnicodeCharCode();
+        if (keyCode == 0) {
+            // Probably Firefox
+            keyCode = event.getNativeEvent().getKeyCode();
+        }
+        if (keyCode == KeyCodes.KEY_ENTER) {
+            addTo();
+        }
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if (event.getSource().equals(selectB)) {
+            addTo();
+        }
+    }
+}
--- office-web/src/main/java/info/yalamanchili/office/client/home/message/CreateMessagePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/home/message/CreateMessagePanel.java
@@ -8,25 +8,18 @@
 package info.yalamanchili.office.client.home.message;
 
 import com.google.common.base.Splitter;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyPressEvent;
-import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
 import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.CreateComposite;
 import info.chili.gwt.fields.DataType;
-import info.chili.gwt.fields.TextAreaField;
+import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.JSONUtils;
 import info.chili.gwt.widgets.ResponseStatusWidget;
-import info.chili.gwt.widgets.SuggestBox;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
-import info.chili.gwt.crud.CreateComposite;
-import info.chili.gwt.rpc.HttpService;
+import info.yalamanchili.office.client.gwt.MultiSelectSuggestBox;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -34,12 +27,34 @@ import java.util.logging.Logger;
  *
  * @author Prashanthi
  */
-public class CreateMessagePanel extends CreateComposite implements KeyPressHandler {
+public class CreateMessagePanel extends CreateComposite {
 
     private static Logger logger = Logger.getLogger(CreateMessagePanel.class.getName());
-    SuggestBox suggestionsBox = null;
-    Button addTo = new Button("Add");
-    TextAreaField tosTB;
+    MultiSelectSuggestBox employeesSB = new MultiSelectSuggestBox() {
+        @Override
+        public void initTosSuggesBox() {
+            HttpService.HttpServiceAsync.instance().doGet(getEmployeeIdsDropDownUrl(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+                @Override
+                public void onResponse(String entityString) {
+                    logger.info(entityString);
+                    Map<String, String> values = JSONUtils.convertKeyValueStringPairs(entityString);
+                    if (values != null) {
+                        suggestionsBox.loadData(values);
+                    }
+                }
+            });
+            HttpService.HttpServiceAsync.instance().doGet(getNotoficationGroupDropDownUrl(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+                @Override
+                public void onResponse(String entityString) {
+                    logger.info(entityString);
+                    Map<String, String> values = JSONUtils.convertKeyValueStringPairs(entityString);
+                    if (values != null) {
+                        suggestionsBox.loadData(values);
+                    }
+                }
+            });
+        }
+    };
 
     public CreateMessagePanel(CreateComposite.CreateCompositeType type) {
         super(type);
@@ -51,28 +66,12 @@ public class CreateMessagePanel extends CreateComposite implements KeyPressHandl
         JSONObject msg = new JSONObject();
         assignEntityValueFromField("subject", msg);
         assignEntityValueFromField("message", msg);
-        JSONArray tos = populateTos();
+        JSONArray tos = employeesSB.getValues();
         if (tos.size() > 0) {
-            msg.put("tos", populateTos());
+            msg.put("tos", tos);
         }
         logger.info(msg.toString());
         return msg;
-    }
-
-    protected JSONArray populateTos() {
-        JSONArray array = new JSONArray();
-        String tos = tosTB.getValue();
-        int i = 0;
-        for (String toStr : splitString(tos)) {
-            if (!toStr.trim().isEmpty()) {
-                JSONObject to = new JSONObject();
-                to.put("id", new JSONString(toStr.trim()));
-                to.put("value", new JSONString(toStr.trim()));
-                array.set(i, to);
-                i++;
-            }
-        }
-        return array;
     }
 
     public static Iterable<String> splitString(String str) {
@@ -109,25 +108,18 @@ public class CreateMessagePanel extends CreateComposite implements KeyPressHandl
 
     @Override
     protected void addListeners() {
-        addTo.addClickHandler(this);
     }
 
     @Override
     protected void configure() {
         setButtonText("Send Message");
-        initTosSuggesBox();
     }
 
     @Override
     protected void addWidgets() {
-        addField("search", false, true, DataType.SUGGEST_FIELD);
-        entityFieldsPanel.add(addTo);
-        addField("tos", false, true, DataType.TEXT_AREA_FIELD);
+        entityFieldsPanel.add(employeesSB);
         addField("subject", false, true, DataType.STRING_FIELD);
         addField("message", false, false, DataType.RICH_TEXT_AREA);
-        suggestionsBox = (SuggestBox) fields.get("search");
-        tosTB = (TextAreaField) fields.get("tos");
-        suggestionsBox.getSuggestBox().addKeyPressHandler(this);
     }
 
     @Override
@@ -139,62 +131,11 @@ public class CreateMessagePanel extends CreateComposite implements KeyPressHandl
         return OfficeWelcome.constants.root_url() + "message";
     }
 
-    protected void initTosSuggesBox() {
-        HttpService.HttpServiceAsync.instance().doGet(getEmployeeIdsDropDownUrl(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String entityString) {
-                logger.info(entityString);
-                Map<String, String> values = JSONUtils.convertKeyValueStringPairs(entityString);
-                if (values != null) {
-                    suggestionsBox.loadData(values);
-                }
-            }
-        });
-        HttpService.HttpServiceAsync.instance().doGet(getNotoficationGroupDropDownUrl(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String entityString) {
-                logger.info(entityString);
-                Map<Integer, String> values = JSONUtils.convertKeyValuePairs(entityString);
-                if (values != null) {
-                    suggestionsBox.loadData(values.values());
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onClick(ClickEvent event) {
-        if (event.getSource().equals(addTo)) {
-            addTo();
-        } else {
-            super.onClick(event);
-        }
-    }
-
-    protected void addTo() {
-        if (suggestionsBox.getValue() != null && !suggestionsBox.getValue().trim().isEmpty()) {
-            tosTB.setValue(tosTB.getValue() + " " + suggestionsBox.getKey());
-            suggestionsBox.setValue("");
-        }
-    }
-
     protected String getEmployeeIdsDropDownUrl() {
         return OfficeWelcome.constants.root_url() + "employee/dropdown/0/1000?column=employeeId&column=firstName&column=lastName";
     }
 
     protected String getNotoficationGroupDropDownUrl() {
         return OfficeWelcome.constants.root_url() + "notification/dropdown/0/1000?column=id&column=name";
-    }
-//TODO move to gwt KeyBoardUtils
-    @Override
-    public void onKeyPress(KeyPressEvent event) {
-        int keyCode = event.getUnicodeCharCode();
-        if (keyCode == 0) {
-            // Probably Firefox
-            keyCode = event.getNativeEvent().getKeyCode();
-        }
-        if (keyCode == KeyCodes.KEY_ENTER) {
-            addTo();
-        }
     }
 }
