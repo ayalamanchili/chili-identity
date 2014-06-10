7750ea470940cb95fdacdfbfbce50dac365fe343
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-06-10 13:57:25 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@4448 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/advancerequisition/AdvanceRequisitionSidePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/advancerequisition/AdvanceRequisitionSidePanel.java
@@ -46,13 +46,14 @@ public class AdvanceRequisitionSidePanel extends ALComposite implements ClickHan
 
     @Override
     protected void configure() {
-
     }
 
     @Override
     protected void addWidgets() {
         advanceRequisitionSidePanel.add(createAdvanceRequisitionLink);
-        advanceRequisitionSidePanel.add(Utils.getLineSeperatorTag("Search"));
+        if (Auth.hasAnyOfRoles(ROLE.ROLE_ADMIN, ROLE.ROLE_ACCOUNTS_PAYABLE, ROLE.ROLE_ACCOUNTS_PAYABLE)) {
+            advanceRequisitionSidePanel.add(Utils.getLineSeperatorTag("Search"));
+        }
         if (Auth.hasAnyOfRoles(ROLE.ROLE_ADMIN, ROLE.ROLE_ACCOUNTS_PAYABLE, ROLE.ROLE_ACCOUNTS_PAYABLE)) {
             advanceRequisitionSidePanel.add(empWidget);
             advanceRequisitionSidePanel.add(viewB);
