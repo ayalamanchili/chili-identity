35d3831cf962bea82b8476e1f774f4ff5fa69132
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-06-10 14:21:19 +0000

    added delete method to advance form
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4449 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/expense/AdvanceRequisitionService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/expense/AdvanceRequisitionService.java
@@ -9,13 +9,17 @@
 package info.yalamanchili.office.expense;
 
 import info.chili.spring.SpringContext;
-import info.yalamanchili.office.Time.CorporateTimeService;
 import info.yalamanchili.office.bpm.OfficeBPMService;
+import info.yalamanchili.office.bpm.OfficeBPMTaskService;
+import info.yalamanchili.office.bpm.types.Task;
+import info.yalamanchili.office.dao.expense.AdvanceRequisitionDao;
 import info.yalamanchili.office.dao.security.SecurityService;
 import info.yalamanchili.office.entity.expense.AdvanceRequisition;
 import info.yalamanchili.office.entity.profile.Employee;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -27,12 +31,34 @@ import org.springframework.stereotype.Component;
 @Scope("request")
 public class AdvanceRequisitionService {
 
+    @Autowired
+    protected AdvanceRequisitionDao advanceRequisitionDao;
+
     public void submitAdvanceRequisition(AdvanceRequisition entity) {
         Map<String, Object> vars = new HashMap<String, Object>();
         vars.put("entity", entity);
         Employee emp = SecurityService.instance().getCurrentUser();
         vars.put("currentEmployee", emp);
         OfficeBPMService.instance().startProcess("advance_requisition_process", vars);
+    }
+
+    protected Task getTaskForTicket(AdvanceRequisition advanceRequisition) {
+        OfficeBPMTaskService taskService = OfficeBPMTaskService.instance();
+        List<Task> tasks = taskService.getTasksForProcessId(advanceRequisition.getBpmProcessId());
+        if (tasks.size() > 0) {
+            return tasks.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public void delete(Long id) {
+        AdvanceRequisition ticket = advanceRequisitionDao.findById(id);
+        Task task = getTaskForTicket(ticket);
+        if (task != null) {
+            OfficeBPMTaskService.instance().deleteTask(task.getId());
+        }
+        advanceRequisitionDao.delete(id);
     }
 
     public static AdvanceRequisitionService instance() {
