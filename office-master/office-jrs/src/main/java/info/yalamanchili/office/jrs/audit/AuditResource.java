03a65894f1410e4440792c8c7eda2e2d15a96556
Phani Yalamanchili <phani.y@sstech.us>
2014-06-13 19:45:19 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@4465 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/audit/AuditResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/audit/AuditResource.java
@@ -19,6 +19,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,13 +37,14 @@ public class AuditResource {
 
     @GET
     @Path("/resent_loginactivity/{start}/{limit}")
+    @PreAuthorize("hasRole('ROLE_CORPORATE_EMPLOYEE')")
     public LoginActivityTable getRecentLoginActivity(@PathParam("start") int start, @PathParam("limit") int limit) {
         return OfficeAuditService.instance().getLoginActivity(start, limit);
     }
 
     @GET
     @Path("/changes/{entityClass}/{id}")
-    public EntityAuditDataTbl getChangesForEntity(@PathParam("entityClass") String entityClass, @PathParam("id") Long id,@QueryParam("ingoreField") List<String> ingoreFields) {
-        return AuditService.instance().getRecentChanges(entityClass, id,ingoreFields);
+    public EntityAuditDataTbl getChangesForEntity(@PathParam("entityClass") String entityClass, @PathParam("id") Long id, @QueryParam("ingoreField") List<String> ingoreFields) {
+        return AuditService.instance().getRecentChanges(entityClass, id, ingoreFields);
     }
 }
