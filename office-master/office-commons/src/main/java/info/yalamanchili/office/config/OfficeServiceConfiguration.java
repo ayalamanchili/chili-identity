e7c1f875d1f8110a7ddc2762d67f51a8c5eca57b
Phani Yalamanchili <phani.y@sstech.us>
2014-08-05 15:05:01 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@4618 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-commons/src/main/java/info/yalamanchili/office/config/OfficeServiceConfiguration.java
+++ office-master/office-commons/src/main/java/info/yalamanchili/office/config/OfficeServiceConfiguration.java
@@ -81,7 +81,7 @@ public class OfficeServiceConfiguration {
     /**
      * list of white listed emails addresses that can receive emails
      */
-    protected String filteredEmailsList = "yphanikumar@gmail.com,prasanthi.p@sstech.mobi,anu@yalamanchili.info,vijay.v@sstech.us";
+    protected String filteredEmailsList = "yphanikumar@gmail.com,prasanthi.p@sstech.mobi,anu@yalamanchili.info";
     /**
      * file path used by data loader tool
      */
