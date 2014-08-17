543d087949889548841c9d696532e5ee2779bff6
Phani Yalamanchili <phani.y@sstech.us>
2014-08-17 12:41:28 +0000

    combine sick and personal to PTO.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4636 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/time/TimeSheetCategory.java
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/time/TimeSheetCategory.java
@@ -22,9 +22,11 @@ public enum TimeSheetCategory {
     Vacation_CarryForward,
     Personal_Earned,
     Personal_Spent,
-    Unpaid,
     Sick_Earned,
     Sick_Spent,
+    PTO_Earned,
+    PTO_Spent,
+    Unpaid,
     JuryDuty,
     Bereavement,
     Maternity,
@@ -33,16 +35,14 @@ public enum TimeSheetCategory {
     public static List<TimeSheetCategory> getLeaveSpentCheckedCategories() {
         List<TimeSheetCategory> res = new ArrayList<TimeSheetCategory>();
         res.add(Vacation_Spent);
-        res.add(Sick_Spent);
-        res.add(Personal_Spent);
+        res.add(PTO_Spent);
         return res;
     }
 
     public static List<TimeSheetCategory> getLeaveSpentCategories() {
         List<TimeSheetCategory> res = new ArrayList<TimeSheetCategory>();
         res.add(Vacation_Spent);
-        res.add(Sick_Spent);
-        res.add(Personal_Spent);
+        res.add(PTO_Spent);
         res.add(Unpaid);
         res.add(JuryDuty);
         return res;
@@ -51,8 +51,7 @@ public enum TimeSheetCategory {
     public static List<TimeSheetCategory> getEarnedCategories() {
         List<TimeSheetCategory> res = new ArrayList<TimeSheetCategory>();
         res.add(Vacation_Earned);
-        res.add(Sick_Earned);
-        res.add(Personal_Earned);
+        res.add(PTO_Earned);
         return res;
     }
 }
