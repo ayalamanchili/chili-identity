7bb3f167071f76d91ecaf0dbf61ca9a0446f9388
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-09-16 15:13:14 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@4720 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/employee/PerformanceEvaluationService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/employee/PerformanceEvaluationService.java
@@ -8,7 +8,10 @@
  */
 package info.yalamanchili.office.employee;
 
+import info.chili.commons.DateUtils;
+import info.chili.commons.PDFUtils;
 import info.chili.spring.SpringContext;
+import info.yalamanchili.office.config.OfficeSecurityConfiguration;
 import info.yalamanchili.office.dao.employee.PerformanceEvaluationDao;
 import info.yalamanchili.office.dao.ext.CommentDao;
 import info.yalamanchili.office.dao.ext.QuestionDao;
@@ -21,8 +24,11 @@ import info.yalamanchili.office.entity.ext.Comment;
 import info.yalamanchili.office.entity.ext.Question;
 import info.yalamanchili.office.entity.ext.QuestionCategory;
 import info.yalamanchili.office.entity.ext.QuestionContext;
+import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.ext.QuestionService;
+import info.yalamanchili.office.template.TemplateService;
 import java.util.List;
+import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -34,10 +40,10 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("request")
 public class PerformanceEvaluationService {
-    
+
     @Autowired
     protected PerformanceEvaluationDao performanceEvaluationDao;
-    
+
     public void updatePerformanceEvaluation(PerformanceEvaluationSaveDto dto) {
         performanceEvaluationDao.save(dto.getPerformanceEvaluation());
         for (QuestionComment qc : dto.getComments()) {
@@ -48,18 +54,18 @@ public class PerformanceEvaluationService {
             CommentDao.instance().save(cmt);
         }
     }
-    
+
     public void createPerformanceEvaluation(PerformanceEvaluationSaveDto dto) {
         PerformanceEvaluation entity = dto.getPerformanceEvaluation();
         entity.setEmployee(EmployeeDao.instance().findById(dto.getEmployeeId()));
         entity = performanceEvaluationDao.getEntityManager().merge(entity);
         createQuestionComments(entity, dto.getComments());
     }
-    
+
     public List<QuestionDto> getQuestions(QuestionCategory category) {
         return QuestionService.instance().getQuestions(category, QuestionContext.PERFORMANCE_EVALUATION_MANGER, 0, 100);
     }
-    
+
     public void createQuestionComments(PerformanceEvaluation perfEval, List<QuestionComment> comments) {
         for (QuestionComment comment : comments) {
             Question qes = QuestionDao.instance().findById(comment.getId());
@@ -67,32 +73,33 @@ public class PerformanceEvaluationService {
             CommentDao.instance().addComment(comment.getComment(), comment.getRating(), perfEval, qes);
         }
     }
-    
+
     public List<QuestionComment> getQuestionComments(Long id, QuestionCategory category, QuestionContext context) {
         return QuestionService.instance().getQuestionComments(id, category, context);
     }
     //TODO move to commons
 
-//    public Response getReport(Long id) {
-//        PerformanceEvaluation evaluation = performanceEvaluationDao.findById(id);
-//        Employee emp = null;
+    public Response getReport(Long id) {
+        PerformanceEvaluation evaluation = performanceEvaluationDao.findById(id);
+        Employee emp = null;
 //        if (evaluation.getApprovedBy() != null) {
 //            emp = EmployeeDao.instance().findEmployeWithEmpId(evaluation.getApprovedBy());
 //        }
-//        String report = TemplateService.instance().process("performance-evaluation.xhtml", evaluation);
-//        byte[] pdf = null;
-//        if (emp == null) {
-//            pdf = PDFUtils.convertToPDF(report);
-//        } else {
-//            OfficeSecurityConfiguration securityConfiguration = OfficeSecurityConfiguration.instance();
-//            pdf = PDFUtils.convertToSignedPDF(report, (emp.getBranch() != null) ? emp.getBranch().name() : null, DateUtils.dateToCalendar(evaluation.getApprovedDate()), securityConfiguration.getKeyStoreName(), emp.getEmployeeId(), emp.getEmployeeId(), securityConfiguration.getKeyStorePassword());
-//        }
-//        return Response
-//                .ok(pdf)
-//                .header("content-disposition", "filename = performance-evaluation.pdf")
-//                .header("Content-Length", pdf.length)
-//                .build();
-//    }
+        String report = TemplateService.instance().process("performance-evaluation.xhtml", evaluation);
+        byte[] pdf = null;
+        if (emp == null) {
+            pdf = PDFUtils.convertToPDF(report);
+        } else {
+            OfficeSecurityConfiguration securityConfiguration = OfficeSecurityConfiguration.instance();
+            pdf = PDFUtils.convertToSignedPDF(report, (emp.getBranch() != null) ? emp.getBranch().name() : null, DateUtils.dateToCalendar(evaluation.getEvaluationDate()), securityConfiguration.getKeyStoreName(), emp.getEmployeeId(), emp.getEmployeeId(), securityConfiguration.getKeyStorePassword());
+        }
+        return Response
+                .ok(pdf)
+                .header("content-disposition", "filename = performance-evaluation.pdf")
+                .header("Content-Length", pdf.length)
+                .build();
+    }
+
     public static PerformanceEvaluationService instance() {
         return SpringContext.getBean(PerformanceEvaluationService.class);
     }
--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/employee/PerformanceEvaluationResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/employee/PerformanceEvaluationResource.java
@@ -24,7 +24,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
@@ -85,6 +87,13 @@ public class PerformanceEvaluationResource extends CRUDResource<PerformanceEvalu
         tableObj.setEntities(performanceEvaluationDao.getPerformanceEvaluationsForEmp(emp));
         tableObj.setSize(getDao().size());
         return tableObj;
+    }
+
+    @GET
+    @Path("/report")
+    @Produces({"application/pdf"})
+    public Response getReport(@QueryParam("id") Long id) {
+        return PerformanceEvaluationService.instance().getReport(id);
     }
 
     @XmlRootElement
