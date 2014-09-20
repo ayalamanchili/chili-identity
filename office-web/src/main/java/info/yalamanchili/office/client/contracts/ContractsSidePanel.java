b550e5a7cb6b2690641f8c16f6b4cd33d684a8fa
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-09-20 12:17:39 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@4738 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/contracts/ContractsSidePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/contracts/ContractsSidePanel.java
@@ -9,15 +9,15 @@ package info.yalamanchili.office.client.contracts;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.composite.ALComposite;
-import info.chili.gwt.config.ChiliClientConfig;
 import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.widgets.ClickableLink;
+import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
 import java.util.logging.Logger;
 
@@ -26,26 +26,28 @@ import java.util.logging.Logger;
  * @author anuyalamanchili
  */
 public class ContractsSidePanel extends ALComposite implements ClickHandler {
-    
+
     private static Logger logger = Logger.getLogger(ContractsSidePanel.class.getName());
     protected FlowPanel panel = new FlowPanel();
     protected Button generateRepB = new Button("Generate");
     protected Label formatL = new Label("Format");
     protected ListBox formatLB = new ListBox();
-    
+    ClickableLink profileBasicReportL = new ClickableLink("Client Infomatin  Report");
+
     public ContractsSidePanel() {
         init(panel);
     }
-    
+
     @Override
     protected void addListeners() {
         generateRepB.addClickHandler(this);
+        profileBasicReportL.addClickHandler(this);
     }
-    
+
     @Override
     protected void configure() {
     }
-    
+
     @Override
     protected void addWidgets() {
         panel.add(formatL);
@@ -54,29 +56,37 @@ public class ContractsSidePanel extends ALComposite implements ClickHandler {
         formatLB.addItem("XML", "xml");
         panel.add(formatLB);
         panel.add(generateRepB);
+        panel.add(profileBasicReportL);
         panel.add(new SearchContractsPanel());
+
     }
-    
+
     @Override
     public void onClick(ClickEvent event) {
-        HttpService.HttpServiceAsync.instance().doGet(getReportURL(), OfficeWelcome.instance().getHeaders(), true,
-                new AsyncCallback<String>() {
-                    @Override
-                    public void onFailure(Throwable arg0) {
-                        Window.alert(arg0.getLocalizedMessage());
-                    }
-                    
-                    @Override
-                    public void onSuccess(String resp) {
-                        Window.open(ChiliClientConfig.instance().getFileDownloadUrl() + resp, "_blank", "");
-                    }
-                });
+
+        if (event.getSource().equals(profileBasicReportL)) {
+            generateBasicInfoReport();
+        }
     }
-    
+
+    protected void generateBasicInfoReport() {
+        HttpService.HttpServiceAsync.instance().doGet(getBasicInfoReportUrl(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String result) {
+                new ResponseStatusWidget().show("Report will be emailed to your primary email");
+            }
+        });
+    }
+
+    protected String getBasicInfoReportUrl() {
+        return OfficeWelcome.constants.root_url() + "profile-reports/employee-basic-info-report";
+    }
+
     protected String getReportFormat() {
         return formatLB.getValue(formatLB.getSelectedIndex());
     }
-    
+
     protected String getReportURL() {
         return OfficeWelcome.constants.root_url() + "contract/report" + "?format=" + getReportFormat();
     }
