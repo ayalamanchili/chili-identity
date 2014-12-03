8bff452476ea63246b05bba9eb929ef12fa18b12
Purna Kodali <purnachaitanya.k@sstech.us>
2014-12-03 16:06:28 +0000

    Creating enum class
    git-svn-id: http://tools.sstech.us/svn/office/trunk@4998 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/expense/expenserpt/ExpenseReportStatus.java
@@ -0,0 +1,19 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.expense.expenserpt;
+
+/**
+ *
+ * @author chaitanya.k
+ */
+public enum ExpenseReportStatus {
+
+    Pending_Approval,
+    Pending_Dispatch_Approval,
+    Complete;
+}
