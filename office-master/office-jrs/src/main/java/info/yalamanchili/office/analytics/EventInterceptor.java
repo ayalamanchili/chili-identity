f3fb4d9b3a0b8ef842809caa7c11bdeba7b36110
Phani Yalamanchili <phani.y@sstech.us>
2014-12-17 04:28:16 +0000

    analytics first cut.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5028 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/analytics/EventInterceptor.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/analytics/EventInterceptor.java
@@ -8,46 +8,92 @@
  */
 package info.yalamanchili.office.analytics;
 
+import info.chili.analytics.model.Event;
 import info.yalamanchili.office.config.OfficeServiceConfiguration;
+import info.yalamanchili.office.dao.security.OfficeSecurityService;
+import java.util.Date;
 import javax.persistence.Entity;
+import javax.ws.rs.core.Response;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author anuyalamanchili will post(async) all activity events to analytics
  * database
  */
-//@Aspect
-//@Component
+@Aspect
+@Component
+@Transactional
 public class EventInterceptor {
 
     @Autowired
     public OfficeServiceConfiguration officeServiceConfiguration;
 
-    @Before("execution(* info.yalamanchili.office.jrs..*.*(..))")
-    public void before(JoinPoint joinPoint) {
+    @Autowired
+    protected OfficeSecurityService officeSecurityService;
 
-    }
+    @Autowired
+    protected MongoOperations mongoTemplate;
 
-    @AfterReturning(pointcut = "execution(* info.yalamanchili.office.jrs..*.*(..))", returning = "result")
-    public void afterReturning(JoinPoint joinPoint, Object result) {
-
-    }
-
-    @Around("execution(* info.yalamanchili.office..*.*(..))")
+    @Around("execution(* info.yalamanchili.office.jrs..*.*(..))")
     public Object profile(ProceedingJoinPoint pjp) throws Throwable {
-       
+        Event event = new Event();
+        event.setEvenTimeStamp(new Date());
+        event.setUser(officeSecurityService.getCurrentUserName());
+        event.setName(pjp.getSignature().toLongString());
+        event.setInput(describeInput(pjp));
         Object output = pjp.proceed();
-
+        event.setOutput(describeOutput(pjp, output));
+        sendEvent(event);
         return output;
+    }
+
+    @Async
+    protected void sendEvent(Event event) {
+        mongoTemplate.save(event);
+    }
+
+    protected String describeOutput(ProceedingJoinPoint pjp, Object result) {
+        StringBuilder sb = new StringBuilder();
+        if (logResultType(result) && result != null) {
+            if (isEntity(result)) {
+                sb.append(result.toString());
+            } else {
+                sb.append(ReflectionToStringBuilder.toString(result));
+            }
+        }
+        return sb.toString();
+    }
+
+    protected String describeInput(ProceedingJoinPoint pjp) {
+        StringBuilder sb = new StringBuilder();
+        for (Object input : pjp.getArgs()) {
+            if (input != null) {
+                if (isEntity(input)) {
+                    sb.append(input.toString());
+                } else {
+                    sb.append(ReflectionToStringBuilder.toString(input));
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    protected boolean logResultType(Object res) {
+        if (res instanceof Response) {
+            return false;
+        } else if (res instanceof byte[]) {
+            return false;
+        }
+        return true;
     }
 
     protected boolean isEntity(Object obj) {
