ba8a9064c3320e8c83e05c984511da17b86166c9
Prasanthi Ponnaganti <prasanthi.p@sstech.us>
2014-12-29 10:21:00 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@5057 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/time/consultant/ConsultantTimeSummaryPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/time/consultant/ConsultantTimeSummaryPanel.java
@@ -59,7 +59,7 @@ public class ConsultantTimeSummaryPanel extends ReadComposite {
         assignFieldValueFromEntity("employee", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("availablePTOHours", entity, DataType.FLOAT_FIELD);
-        assignFieldValueFromEntity("availableVacationHours", entity, DataType.FLOAT_FIELD);
+//        assignFieldValueFromEntity("availableVacationHours", entity, DataType.FLOAT_FIELD);
         assignFieldValueFromEntity("usedUnpaidHours", entity, DataType.FLOAT_FIELD);
         assignFieldValueFromEntity("totalAccumulatedHours", entity, DataType.FLOAT_FIELD);
         assignFieldValueFromEntity("totalUsedHours", entity, DataType.FLOAT_FIELD);
@@ -79,7 +79,7 @@ public class ConsultantTimeSummaryPanel extends ReadComposite {
         addField("employee", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("startDate", true, false, DataType.DATE_FIELD, Alignment.HORIZONTAL);
         addField("availablePTOHours", true, false, DataType.FLOAT_FIELD, Alignment.HORIZONTAL);
-        addField("availableVacationHours", true, false, DataType.FLOAT_FIELD, Alignment.HORIZONTAL);
+//        addField("availableVacationHours", true, false, DataType.FLOAT_FIELD, Alignment.HORIZONTAL);
         addField("usedUnpaidHours", true, false, DataType.FLOAT_FIELD, Alignment.HORIZONTAL);
         addField("totalAccumulatedHours", true, false, DataType.FLOAT_FIELD, Alignment.HORIZONTAL);
         addField("totalUsedHours", true, false, DataType.FLOAT_FIELD, Alignment.HORIZONTAL);
