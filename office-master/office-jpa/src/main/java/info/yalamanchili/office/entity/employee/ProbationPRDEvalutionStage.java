2f2ae12c71be2dd35cd3211791d313da7e1e0848
Benerji Veeramachaneni <benerji.v@sstech.us>
2015-02-16 09:28:15 +0000

    created ProbationPRDEvalution entity in jpa
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5161 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/employee/ProbationPRDEvalutionStage.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.employee;
+
+/**
+ *
+ * @author benerji.v
+ */
+public enum ProbationPRDEvalutionStage {
+    Manager_Review,
+    Employee_Acceptance,
+    HR_Approval,
+    Complete,
+}
