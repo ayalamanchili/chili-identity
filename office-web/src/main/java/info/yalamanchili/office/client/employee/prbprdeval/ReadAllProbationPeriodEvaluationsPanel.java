46ff68d83b90ca1271e14dd80db3ca150c5e352d
Purna Kodali <purnachaitanya.k@sstech.us>
2015-02-18 11:57:05 +0000

    ReadAllProbationPeriodEvaluationsPanel
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5174 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/employee/prbprdeval/ReadAllProbationPeriodEvaluationsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/employee/prbprdeval/ReadAllProbationPeriodEvaluationsPanel.java
@@ -13,13 +13,11 @@ import info.chili.gwt.crud.TableRowOptionsWidget;
 import info.chili.gwt.rpc.HttpService;
 
 import info.chili.gwt.utils.JSONUtils;
-import info.yalamanchili.office.client.Auth;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+
 
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
-import info.yalamanchili.office.client.employee.prefeval.PeformanceSelfEvaluationPanel;
-import info.yalamanchili.office.client.employee.prefeval.PerformanceEvaluationWizard;
-import info.yalamanchili.office.client.profile.employee.TreeEmployeePanel;
 
 import java.util.logging.Logger;
 
@@ -49,10 +47,27 @@ public class ReadAllProbationPeriodEvaluationsPanel extends CRUDReadAllComposite
 
     @Override
     public void deleteClicked(String entityId) {
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String arg0) {
+                postDeleteSuccess();
+            }
+        });
     }
 
     @Override
     public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully Deleted Probation Period Evaluations Information");
+        if (TabPanel.instance().myOfficePanel.isVisible()) {
+            TabPanel.instance().myOfficePanel.entityPanel.clear();
+            TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllProbationPeriodEvaluationsPanel(parentId));
+        }
+        if (TabPanel.instance().homePanel.isVisible()) {
+            TabPanel.instance().homePanel.entityPanel.clear();
+            TabPanel.instance().homePanel.entityPanel.add(new ReadAllProbationPeriodEvaluationsPanel());
+        }
+
     }
 
     @Override
@@ -63,11 +78,11 @@ public class ReadAllProbationPeriodEvaluationsPanel extends CRUDReadAllComposite
     public void preFetchTable(int start) {
         HttpService.HttpServiceAsync.instance().doGet(getReadAllEvaluationsUrl(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String result) {
-                        postFetchTable(result);
-                    }
-                });
+            @Override
+            public void onResponse(String result) {
+                postFetchTable(result);
+            }
+        });
     }
 
     @Override
@@ -91,7 +106,7 @@ public class ReadAllProbationPeriodEvaluationsPanel extends CRUDReadAllComposite
     }
 
     private String getDeleteURL(String entityId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return OfficeWelcome.instance().constants.root_url() + "probation-period-evaluation/delete/" + entityId;
     }
 
     private String getReadAllEvaluationsUrl(Integer start, String tableSize) {
@@ -119,11 +134,10 @@ public class ReadAllProbationPeriodEvaluationsPanel extends CRUDReadAllComposite
     protected void createButtonClicked() {
         HttpService.HttpServiceAsync.instance().doGet(getInitiateEvaluationUrl(), OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String result) {
-                        
-                    }
-                });
+            @Override
+            public void onResponse(String result) {
+            }
+        });
     }
 
     public String getInitiateEvaluationUrl() {
