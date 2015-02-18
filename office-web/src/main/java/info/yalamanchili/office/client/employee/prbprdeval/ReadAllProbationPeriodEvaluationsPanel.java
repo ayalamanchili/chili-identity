305ea882af4941dcffb65a7e54cad5c995347b84
Phani Yalamanchili <phani.y@sstech.us>
2015-02-18 12:51:45 +0000

    SSTO-2070
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5175 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/employee/prbprdeval/ReadAllProbationPeriodEvaluationsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/employee/prbprdeval/ReadAllProbationPeriodEvaluationsPanel.java
@@ -13,11 +13,12 @@ import info.chili.gwt.crud.TableRowOptionsWidget;
 import info.chili.gwt.rpc.HttpService;
 
 import info.chili.gwt.utils.JSONUtils;
-import info.chili.gwt.widgets.ResponseStatusWidget;
 
+import info.chili.gwt.widgets.ResponseStatusWidget;
 
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.profile.employee.TreeEmployeePanel;
 
 import java.util.logging.Logger;
 
@@ -49,11 +50,11 @@ public class ReadAllProbationPeriodEvaluationsPanel extends CRUDReadAllComposite
     public void deleteClicked(String entityId) {
         HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String arg0) {
-                postDeleteSuccess();
-            }
-        });
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
     }
 
     @Override
@@ -78,11 +79,11 @@ public class ReadAllProbationPeriodEvaluationsPanel extends CRUDReadAllComposite
     public void preFetchTable(int start) {
         HttpService.HttpServiceAsync.instance().doGet(getReadAllEvaluationsUrl(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String result) {
-                postFetchTable(result);
-            }
-        });
+                    @Override
+                    public void onResponse(String result) {
+                        postFetchTable(result);
+                    }
+                });
     }
 
     @Override
@@ -134,13 +135,14 @@ public class ReadAllProbationPeriodEvaluationsPanel extends CRUDReadAllComposite
     protected void createButtonClicked() {
         HttpService.HttpServiceAsync.instance().doGet(getInitiateEvaluationUrl(), OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String result) {
-            }
-        });
+                    @Override
+                    public void onResponse(String result) {
+                        new ResponseStatusWidget().show("Review Initiated and manager is notified.");
+                    }
+                });
     }
 
     public String getInitiateEvaluationUrl() {
-        return OfficeWelcome.constants.root_url() + "probation-period-evaluation/initiate-review";
+        return OfficeWelcome.constants.root_url() + "probation-period-evaluation/initiate-review/" + TreeEmployeePanel.instance().getEntityId();
     }
 }
