04ef5fb2a14a162fedaa30ebcbf84d7ff8d580bb
Phani Yalamanchili <phani.y@sstech.us>
2015-02-27 05:02:02 +0000

    gce test profile
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5215 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/config/OfficeClientConfigGCETest.java
@@ -0,0 +1,45 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.config;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.TimeZone;
+import com.google.gwt.i18n.client.constants.TimeZoneConstants;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+public class OfficeClientConfigGCETest implements OfficeClientConfig {
+
+    TimeZoneConstants tzConstants;
+
+    public OfficeClientConfigGCETest() {
+        tzConstants = (TimeZoneConstants) GWT.create(TimeZoneConstants.class);
+    }
+
+    @Override
+    public String getFileUploadUrl() {
+        return "http://71.100.103.92/office-web/office/rpc/fileService";
+    }
+
+    @Override
+    public String getFileDownloadUrl() {
+        return "http://71.100.103.92/office-web/office/rpc/fileService?path=";
+    }
+
+    @Override
+    public String getPortalDocumentationSiteUrl() {
+          return "https://apps.sstech.us/site/office/";
+    }
+
+    @Override
+    public TimeZone getTimeZone() {
+        return TimeZone.createTimeZone(tzConstants.asiaCalcutta());
+    }
+}
