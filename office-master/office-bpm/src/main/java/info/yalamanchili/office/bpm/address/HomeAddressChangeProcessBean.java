c0491a9f76e3ab15089b898357d2db805729ebbd
Phani Yalamanchili <phani.y@sstech.us>
2015-03-04 12:36:09 +0000

    SSTO-1990 address change process -1
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5233 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/address/HomeAddressChangeProcessBean.java
@@ -0,0 +1,32 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.bpm.address;
+
+import info.yalamanchili.office.entity.profile.Address;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+@Component
+@Scope("prototype")
+@Transactional
+public class HomeAddressChangeProcessBean {
+
+    public void sendAddressChangeRequestSubmittedEmail(Address address) {
+
+    }
+
+    public void sendAddressChangeRequestCompletedEmail(Address address) {
+
+    }
+}
