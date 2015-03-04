fd791bc790c274c595d12dfd245873bf1dfe183b
Benerji Veeramachaneni <benerji.v@sstech.us>
2015-03-04 14:06:08 +0000

    changed address completed email
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5234 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/address/HomeAddressChangeProcessBean.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/address/HomeAddressChangeProcessBean.java
@@ -8,7 +8,12 @@
  */
 package info.yalamanchili.office.bpm.address;
 
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.yalamanchili.office.email.Email;
 import info.yalamanchili.office.entity.profile.Address;
+import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.jms.MessagingService;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,10 +28,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class HomeAddressChangeProcessBean {
 
     public void sendAddressChangeRequestSubmittedEmail(Address address) {
-
     }
 
-    public void sendAddressChangeRequestCompletedEmail(Address address) {
+    public void sendAddressChangeRequestCompletedEmail(Address address, Employee employee) {
+        MessagingService messagingService = (MessagingService) SpringContext.getBean("messagingService");
+        Email email = new Email();
+        email.addTo(EmployeeDao.instance().getPrimaryEmail(employee));
+        email.setSubject("Address pdated Review");
+        email.setBody("Your Address updated review as been completed");
+        messagingService.sendEmail(email);
 
     }
 }
