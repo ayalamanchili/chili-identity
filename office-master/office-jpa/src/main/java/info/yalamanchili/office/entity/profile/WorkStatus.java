a686c2852271e3ff51a6c7fe2a956f845ce95a03
Purna Kodali <purnachaitanya.k@sstech.us>
2015-03-05 08:34:46 +0000

    added enum class for WorkStatus.java
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5238 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/profile/WorkStatus.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.profile;
+
+/**
+ *
+ * @author chaitanya.k
+ */
+public enum WorkStatus {
+    
+    /**
+     * @generated
+     */
+    OPT,
+    /**
+     * @generated
+     */
+    H1B,
+    /**
+     * @generated
+     */
+    GREENCARD,
+    /**
+     * @generated
+     */
+    CITIZEN
+  }
