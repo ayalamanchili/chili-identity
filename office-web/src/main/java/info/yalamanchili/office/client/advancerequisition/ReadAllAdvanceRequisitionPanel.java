8387720fa02479093d1d83f04c25470af2eef157
Phani Yalamanchili <phani.y@sstech.us>
2015-03-24 02:12:49 +0000

    SSTO-2152 advance requisition enhanesments
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5312 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/advancerequisition/ReadAllAdvanceRequisitionPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/advancerequisition/ReadAllAdvanceRequisitionPanel.java
@@ -17,6 +17,7 @@ import info.chili.gwt.crud.TableRowOptionsWidget;
 import info.chili.gwt.date.DateUtils;
 import info.chili.gwt.fields.FileField;
 import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.FormatUtils;
 import info.chili.gwt.utils.JSONUtils;
 import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.Auth;
@@ -59,11 +60,11 @@ public class ReadAllAdvanceRequisitionPanel extends CRUDReadAllComposite {
     public void deleteClicked(String entityId) {
         HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String arg0) {
-                postDeleteSuccess();
-            }
-        });
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
     }
 
     @Override
@@ -85,12 +86,12 @@ public class ReadAllAdvanceRequisitionPanel extends CRUDReadAllComposite {
     public void preFetchTable(int start) {
         HttpService.HttpServiceAsync.instance().doGet(getadvanceURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(),
                 false, new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String result) {
-                logger.info(result);
-                postFetchTable(result);
-            }
-        });
+                    @Override
+                    public void onResponse(String result) {
+                        logger.info(result);
+                        postFetchTable(result);
+                    }
+                });
     }
 
     @Override
@@ -112,7 +113,7 @@ public class ReadAllAdvanceRequisitionPanel extends CRUDReadAllComposite {
             JSONObject emp = (JSONObject) entity.get("employee");
             table.setText(i, 1, JSONUtils.toString(emp, "firstName") + " " + JSONUtils.toString(emp, "lastName"));
             table.setText(i, 2, JSONUtils.toString(entity, "purpose"));
-            table.setText(i, 3, JSONUtils.toString(entity, "amount"));
+            table.setText(i, 3, FormatUtils.formarCurrency(JSONUtils.toString(entity, "amount")));
             table.setText(i, 4, DateUtils.getFormatedDate(JSONUtils.toString(entity, "neededBy"), DateTimeFormat.PredefinedFormat.DATE_MEDIUM));
             table.setText(i, 5, JSONUtils.formatEnumString(entity, "status"));
             FileField reportL = new FileField("Print", ChiliClientConfig.instance().getFileDownloadUrl() + "advancerequisition/report" + "&passthrough=true" + "&id=" + JSONUtils.toString(entity, "id"));
