d8e11aaecd7dc6892a6c40691221205124bc9269
Phani Yalamanchili <phani.y@sstech.us>
2015-04-11 21:28:11 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@5366 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/fault/FaultEventProcess.java
@@ -0,0 +1,40 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.bpm.fault;
+
+import info.chili.exception.FaultEventPayload;
+import info.chili.spring.SpringContext;
+import info.chili.bpm.email.GenericTaskCompleteNotification;
+import info.chili.bpm.email.GenericTaskCreateNotification;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+public class FaultEventProcess implements TaskListener {
+
+    @Override
+    public void notify(DelegateTask task) {
+        if ("create".equals(task.getEventName())) {
+            new GenericTaskCreateNotification().notify(task);
+        }
+        if ("complete".equals(task.getEventName())) {
+            handleFaultEventRetry(task);
+            new GenericTaskCompleteNotification().notify(task);
+        }
+    }
+
+    protected void handleFaultEventRetry(DelegateTask task) {
+        FaultEventPayload payload = (FaultEventPayload) task.getVariable("entity");
+        FaultEventHandler handler = (FaultEventHandler) SpringContext.getBean(payload.getPayloadType() + ".FaultEventHandler");
+    }
+
+}
