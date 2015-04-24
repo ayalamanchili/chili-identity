6e14aabb96f523d7b62ab130d678b4db9e33b51d
Benerji Veeramachaneni <benerji.v@sstech.us>
2015-04-24 06:50:19 +0000

    git-svn-id: http://tools.sstech.us/svn/office/trunk@5407 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/tae/Attendence/UpdateTimeRecordPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/tae/Attendence/UpdateTimeRecordPanel.java
@@ -1,3 +1,6 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -112,10 +115,10 @@ public class UpdateTimeRecordPanel extends UpdateComposite {
 
     @Override
     protected void addWidgets() {
-        addEnumField("status", false, true, TimeRecordStatus.names());
-        addEnumField("category", false, true, TimeRecordCategory.names());
-        addField("startDate", false, true, DataType.DATE_FIELD);
-        addField("endDate", false, true, DataType.DATE_FIELD);
+        addEnumField("status", false, false, TimeRecordStatus.names());
+        addEnumField("category", false, false, TimeRecordCategory.names());
+        addField("startDate", false, false, DataType.DATE_FIELD);
+        addField("endDate", false, false, DataType.DATE_FIELD);
         addField("hours", false, true, DataType.FLOAT_FIELD);
         addField("reception", false, false, DataType.FLOAT_FIELD);
         addField("secondFloor", false, false, DataType.FLOAT_FIELD);
