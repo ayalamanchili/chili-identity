399bd2f4edd314a97c7fd2278898f924091fb244
Sadipan Bhattacharjee <sadipan.b@sstech.us>
2015-05-08 11:09:06 +0000

    Basic maven setup for integration testing using testng and selenium
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5462 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-qa-auto/src/test/java/info/yalamanchili/office/qa/AbstractOfficeTest.java
@@ -0,0 +1,150 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 sadipan.b@sstech.us
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.qa;
+
+import info.chili.commons.PropertyFileLoader;
+import java.util.Properties;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+/**
+ *
+ * @author sadipanB
+ */
+public abstract class AbstractOfficeTest {
+
+    protected static Properties properties = null;
+    protected static Properties uimapper = null;
+    
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+    static String browser;
+    boolean loginPassed;
+    
+    @BeforeSuite (description="load property files")
+    public void loadEnvironmentVariables(){
+        System.out.println("Enter loadEnvironment: " + new Date());        
+        
+        if (properties == null) {
+            properties = PropertyFileLoader.loadProperties("/test.properties");
+            System.out.println(properties.toString());
+        }        
+        
+        if (uimapper == null) {
+            uimapper = PropertyFileLoader.loadProperties("/uimapping.properties");
+        }                        
+        
+        System.out.println("Exit loadEnvironment: " + new Date());         
+    }
+    
+    @BeforeTest(description="Launches the Portal")
+    public void launchURL() throws Exception {        
+        System.out.println("Enter launchURL: " + new Date());
+        
+        browser = properties.getProperty("BROWSER").toLowerCase();
+        switch (browser) {
+            case "firefox": 
+                driver = new FirefoxDriver();                
+                break;                
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("CHROME_DRIVER"));
+                driver = new ChromeDriver();
+                break;                
+            case "internetexplorer":
+                System.setProperty("webdriver.ie.driver", properties.getProperty("IE_DRIVER"));
+                driver = new InternetExplorerDriver();
+                break;                
+            case "safari":
+                break;                
+            default:              
+        }
+        
+        driver.get(properties.getProperty("ROOT_URL"));
+        wait = new WebDriverWait(driver, Long.parseLong(properties.getProperty("WAIT_PERIOD")));
+        
+        System.out.println("Exit launchURL: " + new Date());
+    }
+    
+    public boolean login(String username, String password) {
+       
+        loginPassed = true;
+
+        WebElement usernameTB = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gwt-debug-usernameTb")));
+        usernameTB.clear();
+        usernameTB.sendKeys(username);
+
+        WebElement passwordTB = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gwt-debug-passwordTb")));
+        passwordTB.clear();
+        passwordTB.sendKeys(password);
+
+        WebElement loginB = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gwt-debug-loginB")));
+        loginB.click();
+        
+        try {    
+            waitForElement("gwt-debug-welcomeL");            
+        } catch (Exception e) {
+            System.out.println(" Login exception - " + e.getMessage());
+            loginPassed = false;
+        }
+        
+        return loginPassed;
+    }
+    
+    public boolean logout() {        
+        
+        WebElement logoutB = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gwt-debug-logoutCL")));
+        logoutB.click();
+        
+        //wait until login screen reappears
+        WebElement loginB = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gwt-debug-loginB")));
+        
+        return true;
+    }
+    
+    public void waitForTime(long sec) {
+        driver.manage().timeouts().implicitlyWait(sec, TimeUnit.SECONDS);        
+    }
+    
+    public void waitForElement(String elementId) {
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<WebElement>() {
+            @Override
+            public WebElement apply(WebDriver d) {
+                return d.findElement(By.id(elementId));
+            }
+        });        
+    }
+    
+    public void waitForStaleness(WebElement ele) {
+        try {
+            wait.until(ExpectedConditions.stalenessOf(ele));
+        } catch (Exception e) {
+            System.out.println("Staleness Error: " + e.getMessage());
+        }
+    }
+    
+    @AfterTest(description="Closes the Portal Page")
+    public void endSession(){
+        System.out.println("Enter EndSession: " + new Date());
+        driver.close();
+        driver.quit();
+        System.out.println("Exit EndSession: " + new Date());
+    }   
+    
+}
---
+++ office-master/office-qa-auto/src/test/java/info/yalamanchili/office/qa/LoginTest.java
@@ -0,0 +1,47 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 sadipan.b@sstech.us
+ */
+package info.yalamanchili.office.qa;
+
+import java.util.Date;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+
+public class LoginTest extends AbstractOfficeTest {
+
+
+//    @Test(description="test consultant login",groups="login",dependsOnGroups="StaticContents")
+    public void testUserLogin() {
+        System.out.println("Enter UserLogin: " + new Date());
+        String userId = properties.getProperty("USER_ID");
+        String userPwd = properties.getProperty("USER_PWD");        
+        assertTrue(login(userId, userPwd),"Error:Could not login as user ");
+        System.out.println("Exit UserLogin: " + new Date());
+    }
+
+    @Test(description="test admin login",groups="login",dependsOnGroups="StaticContents")
+    public void testAdminLogin() {
+        System.out.println("Enter AdminLogin: " + new Date());
+        String adminId = properties.getProperty("ADMIN_ID");
+        String adminPwd = properties.getProperty("ADMIN_PWD");        
+        assertTrue(login(adminId, adminPwd),"Error:Could not login as admin ");
+        System.out.println("Exit AdminLogin: " + new Date());
+    }
+
+//    @Test(description="test login failure",groups="login",dependsOnGroups="StaticContents")
+    public void testLoginFailure() {
+        System.out.println("Enter loginFailure: " + new Date());
+        assertTrue(!login("user", "incorrectpassword"),"Eror:Login allowed with incorrect credential ");
+        System.out.println("Exit loginFailure: " + new Date());
+    }
+
+    @Test(description="test logout",groups="logout",dependsOnGroups="empcreate")
+    public void testLogout(){
+        System.out.println("Enter logout: " + new Date());
+        assertTrue(logout(),"Error:Could not logout - ");
+        System.out.println("Exit logout: " + new Date());        
+    }
+    
+}
