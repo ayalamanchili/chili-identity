3568405004119ede943e485ccd3602c03308e6d7
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-05-11 14:08:11 +0000

    Dao class has been created. Please refer http://tools.sstech.mobi/jira/browse/SSTO-2220
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5479 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/expense/ImmigrationCheckRequisitionDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.expense;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Madhu.Badiginchala
+ */
+@Repository
+@Scope("prototype")
+public class ImmigrationCheckRequisitionDao {
+    
+}
