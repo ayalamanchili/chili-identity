e7b381a2839ec4dc67fe0f082c7a458a3f867cee
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-05-11 14:11:27 +0000

    Service class has been created. Please refer http://tools.sstech.mobi/jira/browse/SSTO-2221.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5481 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/expense/ImmigrationCheckRequisitionService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.expense;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Madhu.Badiginchala
+ */
+@Component
+@Scope("request")
+public class ImmigrationCheckRequisitionService {
+    
+}
