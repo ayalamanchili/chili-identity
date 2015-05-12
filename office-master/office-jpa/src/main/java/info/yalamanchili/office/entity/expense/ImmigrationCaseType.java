d575e7615c4e0a04deac3dcada1fdae766d1ab12
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-05-12 14:25:48 +0000

    SSTO-2218: Added enum class ImmigrationCaseType for caseType.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5497 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/expense/ImmigrationCaseType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.expense;
+
+/**
+ *
+ * @author Madhu.Badiginchala
+ */
+public enum ImmigrationCaseType {
+    NEW_H1B, 
+    TRANSFER_H1B, 
+    ENTENSION_H1B;
+}
