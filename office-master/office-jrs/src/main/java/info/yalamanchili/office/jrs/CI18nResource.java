8e1fccc73fd4ab99f1fc15fd05a67e5fe6c3a2b2
Phani Yalamanchili <phani.y@sstech.us>
2015-05-13 03:42:21 +0000

    persisted resource bundle changes
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5503 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/CI18nResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/CI18nResource.java
@@ -9,7 +9,10 @@
 package info.yalamanchili.office.jrs;
 
 import info.chili.i18n.dao.Ci18nDao;
+import info.chili.i18n.domain.Ci18nResource;
+import info.chili.i18n.domain.Ci18nResource.Ci18nResourceTable;
 import info.chili.i18n.domain.Ci18nResourceBundle;
+import info.chili.jpa.validation.Validate;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -38,14 +41,36 @@ public class CI18nResource {
 
     @PUT
     @Path("/bundle")
-    public void save(Ci18nResourceBundle bundle) {
-        ci18nDao.save(bundle);
+    @Validate
+    public void saveBundle(Ci18nResourceBundle bundle) {
+        ci18nDao.saveBundle(bundle);
     }
 
     @Path("/bundle/{start}/{limit}")
     @GET
-    public Ci18nResourceBundle.CResourceBundleTable table(@PathParam("start") int start, @PathParam("limit") int limit) {
-        return ci18nDao.queryAll(start, limit);
+    public Ci18nResourceBundle.CResourceBundleTable getBundles(@PathParam("start") int start, @PathParam("limit") int limit) {
+        return ci18nDao.getBundles(start, limit);
     }
 
+    @PUT
+    @Path("/bundle/resource/{bundleId}")
+    @Validate
+    public void addResource(@PathParam("bundleId") Integer bundleId, Ci18nResource resource) {
+        ci18nDao.addResource(bundleId, resource);
+    }
+
+    @PUT
+    @Path("/bundle/resource")
+    @Validate
+    public void updateResource(Ci18nResource resource) {
+        ci18nDao.updateResource(resource);
+    }
+
+    @Path("/bundle/resources/{bundleId}/{start}/{limit}")
+    @GET
+    public Ci18nResourceTable getResources(@PathParam("bundleId") Integer bundleId, @PathParam("start") int start, @PathParam("limit") int limit) {
+        return ci18nDao.getResources(bundleId, start, limit);
+    }
+
+    //TODO delete resource bundle
 }
