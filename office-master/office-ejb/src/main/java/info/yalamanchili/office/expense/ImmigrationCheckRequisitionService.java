3671ec40bf06dcaa3283a374121c2f3331c667c5
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-05-15 08:03:22 +0000

    SSTO-2222 # Check Requisition Service Code Changes
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5521 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/expense/ImmigrationCheckRequisitionService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/expense/ImmigrationCheckRequisitionService.java
@@ -8,6 +8,18 @@
  */
 package info.yalamanchili.office.expense;
 
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.bpm.OfficeBPMService;
+import info.yalamanchili.office.bpm.OfficeBPMTaskService;
+import info.yalamanchili.office.bpm.types.Task;
+import info.yalamanchili.office.dao.expense.ImmigrationCheckRequisitionDao;
+import info.yalamanchili.office.dao.security.OfficeSecurityService;
+import info.yalamanchili.office.entity.expense.ImmigrationCheckRequisition;
+import info.yalamanchili.office.entity.profile.Employee;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -18,5 +30,38 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("request")
 public class ImmigrationCheckRequisitionService {
+    
+    @Autowired
+    protected ImmigrationCheckRequisitionDao immigrationCheckRequisitionDao;
+    
+        public void submitImmigrationCheckRequisition(ImmigrationCheckRequisition entity) {
+        Map<String, Object> vars = new HashMap<>();
+        vars.put("entity", entity);
+        Employee emp = OfficeSecurityService.instance().getCurrentUser();
+        vars.put("currentEmployee", emp);
+        String processId = OfficeBPMService.instance().startProcess("immigrationcheck_requisition_process", vars);
+        entity.setBpmProcessId(processId);
+    }
+
+    protected Task getTaskForTicket(ImmigrationCheckRequisition immigrationCheckRequisition) {
+        OfficeBPMTaskService taskService = OfficeBPMTaskService.instance();
+        List<Task> tasks = taskService.getTasksForProcessId(immigrationCheckRequisition.getBpmProcessId());
+        if (tasks.size() > 0) {
+            return tasks.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public void delete(Long id) {
+        ImmigrationCheckRequisition ticket = immigrationCheckRequisitionDao.findById(id);
+        OfficeBPMTaskService.instance().deleteAllTasksForProcessId(ticket.getBpmProcessId(), true);
+        immigrationCheckRequisitionDao.delete(id);
+    }
+    
+    
+    public static ImmigrationCheckRequisitionService instance() {
+        return SpringContext.getBean(ImmigrationCheckRequisitionService.class);
+    }
     
 }
