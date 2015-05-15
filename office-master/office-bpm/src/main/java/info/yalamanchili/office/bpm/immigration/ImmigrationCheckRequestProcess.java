d83b248cd542b6107674a69fd342bbb8edf9047c
Sadipan Bhattacharjee <sadipan.b@sstech.us>
2015-05-15 06:54:43 +0000

    SSTO-2266 - created BPM components for Immigration Check request process
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5520 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/immigration/ImmigrationCheckRequestProcess.java
@@ -0,0 +1,164 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 sadipan.b@sstech.us
+ */
+/*
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.bpm.immigration;
+
+import info.chili.service.jrs.exception.ServiceException;
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.OfficeRoles;
+import info.yalamanchili.office.bpm.email.GenericTaskCompleteNotification;
+import info.yalamanchili.office.bpm.email.GenericTaskCreateNotification;
+import info.yalamanchili.office.dao.company.CompanyContactDao;
+import info.yalamanchili.office.dao.expense.ImmigrationCheckRequisitionDao;
+import info.yalamanchili.office.dao.ext.CommentDao;
+import info.yalamanchili.office.dao.security.OfficeSecurityService;
+import info.yalamanchili.office.email.Email;
+import info.yalamanchili.office.email.MailUtils;
+import info.yalamanchili.office.entity.expense.ImmigrationCheckRequisition;
+import info.yalamanchili.office.entity.expense.ImmigrationCheckRequisitionStatus;
+import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.jms.MessagingService;
+import java.math.BigDecimal;
+import java.util.Date;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+/**
+ *
+ * @author Sadipan.B
+ */
+public class ImmigrationCheckRequestProcess implements TaskListener  {
+
+    @Override
+    public void notify(DelegateTask task) {
+        if ("create".equals(task.getEventName())) {
+            immigrationCheckRequestTaskCreated(task);
+        }
+        if ("complete".equals(task.getEventName())) {
+            immigrationCheckRequestTaskCompleted(task);
+        }
+    }
+
+    protected void immigrationCheckRequestTaskCreated(DelegateTask task) {
+        if (task.getTaskDefinitionKey().equals("immigrationCheckRequisitionApprovalTask")) {
+            saveImmigrationCheckRequisition(task);
+            assignImmigrationCheckRequisitionTask(task);
+        }
+        new GenericTaskCreateNotification().notify(task);
+    }
+
+    protected void immigrationCheckRequestTaskCompleted(DelegateTask task) {
+        ImmigrationCheckRequisition entity = getRequestFromTask(task);
+        if (entity == null) {
+            return;
+        }
+        Employee currentUser = OfficeSecurityService.instance().getCurrentUser();
+        if (currentUser.getEmployeeId().equals(entity.getEmployee().getEmployeeId())) {
+            throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "SYSTEM", "cannot.self.approve.corp.immigrationCheckrequisition", "You cannot approve your immigrationCheckrequisition task");
+        }
+        switch (task.getTaskDefinitionKey()) {
+            case "immigrationCheckRequisitionApprovalTask":
+                payrollApprovalTaskComplete(entity, task);
+                break;
+            case "immigrationCheckRequisitionFinalApprovalTask":
+                accountApprovalTaskComplete(entity, task);
+                break;
+        }
+    }
+
+    protected void payrollApprovalTaskComplete(ImmigrationCheckRequisition entity, DelegateTask task) {
+        String approvedAmountVar = (String) task.getExecution().getVariable("approvedAmount");
+        BigDecimal approvedAmount;
+        try {
+            approvedAmount = new BigDecimal(approvedAmountVar);
+        } catch (NumberFormatException ex) {
+            throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "SYSTEM", "invalid.approved.amount", "Approved amount must be a valid amount eg: 99.99 ");
+        }
+        entity.setAmount(approvedAmount);
+        //Notes
+        String notes = (String) task.getExecution().getVariable("notes");
+        CommentDao.instance().addComment(notes, entity);
+        //Status
+        String status = (String) task.getExecution().getVariable("status");
+        if (status.equalsIgnoreCase("approved")) {
+            entity.setStatus(ImmigrationCheckRequisitionStatus.Pending_Final_Approval);
+            entity.setApprovedBy(OfficeSecurityService.instance().getCurrentUser().getEmployeeId());
+            entity.setApprovedDate(new Date());
+        } else {
+            entity.setStatus(ImmigrationCheckRequisitionStatus.Rejected);
+            new GenericTaskCompleteNotification().notify(task);
+        }
+        ImmigrationCheckRequisitionDao.instance().save(entity);
+    }
+
+    protected void accountApprovalTaskComplete(ImmigrationCheckRequisition entity, DelegateTask task) {
+        String approvedAmountVar = (String) task.getExecution().getVariable("approvedAmount");
+        BigDecimal approvedAmount;
+        try {
+            approvedAmount = new BigDecimal(approvedAmountVar);
+        } catch (NumberFormatException ex) {
+            throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "SYSTEM", "invalid.approved.amount", "Approved amount must be a valid amount eg: 99.99 ");
+        }
+        entity.setAmount(approvedAmount);
+        //Notes
+        String notes = (String) task.getExecution().getVariable("notes");
+        CommentDao.instance().addComment(notes, entity);
+        //Status
+        String status = (String) task.getExecution().getVariable("status");
+        if (status.equalsIgnoreCase("approved")) {
+            entity.setStatus(ImmigrationCheckRequisitionStatus.Approved);
+            notifyAccountsPayableDept(entity);
+        } else {
+            entity.setStatus(ImmigrationCheckRequisitionStatus.Rejected);
+        }
+        ImmigrationCheckRequisitionDao.instance().save(entity);
+        new GenericTaskCompleteNotification().notifyWithMoreRoles(task, OfficeRoles.OfficeRole.ROLE_PAYROLL_AND_BENIFITS.name());
+    }
+
+    protected void saveImmigrationCheckRequisition(DelegateTask task) {
+        Employee emp = (Employee) task.getExecution().getVariable("currentEmployee");
+        ImmigrationCheckRequisitionDao dao = ImmigrationCheckRequisitionDao.instance();
+        ImmigrationCheckRequisition entity = (ImmigrationCheckRequisition) task.getExecution().getVariable("entity");
+        entity.setBpmProcessId(task.getExecution().getProcessInstanceId());
+        entity.setStatus(ImmigrationCheckRequisitionStatus.Pending_Initial_Approval);
+        entity.setEmployee(emp);
+        entity.setRequestedDate(new Date());
+        entity = dao.save(entity);
+        CommentDao.instance().addComment("Save Immigration Check", entity);
+        task.getExecution().setVariable("entity", entity);
+        task.getExecution().setVariable("entityId", entity.getId());
+    }
+
+    protected void assignImmigrationCheckRequisitionTask(DelegateTask task) {
+        Employee emp = (Employee) task.getExecution().getVariable("currentEmployee");
+        Employee reportsToEmp = CompanyContactDao.instance().getCompanyContactForEmployee(emp, "Reports_To");
+        if (emp.getEmployeeType().getName().equals("Corporate Employee") && reportsToEmp != null) {
+            task.addCandidateUser(reportsToEmp.getEmployeeId());
+        }
+        task.addCandidateGroup(OfficeRoles.OfficeRole.ROLE_PAYROLL_AND_BENIFITS.name());
+    }
+
+    public void notifyAccountsPayableDept(ImmigrationCheckRequisition entity) {
+        MessagingService messagingService = (MessagingService) SpringContext.getBean("messagingService");
+        Email email = new Email();
+        email.addTos(MailUtils.instance().getEmailsAddressesForRoles(OfficeRoles.OfficeRole.ROLE_ACCOUNTS_PAYABLE.name()));
+        email.setSubject("Immigration Requisition Approved For Employee" + entity.getEmployee().getFirstName() + " " + entity.getEmployee().getLastName());
+        email.setBody("Immigration Requisition Approved For Employee" + entity.getEmployee().getFirstName() + " " + entity.getEmployee().getLastName() + " Please Print and Process it");
+        messagingService.sendEmail(email);
+
+    }
+
+    protected ImmigrationCheckRequisition getRequestFromTask(DelegateTask task) {
+        Long entityId = (Long) task.getExecution().getVariable("entityId");
+        if (entityId != null) {
+            return ImmigrationCheckRequisitionDao.instance().findById(entityId);
+        }
+        return null;
+    }    
+}
---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/expense/ImmigrationCheckRequisitionStatus.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.expense;
+
+/**
+ *
+ * @author Sadipan.B
+ */
+
+public enum ImmigrationCheckRequisitionStatus {
+    Open,
+    //TODO remove pending
+    Pending,
+    Pending_Initial_Approval,
+    Pending_Final_Approval,
+    Approved,
+    //TODO remove pending
+    Completed,
+    Rejected,
+}
