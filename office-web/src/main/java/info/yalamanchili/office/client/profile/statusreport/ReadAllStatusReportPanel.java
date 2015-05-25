548f695c3ee11bd31168171c7ccdf15e7eb850cf
Phani Yalamanchili <phani.y@sstech.us>
2015-05-25 04:58:38 +0000

    SSTO-2289
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5599 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/profile/statusreport/ReadAllStatusReportPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/statusreport/ReadAllStatusReportPanel.java
@@ -7,9 +7,12 @@
  */
 package info.yalamanchili.office.client.profile.statusreport;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
 import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.config.ChiliClientConfig;
 import info.chili.gwt.crud.CRUDReadAllComposite;
@@ -18,6 +21,7 @@ import info.chili.gwt.date.DateUtils;
 import info.chili.gwt.fields.FileField;
 import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.ClickableLink;
 import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.Auth;
 import info.yalamanchili.office.client.OfficeWelcome;
@@ -130,7 +134,8 @@ public class ReadAllStatusReportPanel extends CRUDReadAllComposite {
         table.setText(0, 1, getKeyValue("Report Start Date"));
         table.setText(0, 2, getKeyValue("Report End Date"));
         table.setText(0, 3, getKeyValue("Stage"));
-        table.setText(0, 4, getKeyValue("Print"));
+        table.setText(0, 4, getKeyValue("Copy"));
+        table.setText(0, 5, getKeyValue("Print"));
     }
 
     @Override
@@ -141,9 +146,32 @@ public class ReadAllStatusReportPanel extends CRUDReadAllComposite {
             table.setText(i, 1, DateUtils.getFormatedDate(JSONUtils.toString(entity, "reportStartDate"), DateTimeFormat.PredefinedFormat.DATE_LONG));
             table.setText(i, 2, DateUtils.getFormatedDate(JSONUtils.toString(entity, "reportEndDate"), DateTimeFormat.PredefinedFormat.DATE_LONG));
             table.setText(i, 3, JSONUtils.toString(entity, "stage"));
+            ClickableLink copyL = new ClickableLink("Create Copy");
+            copyL.setTitle(JSONUtils.toString(entity, "id"));
+            copyL.addClickHandler(new ClickHandler() {
+
+                @Override
+                public void onClick(ClickEvent event) {
+                    createCopy(((ClickableLink) event.getSource()).getTitle());
+                }
+            });
+            table.setWidget(i, 4, copyL);
             FileField reportL = new FileField("Print", ChiliClientConfig.instance().getFileDownloadUrl() + "statusreport/report" + "&passthrough=true" + "&id=" + JSONUtils.toString(entity, "id"));
-            table.setWidget(i, 4, reportL);
+            table.setWidget(i, 5, reportL);
         }
+    }
+
+    protected void createCopy(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(OfficeWelcome.constants.root_url() + "statusreport/clone/" + entityId, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+
+                    @Override
+                    public void onResponse(String arg0) {
+                        new ResponseStatusWidget().show("Copy created. Plase update and save.");
+                        TabPanel.instance().homePanel.entityPanel.clear();
+                        TabPanel.instance().homePanel.entityPanel.add(new UpdateStatusReportPanel(JSONParser.parseLenient(arg0).isObject()));
+                    }
+                });
     }
 
     @Override
