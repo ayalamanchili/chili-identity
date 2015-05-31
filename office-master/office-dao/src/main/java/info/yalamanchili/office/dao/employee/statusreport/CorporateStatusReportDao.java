5d809da42552a04864c7c785b90014a6047112db
Phani Yalamanchili <phani.y@sstech.us>
2015-05-31 19:30:25 +0000

    SSTO-2238
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5636 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/employee/statusreport/CorporateStatusReportDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/employee/statusreport/CorporateStatusReportDao.java
@@ -7,20 +7,19 @@
  */
 package info.yalamanchili.office.dao.employee.statusreport;
 
-import info.chili.commons.DateUtils;
 import info.chili.dao.CRUDDao;
 import info.chili.spring.SpringContext;
+import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.security.OfficeSecurityService;
-import info.yalamanchili.office.dao.time.TimePeriodDao;
 import info.yalamanchili.office.entity.employee.statusreport.CorporateStatusReport;
 import info.yalamanchili.office.entity.employee.statusreport.CropStatusReportsStatus;
 import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.model.time.TimePeriod;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.xml.bind.annotation.XmlRootElement;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
@@ -31,7 +30,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Scope("prototype")
 public class CorporateStatusReportDao extends CRUDDao<CorporateStatusReport> {
-
+    
     @Override
     public CorporateStatusReport save(CorporateStatusReport entity) {
         if (entity.getId() == null) {
@@ -40,7 +39,7 @@ public class CorporateStatusReportDao extends CRUDDao<CorporateStatusReport> {
         }
         return super.save(entity);
     }
-
+    
     public List<CorporateStatusReport> getReports(Employee emp, int start, int limit) {
         TypedQuery<CorporateStatusReport> query = em.createQuery("from " + CorporateStatusReport.class.getCanonicalName() + " where employee=:empParam", CorporateStatusReport.class);
         query.setParameter("empParam", emp);
@@ -48,27 +47,77 @@ public class CorporateStatusReportDao extends CRUDDao<CorporateStatusReport> {
         query.setMaxResults(limit);
         return query.getResultList();
     }
-
+    
     public Long getReportsSize(Employee emp, int start, int limit) {
         TypedQuery<Long> query = em.createQuery("select count(*) from " + CorporateStatusReport.class.getCanonicalName() + " where employee=:empParam", Long.class);
         query.setParameter("empParam", emp);
         return query.getSingleResult();
     }
-
     
-
+    public List<CorporateStatusReport> search(CorporateStatusReportSearchDto dto) {
+        String queryStr = getSearchReportsQuery(dto);
+        TypedQuery<CorporateStatusReport> query = em.createQuery(queryStr, entityCls);
+        if (queryStr.contains("employeeParam")) {
+            query.setParameter("employeeParam", EmployeeDao.instance().findById(dto.getEmployee().getId()));
+        }
+        return query.getResultList();
+        
+    }
+    
+    protected String getSearchReportsQuery(CorporateStatusReportSearchDto dto) {
+        StringBuilder query = new StringBuilder();
+        query.append("from ").append(CorporateStatusReport.class.getCanonicalName()).append(" where ");
+        if (dto.getEmployee() != null) {
+            query.append("employee=:employeeParam");
+        }
+        return query.toString();
+    }
+    
     public CorporateStatusReportDao() {
         super(CorporateStatusReport.class);
     }
     @PersistenceContext
     protected EntityManager em;
-
+    
     @Override
     public EntityManager getEntityManager() {
         return em;
     }
-
+    
     public static CorporateStatusReportDao instance() {
         return SpringContext.getBean(CorporateStatusReportDao.class);
+    }
+    
+    @XmlRootElement
+    public class CorporateStatusReportSearchDto {
+        
+        protected Employee employee;
+        protected String status;
+        protected TimePeriod statusReportPeriod;
+        
+        public Employee getEmployee() {
+            return employee;
+        }
+        
+        public void setEmployee(Employee employee) {
+            this.employee = employee;
+        }
+        
+        public String getStatus() {
+            return status;
+        }
+        
+        public void setStatus(String status) {
+            this.status = status;
+        }
+        
+        public TimePeriod getStatusReportPeriod() {
+            return statusReportPeriod;
+        }
+        
+        public void setStatusReportPeriod(TimePeriod statusReportPeriod) {
+            this.statusReportPeriod = statusReportPeriod;
+        }
+        
     }
 }
--- office-master/office-ejb/src/main/java/info/yalamanchili/office/employee/statusreport/CorporateStatusReportService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/employee/statusreport/CorporateStatusReportService.java
@@ -8,30 +8,20 @@
 package info.yalamanchili.office.employee.statusreport;
 
 import info.chili.jpa.validation.Validate;
-import info.chili.reporting.ReportGenerator;
 import info.chili.spring.SpringContext;
 import info.yalamanchili.office.bpm.OfficeBPMService;
 import info.yalamanchili.office.bpm.OfficeBPMTaskService;
-import info.yalamanchili.office.config.OfficeServiceConfiguration;
 import info.yalamanchili.office.dao.employee.statusreport.CorporateStatusReportDao;
-import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.security.OfficeSecurityService;
-import info.yalamanchili.office.dao.time.TimePeriodDao;
 import info.yalamanchili.office.entity.employee.statusreport.CorporateStatusReport;
 import info.yalamanchili.office.entity.employee.statusreport.CropStatusReportsStatus;
 import info.yalamanchili.office.entity.profile.Employee;
-import info.yalamanchili.office.jms.MessagingService;
-import info.yalamanchili.office.model.time.TimePeriod;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -71,21 +61,7 @@ public class CorporateStatusReportService {
         CorporateStatusReport entity = corporateStatusReportDao.findById(id);
         OfficeBPMTaskService.instance().deleteAllTasksForProcessId(entity.getBpmProcessId(), true);
         corporateStatusReportDao.delete(id);
-    }
-
-    @Async
-    @Transactional(readOnly = true)
-    public void getCorporateStatusReport(String email) {
-        List<CorporateStatusReport> res = new ArrayList<CorporateStatusReport>();
-        for (Employee emp : EmployeeDao.instance().getEmployeesByType("Corporate Employee")) {
-            CorporateStatusReport dto = mapper.map(emp, CorporateStatusReport.class);
-            dto.setReportStartDate(dto.getReportStartDate());
-            dto.setReportEndDate(dto.getReportEndDate());
-            dto.setReport(dto.getReport());
-            res.add(dto);
-        }
-        MessagingService.instance().emailReport(ReportGenerator.generateExcelReport(res, "Corporate-Status-Report", OfficeServiceConfiguration.instance().getContentManagementLocationRoot()), email);
-    }
+    } 
 
     public static CorporateStatusReportService instance() {
         return SpringContext.getBean(CorporateStatusReportService.class);
