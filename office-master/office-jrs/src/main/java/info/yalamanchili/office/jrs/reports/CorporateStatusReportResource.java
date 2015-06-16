76c3778001bedaab30d818f3943887970429e435
Phani Yalamanchili <phani.y@sstech.us>
2015-06-16 20:02:01 +0000

    SSTO-2238
    git-svn-id: http://tools.sstech.us/svn/office/trunk@5728 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/reports/CorporateStatusReportResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/reports/CorporateStatusReportResource.java
@@ -26,7 +26,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
