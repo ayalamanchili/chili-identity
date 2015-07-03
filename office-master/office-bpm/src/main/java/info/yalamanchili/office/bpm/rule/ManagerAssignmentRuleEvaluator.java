e76b11cc1644b331ac151b61cccc0557359e1f7c
Phani Yalamanchili <phani.y@sstech.us>
2015-07-03 06:26:07 +0000

    SSTO-2346 rule evaluator for manager (reports_to) assignment.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@6042 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/rule/ManagerAssignmentRuleEvaluator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.bpm.rule;
+
+import info.chili.bpm.domain.BPMTaskDelegateRule;
+import info.chili.bpm.task.AbstractTaskDelegate;
+import info.chili.bpm.types.Task;
+import info.yalamanchili.office.dao.company.CompanyContactDao;
+import info.yalamanchili.office.entity.profile.Employee;
+import org.activiti.engine.delegate.DelegateTask;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author ayalamanchili
+ */
+@Component
+public class ManagerAssignmentRuleEvaluator extends AbstractTaskDelegate {
+
+    @Override
+    public Task getDelegationInfo(DelegateTask task, BPMTaskDelegateRule rule) {
+        Task res = new Task();
+        Employee emp = (Employee) task.getExecution().getVariable("currentEmployee");
+        Employee reportsToEmp = CompanyContactDao.instance().getCompanyContactForEmployee(emp, "Reports_To");
+        if (reportsToEmp != null) {
+            task.setAssignee(reportsToEmp.getEmployeeId());
+        } else {
+            //TODO avoid hard coding
+            task.setAssignee("aadmin");
+        }
+        return res;
+    }
+
+}
