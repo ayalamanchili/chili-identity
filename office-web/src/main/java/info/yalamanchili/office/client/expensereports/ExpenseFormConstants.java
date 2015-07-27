03df064984672b44fb74fbddb1434bf388394d64
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-07-27 10:02:06 +0000

    SSTO-2373  Expense Module 2.0
    git-svn-id: http://tools.sstech.us/svn/office/trunk@6191 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-web/src/main/java/info/yalamanchili/office/client/expensereports/ExpenseFormConstants.java
@@ -0,0 +1,33 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.expensereports;
+
+/**
+ *
+ * @author Madhu.Badiginchala
+ */
+public interface ExpenseFormConstants {
+    
+    int DEFAULT_FIELD_WIDTH = 340;
+    int DEFAULT_ITEM_FIELD_WIDTH = 330;
+    int DEFAULT_DES_FIELD_WIDTH = 320;
+    int DEFAULT_PMT_FIELD_WIDTH = 335;
+    // Travel Transportation Panel
+    String EXPENSE_FORM_PURPOSE="expenseFormPurpose";
+    String EXPENSE_PAYMENT_MODE="expensePaymentMode";
+    String EXPENSE_REIMBURSE_PMT_MODE="expenseReimbursePaymentMode";
+    String NAME="name";
+    String LOCATION="location";
+    String START_DATE="startDate";
+    String END_DATE="endDate";
+    String PROJECT_NAME="projectName";
+    String PROJECT_NUMBER="projectNumber";
+    String TRAVEL_EXPENSE="TravelExpense";
+    String EXPENSE_ITEMS="expenseItems";
+}
