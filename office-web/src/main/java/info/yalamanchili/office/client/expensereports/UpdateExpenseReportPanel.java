1dc3055b7367690ab5fc751fdba0d5cbc33e0d66
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-07-27 13:05:44 +0000

    SSTO-2373  Expense Module 2.0
    git-svn-id: http://tools.sstech.us/svn/office/trunk@6212 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/expensereports/UpdateExpenseReportPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/expensereports/UpdateExpenseReportPanel.java
@@ -7,15 +7,21 @@
  */
 package info.yalamanchili.office.client.expensereports;
 
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.crud.CRUDComposite;
-import info.chili.gwt.crud.CreateComposite;
 import info.chili.gwt.crud.UpdateComposite;
-import info.chili.gwt.fields.DataType;
+import info.chili.gwt.date.DateUtils;
+import info.chili.gwt.fields.DateField;
+import info.chili.gwt.fields.EnumField;
+import info.chili.gwt.fields.StringField;
 import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.Alignment;
 import info.chili.gwt.utils.JSONUtils;
@@ -23,6 +29,7 @@ import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
 import info.yalamanchili.office.client.expenseitem.UpdateExpenseItemPanel;
+import static info.yalamanchili.office.client.expensereports.ExpenseFormConstants.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -35,6 +42,28 @@ public class UpdateExpenseReportPanel extends UpdateComposite {
 
     private Logger logger = Logger.getLogger(UpdateExpenseReportPanel.class.getName());
     protected List<UpdateExpenseItemPanel> updateItemPanels = new ArrayList<UpdateExpenseItemPanel>();
+    protected static HTML generalInfo = new HTML("\n"
+            + "<p style=\"border: 1px solid rgb(204, 204, 204); padding: 5px 10px; background: rgb(238, 238, 238);\">"
+            + "<strong style=\"color:#555555\">General Expense Information</strong></p>\n"
+            + "\n"
+            + "<ul>\n"
+            + "</ul>");
+    protected static HTML expenseInfo = new HTML("\n"
+            + "<p style=\"border: 1px solid rgb(204, 204, 204); padding: 5px 10px; background: rgb(238, 238, 238);\">"
+            + "<strong style=\"color:#555555\">Expense Items Information</strong></p>\n"
+            + "\n"
+            + "<ul>\n"
+            + "</ul>");
+    HTML emptyLine = new HTML("<br/>");
+
+    EnumField expenseFormPurpose;
+    StringField name;
+    StringField location;
+    DateField startDate;
+    DateField endDate;
+    StringField projectName;
+    StringField projectNumber;
+    EnumField expenseReimbursePaymentMode;
 
     public UpdateExpenseReportPanel(String id) {
         initUpdateComposite(id, "ExpenseReport", OfficeWelcome.constants);
@@ -44,14 +73,14 @@ public class UpdateExpenseReportPanel extends UpdateComposite {
     public void loadEntity(String entityId) {
         HttpService.HttpServiceAsync.instance().doGet(getReadURI(), OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String response) {
-                logger.info(response);
-                entity = (JSONObject) JSONParser.parseLenient(response);
-                populateFieldsFromEntity(entity);
+                    @Override
+                    public void onResponse(String response) {
+                        logger.info(response);
+                        entity = (JSONObject) JSONParser.parseLenient(response);
+                        populateFieldsFromEntity(entity);
 
-            }
-        });
+                    }
+                });
     }
 
     protected String getReadURI() {
@@ -60,18 +89,37 @@ public class UpdateExpenseReportPanel extends UpdateComposite {
 
     @Override
     protected JSONObject populateEntityFromFields() {
-        assignEntityValueFromField("name", entity);
-        assignEntityValueFromField("description", entity);
-        assignEntityValueFromField("startDate", entity);
-        assignEntityValueFromField("endDate", entity);
+        if (expenseFormPurpose.getValue() != null) {
+            entity.put(EXPENSE_FORM_PURPOSE, new JSONString(expenseFormPurpose.getValue()));
+        }
+        if (name.getValue() != null) {
+            entity.put(NAME, new JSONString(name.getValue()));
+        }
+        if (location.getValue() != null) {
+            entity.put(LOCATION, new JSONString(location.getValue()));
+        }
+        if (startDate.getDate() != null) {
+            entity.put(START_DATE, new JSONString(DateUtils.toDateString(startDate.getDate())));
+        }
+        if (endDate.getDate() != null) {
+            entity.put(END_DATE, new JSONString(DateUtils.toDateString(endDate.getDate())));
+        }
+        if (projectName.getValue() != null) {
+            entity.put(PROJECT_NAME, new JSONString(projectName.getValue()));
+        }
+        if (projectNumber.getValue() != null) {
+            entity.put(PROJECT_NUMBER, new JSONString(projectNumber.getValue()));
+        }
+        if (expenseReimbursePaymentMode.getValue() != null) {
+            entity.put(EXPENSE_REIMBURSE_PMT_MODE, new JSONString(expenseReimbursePaymentMode.getValue()));
+        }
         JSONArray items = new JSONArray();
         int i = 0;
         for (UpdateExpenseItemPanel panel : updateItemPanels) {
             items.set(i, panel.populateEntityFromFields());
             i++;
         }
-        entity.put("expenseItems", items);
-        assignEntityValueFromField("status", entity);
+        entity.put(EXPENSE_ITEMS, items);
         logger.info(entity.toString());
         return entity;
     }
@@ -80,16 +128,16 @@ public class UpdateExpenseReportPanel extends UpdateComposite {
     protected void updateButtonClicked() {
         HttpService.HttpServiceAsync.instance().doPut(getURI(), entity.toString(),
                 OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
-            @Override
-            public void onFailure(Throwable arg0) {
-                handleErrorResponse(arg0);
-            }
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        handleErrorResponse(arg0);
+                    }
 
-            @Override
-            public void onSuccess(String arg0) {
-                postUpdateSuccess(arg0);
-            }
-        });
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postUpdateSuccess(arg0);
+                    }
+                });
     }
 
     @Override
@@ -99,12 +147,31 @@ public class UpdateExpenseReportPanel extends UpdateComposite {
 
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
-        assignFieldValueFromEntity("name", entity, DataType.STRING_FIELD);
-        assignFieldValueFromEntity("description", entity, DataType.TEXT_AREA_FIELD);
-        assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
-        assignFieldValueFromEntity("endDate", entity, DataType.DATE_FIELD);
+        if (entity.get(EXPENSE_FORM_PURPOSE) != null) {
+            expenseFormPurpose.selectValue(entity.get(EXPENSE_FORM_PURPOSE).isString().stringValue());
+        }
+        if (entity.get(NAME) != null) {
+            name.setValue(entity.get(NAME).isString().stringValue());
+        }
+        if (entity.get(LOCATION) != null) {
+            location.setValue(entity.get(LOCATION).isString().stringValue());
+        }
+        if (entity.get(START_DATE) != null) {
+            startDate.setValue(entity.get(START_DATE).isString().stringValue());
+        }
+        if (entity.get(END_DATE) != null) {
+            endDate.setValue(entity.get(END_DATE).isString().stringValue());
+        }
+        if (entity.get(PROJECT_NAME) != null) {
+            projectName.setValue(entity.get(PROJECT_NAME).isString().stringValue());
+        }
+        if (entity.get(PROJECT_NUMBER) != null) {
+            projectNumber.setValue(entity.get(PROJECT_NUMBER).isString().stringValue());
+        }
+        if (entity.get(EXPENSE_REIMBURSE_PMT_MODE) != null) {
+            expenseReimbursePaymentMode.selectValue(entity.get(EXPENSE_REIMBURSE_PMT_MODE).isString().stringValue());
+        }
         JSONArray expenseItems = JSONUtils.toJSONArray(entity.get("expenseItems"));
-        assignFieldValueFromEntity("status", entity, DataType.ENUM_FIELD);
         populateExpenseItems(expenseItems);
     }
 
@@ -120,7 +187,7 @@ public class UpdateExpenseReportPanel extends UpdateComposite {
 
     @Override
     protected void postUpdateSuccess(String result) {
-        new ResponseStatusWidget().show("Successfully Updated Employee ExpenseCategory Information");
+        new ResponseStatusWidget().show("Successfully Updated Employee Expense Form Information");
         TabPanel.instance().expensePanel.entityPanel.clear();
         TabPanel.instance().expensePanel.entityPanel.add(new ReadAllExpenseReportsPanel());
     }
@@ -131,15 +198,44 @@ public class UpdateExpenseReportPanel extends UpdateComposite {
 
     @Override
     protected void configure() {
+        expenseFormPurpose.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        name.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        location.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        startDate.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        endDate.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        projectName.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        projectNumber.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        expenseReimbursePaymentMode.getLabel().getElement().getStyle().setWidth(DEFAULT_FIELD_WIDTH, Style.Unit.PX);
+        generalInfo.setAutoHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+        expenseInfo.setAutoHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
     }
 
     @Override
     protected void addWidgets() {
-        addField("name", false, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
-        addField("description", false, false, DataType.TEXT_AREA_FIELD, Alignment.HORIZONTAL);
-        addField("startDate", false, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
-        addField("endDate", false, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
-        addEnumField("status", false, false, ExpenseReportStatus.names());
+        expenseFormPurpose = new EnumField(OfficeWelcome.constants,
+                EXPENSE_FORM_PURPOSE, TRAVEL_EXPENSE, false, true, ExpenseFormPurpose.names(), Alignment.HORIZONTAL);
+        expenseReimbursePaymentMode = new EnumField(OfficeWelcome.constants,
+                EXPENSE_REIMBURSE_PMT_MODE, TRAVEL_EXPENSE, false, true, ExpenseReimbursePaymentMode.names(), Alignment.HORIZONTAL);
+        name = new StringField(OfficeWelcome.constants, NAME, TRAVEL_EXPENSE, false, true, Alignment.HORIZONTAL);
+        location = new StringField(OfficeWelcome.constants, LOCATION, TRAVEL_EXPENSE, false, true, Alignment.HORIZONTAL);
+        startDate = new DateField(OfficeWelcome.constants, START_DATE, TRAVEL_EXPENSE, false, true, Alignment.HORIZONTAL);
+        endDate = new DateField(OfficeWelcome.constants, END_DATE, TRAVEL_EXPENSE, false, true, Alignment.HORIZONTAL);
+        projectName = new StringField(OfficeWelcome.constants, PROJECT_NAME, TRAVEL_EXPENSE, false, false, Alignment.HORIZONTAL);
+        projectNumber = new StringField(OfficeWelcome.constants, PROJECT_NUMBER, TRAVEL_EXPENSE, false, false, Alignment.HORIZONTAL);
+        name.getElement().getStyle().setProperty("float", "left");
+        startDate.getElement().getStyle().setProperty("float", "left");
+        projectName.getElement().getStyle().setProperty("float", "left");
+        entityFieldsPanel.add(expenseFormPurpose);
+        entityFieldsPanel.add(generalInfo);
+        entityFieldsPanel.add(name);
+        entityFieldsPanel.add(location);
+        entityFieldsPanel.add(startDate);
+        entityFieldsPanel.add(endDate);
+        entityFieldsPanel.add(projectName);
+        entityFieldsPanel.add(projectNumber);
+        entityFieldsPanel.add(expenseReimbursePaymentMode);
+        entityFieldsPanel.add(expenseInfo);
+        alignFields();
         alignFields();
     }
 
