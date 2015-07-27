f17dacaeab9f02235d98cb970051d6043b498f62
Phani Yalamanchili <phani.y@sstech.us>
2015-07-27 20:01:37 +0000

    SSTO-2392 chili jms hook up.
    git-svn-id: http://tools.sstech.us/svn/office/trunk@6219 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/OfficeServiceJobConfiguration.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/OfficeServiceJobConfiguration.java
@@ -7,6 +7,7 @@
  */
 package info.yalamanchili.office.jrs;
 
+import info.chili.jms.CMessagingService;
 import info.yalamanchili.office.Time.AssociateTimeAccuralService;
 import info.yalamanchili.office.Time.CorporateTimeAccuralService;
 import info.yalamanchili.office.Time.TimeJobService;
@@ -17,8 +18,8 @@ import info.yalamanchili.office.dao.security.OfficeSecurityService;
 import info.yalamanchili.office.dao.time.TimePeriodDao;
 import info.yalamanchili.office.employee.perfeval.PerformanceEvaluationQuestionsFactory;
 import info.yalamanchili.office.employee.probeval.ProbationPeriodEvaluationQuestionsFactory;
+import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.reports.profile.ProfileReportsService;
-import info.yalamanchili.office.reports.recruiting.RecruitingReportsService;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.stereotype.Component;
 
@@ -28,59 +29,59 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class OfficeServiceJobConfiguration {
-
+    
     @ManagedOperation
     public void processMonthlyTimeAccrual() {
         CorporateTimeAccuralService.instance().accureMonthlyTime();
         AssociateTimeAccuralService.instance().accureMonthlyConsTime();
     }
-
+    
     @ManagedOperation
     public void revertRecentMonthlyTimeAccrual() {
         CorporateTimeAccuralService.instance().revertRecentPTOAccruedChanges();
         AssociateTimeAccuralService.instance().revertRecentPTOAccruedChanges();
     }
-
+    
     @ManagedOperation
     public void syncPerformanceEvaluationQuestions() {
         PerformanceEvaluationQuestionsFactory.instance().syncQuestions();
     }
-
+    
     @ManagedOperation
     public void syncProbationPRDEvaluationQuestions() {
         ProbationPeriodEvaluationQuestionsFactory.instance().syncProbationQuestions();
     }
-
+    
     @ManagedOperation
     public void syncNotificationGroupsForRoles() {
         NotificationGroupDao.instance().syncNotificationGroupsForRoles();
     }
-
+    
     @ManagedOperation
     public void syncNotSubmitted() {
 //        CorporateStatusReportDao.instance().notSubmittedReport();
     }
-
+    
     @ManagedOperation
     public void syncNotificationGroupForRetirementPlanOptIns() {
         NotificationGroupDao.instance().syncRetirementPlanOptInNotificationGroup();
     }
-
+    
     @ManagedOperation
     public void syncCorpEmployeeRoles() {
         EmployeeDao.instance().syncCorpEmployeeRoles();
     }
-
+    
     @ManagedOperation
     public void sendTodaysLeaveNotifications() {
         TimeJobService.instance().sendTodaysLeaveNotifications();
     }
-
+    
     @ManagedOperation
     public void syncUserCerts() {
         OfficeSecurityService.instance().syncUserCerts();
     }
-
+    
     @ManagedOperation
     public void createCert(String employeeId) {
         OfficeSecurityService.instance().createUserCert(employeeId);
@@ -93,14 +94,19 @@ public class OfficeServiceJobConfiguration {
     public void indexAllResumes() {
         SkillSetDao.instance().indexAllResumes();
     }
-
+    
     @ManagedOperation
     public void sycnTimePeriods() {
         TimePeriodDao.instance().syncWeeklyTimePeriods();
     }
-
+    
     @ManagedOperation
     public void sendIncompleteProfileEmails() {
         ProfileReportsService.instance().sendMissingProfileInfoEmail();
+    }
+    
+    @ManagedOperation
+    public void test() {
+        CMessagingService.instance().send(new Employee());
     }
 }
