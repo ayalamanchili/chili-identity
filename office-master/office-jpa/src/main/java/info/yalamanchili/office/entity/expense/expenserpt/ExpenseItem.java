2e15c2d715eb1c1119896b57a774d6f2e9e1ec51
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-08-04 13:56:08 +0000

    SSTO-2373  Expense Module 2.0
    git-svn-id: http://tools.sstech.us/svn/office/trunk@6302 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/expense/expenserpt/ExpenseItem.java
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/expense/expenserpt/ExpenseItem.java
@@ -59,6 +59,7 @@ public class ExpenseItem extends AbstractEntity {
     /*
      *
      */
+    @NotNull(message = "{expenseitem.amount.not.empty.msg}")
     @Min(1)
     private BigDecimal amount;
     /*
