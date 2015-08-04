78522fb7263206d6eedab85fe0246ab7a296e9ff
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-08-04 13:50:02 +0000

    SSTO-2373  Expense Module 2.0
    git-svn-id: http://tools.sstech.us/svn/office/trunk@6300 04aae24c-3eb2-d842-8c26-b4285eb26770

--- office-web/src/main/java/info/yalamanchili/office/client/expensereports/ExpenseReportsSidePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/expensereports/ExpenseReportsSidePanel.java
@@ -9,11 +9,20 @@ package info.yalamanchili.office.client.expensereports;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.composite.ALComposite;
 import info.chili.gwt.crud.CreateComposite;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.utils.Utils;
 import info.chili.gwt.widgets.ClickableLink;
+import info.chili.gwt.widgets.SuggestBox;
+import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -24,7 +33,9 @@ public class ExpenseReportsSidePanel extends ALComposite implements ClickHandler
 
     private static Logger logger = Logger.getLogger(ExpenseReportsSidePanel.class.getName());
     public FlowPanel expenseCategoryReportsSidePanel = new FlowPanel();
-    ClickableLink createExpenseReportsLink = new ClickableLink("Create Expense Report");
+    ClickableLink createExpenseReportsLink = new ClickableLink("Submit Expense Report");
+    SuggestBox employeeSB = new SuggestBox(OfficeWelcome.constants, "employee", "Employee", false, false);
+    Button viewB = new Button("View");
 
     public ExpenseReportsSidePanel() {
         init(expenseCategoryReportsSidePanel);
@@ -33,17 +44,30 @@ public class ExpenseReportsSidePanel extends ALComposite implements ClickHandler
     @Override
     protected void addListeners() {
         createExpenseReportsLink.addClickHandler(this);
+        viewB.addClickHandler(this);
     }
 
     @Override
     protected void configure() {
+        HttpService.HttpServiceAsync.instance().doGet(getEmployeeIdsDropDownUrl(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
+        @Override
+        public void onResponse(String entityString) {
+        logger.info(entityString);
+        Map<String, String> values = JSONUtils.convertKeyValueStringPairs(entityString);
+        if (values != null) {
+           employeeSB.loadData(values);
+        }
+        }
+        });
     }
 
     @Override
     protected void addWidgets() {
-//        if (Auth.isAdmin() || Auth.isHR()) {
-            expenseCategoryReportsSidePanel.add(createExpenseReportsLink);
-//        }
+        expenseCategoryReportsSidePanel.add(createExpenseReportsLink);
+        expenseCategoryReportsSidePanel.add(Utils.getLineSeperatorTag("Search"));
+        expenseCategoryReportsSidePanel.add(employeeSB);
+        expenseCategoryReportsSidePanel.add(viewB);
+
     }
 
     @Override
@@ -52,5 +76,18 @@ public class ExpenseReportsSidePanel extends ALComposite implements ClickHandler
             TabPanel.instance().expensePanel.entityPanel.clear();
             TabPanel.instance().expensePanel.entityPanel.add(new CreateExpenseReportPanel(CreateComposite.CreateCompositeType.CREATE));
         }
+        if (event.getSource().equals(viewB)) {
+            TabPanel.instance().expensePanel.entityPanel.clear();
+            TabPanel.instance().expensePanel.entityPanel.add(new ReadAllExpenseReportsPanel(getTravelAuthURL(0, "10")));
+        }
+    }
+    
+    private String getTravelAuthURL(Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "expensereport/" + employeeSB.getKey() + "/" + start.toString() + "/"
+                + limit.toString();
+    }
+
+    private String getEmployeeIdsDropDownUrl() {
+        return URL.encode(OfficeWelcome.constants.root_url() + "employee/employees-by-type/dropdown/0/10000?column=id&column=firstName&column=lastName&employee-type=Corporate Employee&employee-type=Employee");
     }
 }
