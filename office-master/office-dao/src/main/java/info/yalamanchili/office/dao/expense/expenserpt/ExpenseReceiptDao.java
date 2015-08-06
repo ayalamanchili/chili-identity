3644846f4c167565cf1334b8acc1444c99ddc58f
Madhu Badiginchala <Madhu.Badiginchala@sstech.us>
2015-08-06 15:56:22 +0000

    SSTO-2373  Expense Module 2.0
    git-svn-id: http://tools.sstech.us/svn/office/trunk@6312 04aae24c-3eb2-d842-8c26-b4285eb26770

---
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/expense/expenserpt/ExpenseReceiptDao.java
@@ -0,0 +1,40 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.expense.expenserpt;
+
+import info.chili.dao.CRUDDao;
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.entity.expense.expenserpt.ExpenseReceipt;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Scope("prototype")
+public class ExpenseReceiptDao extends CRUDDao<ExpenseReceipt> {
+
+    public ExpenseReceiptDao() {
+        super(ExpenseReceipt.class);
+    }
+    @PersistenceContext
+    protected EntityManager em;
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public static ExpenseReceiptDao instance() {
+        return SpringContext.getBean(ExpenseReceiptDao.class);
+    }
+}
+
+   
