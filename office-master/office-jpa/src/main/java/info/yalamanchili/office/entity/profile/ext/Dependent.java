fc222725f3a85b46ffe1c673255958d860e0e056
madhu badiginchala <madhu.badiginchala@sstech.us>
2015-08-19 16:28:40 +0530

    OnBoarding Employee

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/profile/ext/Dependent.java
@@ -0,0 +1,45 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.profile.ext;
+
+import info.chili.jpa.AbstractHandleEntity;
+
+/**
+ *
+ * @author Madhu.Badiginchala
+ */
+public class Dependent extends AbstractHandleEntity {
+    
+     private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private String firstName;
+    /**
+     *
+     */
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
