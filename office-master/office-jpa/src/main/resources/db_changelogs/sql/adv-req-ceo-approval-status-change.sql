2a21a5a9cd5125259a75711d210d532f5c06d5c4
prasanthi ponnaganti <prasanthi.p@sstech.us>
2015-09-01 22:06:39 +0530

    ceo approval status

--- office-master/office-jpa/src/main/resources/db_changelogs/sql/adv-req-ceo-approval-status-change.sql
+++ office-master/office-jpa/src/main/resources/db_changelogs/sql/adv-req-ceo-approval-status-change.sql
@@ -1,1 +1,2 @@
 update AdvanceRequisition set status='Pending_CEO_Approval' where status='Pending_Final_Approval';
+update AdvanceRequisition_aud set status='Pending_CEO_Approval' where status='Pending_Final_Approval';
