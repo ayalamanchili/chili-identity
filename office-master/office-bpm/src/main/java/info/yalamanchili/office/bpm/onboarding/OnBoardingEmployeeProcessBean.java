bfe6f06f0700ba32ff2f245d9bd553d49b290e52
radhika.mukkala@sstech.us <>
2015-09-24 16:26:12 +0530

    Employee name has been modified

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/onboarding/OnBoardingEmployeeProcessBean.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/onboarding/OnBoardingEmployeeProcessBean.java
@@ -42,7 +42,7 @@ public class OnBoardingEmployeeProcessBean {
         subject.append("System Soft employee on boarding email");
         email.setSubject(subject.toString());
         Map<String, Object> emailCtx = new HashMap<>();
-        emailCtx.put("employeeName", "asdf");
+        emailCtx.put("employeeName", emp.getFirstName()+" "+emp.getLastName());
         emailCtx.put("currentEmployee", "cemp");
         email.setTemplateName("on_board_employee_template.html");
         email.setContext(emailCtx);
@@ -62,7 +62,7 @@ public class OnBoardingEmployeeProcessBean {
         subject.append("System Soft employee on boarding completed!");
         email.setSubject(subject.toString());
         Map<String, Object> emailCtx = new HashMap<>();
-        emailCtx.put("employeeName", "abc");
+        emailCtx.put("employeeName", emp.getFirstName()+" "+emp.getLastName());
         // TODO new template for completed email
         email.setTemplateName("on_board_employee_template.html");
         email.setContext(emailCtx);
