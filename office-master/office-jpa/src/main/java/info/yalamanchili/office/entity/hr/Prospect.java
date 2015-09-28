0d9d1f01e43a29a829a83bb404fbcd47b8951352
radhika.mukkala@sstech.us <>
2015-09-28 20:05:34 +0530

    Added Proper annotations for Prospect in jpa

--- office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/hr/Prospect.java
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/hr/Prospect.java
@@ -10,33 +10,45 @@ package info.yalamanchili.office.entity.hr;
 
 import info.chili.jpa.AbstractEntity;
 import info.yalamanchili.office.entity.profile.Contact;
+import info.yalamanchili.office.entity.profile.Employee;
 import java.util.Date;
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlRootElement;
 import org.hibernate.annotations.ForeignKey;
+import org.hibernate.envers.Audited;
+import org.hibernate.search.annotations.Indexed;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  *
  * @author radhika.mukkala
  */
+@Indexed
+@XmlRootElement
+@Entity
+@Audited
 public class Prospect extends AbstractEntity{
     private static final long serialVersionUID = 1L;
     
-    @OneToOne(cascade = CascadeType.MERGE)
+    @OneToOne(cascade = CascadeType.ALL)
     @ForeignKey(name="FK_Contact_Prospect")
     protected Contact contact;
     
-    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    /*@OneToOne(cascade = CascadeType.ALL)
+    protected Employee emp; */
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
     protected Date startDate;
     
     protected String screenedBy;
     protected String referredBy;
     
-    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Temporal(javax.persistence.TemporalType.DATE)
     protected Date processDocSentDate;
 
     @NotEmpty(message = "{resumeUrl.not.empty.msg}")
