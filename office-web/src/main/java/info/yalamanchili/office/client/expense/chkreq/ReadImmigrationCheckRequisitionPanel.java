6119eb2f4c3f7980bcdb1f00f561b5f211cf0179
phani <phani@192.168.0.195>
2015-09-29 04:57:50 -0400

    check req transfer with employename

--- office-web/src/main/java/info/yalamanchili/office/client/expense/chkreq/ReadImmigrationCheckRequisitionPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/expense/chkreq/ReadImmigrationCheckRequisitionPanel.java
@@ -30,60 +30,64 @@ import java.util.logging.Logger;
  * @author benerji.v
  */
 public class ReadImmigrationCheckRequisitionPanel extends ReadComposite {
-
+    
     private static ReadImmigrationCheckRequisitionPanel instance;
     private static Logger logger = Logger.getLogger(ReadImmigrationCheckRequisitionPanel.class.getName());
     SelectEmployeeWidget selectEmployeeWidgetF = new SelectEmployeeWidget("Employee", false, true);
     SuggestBox employeeSB = new SuggestBox(OfficeWelcome.constants, "employee", "Employee", true, false, Alignment.HORIZONTAL);
-
+    
     public static ReadImmigrationCheckRequisitionPanel instance() {
         return instance;
     }
-
+    
     public ReadImmigrationCheckRequisitionPanel(String id) {
         instance = this;
         initReadComposite(id, "ImmigrationCheckRequisition", OfficeWelcome.constants);
     }
-
+    
     @Override
     public void loadEntity(String entityId) {
         HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        entity = (JSONObject) JSONParser.parseLenient(response);
-                        populateFieldsFromEntity(entity);
-                        populateComments(JSONUtils.toJSONArray(entity.get("items")));
-                    }
-                });
+            @Override
+            public void onResponse(String response) {
+                entity = (JSONObject) JSONParser.parseLenient(response);
+                populateFieldsFromEntity(entity);
+                populateComments(JSONUtils.toJSONArray(entity.get("items")));
+            }
+        });
     }
-
+    
     protected void populateComments(JSONArray items) {
         entityFieldsPanel.add(new ReadAllImmigrationCheckRequisitionItems(items));
         entityFieldsPanel.add(new ReadAllCommentsPanel(getEntityId(), "info.yalamanchili.office.entity.expense.ImmigrationCheckRequisition"));
     }
-
+    
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
         JSONObject emp = (JSONObject) entity.get("employee");
-        employeeSB.setValue(emp.get("firstName").isString().stringValue());
+        if (emp != null) {
+            employeeSB.setValue(emp.get("firstName").isString().stringValue());
+        } else {
+            employeeSB.setValue(JSONUtils.toString(entity, "employeeName"));
+        }
         assignFieldValueFromEntity("attorneyName", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("mailingAddress", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("neededByDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("purpose", entity, DataType.TEXT_AREA_FIELD);
         assignFieldValueFromEntity("caseType", entity, DataType.ENUM_FIELD);
     }
-
+    
     @Override
     protected void addListeners() {
-
+        
     }
-
+    
     @Override
     protected void configure() {
         employeeSB.getLabel().getElement().getStyle().setWidth(145, Style.Unit.PX);
     }
-
+    
     @Override
     protected void addWidgets() {
         entityFieldsPanel.add(employeeSB);
@@ -94,36 +98,36 @@ public class ReadImmigrationCheckRequisitionPanel extends ReadComposite {
         addEnumField("caseType", true, true, ImmigrationCaseType.names(), Alignment.HORIZONTAL);
         alignFields();
     }
-
+    
     @Override
     protected void addWidgetsBeforeCaptionPanel() {
-
+        
     }
-
+    
     @Override
     protected String getURI() {
         return OfficeWelcome.constants.root_url() + "checkrequisition/" + getEntityId();
     }
-
+    
     @Override
     protected boolean enableAudit() {
         return Auth.hasAnyOfRoles(Auth.ROLE.ROLE_EXPENSE);
     }
-
+    
     @Override
     protected String getAuditUrl() {
         return OfficeWelcome.instance().constants.root_url() + "audit/changes/" + "info.yalamanchili.office.entity.expense.ImmigrationCheckRequisition" + "/" + getEntityId();
     }
-
+    
     @Override
     protected boolean enableViewTasks() {
         return Auth.hasAnyOfRoles(Auth.ROLE.ROLE_ADMIN, Auth.ROLE.ROLE_ACCOUNTS_PAYABLE, Auth.ROLE.ROLE_PAYROLL_AND_BENIFITS);
     }
-
+    
     @Override
     protected void displayTasks() {
         String tasksUrl = OfficeWelcome.constants.root_url() + "bpm/tasks/process/";
         tasksDP.setContent(new ReadAllTasks(tasksUrl + JSONUtils.toString(getEntity(), "bpmProcessId") + "/", true));
     }
-
+    
 }
