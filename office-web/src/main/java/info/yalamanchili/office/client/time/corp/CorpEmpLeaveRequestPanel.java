0432934989fff194deaa91ed342d7e6c3b1966cc
Phani Yalamanchili <phani.y@sstech.us>
2015-10-07 17:14:09 -0400

    http://tools.sstech.mobi/jira/browse/SSTO-2368 notify employees on leave.

--- office-web/src/main/java/info/yalamanchili/office/client/time/corp/CorpEmpLeaveRequestPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/time/corp/CorpEmpLeaveRequestPanel.java
@@ -184,7 +184,7 @@ public class CorpEmpLeaveRequestPanel extends CreateComposite {
     };
 
     protected String getEmployeeIdsDropDownUrl() {
-        return URL.encode(OfficeWelcome.constants.root_url() + "employee/employees-by-type/dropdown/0/10000?column=id&column=firstName&column=lastName&employee-type=Corporate Employee");
+        return URL.encode(OfficeWelcome.constants.root_url() + "employee/employees-by-type/dropdown/0/10000?column=employeeId&column=firstName&column=lastName&employee-type=Corporate Employee");
     }
 
     @Override
