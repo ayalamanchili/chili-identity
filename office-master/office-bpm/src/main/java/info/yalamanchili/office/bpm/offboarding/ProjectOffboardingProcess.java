9e121f0fb7b4be6d5f5f3501aeeba2bc717b3a2f
radhika.mukkala@sstech.us <>
2015-10-14 20:13:48 +0530

    associate project offboarding process

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/offboarding/ProjectOffboardingProcess.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/offboarding/ProjectOffboardingProcess.java
@@ -9,12 +9,15 @@
 package info.yalamanchili.office.bpm.offboarding;
 
 import info.chili.email.Email;
+import info.chili.security.domain.CRole;
 import info.chili.spring.SpringContext;
+import info.yalamanchili.office.OfficeRoles;
 import info.yalamanchili.office.bpm.email.GenericTaskCompleteNotification;
 import info.yalamanchili.office.bpm.email.GenericTaskCreateNotification;
 import info.yalamanchili.office.bpm.rule.RuleBasedTaskDelegateListner;
 import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.security.OfficeSecurityService;
+import info.yalamanchili.office.email.MailUtils;
 import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.jms.MessagingService;
 import org.activiti.engine.delegate.DelegateTask;
@@ -45,7 +48,7 @@ public class ProjectOffboardingProcess extends RuleBasedTaskDelegateListner {
     }
 
     public void notifyEmployee(ProjectOffBoardingDto dto) {
-        MessagingService messagingService = (MessagingService) SpringContext.getBean("messagingService");
+       /* MessagingService messagingService = (MessagingService) SpringContext.getBean("messagingService");
         Email email = new Email();
         Employee currentUser = OfficeSecurityService.instance().getCurrentUser();
         Employee associateEmployee = EmployeeDao.instance().findById(dto.getEmployeeId());
@@ -53,6 +56,14 @@ public class ProjectOffboardingProcess extends RuleBasedTaskDelegateListner {
         email.setSubject("Project Offboarding Approved For Employee" + associateEmployee.getFirstName() + " " + associateEmployee.getLastName());
         String messageText = "Hi " + associateEmployee.getFirstName() + " " + associateEmployee.getLastName() + "/n" + "Your Project Offboarding task has been submitted by " + currentUser.getFirstName();
         email.setBody(messageText);
-        messagingService.sendEmail(email);
+        messagingService.sendEmail(email);*/
+        
+        MessagingService messagingService = (MessagingService) SpringContext.getBean("messagingService");
+        Employee associateEmployee = EmployeeDao.instance().findById(dto.getEmployeeId());
+        Email email1 = new Email();
+        email1.addTos(MailUtils.instance().getEmailsAddressesForRoles(OfficeRoles.OfficeRole.ROLE_HR.name(), OfficeRoles.OfficeRole.ROLE_GC_IMMIGRATION.name(), OfficeRoles.OfficeRole.ROLE_H1B_IMMIGRATION.name(), OfficeRoles.OfficeRole.ROLE_RECRUITER.name() ));
+        email1.setSubject("Project Offboarding details for Employee" + associateEmployee.getFirstName() + " " + associateEmployee.getLastName());
+        email1.setBody("Project Offboarding details has been updated for Employee" + associateEmployee.getFirstName() + " " + associateEmployee.getLastName()+"\n Client : "+dto.getClientName()+"\n Vendor : "+dto.getVendorName()+"\n End Date of the project: "+dto.getEndDate()+"\n This employee project has been offboarded:" + associateEmployee.getFirstName() + " " + associateEmployee.getLastName()+ " Please Proceed your actions!!!!!");
+        messagingService.sendEmail(email1);
     }
 }
