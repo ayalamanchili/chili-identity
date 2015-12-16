0e5e9fe89bc83521090d26ef2246f822863311bb
prasanthi ponnaganti <prasanthi.p@sstech.us>
2015-12-16 12:25:27 +0530

    Created new role *Billing/Invoicing* team

--- office-web/src/main/java/info/yalamanchili/office/client/Auth.java
+++ office-web/src/main/java/info/yalamanchili/office/client/Auth.java
@@ -42,7 +42,8 @@ public class Auth {
         ROLE_SELFSERVICE_TRIAGE,
         ROLE_BIS_VIEW,
         ROLE_PROSPECTS_MANAGER,
-        ROLE_CONTRACTS_FULL_VIEW
+        ROLE_CONTRACTS_FULL_VIEW,
+        ROLE_BILLING_AND_INVOICING
     }
 
     public static boolean isAdmin() {
@@ -120,11 +121,11 @@ public class Auth {
         String empType = JSONUtils.toString(emp.get("employeeType"), "name");
         return empType != null && empType.equals("W2 Contractor");
     }
-    
+
     public static boolean isW2Contractor() {
         return JSONUtils.toString(OfficeWelcome.instance().employee.get("employeeType"), "name").equals("W2 Contractor");
     }
-    
+
     public static boolean isItDeveloper() {
         return OfficeWelcome.instance().roles.contains(ROLE.ROLE_IT_DEVELOPER.name());
     }
