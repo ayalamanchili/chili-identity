4d5164165e96352a97ad0c814d3d38b135e7d56d
kramisetti <Kiranmai.Ramisetti@sstech.us>
2015-12-17 20:08:02 +0530

    add unique Expense Report Number to Read and ReadAll Panels

--- office-web/src/main/java/info/yalamanchili/office/client/expensereports/ExpenseFormConstants.java
+++ office-web/src/main/java/info/yalamanchili/office/client/expensereports/ExpenseFormConstants.java
@@ -13,32 +13,32 @@ package info.yalamanchili.office.client.expensereports;
  * @author Madhu.Badiginchala
  */
 public interface ExpenseFormConstants {
-    
+
     int DEFAULT_FIELD_WIDTH = 340;
     int DEFAULT_ITEM_FIELD_WIDTH = 330;
     int DEFAULT_DIFF_FIELD_WIDTH = 250;
     int DEFAULT_CAT_FIELD_WIDTH = 240;
     int DEFAULT_DESC_FIELD_WIDTH = 260;
     // Travel Transportation Panel
-    String EXPENSE_FORM_TYPE="expenseFormType";
-    String EXPENSE_PAYMENT_MODE="expensePaymentMode";
-    String NAME="name";
-    String LOCATION="location";
-    String START_DATE="startDate";
-    String END_DATE="endDate";
-    String PROJECT_NAME="projectName";
-    String PROJECT_NUMBER="projectNumber";
-    String EXPENSE_REPORT="ExpenseReport";
-    String EXPENSE_ITEM="ExpenseItem";
-    String EXPENSE_ITEMS="expenseItems";
-    String EXPENSE_DATE="expenseDate";
-    String PURPOSE="purpose";
-    String DESCRIPTION="description";
-    String REMARK="remark";
-    String AMOUNT="amount";
-    String CATEGORY="category";
-    String TOTAL_EXPENSES="totalExpenses";
-    String EXPENSE_RECEIPT="expenseReceipts";
-    String EXPENSE_MILES="expenseMiles";
-
+    String EXPENSE_FORM_TYPE = "expenseFormType";
+    String EXPENSE_PAYMENT_MODE = "expensePaymentMode";
+    String NAME = "name";
+    String LOCATION = "location";
+    String START_DATE = "startDate";
+    String END_DATE = "endDate";
+    String PROJECT_NAME = "projectName";
+    String PROJECT_NUMBER = "projectNumber";
+    String EXPENSE_REPORT = "ExpenseReport";
+    String EXPENSE_ITEM = "ExpenseItem";
+    String EXPENSE_ITEMS = "expenseItems";
+    String EXPENSE_DATE = "expenseDate";
+    String PURPOSE = "purpose";
+    String DESCRIPTION = "description";
+    String REMARK = "remark";
+    String AMOUNT = "amount";
+    String CATEGORY = "category";
+    String TOTAL_EXPENSES = "totalExpenses";
+    String EXPENSE_RECEIPT = "expenseReceipts";
+    String EXPENSE_MILES = "expenseMiles";
+    String EXPENSE_REPORT_NUMBER = "expenseReportNumber";
 }
