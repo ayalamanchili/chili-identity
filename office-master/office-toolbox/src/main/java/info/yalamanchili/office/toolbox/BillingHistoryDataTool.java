c69d720fc3e1d1118b7087ac5c13bc098b59177d
madhu badiginchala <madhu.badiginchala@sstech.us>
2015-12-17 21:29:26 +0530

    BIS -  Data Migration

--- office-master/office-toolbox/src/main/java/info/yalamanchili/office/toolbox/BillingHistoryDataTool.java
+++ office-master/office-toolbox/src/main/java/info/yalamanchili/office/toolbox/BillingHistoryDataTool.java
@@ -1,3 +1,6 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -114,7 +117,7 @@ public class BillingHistoryDataTool {
     }
 
     protected String getDataFileUrl() {
-        return OfficeServiceConfiguration.instance().getContentManagementLocationRoot() + "BIS_ClientContactData.xlsx";
+        return OfficeServiceConfiguration.instance().getContentManagementLocationRoot() + "BIS_Billinghistory.xlsx";
     }
 
     protected String convertDcimalToWhole(String id) {
