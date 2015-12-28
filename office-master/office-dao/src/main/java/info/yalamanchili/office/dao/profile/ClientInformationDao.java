737b24cf69c5d236c019e03fec57929bd2cfda11
radhika.mukkala@sstech.us <>
2015-12-28 15:41:45 +0530

    pre populate prev proj end date

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/ClientInformationDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/ClientInformationDao.java
@@ -7,10 +7,13 @@ import info.chili.dao.CRUDDao;
 import info.chili.spring.SpringContext;
 import info.yalamanchili.office.cache.OfficeCacheKeys;
 import info.yalamanchili.office.entity.profile.ClientInformation;
+import java.text.SimpleDateFormat;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Scope;
 
 import org.springframework.stereotype.Repository;
@@ -31,6 +34,16 @@ public class ClientInformationDao extends CRUDDao<ClientInformation> {
     public ClientInformation save(ClientInformation entity) {
         return super.save(entity);
     }
+    
+    @Cacheable(value = OfficeCacheKeys.CLIENTINFORMATION, key = "{#root.methodName,#id}")
+    public String queryForPrevProjEndDate(Long id) {
+        Query query1 = getEntityManager().createQuery("from " + entityCls.getCanonicalName() + " WHERE employee.id=:employeeIdParam AND endDate = (select max(endDate) from ClientInformation)", entityCls);
+        query1.setParameter("employeeIdParam", id);
+        ClientInformation ci = (ClientInformation) query1.getSingleResult();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        return sdf.format(ci.getClientProject().getEndDate());
+    }
+
 
     @Override
     public EntityManager getEntityManager() {
