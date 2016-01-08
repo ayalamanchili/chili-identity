0544863bf1051a7008374776922deaeb196a0319
radhika.mukkala@sstech.us <>
2016-01-08 15:12:28 +0530

    Bis Reports

--- office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllVendorLocationPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllVendorLocationPanel.java
@@ -7,16 +7,10 @@ package info.yalamanchili.office.client.reports;
 
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.user.client.Window;
-import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.crud.CRUDReadAllComposite;
 import info.chili.gwt.crud.TableRowOptionsWidget;
-import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.JSONUtils;
-import info.chili.gwt.widgets.GenericPopup;
 import info.yalamanchili.office.client.OfficeWelcome;
-import info.yalamanchili.office.client.TabPanel;
-import info.yalamanchili.office.client.contracts.ReadContractsPanel;
 import java.util.logging.Logger;
 
 /**
@@ -28,31 +22,13 @@ public class ReadAllVendorLocationPanel extends CRUDReadAllComposite {
     private static Logger logger = Logger.getLogger(ReadAllVendorLocationPanel.class.getName());
     public static ReadAllVendorLocationPanel instance;
 
-    public ReadAllVendorLocationPanel() {
-        instance = this;
-        initTable("Contract", OfficeWelcome.constants);
-    }
-
     public ReadAllVendorLocationPanel(JSONArray array) {
         instance = this;
         initTable("Contract", array, OfficeWelcome.constants);
     }
 
-    public ReadAllVendorLocationPanel(String parentId) {
-        instance = this;
-        this.parentId = parentId;
-        initTable("Contract", OfficeWelcome.constants);
-    }
-
     @Override
     public void viewClicked(String entityId) {
-        TabPanel.instance().reportingPanel.entityPanel.clear();
-        TabPanel.instance().reportingPanel.entityPanel.add(new ReadContractsPanel(getEntity(entityId)));
-    }
-
-    @Override
-    protected void onQuickView(int row, String id) {
-        new GenericPopup(new ReadContractsPanel(getEntity(id)), Window.getClientWidth() / 3, 0).show();
     }
 
     @Override
@@ -69,15 +45,6 @@ public class ReadAllVendorLocationPanel extends CRUDReadAllComposite {
 
     @Override
     public void preFetchTable(int start) {
-        // TODO externalize the limit size for read all
-        HttpService.HttpServiceAsync.instance().doGet(getReadAllContractsURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
-                new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String result) {
-                        logger.info(result);
-                        postFetchTable(result);
-                    }
-                });
     }
 
     @Override
@@ -95,7 +62,6 @@ public class ReadAllVendorLocationPanel extends CRUDReadAllComposite {
         for (int i = 1; i <= entities.size(); i++) {
             JSONObject entity = (JSONObject) entities.get(i - 1);
             addOptionsWidget(i, entity);
-            OfficeWelcome.instance().logger.info(entity.toString());
             table.setText(i, 1, JSONUtils.toString(entity, "name"));
             table.setText(i, 2, JSONUtils.toString(entity, "street1") + JSONUtils.toString(entity, "street2"));
             table.setText(i, 3, JSONUtils.toString(entity, "city"));
@@ -108,19 +74,8 @@ public class ReadAllVendorLocationPanel extends CRUDReadAllComposite {
         createOptionsWidget(TableRowOptionsWidget.OptionsType.READ, row, JSONUtils.toString(entity, "id"));
     }
 
-    private String getReadAllContractsURL(Integer start, String limit) {
-        return OfficeWelcome.constants.root_url() + "contract/" + start.toString() + "/" + limit.toString();
-    }
-
     @Override
     protected boolean enableQuickView() {
-        return true;
+        return false;
     }
-
-    @Override
-    protected boolean enablePersistedQuickView() {
-        return true;
-    }
-
 }
-
