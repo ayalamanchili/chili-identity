105008b830d9391f4c239acb57d3ab42db443cb3
radhika.mukkala@sstech.us <>
2016-01-08 15:10:45 +0530

    Bis Reports

--- office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllClientReportPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllClientReportPanel.java
@@ -8,19 +8,12 @@ package info.yalamanchili.office.client.reports;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.user.client.Window;
-import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.crud.CRUDReadAllComposite;
 import info.chili.gwt.crud.TableRowOptionsWidget;
 import info.chili.gwt.date.DateUtils;
-import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.FormatUtils;
 import info.chili.gwt.utils.JSONUtils;
-import info.chili.gwt.widgets.GenericPopup;
 import info.yalamanchili.office.client.OfficeWelcome;
-import info.yalamanchili.office.client.TabPanel;
-import info.yalamanchili.office.client.contracts.ClientInformationStatus;
-import info.yalamanchili.office.client.contracts.ReadContractsPanel;
 import java.util.logging.Logger;
 
 /**
@@ -32,31 +25,13 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
     private static Logger logger = Logger.getLogger(ReadAllClientReportPanel.class.getName());
     public static ReadAllClientReportPanel instance;
 
-    public ReadAllClientReportPanel() {
-        instance = this;
-        initTable("Contract", OfficeWelcome.constants);
-    }
-
     public ReadAllClientReportPanel(JSONArray array) {
         instance = this;
         initTable("Contract", array, OfficeWelcome.constants);
     }
 
-    public ReadAllClientReportPanel(String parentId) {
-        instance = this;
-        this.parentId = parentId;
-        initTable("Contract", OfficeWelcome.constants);
-    }
-
     @Override
     public void viewClicked(String entityId) {
-        TabPanel.instance().reportingPanel.entityPanel.clear();
-        TabPanel.instance().reportingPanel.entityPanel.add(new ReadContractsPanel(getEntity(entityId)));
-    }
-
-    @Override
-    protected void onQuickView(int row, String id) {
-        new GenericPopup(new ReadContractsPanel(getEntity(id)), Window.getClientWidth() / 3, 0).show();
     }
 
     @Override
@@ -73,15 +48,6 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
 
     @Override
     public void preFetchTable(int start) {
-        // TODO externalize the limit size for read all
-        HttpService.HttpServiceAsync.instance().doGet(getReadAllContractsURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
-                new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String result) {
-                        logger.info(result);
-                        postFetchTable(result);
-                    }
-                });
     }
 
     @Override
@@ -94,8 +60,7 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
         table.setText(0, 5, getKeyValue("StartDate"));
         table.setText(0, 6, getKeyValue("EndDate"));
         table.setText(0, 7, getKeyValue("SubContractor"));
-        table.setText(0, 8, getKeyValue("SubContractorPayRate"));
-        table.setText(0, 9, getKeyValue("Status"));
+        table.setText(0, 8, getKeyValue("SC-PayRate"));
     }
 
     @Override
@@ -103,7 +68,6 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
         for (int i = 1; i <= entities.size(); i++) {
             JSONObject entity = (JSONObject) entities.get(i - 1);
             addOptionsWidget(i, entity);
-            OfficeWelcome.instance().logger.info(entity.toString());
             table.setText(i, 1, JSONUtils.toString(entity, "employee"));
             table.setText(i, 2, JSONUtils.toString(entity, "consultantJobTitle"));
             table.setText(i, 3, JSONUtils.toString(entity, "vendor"));
@@ -112,7 +76,6 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
             table.setText(i, 6, DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
             table.setText(i, 7, JSONUtils.toString(entity, "subContractorName"));
             table.setText(i, 8, FormatUtils.formarCurrency(JSONUtils.toString(entity, "subcontractorPayRate")));
-            setEnumColumn(i, 9, entity, ClientInformationStatus.class.getSimpleName(), "status");
         }
     }
 
@@ -121,18 +84,9 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
         createOptionsWidget(TableRowOptionsWidget.OptionsType.READ, row, JSONUtils.toString(entity, "id"));
     }
 
-    private String getReadAllContractsURL(Integer start, String limit) {
-        return OfficeWelcome.constants.root_url() + "contract/" + start.toString() + "/" + limit.toString();
-    }
-
     @Override
     protected boolean enableQuickView() {
-        return true;
-    }
-
-    @Override
-    protected boolean enablePersistedQuickView() {
-        return true;
+        return false;
     }
 }
 
