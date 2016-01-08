6530081dac667f60d6772714b03f7ea3c9b1918a
radhika.mukkala@sstech.us <>
2016-01-08 15:12:02 +0530

    Bis Reports

--- office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllEmployeesJoinedPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllEmployeesJoinedPanel.java
@@ -8,19 +8,12 @@ package info.yalamanchili.office.client.reports;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.user.client.Window;
-import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.crud.CRUDReadAllComposite;
 import info.chili.gwt.crud.TableRowOptionsWidget;
 import info.chili.gwt.date.DateUtils;
-import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.FormatUtils;
 import info.chili.gwt.utils.JSONUtils;
-import info.chili.gwt.widgets.GenericPopup;
 import info.yalamanchili.office.client.OfficeWelcome;
-import info.yalamanchili.office.client.TabPanel;
-import info.yalamanchili.office.client.contracts.ReadContractsPanel;
-import info.yalamanchili.office.client.profile.employee.ReadEmployeePanel;
 import java.util.logging.Logger;
 
 /**
@@ -32,31 +25,13 @@ public class ReadAllEmployeesJoinedPanel extends CRUDReadAllComposite {
     private static Logger logger = Logger.getLogger(ReadAllEmployeesJoinedPanel.class.getName());
     public static ReadAllEmployeesJoinedPanel instance;
 
-    public ReadAllEmployeesJoinedPanel() {
-        instance = this;
-        initTable("Employee", OfficeWelcome.constants);
-    }
-
     public ReadAllEmployeesJoinedPanel(JSONArray array) {
         instance = this;
         initTable("Employee", array, OfficeWelcome.constants);
     }
 
-    public ReadAllEmployeesJoinedPanel(String parentId) {
-        instance = this;
-        this.parentId = parentId;
-        initTable("Employee", OfficeWelcome.constants);
-    }
-
     @Override
     public void viewClicked(String entityId) {
-        TabPanel.instance().reportingPanel.entityPanel.clear();
-        TabPanel.instance().reportingPanel.entityPanel.add(new ReadEmployeePanel(getEntity(entityId)));
-    }
-
-    @Override
-    protected void onQuickView(int row, String id) {
-        new GenericPopup(new ReadEmployeePanel(getEntity(id)), Window.getClientWidth() / 3, 0).show();
     }
 
     @Override
@@ -73,16 +48,6 @@ public class ReadAllEmployeesJoinedPanel extends CRUDReadAllComposite {
 
     @Override
     public void preFetchTable(int start) {
-        // TODO externalize the limit size for read all
-        logger.info("urllllllllll :"+getReadAllContractsURL(start, OfficeWelcome.constants.tableSize()));
-        HttpService.HttpServiceAsync.instance().doGet(getReadAllContractsURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
-                new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String result) {
-                        logger.info(result);
-                        postFetchTable(result);
-                    }
-                });
     }
 
     @Override
@@ -91,7 +56,7 @@ public class ReadAllEmployeesJoinedPanel extends CRUDReadAllComposite {
         table.setText(0, 1, getKeyValue("Employee"));
         table.setText(0, 2, getKeyValue("Joining Date"));
         table.setText(0, 3, getKeyValue("Relieving Date"));
-        table.setText(0, 4, getKeyValue("Phone No"));
+        table.setText(0, 4, getKeyValue("Phone Number"));
     }
 
     @Override
@@ -99,7 +64,7 @@ public class ReadAllEmployeesJoinedPanel extends CRUDReadAllComposite {
         for (int i = 1; i <= entities.size(); i++) {
             JSONObject entity = (JSONObject) entities.get(i - 1);
             addOptionsWidget(i, entity);
-            table.setText(i, 1, JSONUtils.toString(entity, "firstName")+" "+JSONUtils.toString(entity, "lastName"));
+            table.setText(i, 1, JSONUtils.toString(entity, "firstName") + " " + JSONUtils.toString(entity, "lastName"));
             table.setText(i, 2, DateUtils.getFormatedDate(JSONUtils.toString(entity, "startDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
             table.setText(i, 3, DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
             table.setText(i, 4, FormatUtils.formatPhoneNumber(JSONUtils.toString(entity, "phoneNumber")));
@@ -111,16 +76,8 @@ public class ReadAllEmployeesJoinedPanel extends CRUDReadAllComposite {
         createOptionsWidget(TableRowOptionsWidget.OptionsType.READ, row, JSONUtils.toString(entity, "id"));
     }
 
-    private String getReadAllContractsURL(Integer start, String limit) {
-        return OfficeWelcome.constants.root_url() + "employee/" + start.toString() + "/" + limit.toString();
-    }
-
     @Override
     protected boolean enableQuickView() {
-        return true;
-    }
-    @Override
-    protected boolean enablePersistedQuickView() {
-        return true;
+        return false;
     }
 }
