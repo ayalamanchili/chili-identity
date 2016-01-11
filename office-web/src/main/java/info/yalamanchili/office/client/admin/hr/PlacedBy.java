eef1d45e57b49f545472ddc9536148d3de15ed46
radhika.mukkala@sstech.us <>
2016-01-11 11:06:12 +0530

    Prospects New Fields

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/hr/PlacedBy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.hr;
+
+/**
+ *
+ * @author radhika.mukkala
+ */
+public enum PlacedBy {
+
+    Own_Placement, By_Recruiter, Corporate_Solutions_Team;
+
+    public static String[] names() {
+        PlacedBy[] values = values();
+        String[] names = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            names[i] = values[i].name();
+        }
+        return names;
+    }
+}
