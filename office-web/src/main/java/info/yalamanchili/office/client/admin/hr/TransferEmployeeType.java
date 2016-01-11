6454ff3ec9f2796f83df93f05229ed1b6fc2a7e2
radhika.mukkala@sstech.us <>
2016-01-11 11:06:28 +0530

    Prospects New Fields

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/hr/TransferEmployeeType.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.hr;
+
+/**
+ *
+ * @author radhika.mukkala
+ */
+public enum TransferEmployeeType {
+
+    Corporate_Employee, Field_Employee;
+
+    public static String[] names() {
+        TransferEmployeeType[] values = values();
+        String[] names = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            names[i] = values[i].name();
+        }
+        return names;
+    }
+}
