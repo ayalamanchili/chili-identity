c279a19b395d3a358a0d02082ac31c627f2f7bff
radhika.mukkala@sstech.us <>
2016-01-14 18:15:38 +0530

    prospects reports

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/hr/ProspectDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/hr/ProspectDao.java
@@ -8,14 +8,19 @@
  */
 package info.yalamanchili.office.dao.hr;
 
+import com.google.common.base.Strings;
 import info.chili.dao.CRUDDao;
 import info.chili.service.jrs.types.Entry;
 import info.yalamanchili.office.entity.hr.PetitionFor;
 import info.yalamanchili.office.entity.hr.Prospect;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TemporalType;
 import javax.persistence.TypedQuery;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
@@ -45,9 +50,45 @@ public class ProspectDao extends CRUDDao<Prospect> {
         Query findAllQuery = getEntityManager().createQuery("SELECT NEW " + Entry.class.getCanonicalName() + "(prospect.id,prospect.contact.firstName,prospect.contact.lastName)" + " FROM " + Prospect.class.getCanonicalName() + " prospect)");
         return findAllQuery.getResultList();
     }
+    
+    public List<Entry> searchforfirstname() {
+        Query findAllQuery = getEntityManager().createQuery("SELECT NEW " + Entry.class.getCanonicalName() + "(prospect.id,prospect.contact.firstName)" + " FROM " + Prospect.class.getCanonicalName() + " prospect)");
+        return findAllQuery.getResultList();
+    }
+    
+    public List<Entry> searchforlastname() {
+        Query findAllQuery = getEntityManager().createQuery("SELECT NEW " + Entry.class.getCanonicalName() + "(prospect.id,prospect.contact.lastName)" + " FROM " + Prospect.class.getCanonicalName() + " prospect)");
+        return findAllQuery.getResultList();
+    }
 
-    public void report(ProspectReportDto dto) {
+    public List<Prospect> report(ProspectReportDto dto) {
 //TODO implement report
+        String searchQuery = getSearchQuery(dto);
+        TypedQuery<Prospect> query = em.createQuery(searchQuery, Prospect.class);
+        if (dto.getJoiningDateFrom() != null) {
+            query.setParameter("startDateParam", dto.getJoiningDateFrom(), TemporalType.DATE);
+        }
+        if (dto.getJoiningDateTo() != null) {
+            query.setParameter("endDateParam", dto.getJoiningDateTo(), TemporalType.DATE);
+        }
+        return query.getResultList();
+    }
+
+    protected String getSearchQuery(ProspectReportDto searchDto) {
+        //TODO should we filter search query by date like reports?
+        StringBuilder queryStr = new StringBuilder();
+        queryStr.append("SELECT p from ").append(Prospect.class.getCanonicalName());
+        queryStr.append(" p where ");
+        if (!Strings.isNullOrEmpty(searchDto.getStatus().name())) {
+            queryStr.append("p.status = '").append(searchDto.getStatus().name().trim()).append("' ").append(" and ");
+        }
+        if ((searchDto.getJoiningDateFrom()) != null) {
+            queryStr.append("p.dateOfJoining BETWEEN :startDateParam and ");
+        }
+        if ((searchDto.getJoiningDateTo()) != null) {
+            queryStr.append(":endDateParam and ");
+        }
+        return queryStr.toString().substring(0, queryStr.toString().lastIndexOf("and"));
     }
 
     public ProspectGraphDto graph() {
