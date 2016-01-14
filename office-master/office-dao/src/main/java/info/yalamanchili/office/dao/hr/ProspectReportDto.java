c868ab75a306de5cc390ed809a376a504dfff2f3
radhika.mukkala@sstech.us <>
2016-01-14 18:15:49 +0530

    prospects reports

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/hr/ProspectReportDto.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/hr/ProspectReportDto.java
@@ -10,6 +10,8 @@ package info.yalamanchili.office.dao.hr;
 
 import info.yalamanchili.office.entity.hr.ProspectStatus;
 import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
