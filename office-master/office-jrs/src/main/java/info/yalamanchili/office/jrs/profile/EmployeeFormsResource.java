76a0504cad9fdcc7871f360835f78805d82e36f0
radhika.mukkala@sstech.us <>
2016-02-04 18:59:42 +0530

    Employee Onboarding process enhancements

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeFormsResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeFormsResource.java
@@ -8,20 +8,20 @@
  */
 package info.yalamanchili.office.jrs.profile;
 
+import info.chili.dao.CRUDDao;
+import info.chili.jpa.validation.Validate;
 import info.yalamanchili.office.dao.expense.BankAccountDao;
 import info.yalamanchili.office.dao.profile.EmployeeDao;
-import info.yalamanchili.office.dao.profile.ext.DependentDao;
-import info.yalamanchili.office.dao.profile.ext.EmployeeAdditionalDetailsDao;
 import info.yalamanchili.office.dto.onboarding.JoiningFormsDto;
 import info.yalamanchili.office.entity.expense.BankAccount;
-import info.yalamanchili.office.entity.profile.Address;
 import info.yalamanchili.office.entity.profile.Employee;
-import info.yalamanchili.office.entity.profile.ext.Dependent;
-import info.yalamanchili.office.entity.profile.ext.EmployeeAdditionalDetails;
+import info.yalamanchili.office.jrs.CRUDResource;
 import info.yalamanchili.office.profile.EmployeeFormsService;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,6 +31,9 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 /**
  *
@@ -42,7 +45,7 @@ import javax.ws.rs.core.Response;
 @Scope("request")
 @Produces("application/json")
 @Consumes("application/json")
-public class EmployeeFormsResource {
+public class EmployeeFormsResource extends CRUDResource<BankAccount> {
 
     @Autowired
     protected EmployeeFormsService employeeFormsService;
@@ -56,6 +59,86 @@ public class EmployeeFormsResource {
         }
         BankAccount ba = BankAccountDao.instance().find(emp);
         return ba;
+    }
+    
+    @GET
+    @Override
+    @Path("/{id}")
+    public BankAccount read(@PathParam("id") Long id) {
+        return BankAccountDao.instance().find(id);
+    }
+    
+    @GET
+    @Path("/achs/{id}")
+    public BankAccountTable getBankAccounts(@PathParam("id") Long employeeId) {
+        BankAccountTable table = new BankAccountTable();
+        Employee emp = null;
+        List<BankAccount> accs = new ArrayList();
+        if (employeeId != null) {
+            emp = EmployeeDao.instance().findById(employeeId);
+        }
+        accs.addAll(BankAccountDao.instance().findAll(emp));
+        table.setEntities(accs);
+        table.setSize((long)accs.size());
+        return table;
+    }
+
+
+    @PUT
+    @Path("/ach-delete/{id}")
+    public void deleteBankAccount(@PathParam("id") Long employeeId) {
+        Employee emp = null;
+        if (employeeId != null) {
+            emp = EmployeeDao.instance().findById(employeeId);
+        }
+        BankAccount ba = BankAccountDao.instance().find(emp);
+        if (ba != null) {
+            BankAccountDao.instance().delete(ba);
+        }
+
+    }
+
+    @PUT
+    @Path("/ach-save/{id}")
+    @Validate
+    public BankAccount save(@PathParam("id") Long employeeId, BankAccount entity) {
+        Employee emp = EmployeeDao.instance().findById(employeeId);
+       return BankAccountDao.instance().save(entity, employeeId, emp.getClass().getCanonicalName());
+    }
+
+    @PUT
+    @Path("/update")
+    @Validate
+    public BankAccount update(BankAccount entity) {
+        BankAccount result = BankAccountDao.instance().findById(entity.getId());
+        result.setBankAccountNumber(entity.getBankAccountNumber());
+        result.setBankRoutingNumber(entity.getBankRoutingNumber());
+        if (entity.getAccountFirstName() != null) {
+            result.setAccountFirstName(entity.getAccountFirstName());
+        } else {
+            result.setAccountFirstName(null);
+        }
+        if (entity.getAccountLastName() != null) {
+            result.setAccountLastName(entity.getAccountLastName());
+        } else {
+            result.setAccountLastName(null);
+        }
+        if (entity.getBankName() != null) {
+            result.setBankName(entity.getBankName());
+        } else {
+            result.setBankName(null);
+        }
+        if (entity.getBankAddress1() != null) {
+            result.setBankAddress1(entity.getBankAddress1());
+        } else {
+            result.setBankAddress1(null);
+        }
+        if (entity.getBankAddress2() != null) {
+            result.setBankAddress2(entity.getBankAddress2());
+        } else {
+            result.setBankAddress2(null);
+        }
+        return result;
     }
 
     @GET
@@ -84,4 +167,33 @@ public class EmployeeFormsResource {
         return employeeFormsService.printJoiningForm(emp);
     }
 
+    @Override
+    public CRUDDao getDao() {
+        return null;
+    }
+    
+    @XmlRootElement
+    @XmlType
+    public static class BankAccountTable implements java.io.Serializable {
+
+        protected Long size;
+        protected List<BankAccount> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<BankAccount> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<BankAccount> entities) {
+            this.entities = entities;
+        }
+    }
 }
