b54f2e5cf858f2b09473e45da991904699ab2626
radhika.mukkala@sstech.us <>
2016-02-04 19:04:15 +0530

    Employee Onboarding process enhancements

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/emergencycnt/ReadAllDependentsPanel.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.emergencycnt;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.CRUDReadAllComposite;
+import info.chili.gwt.crud.TableRowOptionsWidget;
+import info.chili.gwt.date.DateUtils;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.profile.employee.DependentOptionsPanel;
+import info.yalamanchili.office.client.profile.employee.TreeEmployeePanel;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author radhika.mukkala
+ */
+public class ReadAllDependentsPanel extends CRUDReadAllComposite {
+
+    private static Logger logger = Logger.getLogger(ReadAllDependentsPanel.class.getName());
+    private static ReadAllDependentsPanel instance;
+    protected String targetClassName;
+
+    public static ReadAllDependentsPanel instance() {
+        return instance;
+    }
+
+    public ReadAllDependentsPanel(String parentId, String targetClassName) {
+        instance = this;
+        this.parentId = parentId;
+        this.targetClassName = targetClassName;
+        initTable("Dependent", OfficeWelcome.constants);
+    }
+    
+    public ReadAllDependentsPanel(String parentId) {
+        instance = this;
+        this.parentId = parentId;
+        initTable("Dependent", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void preFetchTable(int start) {
+        HttpService.HttpServiceAsync.instance().doGet(getReadAllURL(start, OfficeWelcome.constants.tableSize()), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String result) {
+                        postFetchTable(result);
+                    }
+                });
+
+    }
+
+    @Override
+    public void createTableHeader() {
+        table.setText(0, 0, getKeyValue("Table_Action"));
+        table.setText(0, 1, getKeyValue("First Name"));
+        table.setText(0, 2, getKeyValue("Last Name"));
+        table.setText(0, 3, getKeyValue("Date Of Birth"));
+        table.setText(0, 4, getKeyValue("Relation"));
+    }
+
+    @Override
+    public void fillData(JSONArray entities) {
+        for (int i = 1; i <= entities.size(); i++) {
+            JSONObject entity = (JSONObject) entities.get(i - 1);
+            addOptionsWidget(i, entity);
+            table.setText(i, 1, JSONUtils.toString(entity, "dfirstName"));
+            table.setText(i, 2, JSONUtils.toString(entity, "dlastName"));
+            table.setText(i, 3, DateUtils.getFormatedDate(JSONUtils.toString(entity, "ddateOfBirth"), DateTimeFormat.PredefinedFormat.DATE_MEDIUM));
+            table.setText(i, 4, JSONUtils.toString(entity, "relationship"));
+        }
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        if (Auth.isAdmin() || Auth.isHR()) {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ_UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+        } else {
+            createOptionsWidget(TableRowOptionsWidget.OptionsType.READ, row, JSONUtils.toString(entity, "id"));
+        }
+    }
+
+    // TODO move to composite
+    public String getReadAllURL(Integer start, String limit) {
+        return OfficeWelcome.constants.root_url() + "employee/dependents/" + parentId + "/" + start.toString()
+                + "/" + limit.toString();
+    }
+
+    @Override
+    public void viewClicked(String entityId) {
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadDependentPanel(entityId));
+
+    }
+
+    @Override
+    public void deleteClicked(String entityId) {
+        logger.info("get delete url ... "+getDeleteURL(entityId));
+        HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String arg0) {
+                        postDeleteSuccess();
+                    }
+                });
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully Deleted Dependent Information");
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllDependentsPanel(TreeEmployeePanel.instance().getEntityId()));
+        TabPanel.instance().myOfficePanel.entityPanel.add(new DependentOptionsPanel());
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+        TabPanel.instance().myOfficePanel.entityPanel.clear();
+        TabPanel.instance().myOfficePanel.entityPanel.add(new UpdateDependentPanel(getEntity(entityId)));
+
+    }
+
+    protected String getDeleteURL(String entityId) {
+        return OfficeWelcome.instance().constants.root_url() + "dependent/delete/" + entityId;
+    }
+}
