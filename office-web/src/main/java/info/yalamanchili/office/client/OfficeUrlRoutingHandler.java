f5fe44a3542a382645dde00e12d872a984cade60
ramana lukalapu <ramana.lukalapu@sstech.us>
2016-02-09 12:45:15 +0530

    support for comment email links for Advance Requisition

--- office-web/src/main/java/info/yalamanchili/office/client/OfficeUrlRoutingHandler.java
+++ office-web/src/main/java/info/yalamanchili/office/client/OfficeUrlRoutingHandler.java
@@ -52,6 +52,9 @@ public class OfficeUrlRoutingHandler implements ValueChangeHandler<String> {
                 case "info.yalamanchili.office.entity.expense.ImmigrationCheckRequisition":
                     checkRequisitionNavigation(params);
                     break;
+                case "info.yalamanchili.office.entity.expense.AdvanceRequisition":
+                    advanceRequisitionNavigation(params);
+                    break;
                 //TODO add navigation to add entities that have comment
             }
         }
@@ -71,6 +74,12 @@ public class OfficeUrlRoutingHandler implements ValueChangeHandler<String> {
         TabPanel.instance().tabPanel.selectTab(TabPanel.instance().expensePanel, false);
         ReadAllImmigrationCheckRequisitionPanel readAllCheckRequisition = new ReadAllImmigrationCheckRequisitionPanel(new JSONArray());
         readAllCheckRequisition.viewClicked(params.get("id"));
+    }
+    
+    protected void advanceRequisitionNavigation(Map<String, String> params) {
+        TabPanel.instance().tabPanel.selectTab(TabPanel.instance().expensePanel, false);
+        ReadAllAdvanceRequisitionPanel readAllAdvanceRequisition = new ReadAllAdvanceRequisitionPanel(new JSONArray());
+        readAllAdvanceRequisition.viewClicked(params.get("id"));
     }
 
     protected void taskNavigation(Map<String, String> params) {
