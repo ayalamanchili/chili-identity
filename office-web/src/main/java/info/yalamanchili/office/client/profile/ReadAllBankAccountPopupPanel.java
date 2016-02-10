e67863865c336dd00d03bf26ea5b9d3852328627
kramisetti <Kiranmai.Ramisetti@sstech.us>
2016-02-10 17:04:14 +0530

    Option to update ACH Details to the employee from profile panel

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/ReadAllBankAccountPopupPanel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile;
+
+import com.google.gwt.json.client.JSONObject;
+import info.chili.gwt.crud.CreateComposite;
+import info.chili.gwt.crud.TableRowOptionsWidget;
+import info.chili.gwt.utils.JSONUtils;
+import info.chili.gwt.widgets.GenericPopup;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.profile.employee.ReadAllBankAcctWidget;
+
+/**
+ *
+ * @author Kiranmai.Ramisetti
+ */
+class ReadAllBankAccountPopupPanel extends ReadAllBankAcctWidget {
+
+    public ReadAllBankAccountPopupPanel(String parentId) {
+        super(parentId);
+    }
+
+    @Override
+    protected void addOptionsWidget(int row, JSONObject entity) {
+        createOptionsWidget(TableRowOptionsWidget.OptionsType.UPDATE_DELETE, row, JSONUtils.toString(entity, "id"));
+    }
+
+    @Override
+    public void updateClicked(String entityId) {
+        UpdateBankAccountPopupPanel updateBankAcct = new UpdateBankAccountPopupPanel(getEntity(entityId));
+        new GenericPopup(updateBankAcct).show();
+    }
+
+    @Override
+    public void postDeleteSuccess() {
+        new ResponseStatusWidget().show("Successfully Deleted BankAccount Information");
+        ProfileHome.instance().refreshBankAccountPanel();
+    }
+
+    @Override
+    protected void configureCreateButton() {
+        createButton.setVisible(true);
+    }
+
+    @Override
+    protected void createButtonClicked() {
+        CreateBankAccountPopupPanel createPanel = new CreateBankAccountPopupPanel(CreateComposite.CreateCompositeType.CREATE);;
+        new GenericPopup(createPanel).show();
+    }
+}
+
