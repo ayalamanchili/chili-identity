96ffe6e23eb4d32c32ce9c90e0eb111117383496
radhika.mukkala@sstech.us <>
2016-03-02 17:21:35 +0530

    BIS Reports Performance tuning - multiple projects reports

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/client/ContractReportResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/client/ContractReportResource.java
@@ -8,11 +8,20 @@
  */
 package info.yalamanchili.office.jrs.client;
 
-import info.yalamanchili.office.cache.OfficeCacheKeys;
+import info.chili.reporting.ReportGenerator;
 import info.yalamanchili.office.client.ContractReportService;
+import info.yalamanchili.office.client.ContractService;
+import info.yalamanchili.office.config.OfficeServiceConfiguration;
 import info.yalamanchili.office.dao.profile.EmployeeDao.EmployeeTable;
 import info.yalamanchili.office.dao.security.OfficeSecurityService;
+import info.yalamanchili.office.dto.client.ContractDto;
+import info.yalamanchili.office.dto.client.ContractDto.ContractTable;
+import info.yalamanchili.office.dto.client.ContractSearchDto;
+import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.jms.MessagingService;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
@@ -24,8 +33,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,23 +49,47 @@ import org.springframework.transaction.annotation.Transactional;
 @Consumes("application/json")
 @Transactional(readOnly = true)
 public class ContractReportResource {
-    
+
     @Autowired
     protected Mapper mapper;
     @PersistenceContext
     protected EntityManager em;
-    
-    
+
     @PUT
     @Path("/search-emp-between-days/{start}/{limit}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CONTRACTS_FULL_VIEW')")
     public EmployeeTable employeeJoinedOrLeftSearch(@PathParam("start") int start, @PathParam("limit") int limit, @QueryParam("startDate") Date startDate, @QueryParam("endDate") Date endDate, @QueryParam("value") String value) {
         return ContractReportService.instance().empJoinedLeftInAPeriod(start, limit, startDate, endDate, value);
     }
-    
+
     @GET
     @Path("/search-emp-between-days-report")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CONTRACTS_FULL_VIEW')")
     public void employeeJoinedOrLeftReport(@QueryParam("startDate") Date startDate, @QueryParam("endDate") Date endDate, @QueryParam("value") String value) {
-        String email = OfficeSecurityService.instance().getCurrentUser().getPrimaryEmail().getEmail();
+        String email = currentEmpEmail();
         ContractReportService.instance().empJoinedLeftInAPeriodReport(0, 10000, startDate, endDate, value, email);
+    }
+
+    @PUT
+    @Path("/multiple-cpds")
+    public ContractTable getMultipleCpds() {
+        List<ContractDto> dtos = ContractReportService.instance().getMultipleCpds();
+        ContractDto.ContractTable resultTable = new ContractDto.ContractTable();
+        if (dtos != null && dtos.size() > 0) {
+            resultTable.setEntities(dtos);
+        }
+        return resultTable;
+    }
+
+    @GET
+    @Path("/multiple-cpds-report")
+    public void multipleCPDsReport() {
+        String email = currentEmpEmail();
+        ContractTable table = getMultipleCpds();
+        ContractReportService.instance().multipleCPDsReport(table, email);
+    }
+
+    private String currentEmpEmail() {
+        return OfficeSecurityService.instance().getCurrentUser().getPrimaryEmail().getEmail();
     }
 }
