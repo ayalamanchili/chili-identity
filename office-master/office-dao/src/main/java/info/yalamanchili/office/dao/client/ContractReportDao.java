5d2079ba05b2396b1036e0dbda4ad63b5da55abb
radhika.mukkala@sstech.us <>
2016-03-04 14:32:05 +0530

    BIS Reports Performace tuning - ended projects report

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/client/ContractReportDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/client/ContractReportDao.java
@@ -21,8 +21,6 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 /**
  *
  * @author radhika.mukkala
