fd20a9d9503060701c952bd6568507c15bd9db8a
radhika.mukkala@sstech.us <>
2016-03-04 14:32:57 +0530

    BIS Reports Performace tuning - ended projects report

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/ClientInformationDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/ClientInformationDao.java
@@ -8,17 +8,12 @@ import info.chili.spring.SpringContext;
 import info.yalamanchili.office.cache.OfficeCacheKeys;
 import info.yalamanchili.office.entity.profile.ClientInformation;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.annotation.Scope;
-
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Scope("prototype")
@@ -48,27 +43,7 @@ public class ClientInformationDao extends CRUDDao<ClientInformation> {
             return null;
         }
     }
-
-    @Transactional(readOnly = true)
-    public List<ClientInformation> queryForProjEndBetweenDays(int start, int limit, Date startDate, Date endDate, String value) {
-        if (value.equalsIgnoreCase("enddate")) {
-            Query findAllQuery = getEntityManager().createQuery("from " + ClientInformation.class.getCanonicalName() + " WHERE endDate>=:startDateParam AND endDate<=:endDateParam", entityCls);
-            findAllQuery.setParameter("startDateParam", startDate);
-            findAllQuery.setParameter("endDateParam", endDate);
-            findAllQuery.setFirstResult(start);
-            findAllQuery.setMaxResults(limit);
-            return findAllQuery.getResultList();
-        }
-        else {
-            Query findAllQuery = getEntityManager().createQuery("from " + ClientInformation.class.getCanonicalName() + " WHERE startDate>=:startDateParam AND startDate<=:endDateParam", entityCls);
-            findAllQuery.setParameter("startDateParam", startDate);
-            findAllQuery.setParameter("endDateParam", endDate);
-            findAllQuery.setFirstResult(start);
-            findAllQuery.setMaxResults(limit);
-            return findAllQuery.getResultList();
-        }
-    }
-
+    
     @Override
     public EntityManager getEntityManager() {
         return em;
