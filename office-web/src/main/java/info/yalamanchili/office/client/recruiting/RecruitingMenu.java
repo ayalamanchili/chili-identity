c2f9a1d45749b6edd2d1000996f3e3ee23d51d94
ramana lukalapu <ramana.lukalapu@sstech.us>
2016-03-04 20:09:03 +0530

    cleared the sidepanels

--- office-web/src/main/java/info/yalamanchili/office/client/recruiting/RecruitingMenu.java
+++ office-web/src/main/java/info/yalamanchili/office/client/recruiting/RecruitingMenu.java
@@ -13,16 +13,13 @@ import com.google.gwt.user.client.Command;
 import info.chili.gwt.widgets.CMenuBar;
 import info.yalamanchili.office.client.Auth;
 import info.yalamanchili.office.client.TabPanel;
-import info.yalamanchili.office.client.practice.PracticeSidePanel;
 import info.yalamanchili.office.client.practice.ReadAllPracticePanel;
 import info.yalamanchili.office.client.profile.certification.CertificationSidePanel;
 import info.yalamanchili.office.client.profile.certification.ReadAllCertificationsPanel;
 import info.yalamanchili.office.client.profile.skill.ReadAllSkillsPanel;
 import info.yalamanchili.office.client.profile.skill.SkillSidePanel;
 import info.yalamanchili.office.client.profile.technologyGroup.ReadAllTechnologyGroupPanel;
-import info.yalamanchili.office.client.profile.technologyGroup.TechnologyGroupSidePanel;
 import info.yalamanchili.office.client.recruiting.reports.ResumeSearchWidget;
-import info.yalamanchili.office.client.recruiting.reports.SkillSetReportSidePanel;
 
 /**
  *
@@ -57,7 +54,6 @@ public class RecruitingMenu extends CMenuBar {
             TabPanel.instance().getRecruitingPanel().entityPanel.clear();
             TabPanel.instance().getRecruitingPanel().sidePanel.clear();
             TabPanel.instance().getRecruitingPanel().entityPanel.add(new ReadAllPracticePanel());
-            //TabPanel.instance().getRecruitingPanel().sidePanelTop.add(new PracticeSidePanel());
         }
     };
     static Command technologyGroupMaintainenceCmd = new Command() {
@@ -66,7 +62,6 @@ public class RecruitingMenu extends CMenuBar {
             TabPanel.instance().getRecruitingPanel().entityPanel.clear();
             TabPanel.instance().getRecruitingPanel().sidePanel.clear();
             TabPanel.instance().getRecruitingPanel().entityPanel.add(new ReadAllTechnologyGroupPanel());
-            //TabPanel.instance().getRecruitingPanel().sidePanelTop.add(new TechnologyGroupSidePanel());
         }
     };
     static Command reportsCmd = new Command() {
@@ -74,7 +69,6 @@ public class RecruitingMenu extends CMenuBar {
         public void execute() {
             TabPanel.instance().getRecruitingPanel().entityPanel.clear();
             TabPanel.instance().getRecruitingPanel().sidePanel.clear();
-            TabPanel.instance().getRecruitingPanel().sidePanel.add(new SkillSetReportSidePanel());
             TabPanel.instance().getRecruitingPanel().sidePanel.add(new SkillSetSearchPanel());
             TabPanel.instance().getRecruitingPanel().sidePanel.add(new ResumeSearchWidget());
 
