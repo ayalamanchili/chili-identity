632797545f084da4ec311b2be8db8490d8867ad5
radhika.mukkala@sstech.us <>
2016-03-04 14:32:36 +0530

    BIS Reports Performace tuning - ended projects report

--- office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllEndProjectPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllEndProjectPanel.java
@@ -11,8 +11,6 @@ package info.yalamanchili.office.client.reports;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.json.client.JSONParser;
-import com.google.gwt.json.client.JSONValue;
 import info.chili.gwt.crud.CRUDReadAllComposite;
 import info.chili.gwt.crud.TableRowOptionsWidget;
 import info.chili.gwt.date.DateUtils;
@@ -59,12 +57,15 @@ public class ReadAllEndProjectPanel extends CRUDReadAllComposite {
     public void createTableHeader() {
         table.setText(0, 0, getKeyValue("Table_Action"));
         table.setText(0, 1, getKeyValue("Employee"));
-        table.setText(0, 2, getKeyValue("Recruiter"));
+        table.setText(0, 2, getKeyValue("Client"));
         table.setText(0, 3, getKeyValue("Vendor"));
-        table.setText(0, 4, getKeyValue("SubContractor"));
-        table.setText(0, 5, getKeyValue("BillRate"));
-        table.setText(0, 6, getKeyValue("StartDate"));
-        table.setText(0, 7, getKeyValue("EndDate"));
+        table.setText(0, 4, getKeyValue("StartDate"));
+        table.setText(0, 5, getKeyValue("EndDate"));
+        table.setText(0, 6, getKeyValue("BillRate"));
+        table.setText(0, 7, getKeyValue("SubC-PayRate"));
+        table.setText(0, 8, getKeyValue("SubContractor"));
+        table.setText(0, 9, getKeyValue("Placed By"));
+
     }
 
     @Override
@@ -72,38 +73,15 @@ public class ReadAllEndProjectPanel extends CRUDReadAllComposite {
         for (int i = 1; i <= entities.size(); i++) {
             JSONObject entity = (JSONObject) entities.get(i - 1);
             addOptionsWidget(i, entity);
-            JSONObject emp = (JSONObject) entity.get("employee");
-            JSONObject vendor = (JSONObject) entity.get("vendor");
-            if (entity.get("subcontractor") != null) {
-                JSONObject subcontractor = (JSONObject) entity.get("subcontractor");
-                table.setText(i, 4, JSONUtils.toString(subcontractor, "name"));
-            }
-            table.setText(i, 1, JSONUtils.toString(emp, "firstName") + " " + JSONUtils.toString(emp, "lastName"));
-            JSONValue value = entity.get("recruiters");
-            JSONObject object;
-            JSONArray array;
-            if (value != null) {
-                if ((object = value.isObject()) != null) {
-                    table.setText(i, 2, object.get("firstName").isString().stringValue() + " " + object.get("lastName").isString().stringValue());
-                } else if ((array = value.isArray()) != null) {
-                    String recruiters = "";
-                    if (array != null) {
-                        for (int index = 0; index <= array.size() - 1; index++) {
-                            JSONObject obj = (JSONObject) JSONParser.parse(array.get(index).toString());
-                            if (index <= array.size() - 2) {
-                                recruiters = recruiters.concat(obj.get("firstName").isString().stringValue() + " " + obj.get("lastName").isString().stringValue() + " , ");
-                            } else if (index == array.size() - 1) {
-                                recruiters = recruiters.concat(obj.get("firstName").isString().stringValue() + " " + obj.get("lastName").isString().stringValue());
-                            }
-                        }
-                        table.setText(i, 2, recruiters);
-                    }
-                }
-            }
-            table.setText(i, 3, JSONUtils.toString(vendor, "name"));
-            table.setText(i, 5, FormatUtils.formarCurrency(JSONUtils.toString(entity, "billingRate")));
-            table.setText(i, 6, DateUtils.getFormatedDate(JSONUtils.toString(entity, "startDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
-            table.setText(i, 7, DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
+            table.setText(i, 1, JSONUtils.toString(entity, "employee"));
+            table.setText(i, 2, JSONUtils.toString(entity, "client"));
+            table.setText(i, 3, JSONUtils.toString(entity, "vendor"));
+            table.setText(i, 4, DateUtils.getFormatedDate(JSONUtils.toString(entity, "startDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
+            table.setText(i, 5, DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
+            table.setText(i, 6, FormatUtils.formarCurrency(JSONUtils.toString(entity, "billingRate")));
+            table.setText(i, 7, FormatUtils.formarCurrency(JSONUtils.toString(entity, "subcontractorPayRate")));
+            table.setText(i, 8, JSONUtils.toString(entity, "subContractorName"));
+            table.setText(i, 9, JSONUtils.toString(entity, "recruiter"));
         }
     }
 
