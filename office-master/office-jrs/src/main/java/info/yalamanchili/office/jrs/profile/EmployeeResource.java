c05b63ccec1d244fca72aae9b1b423ad6f28113b
radhika.mukkala@sstech.us <>
2016-03-09 18:29:28 +0530

    fix null pointer issue with skillset

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeResource.java
@@ -419,7 +419,6 @@ public class EmployeeResource extends CRUDResource<Employee> {
     /* Dependent */
     @GET
     @Path("/dependents/{id}/{start}/{limit}")
-    @PrivacyAware(key = PrivacyData.DEPENDENTS)
     public DependentTable getDependents(@PathParam("id") long id, @PathParam("start") int start,
             @PathParam("limit") int limit) {
         DependentTable tableObj = new DependentTable();
