47e049d7965bd9a7fad6e1818caaf3fe0b048a94
prasanthi ponnaganti <prasanthi.p@sstech.us>
2016-03-30 16:03:10 +0530

    status removed

---
+++ office-master/office-jpa/src/main/resources/db_changelogs/sql/remove_status_new.sql
@@ -0,0 +1,8 @@
+--
+-- System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+--
+
+
+update Prospect set status='IN_PROGRESS' where status='NEW';
+update Prospect_aud set status='IN_PROGRESS' where status='NEW';
+
