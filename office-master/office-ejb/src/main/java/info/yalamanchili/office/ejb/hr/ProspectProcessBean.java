5b9438aa8daeb6092df9b9c846f3f082c9463929
radhika.mukkala@sstech.us <>
2016-04-11 14:31:23 +0530

    prospect 2.0

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/ejb/hr/ProspectProcessBean.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/ejb/hr/ProspectProcessBean.java
@@ -9,15 +9,12 @@
 package info.yalamanchili.office.ejb.hr;
 
 import info.chili.spring.SpringContext;
-import info.yalamanchili.office.OfficeRoles;
 import info.yalamanchili.office.bpm.OfficeBPMService;
 import info.yalamanchili.office.dao.profile.EmployeeDao;
-import info.yalamanchili.office.email.MailUtils;
 import info.yalamanchili.office.entity.hr.Prospect;
 import info.yalamanchili.office.entity.hr.ProspectStatus;
 import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.jms.MessagingService;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -59,9 +56,6 @@ public class ProspectProcessBean {
     }
 
     public void notifyCaseManager(Prospect entity, List<Long> employees) {
-        if (entity.getManager() == null) {
-            return;
-        }
         info.chili.email.Email email = new info.chili.email.Email();
         if (entity.getStatus().equals(ProspectStatus.IN_PROGRESS)) {
             email.addTo(EmployeeDao.instance().findById(entity.getManager()).getPrimaryEmail().getEmail());
@@ -77,23 +71,6 @@ public class ProspectProcessBean {
         email.setSubject("New Prospect " + entity.getContact().getFirstName() + " " + entity.getContact().getLastName() + " Has Created");
         String messageText = "Navigate To MyOffice --> Prospect --> Search -> Click on update to change the status with appropriate comments";
         email.setBody(messageText);
-        MessagingService.instance().sendEmail(email);
-    }
-
-    public void sendIPProspectsNotification(List<Prospect> inProgressProspects, int inprogressCount) {
-        info.chili.email.Email email = new info.chili.email.Email();
-        List<String> names = new ArrayList();
-        email.addTos(MailUtils.instance().getEmailsAddressesForRoles(OfficeRoles.OfficeRole.ROLE_PROSPECTS_MANAGER.name()));
-        email.setHtml(Boolean.TRUE);
-        for (Prospect prospect : inProgressProspects) {
-            String name = prospect.getContact().getFirstName() + " " + prospect.getContact().getLastName();
-            names.add(name);
-        }
-        email.setSubject("The Following " + inprogressCount + "In-Progress Prospects status not changed from more than 3 days\n");
-        HashMap<String, Object> emailContext = new HashMap();
-        emailContext.put("names", names);
-        email.setContext(emailContext);
-        email.setTemplateName("prospects_template.html");
         MessagingService.instance().sendEmail(email);
     }
 }
