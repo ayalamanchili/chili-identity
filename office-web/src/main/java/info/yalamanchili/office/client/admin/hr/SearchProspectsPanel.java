e5ae0dce394eb56705d7bc26b8d4f0c144daa989
prasanthi ponnaganti <prasanthi.p@sstech.us>
2016-05-02 16:24:08 +0530

    prospects chagnes

--- office-web/src/main/java/info/yalamanchili/office/client/admin/hr/SearchProspectsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/hr/SearchProspectsPanel.java
@@ -73,39 +73,14 @@ public class SearchProspectsPanel extends SearchComposite {
                 Map<String, String> values = JSONUtils.convertKeyValueStringPairs(entityString);
                 if (values != null) {
                     employeeSB.loadData(values);
-                }
-            }
-        });
-        HttpService.HttpServiceAsync.instance().doGet(getcaseManagnerIdsDropDownUrl1(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String entityString) {
-                Map<String, String> values = JSONUtils.convertKeyValueStringPairs(entityString);
-                if (values != null) {
                     caseManagerSB.loadData(values);
                 }
             }
         });
-        HttpService.HttpServiceAsync.instance().doGet(getcaseManagnerIdsDropDownUrl2(), OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
-            @Override
-            public void onResponse(String entityString) {
-                Map<String, String> values = JSONUtils.convertKeyValueStringPairs(entityString);
-                if (values != null) {
-                    caseManagerSB.loadData(values);
-                }
-            }
-        });
-    }
-
-    private String getcaseManagnerIdsDropDownUrl1() {
-        return URL.encode(OfficeWelcome.constants.root_url() + "employee/employees-by-role/dropdown/" + Auth.ROLE.ROLE_H1B_IMMIGRATION + "/0/10000");
-    }
-
-    private String getcaseManagnerIdsDropDownUrl2() {
-        return URL.encode(OfficeWelcome.constants.root_url() + "employee/employees-by-role/dropdown/" + Auth.ROLE.ROLE_GC_IMMIGRATION + "/0/10000");
     }
 
     private String getEmployeeIdsDropDownUrl() {
-        return URL.encode(OfficeWelcome.constants.root_url() + "employee/employees-by-role/dropdown/" + Auth.ROLE.ROLE_RECRUITER + "/0/10000");
+        return URL.encode(OfficeWelcome.constants.root_url() + "employee/employees-by-role/dropdown/" + Auth.ROLE.ROLE_CORPORATE_EMPLOYEE + "/0/10000");
     }
 
     protected String getFirstNameDropDownUrl() {
