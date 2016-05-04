aa8e12d6b2a8e0898caf3a07de0418eaaee01534
ramana lukalapu <ramana.lukalapu@sstech.us>
2016-05-04 19:55:58 +0530

    i fixed the problem for when we delete any phone number , readall employee panel cache cleared

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/PhoneResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/PhoneResource.java
@@ -4,18 +4,22 @@
 package info.yalamanchili.office.jrs.profile;
 
 import info.chili.dao.CRUDDao;
+import info.yalamanchili.office.cache.OfficeCacheKeys;
 import info.yalamanchili.office.dao.profile.PhoneDao;
 import info.yalamanchili.office.entity.profile.Phone;
 import info.yalamanchili.office.jrs.CRUDResource;
 
 import java.util.List;
+import javax.ws.rs.PUT;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +36,14 @@ public class PhoneResource extends CRUDResource<Phone> {
     @Override
     public CRUDDao getDao() {
         return phoneDao;
+    }
+    
+    @PUT
+    @Path("/delete/{id}")
+    @Override
+    @CacheEvict(value = OfficeCacheKeys.EMPLOYEES, allEntries = true)
+    public void delete(@PathParam("id") Long id) {
+        getDao().delete(id);
     }
 
     @XmlRootElement
