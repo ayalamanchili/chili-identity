6c734eb67d7507b0685ad5248de96d399691002a
prasanthi ponnaganti <prasanthi.p@sstech.us>
2016-05-06 16:06:10 +0530

    fix the sub contructor status

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/clientinformation/NewClientInformationProcess.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/clientinformation/NewClientInformationProcess.java
@@ -58,7 +58,7 @@ public class NewClientInformationProcess extends RuleBasedTaskDelegateListner {
                 entity.setStatus(ClientInformationStatus.CANCELED);
             }
         }
-        if (task.getTaskDefinitionKey().equals("newClientInfoSubmitToContractsAdminTask")) {
+        if (task.getTaskDefinitionKey().equals("newClientInfoSUBCTask")) {
             if (status.equalsIgnoreCase("approved")) {
                 entity.setStatus(ClientInformationStatus.COMPLETED);
             } else {
