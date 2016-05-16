afcb2ed95e7f040052bb274009be96dda90f0894
radhika.mukkala@sstech.us <>
2016-05-16 20:50:20 +0530

    invoice tracker

---
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/invoice/ReadInvoicePanel.java
@@ -0,0 +1,117 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.admin.invoice;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.ReadComposite;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.JSONUtils;
+import info.yalamanchili.office.client.Auth;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.profile.cllientinfo.InvoiceFrequency;
+import info.yalamanchili.office.client.profile.employee.SelectEmployeeWidget;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author prasanthi.p
+ */
+public class ReadInvoicePanel extends ReadComposite {
+
+    private static Logger logger = Logger.getLogger(ReadInvoicePanel.class.getName());
+    SelectEmployeeWidget employeeSb = new SelectEmployeeWidget("employee", true, false);
+    private static ReadInvoicePanel instance;
+
+    public static ReadInvoicePanel instance() {
+        return instance;
+    }
+
+    public ReadInvoicePanel(JSONObject entity) {
+        instance = this;
+        initReadComposite(entity, "Invoice", OfficeWelcome.constants);
+    }
+
+    public ReadInvoicePanel(String id) {
+        initReadComposite(id, "Invoice", OfficeWelcome.constants);
+    }
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        entity = (JSONObject) JSONParser.parseLenient(response);
+                        populateFieldsFromEntity(entity);
+                    }
+                });
+
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+       assignFieldValueFromEntity("employee", entity, null);
+        assignFieldValueFromEntity("itemNumber", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("invoiceNumber", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("endDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("invoiceDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("billingRate", entity, DataType.CURRENCY_FIELD);
+        assignFieldValueFromEntity("overTimeBillingRate", entity, DataType.CURRENCY_FIELD);
+        assignFieldValueFromEntity("invoiceFrequency", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("notes", entity, DataType.TEXT_AREA_FIELD);
+
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addDropDown("employee", employeeSb);
+        addField("itemNumber", true, false, DataType.STRING_FIELD);
+        addField("invoiceNumber", true, false, DataType.STRING_FIELD);
+        addField("startDate", true, false, DataType.DATE_FIELD);
+        addField("endDate", true, false, DataType.DATE_FIELD);
+        addField("invoiceDate", true, false, DataType.DATE_FIELD);
+        addField("billingRate", true, false, DataType.CURRENCY_FIELD);
+        addField("overTimeBillingRate", true, false, DataType.CURRENCY_FIELD);
+        addEnumField("invoiceFrequency", true, false, InvoiceFrequency.names());
+        addField("notes", true, false, DataType.TEXT_AREA_FIELD);
+
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "invoice/" + entityId;
+    }
+
+    @Override
+    protected boolean enableAudit() {
+        return Auth.hasAnyOfRoles(Auth.ROLE.ROLE_ADMIN, Auth.ROLE.ROLE_CONTRACTS_ADMIN, Auth.ROLE.ROLE_BILLING_AND_INVOICING, Auth.ROLE.ROLE_CONTRACTS_FULL_VIEW);
+    }
+
+    @Override
+    protected String getAuditUrl() {
+        return OfficeWelcome.instance().constants.root_url() + "audit/changes/" + "info.yalamanchili.office.entity.client.Invoice" + "/" + getEntityId();
+    }
+}
