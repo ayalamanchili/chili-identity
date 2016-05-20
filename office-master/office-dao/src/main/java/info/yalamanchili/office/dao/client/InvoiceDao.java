cbf67f1f78ee88bbb369d609b58e12036778a8a0
prasanthi ponnaganti <prasanthi.p@sstech.us>
2016-05-20 14:55:58 +0530

    search invoice for employee

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/client/InvoiceDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/client/InvoiceDao.java
@@ -1,4 +1,3 @@
-
 /**
  * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
  */
@@ -12,8 +11,10 @@ package info.yalamanchili.office.dao.client;
 import info.chili.dao.CRUDDao;
 import info.chili.spring.SpringContext;
 import info.yalamanchili.office.entity.client.Invoice;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
@@ -41,5 +42,16 @@ public class InvoiceDao extends CRUDDao<Invoice> {
         return SpringContext.getBean(InvoiceDao.class);
     }
 
-}
+    protected String getSearchQuery(String empFirstName, String empLastName) {
+        // write sql query to search the employee in invocie table through clieinfo 
+        return null;
+    }
 
+    public List<Invoice> search(String empFirstName, String empLastName, int start, int limit) {
+        String searchQuery = getSearchQuery(empFirstName, empLastName);
+        TypedQuery<Invoice> query = em.createQuery(searchQuery, Invoice.class);
+        query.setFirstResult(start);
+        query.setMaxResults(limit);
+        return query.getResultList();
+    }
+}
