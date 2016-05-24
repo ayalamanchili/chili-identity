7c31216807bba53cb3fbaf347b6c26b63d9e49ee
ramana lukalapu <ramana.lukalapu@sstech.us>
2016-05-24 22:11:29 +0530

    added new feature called OutOfOffice(Workfromhome, outofoffice) changes done in bpmn process

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/time/OutOfOfficeRequestProcess.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/time/OutOfOfficeRequestProcess.java
@@ -8,87 +8,58 @@
  */
 package info.yalamanchili.office.bpm.time;
 
-import info.yalamanchili.office.OfficeRoles;
-import info.yalamanchili.office.bpm.email.GenericTaskCompleteNotification;
-import info.yalamanchili.office.bpm.email.GenericTaskCreateNotification;
-import info.yalamanchili.office.dao.company.CompanyContactDao;
-import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.chili.service.jrs.exception.ServiceException;
+import info.yalamanchili.office.bpm.rule.RuleBasedTaskDelegateListner;
+import info.yalamanchili.office.dao.security.OfficeSecurityService;
 import info.yalamanchili.office.dao.time.OutOfOfficeDao;
 import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.entity.time.OutOfOfficeRequest;
 import info.yalamanchili.office.entity.time.OutOfOfficeRequestStatus;
 import org.activiti.engine.delegate.DelegateTask;
-import org.activiti.engine.delegate.TaskListener;
 
 /**
  *
  * @author Ramana.Lukalapu
  */
-public class OutOfOfficeRequestProcess implements TaskListener {
+public class OutOfOfficeRequestProcess extends RuleBasedTaskDelegateListner {
 
     @Override
-    public void notify(DelegateTask task) {
-        if ("create".equals(task.getEventName())) {
-            outOfOfficeTaskCreated(task);
-            new GenericTaskCreateNotification().notify(task);
-        }
+    public void processTask(DelegateTask task) {
+        super.processTask(task);
         if ("complete".equals(task.getEventName())) {
             outOfOfficeTaskCompleted(task);
-            new GenericTaskCompleteNotification().notify(task);
-        }
-
-    }
-
-    protected void outOfOfficeTaskCreated(DelegateTask task) {
-        saveOutOfOfficeTask(task);
-        if (task.getTaskDefinitionKey().equals("outOfOfficeMgrApprovalTask")) {
-            assignManagerReviewTask(task);
-        }
-    }
-
-    protected void saveOutOfOfficeTask(DelegateTask task) {
-        OutOfOfficeRequest entity = getOutOfOfficeTask(task);
-        if (entity != null) {
-            if ("managerReviewTask".equals(task.getTaskDefinitionKey())) {
-                entity.setStatus(OutOfOfficeRequestStatus.PENDING_MANAGER_APPROVAL);
-            }
-            entity = OutOfOfficeDao.instance().save(entity);
-            task.getExecution().setVariable("entity", entity);
-            task.getExecution().setVariable("entityId", entity.getId());
-        }
-    }
-
-    protected void assignManagerReviewTask(DelegateTask task) {
-        Long id = getOutOfOfficeTask(task).getEmployee().getId();
-        Employee emp = EmployeeDao.instance().findById(id);
-        Employee manager = CompanyContactDao.instance().getCompanyContactForEmployee(emp, "Reports_To");
-        if (manager != null) {
-            task.addCandidateUser(manager.getEmployeeId());
-        } else {
-            task.addCandidateGroup(OfficeRoles.OfficeRole.ROLE_HR_ADMINSTRATION.name());
         }
     }
 
     protected void outOfOfficeTaskCompleted(DelegateTask task) {
-        OutOfOfficeRequest entity = getOutOfOfficeTask(task);
+        OutOfOfficeRequest entity = getRequestFromTask(task);
         if (entity == null) {
             return;
         }
-        String status = (String) task.getExecution().getVariable("status");
-        if ("managerReviewTask".equals(task.getTaskDefinitionKey())) {
-            if (status.equalsIgnoreCase("approved")) {
-                entity.setStatus(OutOfOfficeRequestStatus.APPROVED);
-            } else {
-                entity.setStatus(OutOfOfficeRequestStatus.REJECTED);
-            }
+        Employee currentUser = OfficeSecurityService.instance().getCurrentUser();
+        if (currentUser.getEmployeeId().equals(entity.getEmployee().getEmployeeId())) {
+            throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "SYSTEM", "cannot.self.approve.corp.travelauthorization", "You cannot approve your travel authorization task");
         }
-        OutOfOfficeDao.instance().save(entity);
+        if (task.getTaskDefinitionKey().equals("outOfOfficeMgrApprovalTask")) {
+            managerApprovalTaskComplete(entity, task);
+        }
     }
 
-    protected OutOfOfficeRequest getOutOfOfficeTask(DelegateTask task) {
-        Long tsId = (Long) task.getExecution().getVariable("entityId");
-        if (tsId != null) {
-            return OutOfOfficeDao.instance().findById(tsId);
+    protected void managerApprovalTaskComplete(OutOfOfficeRequest entity, DelegateTask task) {
+        String status = (String) task.getExecution().getVariable("status");
+        if (status.equalsIgnoreCase("approved")) {
+            entity.setStatus(OutOfOfficeRequestStatus.APPROVED);
+        } else {
+            entity.setStatus(OutOfOfficeRequestStatus.REJECTED);
+        }
+        OutOfOfficeDao.instance().save(entity);
+        task.getExecution().setVariable("entity", entity);
+    }
+
+    protected OutOfOfficeRequest getRequestFromTask(DelegateTask task) {
+        Long entityId = (Long) task.getExecution().getVariable("entityId");
+        if (entityId != null) {
+            return OutOfOfficeDao.instance().findById(entityId);
         }
         return null;
     }
