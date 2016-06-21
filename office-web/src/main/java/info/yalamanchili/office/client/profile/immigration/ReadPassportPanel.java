48ec47cdf8dd1198cf9fec00e407f8421706ef72
rohith vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2016-06-21 16:02:36 -0400

    added additional fields into passport record to read

--- office-web/src/main/java/info/yalamanchili/office/client/profile/immigration/ReadPassportPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/immigration/ReadPassportPanel.java
@@ -25,16 +25,30 @@ public class ReadPassportPanel extends ReadComposite {
     private static Logger logger = Logger.getLogger(ReadPassportPanel.class.getName());
 
     public ReadPassportPanel(JSONObject entity) {
-        initReadComposite(entity, "Passport", OfficeWelcome.constants);
+        initReadComposite(entity, "Passport", OfficeWelcome.constants2);
     }
 
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
         assignFieldValueFromEntity("passportNumber", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("doYouHoldAValidPassport", entity, DataType.BOOLEAN_FIELD);
         assignFieldValueFromEntity("passportIssuedDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("passportExpiryDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("passportExpirationAlertIndicator", entity, DataType.BOOLEAN_FIELD);
         assignFieldValueFromEntity("passportCountryOfIssuance", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("dateOfBirth", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("stateOfBirth", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("placeOfBirth", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("countryOfBirth", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("nationalityIs", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("countryOfNationality", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("passportStateOfIssuance", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("identificationMarks", entity, DataType.TEXT_AREA_FIELD);
+        assignFieldValueFromEntity("haveYouEverLostPassport", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("travelDocumentNumber", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("commentS", entity, DataType.TEXT_AREA_FIELD);
     }
+    
 
     @Override
     protected void addListeners() {
@@ -49,9 +63,22 @@ public class ReadPassportPanel extends ReadComposite {
     @Override
     protected void addWidgets() {
         addField("passportNumber", true, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("doYouHoldAValidPassport", true, true, DataType.BOOLEAN_FIELD, Alignment.HORIZONTAL);
         addField("passportIssuedDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
         addField("passportExpiryDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
+        addField("passportExpirationAlertIndicator", true, false, DataType.BOOLEAN_FIELD, Alignment.HORIZONTAL);
         addEnumField("passportCountryOfIssuance", true, true, CountryFactory.getCountries().toArray(new String[0]), Alignment.HORIZONTAL);
+        addField("dateOfBirth", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
+        addField("stateOfBirth", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("placeOfBirth", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addEnumField("countryOfBirth", true, true, CountryFactory.getCountries().toArray(new String[0]), Alignment.HORIZONTAL);
+        addField("nationalityIs", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addEnumField("countryOfNationality", true, false, CountryFactory.getCountries().toArray(new String[0]), Alignment.HORIZONTAL);
+        addField("passportStateOfIssuance", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("identificationMarks", true, false, DataType.TEXT_AREA_FIELD, Alignment.HORIZONTAL);
+        addField("haveYouEverLostPassport", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("travelDocumentNumber", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("commentS", true, false, DataType.TEXT_AREA_FIELD, Alignment.HORIZONTAL);
         alignFields();
 
     }
