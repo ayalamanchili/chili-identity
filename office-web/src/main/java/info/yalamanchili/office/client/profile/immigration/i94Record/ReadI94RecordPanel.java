80922acea3d638bb71f7e496a495db5458c9df9d
Sudharani bandaru <sudharani.bandaru@SSTECHLAPTOP362.sstech.internal>
2016-06-21 13:38:51 -0500

    SSTO- 3063 Addition to existing I-94 record

--- office-web/src/main/java/info/yalamanchili/office/client/profile/immigration/i94Record/ReadI94RecordPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/immigration/i94Record/ReadI94RecordPanel.java
@@ -29,11 +29,21 @@ public class ReadI94RecordPanel extends ReadComposite {
 
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
+        //Added new fields for I-94 by Sudha #SSTO-3063        
         assignFieldValueFromEntity("i94RecordNumber", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("fullName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("portOfEntry", entity, DataType.STRING_FIELD);        
         assignFieldValueFromEntity("dateofEntry", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("arrivalMethod", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("modeOfTravel", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("i94ValidFromDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("admitUntilDate", entity, DataType.DATE_FIELD);
-        assignFieldValueFromEntity("portOfEntry", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("durationValidityInd", entity, DataType.BOOLEAN_FIELD);        
+        assignFieldValueFromEntity("expirationAlertInd", entity, DataType.BOOLEAN_FIELD);
+        assignFieldValueFromEntity("reentryExpiryDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("coApplicantTravel", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("classOfAdmission", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("comments", entity, DataType.STRING_FIELD);        
     }
 
     @Override
@@ -48,11 +58,21 @@ public class ReadI94RecordPanel extends ReadComposite {
 
     @Override
     protected void addWidgets() {
+        //Added new fields for I-94 by Sudha #SSTO-3063
         addField("i94RecordNumber", true, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
-        addField("dateofEntry", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
-        addField("admitUntilDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
+        addField("fullName", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("portOfEntry", true, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
-        addField("classOfAdmission", true, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("dateofEntry", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);        
+        addField("arrivalMethod", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("modeOfTravel", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("i94ValidFromDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
+        addField("admitUntilDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
+        addField("durationValidityInd", true, true, DataType.BOOLEAN_FIELD, Alignment.HORIZONTAL);
+        addField("expirationAlertInd", true, false, DataType.BOOLEAN_FIELD, Alignment.HORIZONTAL);
+        addField("reentryExpiryDate", true, false, DataType.DATE_FIELD, Alignment.HORIZONTAL);
+        addField("coApplicantTravel", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("classOfAdmission", true, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);        
+        addField("comments", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);                
         alignFields();
 
     }
