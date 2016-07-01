2115ceeb74991b20dac6712ad0493bbb9aad1f0c
Bhavana Atluri <bhavana.atluri@sstech.us>
2016-07-01 09:33:43 -0400

    Added functionality to display Invoice Sent Date

--- office-web/src/main/java/info/yalamanchili/office/client/admin/invoice/ReadInvoicePanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/invoice/ReadInvoicePanel.java
@@ -71,6 +71,7 @@ public class ReadInvoicePanel extends ReadComposite {
         assignFieldValueFromEntity("startDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("endDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("invoiceDate", entity, DataType.DATE_FIELD);
+        assignFieldValueFromEntity("invoiceSentDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("billingRate", clientInformation, DataType.CURRENCY_FIELD);
         assignFieldValueFromEntity("hours", entity, DataType.CURRENCY_FIELD);
         assignFieldValueFromEntity("overTimeBillingRate", entity, DataType.CURRENCY_FIELD);
@@ -96,7 +97,8 @@ public class ReadInvoicePanel extends ReadComposite {
         addField("invoiceNumber", true, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("startDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
         addField("endDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
-        addField("invoiceDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);        
+        addField("invoiceDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);  
+        addField("invoiceSentDate", true, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);  
         addField("billingRate", true, true, DataType.CURRENCY_FIELD, Alignment.HORIZONTAL);
         addField("overTimeBillingRate", true, false, DataType.CURRENCY_FIELD, Alignment.HORIZONTAL);
         addField("hours", true, false, DataType.CURRENCY_FIELD, Alignment.HORIZONTAL);
