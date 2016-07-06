16c1a52c4471a1ff730857b6e1851a0752b57032
Bhavana Atluri <bhavana.atluri@sstech.us>
2016-07-06 14:45:58 -0400

    Added ImmigrationCaseTable inner class to display the Immigration cases

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/immigration/ImmigrationCaseResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/immigration/ImmigrationCaseResource.java
@@ -14,10 +14,18 @@ import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.profile.immigration.ImmigrationCaseDao;
 import info.yalamanchili.office.dao.security.OfficeSecurityService;
 import info.yalamanchili.office.entity.immigration.ImmigrationCase;
+import info.yalamanchili.office.entity.immigration.ImmigrationCaseStatus;
+import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.jrs.CRUDResource;
+import java.util.Date;
+import java.util.List;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -48,7 +56,45 @@ public class ImmigrationCaseResource extends CRUDResource<ImmigrationCase> {
     public ImmigrationCase save(@PathParam("employeeId") Long employeeId, ImmigrationCase entity) {
         entity.setEmployee(EmployeeDao.instance().findById(employeeId));
         entity.setCreatedBy(OfficeSecurityService.instance().getCurrentUserName());
+        entity.setImmigrationCaseStatus(ImmigrationCaseStatus.New);
+        entity.setCreatedDate(new Date());
         return immigrationCaseDao.save(entity);
     }
 
+    @GET
+    @Path("/{empId}/{start}/{limit}")
+    public ImmigrationCaseTable table(@PathParam("empId") long empId,@PathParam("start") int start, @PathParam("limit") int limit) {
+        
+        Employee emp = EmployeeDao.instance().findById(empId);        
+        ImmigrationCaseTable tableObj = new ImmigrationCaseTable();
+        tableObj.setEntities(immigrationCaseDao.instance().getImmigrationCases(emp,start, limit));
+        tableObj.setSize(immigrationCaseDao.instance().getImmigrationCaseSize(emp, start, limit));
+        return tableObj;
+    }
+    
+    
+    @XmlRootElement
+    @XmlType
+    public static class ImmigrationCaseTable implements java.io.Serializable {
+
+        protected Long size;
+        protected List<ImmigrationCase> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<ImmigrationCase> getEntities() {
+            return entities;
+        }
+
+        public void setEntities(List<ImmigrationCase> entities) {
+            this.entities = entities;
+        }
+    }
 }
