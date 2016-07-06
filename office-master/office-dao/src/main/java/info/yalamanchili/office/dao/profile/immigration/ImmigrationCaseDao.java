a84e0a3f52ad9fa8c3ec1f12fc4c818eb025c99d
Bhavana Atluri <bhavana.atluri@sstech.us>
2016-07-06 14:48:29 -0400

    Added getImmigrationCases and getImmigrationCaseSize functionalities

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/immigration/ImmigrationCaseDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/immigration/ImmigrationCaseDao.java
@@ -11,8 +11,11 @@ package info.yalamanchili.office.dao.profile.immigration;
 import info.chili.dao.CRUDDao;
 import info.chili.spring.SpringContext;
 import info.yalamanchili.office.entity.immigration.ImmigrationCase;
+import info.yalamanchili.office.entity.profile.Employee;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
@@ -40,5 +43,19 @@ public class ImmigrationCaseDao extends CRUDDao<ImmigrationCase>{
         return em;
     }
     
-    
+    public List<ImmigrationCase> getImmigrationCases(Employee emp, int start, int limit) {
+        //TODO order by created date
+        Query query = getEntityManager().createQuery("from " + ImmigrationCase.class.getCanonicalName() + " st where st.employee=:employeeParam order by st.createdDate DESC", ImmigrationCase.class);
+        query.setParameter("employeeParam", emp);
+        query.setFirstResult(start);
+        query.setMaxResults(limit);
+        return query.getResultList();
+    }
+
+    public Long getImmigrationCaseSize(Employee emp, int start, int limit) {
+        Query query = getEntityManager().createQuery("select count (*) from " + ImmigrationCase.class.getCanonicalName() + " st where st.employee=:employeeParam", Long.class);
+        query.setParameter("employeeParam", emp);
+        return (Long) query.getSingleResult();
+
+    }
 }
