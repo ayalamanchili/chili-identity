6e5f0cf7d0380571a817c74904727dac24e539cc
sandeep sunchu <sandeep.sunchu@sstech.us>
2016-07-06 16:29:38 -0400

    ReadImmigrationCasePanel

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/immigration/immigrationcase/ReadImmigrationCasePanel.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.immigration.immigrationcase;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.ReadComposite;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.Alignment;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.expense.chkreq.ImmigrationCaseType;
+import info.yalamanchili.office.client.expense.chkreq.SponsorType;
+
+/**
+ *
+ * @author Sandeep Sunchu <sandeep.sunchu@sstech.us>
+ */
+public class ReadImmigrationCasePanel extends ReadComposite {
+    
+    private static ReadImmigrationCasePanel instance;
+    
+    public static ReadImmigrationCasePanel instance() {
+        return instance;
+    }
+
+    public ReadImmigrationCasePanel(JSONObject entity) {
+        instance = this;
+        initReadComposite(entity, "ImmigrationCase", OfficeWelcome.constants2);
+    }
+    
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+            @Override
+            public void onResponse(String response) {
+                entity = (JSONObject) JSONParser.parseLenient(response);
+                populateFieldsFromEntity(entity);
+            }
+        });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("sponsorType", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("immigrationCaseType", entity, DataType.ENUM_FIELD);
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addEnumField("sponsorType", true, true, SponsorType.names(), Alignment.HORIZONTAL);
+        addEnumField("immigrationCaseType", true, true, ImmigrationCaseType.names(), Alignment.HORIZONTAL);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "immigrationcase/" + entityId;
+    }
+
+   
+    
+}
