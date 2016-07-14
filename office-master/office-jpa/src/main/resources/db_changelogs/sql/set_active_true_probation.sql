01dfa35c5f0bfbd95a03784bb822b93778ae63ef
ramana lukalapu <ramana.lukalapu@sstech.us>
2016-07-14 20:31:45 +0530

    I have done the script for active boolean field is set to true for all  probation evaluation periods

---
+++ office-master/office-jpa/src/main/resources/db_changelogs/sql/set_active_true_probation.sql
@@ -0,0 +1,6 @@
+--
+-- System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+--
+
+update probationperiodevaluation set active='true' where active is null;
+update probationperiodevaluation_aud set active='true' where active is null;
