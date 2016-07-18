dec7cec40ab814a842c9de9330f449db0ff377d9
Bhavana Atluri <bhavana.atluri@sstech.us>
2016-07-18 08:58:51 -0400

    Added new dependent variables

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/dto/profile/DependentDto.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/dto/profile/DependentDto.java
@@ -8,8 +8,10 @@
  */
 package info.yalamanchili.office.dto.profile;
 
+import info.yalamanchili.office.entity.profile.Sex;
 import info.yalamanchili.office.entity.profile.ext.Dependent;
 import info.yalamanchili.office.entity.profile.ext.Relationship;
+import info.yalamanchili.office.entity.profile.ext.Salutation;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.EnumType;
@@ -21,6 +23,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import org.dozer.Mapper;
 import org.hibernate.search.annotations.Field;
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
@@ -33,11 +36,22 @@ public class DependentDto implements Serializable {
 
     protected Long id;
     
+    /**
+     * Title
+     */
+    @Enumerated(EnumType.STRING)
+    protected Salutation salutation;
+    
     @NotEmpty(message = "{firstName.not.empty.msg}")
     private String dfirstName;
 
     @NotEmpty(message = "{lastName.not.empty.msg}")
     private String dlastName;
+    
+     /**
+     *Middle Name
+     */
+    protected String dmiddleName;
 
     @Temporal(javax.persistence.TemporalType.DATE)
     @Past
@@ -48,6 +62,23 @@ public class DependentDto implements Serializable {
     @Field
     @NotNull(message = "{relation.not.empty.msg}")
     private Relationship relationship;
+    
+    /**
+     *Gender
+     */
+    @Enumerated(EnumType.STRING)
+    protected Sex gender;
+
+    /**
+     *Email
+     */
+    @Email(message = "Enter a valid email address ")
+    protected String email;
+    
+    /**
+     *Phone number
+     */
+    protected String phoneNumber;
 
     public Long getId() {
         return id;
@@ -56,6 +87,14 @@ public class DependentDto implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+    
+    public Salutation getSalutation() {
+        return salutation;
+    }
+
+    public void setSalutation(Salutation salutation) {
+        this.salutation = salutation;
+    }
 
     public String getDfirstName() {
         return dfirstName;
@@ -63,6 +102,14 @@ public class DependentDto implements Serializable {
 
     public void setDfirstName(String dfirstName) {
         this.dfirstName = dfirstName;
+    }
+    
+    public String getDmiddleName() {
+        return dmiddleName;
+    }
+    
+    public void setDmiddleName(String dmiddleName) {
+        this.dmiddleName = dmiddleName;
     }
 
     public String getDlastName() {
@@ -88,10 +135,34 @@ public class DependentDto implements Serializable {
     public void setRelationship(Relationship relationship) {
         this.relationship = relationship;
     }
+    
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
     @Override
     public String toString() {
-        return "DependentDto{" + "id=" + id + ", dfirstName=" + dfirstName + ", dlastName=" + dlastName + ", ddateOfBirth=" + ddateOfBirth + ", relationship=" + relationship + '}';
+        return "DependentDto{" + "id=" + id + ", salutation=" + salutation +", dfirstName=" + dfirstName +", dmiddleName=" + dmiddleName + ", dlastName=" + dlastName + ", ddateOfBirth=" + ddateOfBirth + ", relationship=" + relationship + ", gender=" + gender + ", email=" + email + ", phoneNumber=" + phoneNumber +'}';
     }
 
     //TODO move this to seperate class?
