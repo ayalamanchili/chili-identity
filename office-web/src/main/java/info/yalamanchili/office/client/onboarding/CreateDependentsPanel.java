92982aba166cf828ed773d5856a6c4ddd35db2f2
Bhavana Atluri <bhavana.atluri@sstech.us>
2016-07-18 08:53:57 -0400

    Added functionality for Immigration Case Dependents

--- office-web/src/main/java/info/yalamanchili/office/client/onboarding/CreateDependentsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/onboarding/CreateDependentsPanel.java
@@ -21,6 +21,8 @@ import info.chili.gwt.widgets.ClickableLink;
 import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
+import info.yalamanchili.office.client.profile.contact.Salutation;
+import info.yalamanchili.office.client.profile.contact.Sex;
 import info.yalamanchili.office.client.profile.emergencycnt.ReadAllDependentsPanel;
 import info.yalamanchili.office.client.profile.employee.TreeEmployeePanel;
 import java.util.logging.Logger;
@@ -35,17 +37,26 @@ public class CreateDependentsPanel extends CreateComposite implements ClickHandl
     ClickableLink deleteB = new ClickableLink("Remove Dependent");
     EmployeeOnboardingPanel parentPanel;
     private int index;
+    protected String targetClassName;
+    protected String parentId;
 
     public CreateDependentsPanel(EmployeeOnboardingPanel parent, int idx) {
         super(CreateComposite.CreateCompositeType.CREATE);
-        initCreateComposite("Dependent", OfficeWelcome.constants);
+        initCreateComposite("Dependent", OfficeWelcome.constants2);
         this.index = idx;
         this.parentPanel = parent;
     }
 
     public CreateDependentsPanel(CreateCompositeType type) {
         super(type);
-        initCreateComposite("Dependent", OfficeWelcome.constants);
+        initCreateComposite("Dependent", OfficeWelcome.constants2);
+    }
+    
+    public CreateDependentsPanel(CreateCompositeType type,String parentId,String targetClassName) {
+        super(type);
+        this.targetClassName = targetClassName;
+        this.parentId = parentId;
+        initCreateComposite("Dependent", OfficeWelcome.constants2);
     }
 
     @Override
@@ -58,10 +69,15 @@ public class CreateDependentsPanel extends CreateComposite implements ClickHandl
             entityActionsPanel.add(deleteB);
         }
         entity = new JSONObject();
+        assignEntityValueFromField("salutation", entity);
         assignEntityValueFromField("dfirstName", entity);
+        assignEntityValueFromField("dmiddleName", entity);
         assignEntityValueFromField("dlastName", entity);
         assignEntityValueFromField("ddateOfBirth", entity);
         assignEntityValueFromField("relationship", entity);
+        assignEntityValueFromField("gender", entity);
+        assignEntityValueFromField("email", entity);
+        assignEntityValueFromField("phoneNumber", entity);
         entity.put("targetEntityName", new JSONString("targetEntityName"));
         entity.put("targetEntityId", new JSONString("0"));
         return entity;
@@ -94,7 +110,7 @@ public class CreateDependentsPanel extends CreateComposite implements ClickHandl
     protected void postCreateSuccess(String result) {
         new ResponseStatusWidget().show("Successfully Added Dependent");
         TabPanel.instance().myOfficePanel.entityPanel.clear();
-        TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllDependentsPanel(
+            TabPanel.instance().myOfficePanel.entityPanel.add(new ReadAllDependentsPanel(
                 TreeEmployeePanel.instance().getEntityId()));
     }
 
@@ -109,10 +125,15 @@ public class CreateDependentsPanel extends CreateComposite implements ClickHandl
 
     @Override
     protected void addWidgets() {
+        addEnumField("salutation", false, false, Salutation.names(), Alignment.HORIZONTAL);
         addField("dfirstName", false, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("dmiddleName", false, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("dlastName", false, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("ddateOfBirth", false, true, DataType.DATE_FIELD, Alignment.HORIZONTAL);
         addEnumField("relationship", false, true, Relationship.names(), Alignment.HORIZONTAL);
+        addEnumField("gender", false, false, Sex.names(), Alignment.HORIZONTAL);
+        addField("email", false, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("phoneNumber", false, false, DataType.LONG_FIELD, Alignment.HORIZONTAL);
         alignFields();
     }
 
@@ -123,7 +144,7 @@ public class CreateDependentsPanel extends CreateComposite implements ClickHandl
 
     @Override
     protected String getURI() {
-        return OfficeWelcome.constants.root_url() + "employee/dependent/"
+       return OfficeWelcome.constants.root_url() + "employee/dependent/"
                 + TreeEmployeePanel.instance().getEntityId();
     }
 
