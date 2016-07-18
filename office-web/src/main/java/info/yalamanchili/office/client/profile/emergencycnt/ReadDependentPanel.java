a15d3a3be63fdce05c06dce9c1d2cb619be46627
Bhavana Atluri <bhavana.atluri@sstech.us>
2016-07-18 08:47:49 -0400

    Added code to populate Immigration Case dependents

--- office-web/src/main/java/info/yalamanchili/office/client/profile/emergencycnt/ReadDependentPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/emergencycnt/ReadDependentPanel.java
@@ -17,6 +17,8 @@ import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.Alignment;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.onboarding.Relationship;
+import info.yalamanchili.office.client.profile.contact.Salutation;
+import info.yalamanchili.office.client.profile.contact.Sex;
 import java.util.logging.Logger;
 
 /**
@@ -34,13 +36,14 @@ public class ReadDependentPanel extends ReadComposite {
 
     public ReadDependentPanel(JSONObject entity) {
         instance = this;
-        initReadComposite(entity, "Dependent", OfficeWelcome.constants);
+        initReadComposite(entity, "Dependent", OfficeWelcome.constants2);
     }
 
     public ReadDependentPanel(String id) {
-        initReadComposite(id, "Dependent", OfficeWelcome.constants);
+        instance = this;
+        initReadComposite(id, "Dependent", OfficeWelcome.constants2);
     }
-
+    
     @Override
     public void loadEntity(String entityId) {
         HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
@@ -55,10 +58,15 @@ public class ReadDependentPanel extends ReadComposite {
 
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
+        assignFieldValueFromEntity("salutation", entity, DataType.ENUM_FIELD);
         assignFieldValueFromEntity("dfirstName", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("dmiddleName", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("dlastName", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("ddateOfBirth", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("relationship", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("gender", entity, DataType.ENUM_FIELD);
+        assignFieldValueFromEntity("email", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("phoneNumber", entity, DataType.LONG_FIELD);
     }
 
     @Override
@@ -71,10 +79,15 @@ public class ReadDependentPanel extends ReadComposite {
 
     @Override
     protected void addWidgets() {
+        addEnumField("salutation", true, false, Salutation.names(), Alignment.HORIZONTAL);
         addField("dfirstName", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("dmiddleName", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("dlastName", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("ddateOfBirth", true, false, DataType.DATE_FIELD, Alignment.HORIZONTAL);
         addEnumField("relationship", true, false, Relationship.names(), Alignment.HORIZONTAL);
+        addEnumField("gender", true, false, Sex.names(), Alignment.HORIZONTAL);
+        addField("email", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("phoneNumber", true, false, DataType.LONG_FIELD, Alignment.HORIZONTAL);
         alignFields();
     }
 
