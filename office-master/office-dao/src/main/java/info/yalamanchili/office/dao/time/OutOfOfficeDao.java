83a1429e62c07be2ec38c42aefe0ee1fc8bba691
Bhavana Atluri <bhavana.atluri@sstech.us>
2016-08-02 10:26:40 -0400

    Implemented the Advanced Search Query functionality

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/time/OutOfOfficeDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/time/OutOfOfficeDao.java
@@ -12,10 +12,11 @@ import info.chili.commons.DateUtils;
 import info.chili.dao.CRUDDao;
 import info.chili.spring.SpringContext;
 import info.yalamanchili.office.cache.OfficeCacheKeys;
+import info.yalamanchili.office.entity.profile.Employee;
 import info.yalamanchili.office.entity.time.OutOfOfficeRequest;
-import info.yalamanchili.office.entity.time.OutOfOfficeRequestStatus;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -34,6 +35,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Scope("prototype")
 public class OutOfOfficeDao extends CRUDDao<OutOfOfficeRequest> {
+    
+    private final static Logger logger = Logger.getLogger(OutOfOfficeDao.class.getName());
 
     @PersistenceContext
     protected EntityManager em;
@@ -58,8 +61,7 @@ public class OutOfOfficeDao extends CRUDDao<OutOfOfficeRequest> {
     }
 
     public List<OutOfOfficeRequest> queryForCurrentWeekRequests(Integer start, Integer limit) {
-        TypedQuery<OutOfOfficeRequest> query = getEntityManager().createQuery("from " + entityCls.getCanonicalName() + " where status=:statusParam and (startDate <=:dateRangeEndParam ) and (endDate >=:dateRangeStartParam))", entityCls);
-        query.setParameter("statusParam", OutOfOfficeRequestStatus.Approved);
+        TypedQuery<OutOfOfficeRequest> query = getEntityManager().createQuery("from " + entityCls.getCanonicalName() + " where (startDate <=:dateRangeEndParam ) and (endDate >=:dateRangeStartParam))", entityCls);
         query.setParameter("dateRangeStartParam", DateUtils.getNextDay(new Date(), -1), TemporalType.DATE);
         query.setParameter("dateRangeEndParam", DateUtils.getNextDay(new Date(), 1), TemporalType.DATE);
         return query.getResultList();
@@ -89,6 +91,65 @@ public class OutOfOfficeDao extends CRUDDao<OutOfOfficeRequest> {
     @Override
     public EntityManager getEntityManager() {
         return em;
+    }
+    
+    public javax.persistence.Query getSearchQuery(EntityManager em, OutOfOfficeRequest entity) {
+        String queryStr = createSearchQuery(entity);
+        javax.persistence.Query query = em.createQuery(queryStr);
+        query = getReportQueryWithParams(queryStr, query, entity);
+        return query;
+    }
+    
+    private Query getReportQueryWithParams(String qryStr, Query query, OutOfOfficeRequest entity) {
+        if (qryStr.contains("startDateParam")) {
+            query.setParameter("startDateParam", entity.getStartDate(), TemporalType.DATE);
+        }
+        if (qryStr.contains("endDateParam")) {
+            query.setParameter("endDateParam", entity.getEndDate(), TemporalType.DATE);
+        }
+        if (qryStr.contains("statusParam")) {
+            query.setParameter("statusParam", entity.getStatus().toString().trim());
+        }
+        if (qryStr.contains("outOfOfficeTypeParam")) {
+            query.setParameter("outOfOfficeTypeParam", entity.getOutOfOfficeType().toString().trim());
+        }
+        if (qryStr.contains("firstNameParam")) {
+            query.setParameter("firstNameParam", entity.getEmployee().getFirstName().trim());
+        }
+        if (qryStr.contains("lastNameParam")) {
+            query.setParameter("lastNameParam", entity.getEmployee().getLastName().trim());
+        }
+        return query;
+    }
+    
+    private String createSearchQuery(OutOfOfficeRequest entity) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT DISTINCT ").append("outOfOfficeRequest").append(" FROM ").append(OutOfOfficeRequest.class.getCanonicalName()).append(" ").append("outOfOfficeRequest");
+        sb.append(" , ");
+        sb.append(Employee.class.getCanonicalName()).append(" employee WHERE outofofficerequest.employee.id=employee.id");
+        
+        if (entity.getStartDate() != null) {
+            sb.append(" AND startDate BETWEEN :startDateParam AND :endDateParam");
+        }
+        if (entity.getEndDate() != null) {
+            sb.append(" OR endDate BETWEEN :startDateParam AND :endDateParam)");
+        }
+        if (entity.getStatus() != null) {
+            sb.append(" AND status LIKE :statusParam ");
+        }
+        if (entity.getOutOfOfficeType()!= null) {
+            sb.append(" AND outOfOfficeType LIKE :outOfOfficeTypeParam ");
+        }
+        if (entity.getEmployee() != null) {
+            if(entity.getEmployee().getFirstName() != null){
+                sb.append(" AND employee.firstName LIKE :firstNameParam ");
+            }
+            if(entity.getEmployee().getLastName() != null){
+                sb.append(" AND employee.lastName LIKE :lastNameParam ");
+            }
+        }
+        logger.info("search query : " + sb.toString());
+        return sb.toString();
     }
 
 }
