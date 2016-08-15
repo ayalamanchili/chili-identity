34eda5c6a20f635087e90a29bf5764082d3b102e
rohith vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2016-08-15 10:55:52 -0400

    change date format

--- office-web/src/main/java/info/yalamanchili/office/client/releasenotes/ReadAllReleaseNotesPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/releasenotes/ReadAllReleaseNotesPanel.java
@@ -14,6 +14,7 @@ import com.google.gwt.user.client.Window;
 import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.crud.CRUDReadAllComposite;
 import info.chili.gwt.crud.TableRowOptionsWidget;
+import info.chili.gwt.date.DateUtils;
 import info.chili.gwt.rpc.HttpService;
 import info.chili.gwt.utils.JSONUtils;
 import info.chili.gwt.widgets.GenericPopup;
@@ -109,7 +110,7 @@ public class ReadAllReleaseNotesPanel extends CRUDReadAllComposite {
             JSONObject entity = (JSONObject) entities.get(i - 1);
             addOptionsWidget(i, entity);
             table.setText(i, 1, JSONUtils.toString(entity, "summary"));
-            table.setText(i, 2, info.chili.gwt.date.DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));            
+            table.setText(i, 2, getFormattedDate(DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT)));            
             table.setText(i, 3, JSONUtils.toString(entity, "userIds"));
             table.setText(i, 4, JSONUtils.toString(entity, "roles"));
             
@@ -147,5 +148,14 @@ public class ReadAllReleaseNotesPanel extends CRUDReadAllComposite {
     protected void configureCreateButton() {
         createButton.setText("Create");
         createButton.setVisible(true);
+    }
+    
+    private String getFormattedDate(String date) {
+        String[] dates = date.split("-");
+        String formatteddate = "";
+        formatteddate = formatteddate.concat(dates[dates.length - 2]).concat("/");
+        formatteddate = formatteddate.concat(dates[dates.length - 1]).concat("/");
+        formatteddate = formatteddate.concat(dates[0]);
+        return formatteddate;
     }
 }
