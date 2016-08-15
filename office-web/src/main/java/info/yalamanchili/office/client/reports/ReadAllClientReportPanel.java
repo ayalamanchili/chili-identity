ce07f963c8b80b1a4931f1a9b077e533f9e34c77
rohith vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2016-08-15 10:39:13 -0400

    change date format

--- office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllClientReportPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/reports/ReadAllClientReportPanel.java
@@ -75,8 +75,8 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
             table.setText(i, 2, JSONUtils.toString(entity, "consultantJobTitle"));
             table.setText(i, 3, JSONUtils.toString(entity, "vendor"));
             table.setText(i, 4, FormatUtils.formarCurrency(JSONUtils.toString(entity, "billingRate")));
-            table.setText(i, 5, DateUtils.getFormatedDate(JSONUtils.toString(entity, "startDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
-            table.setText(i, 6, DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
+            table.setText(i, 5, getFormattedDate(DateUtils.getFormatedDate(JSONUtils.toString(entity, "startDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT)));
+            table.setText(i, 6, getFormattedDate(DateUtils.getFormatedDate(JSONUtils.toString(entity, "endDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT)));
             table.setText(i, 7, JSONUtils.toString(entity, "subContractorName"));
             table.setText(i, 8, FormatUtils.formarCurrency(JSONUtils.toString(entity, "subcontractorPayRate")));
         }
@@ -90,6 +90,15 @@ public class ReadAllClientReportPanel extends CRUDReadAllComposite {
     @Override
     protected boolean enableQuickView() {
         return false;
+    }
+    
+    private String getFormattedDate(String date) {
+        String[] dates = date.split("-");
+        String formatteddate = "";
+        formatteddate = formatteddate.concat(dates[dates.length - 2]).concat("/");
+        formatteddate = formatteddate.concat(dates[dates.length - 1]).concat("/");
+        formatteddate = formatteddate.concat(dates[0]);
+        return formatteddate;
     }
 }
 
