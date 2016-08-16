cf55da71046e9939c282f9be714e249e4096d990
rohith vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2016-08-16 16:27:35 -0400

    add MSA validity period field

--- office-web/src/main/java/info/yalamanchili/office/client/admin/client/ReadClientPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/client/ReadClientPanel.java
@@ -58,6 +58,7 @@ public class ReadClientPanel extends ReadComposite {
         assignFieldValueFromEntity("name", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("description", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("website", entity, DataType.STRING_FIELD);
+        assignFieldValueFromEntity("msaExpDate", entity, DataType.DATE_FIELD);
         assignFieldValueFromEntity("paymentTerms", entity, DataType.STRING_FIELD);
         assignFieldValueFromEntity("clientinvFrequency", entity, DataType.ENUM_FIELD);
         assignFieldValueFromEntity("clientFee", entity, DataType.FLOAT_FIELD);
@@ -80,6 +81,7 @@ public class ReadClientPanel extends ReadComposite {
         addField("name", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("description", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addField("website", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("msaExpDate", true, false, DataType.DATE_FIELD, Alignment.HORIZONTAL);
         addField("paymentTerms", true, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
         addEnumField("clientinvFrequency", true, true, InvoiceFrequency.names(), Alignment.HORIZONTAL);
         addField("clientFee", true, false, DataType.FLOAT_FIELD, Alignment.HORIZONTAL);
