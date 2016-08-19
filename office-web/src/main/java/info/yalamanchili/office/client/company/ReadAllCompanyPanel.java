07d3dc460ec58c03c7fa38183013fda928a7c721
HemaLatha Duggirala <hemalatha.duggirala@sstech.us>
2016-08-19 15:09:01 +0530

    fixed change date format for ReadAllCompanyPanel

--- office-web/src/main/java/info/yalamanchili/office/client/company/ReadAllCompanyPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/company/ReadAllCompanyPanel.java
@@ -97,8 +97,7 @@ public class ReadAllCompanyPanel extends CRUDReadAllComposite {
             JSONObject entity = (JSONObject) entities.get(i - 1);
             addOptionsWidget(i, entity);
             table.setText(i, 1, JSONUtils.toString(entity, "name"));
-            table.setText(i, 2, getFormattedDate(DateUtils.getFormatedDate(JSONUtils.toString(entity, "establishedDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT)));
-            
+            table.setText(i, 2, DateUtils.getFormatedDate(JSONUtils.toString(entity, "establishedDate"), DateTimeFormat.PredefinedFormat.DATE_SHORT));
             table.setWidget(i, 3, new ImageField("Picture", JSONUtils.toString(entity, "logoURL"), JSONUtils.toString(entity, "id"), 50, 50, false));
         }
     }
@@ -115,6 +114,7 @@ public class ReadAllCompanyPanel extends CRUDReadAllComposite {
     private String getCompanyPanelURL(Integer start, String limit) {
         return OfficeWelcome.constants.root_url() + "company/" + start.toString() + "/" + limit.toString();
     }
+
     @Override
     protected void createButtonClicked() {
         TabPanel.instance().getAdminPanel().entityPanel.clear();
@@ -123,10 +123,10 @@ public class ReadAllCompanyPanel extends CRUDReadAllComposite {
 
     @Override
     protected void configureCreateButton() {
-            createButton.setText("Create Company");
-            createButton.setVisible(true);
+        createButton.setText("Create Company");
+        createButton.setVisible(true);
     }
-    
+
     private String getFormattedDate(String date) {
         String[] dates = date.split("-");
         String formatteddate = "";
