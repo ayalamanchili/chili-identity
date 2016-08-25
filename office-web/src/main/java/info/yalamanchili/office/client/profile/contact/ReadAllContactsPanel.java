9514b65c7ecdd442a4c6e43f3ae19deadcdfe24b
HemaLatha Duggirala <hemalatha.duggirala@sstech.us>
2016-08-25 17:15:12 +0530

    Changes in ReadAllPanel of Vendor Recruiter Contacts and AccPayable Contacts

--- office-web/src/main/java/info/yalamanchili/office/client/profile/contact/ReadAllContactsPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/contact/ReadAllContactsPanel.java
@@ -13,6 +13,7 @@ import com.google.gwt.json.client.JSONObject;
 import info.chili.gwt.callback.ALAsyncCallback;
 import info.chili.gwt.crud.CRUDReadAllComposite;
 import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.FormatUtils;
 import info.chili.gwt.utils.JSONUtils;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
@@ -42,12 +43,12 @@ public abstract class ReadAllContactsPanel extends CRUDReadAllComposite {
     public void preFetchTable(int start) {
         HttpService.HttpServiceAsync.instance().doGet(getContactsURL(start, OfficeWelcome.constants.tableSize()),
                 OfficeWelcome.instance().getHeaders(), true, new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String result) {
-                        logger.info(result);
-                        postFetchTable(result);
-                    }
-                });
+            @Override
+            public void onResponse(String result) {
+                logger.info(result);
+                postFetchTable(result);
+            }
+        });
     }
 
     protected abstract String getContactsURL(Integer start, String limit);
@@ -56,23 +57,30 @@ public abstract class ReadAllContactsPanel extends CRUDReadAllComposite {
     public void createTableHeader() {
         table.setText(0, 0, getKeyValue("Table_Action"));
         table.setText(0, 1, getKeyValue("First Name"));
-        table.setText(0, 2, getKeyValue("Middle Initial"));
-        table.setText(0, 3, getKeyValue("Last Name"));
-        table.setText(0, 4, getKeyValue("Email"));
-        table.setText(0, 5, getKeyValue("Sex"));
+        table.setText(0, 2, getKeyValue("Last Name"));
+        table.setText(0, 3, getKeyValue("Email"));
+        table.setText(0, 4, getKeyValue("Phone Number"));
     }
 
     @Override
     public void fillData(JSONArray entities) {
         for (int i = 1; i <= entities.size(); i++) {
             JSONObject entity = (JSONObject) entities.get(i - 1);
+            JSONObject phones;
             addOptionsWidget(i, entity);
             table.setText(i, 1, JSONUtils.toString(entity, "firstName"));
-            table.setText(i, 2, JSONUtils.toString(entity, "middleInitial"));
-            table.setText(i, 3, JSONUtils.toString(entity, "lastName"));
-            table.setText(i, 4, JSONUtils.toString(entity, "email"));
-            table.setText(i, 5, JSONUtils.toString(entity, "sex"));
+            table.setText(i, 2, JSONUtils.toString(entity, "lastName"));
+            table.setText(i, 3, JSONUtils.toString(entity, "email"));
+            if (entity.get("phones") instanceof JSONObject) {
+                phones = (JSONObject) entity.get("phones");
+                table.setText(i, 4, FormatUtils.formatPhoneNumber(JSONUtils.toString(phones, "phoneNumber")));
+            } else if (entity.get("phones") instanceof JSONArray) {
+                JSONArray phonesArray = (JSONArray) entity.get("phones");
+                phones = (JSONObject) phonesArray.get(0);
+                table.setText(i, 4, FormatUtils.formatPhoneNumber(JSONUtils.toString(phones, "phoneNumber")));
+            }
         }
+
     }
 
     @Override
@@ -88,11 +96,11 @@ public abstract class ReadAllContactsPanel extends CRUDReadAllComposite {
     public void deleteClicked(String entityId) {
         HttpService.HttpServiceAsync.instance().doPut(getDeleteURL(entityId), null, OfficeWelcome.instance().getHeaders(), true,
                 new ALAsyncCallback<String>() {
-                    @Override
-                    public void onResponse(String arg0) {
-                        postDeleteSuccess();
-                    }
-                });
+            @Override
+            public void onResponse(String arg0) {
+                postDeleteSuccess();
+            }
+        });
     }
 
     protected abstract String getDeleteURL(String entityId);
