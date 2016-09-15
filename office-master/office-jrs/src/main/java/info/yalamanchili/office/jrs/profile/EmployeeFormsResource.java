f8de63e637fde792863a4e8d640842b61d803325
radhika.mukkala@sstech.us <>
2016-09-15 16:51:33 +0530

    add roles and responsibilities forms in read joining form panel

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeFormsResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/EmployeeFormsResource.java
@@ -28,6 +28,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -185,6 +186,14 @@ public class EmployeeFormsResource extends CRUDResource<BankAccount> {
     public Response printJoiningForm(@PathParam("id") Long employeeId) {
         Employee emp = EmployeeDao.instance().findById(employeeId);
         return employeeFormsService.printJoiningForm(emp);
+    }
+
+    @GET
+    @Path("/roles-responsibilities")
+    @Produces({"application/pdf"})
+    public Response printRolesAndRespForm(@QueryParam("id") Long employeeId) {
+        Employee emp = EmployeeDao.instance().findById(employeeId);
+        return employeeFormsService.printRolesAndRespForm(emp);
     }
 
     @Override
