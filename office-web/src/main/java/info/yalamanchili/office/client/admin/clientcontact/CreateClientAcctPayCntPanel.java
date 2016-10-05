1d8ddab6383e25e17aa70c79d51c37f413d821bd
rohith vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2016-10-05 12:17:00 -0400

    add client account payable contact link

--- office-web/src/main/java/info/yalamanchili/office/client/admin/clientcontact/CreateClientAcctPayCntPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/clientcontact/CreateClientAcctPayCntPanel.java
@@ -9,6 +9,7 @@
 package info.yalamanchili.office.client.admin.clientcontact;
 
 import info.chili.gwt.crud.CreateComposite;
+import info.chili.gwt.widgets.GenericPopup;
 import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
@@ -25,15 +26,31 @@ public class CreateClientAcctPayCntPanel extends CreateContactPanel {
         super(type);
     }
 
+    String clientId;
+
+    public CreateClientAcctPayCntPanel(String clientId, CreateComposite.CreateCompositeType type) {
+        super(type);
+        this.clientId = clientId;
+    }
+
     @Override
     protected void postCreateSuccess(String result) {
         new ResponseStatusWidget().show("Successfully Added Client Account Payable Contact");
-        TabPanel.instance().adminPanel.entityPanel.clear();
-        TabPanel.instance().adminPanel.entityPanel.add(new ReadAllClientAcctPayContacts(TreeClientPanel.instance().getEntityId()));
+        if (SelectClientAcctPayContact.instance() != null) {
+            SelectClientAcctPayContact.instance().fireEvent();
+            GenericPopup.hideIfOpen();
+        } else {
+            TabPanel.instance().adminPanel.entityPanel.clear();
+            TabPanel.instance().adminPanel.entityPanel.add(new ReadAllClientAcctPayContacts(TreeClientPanel.instance().getEntityId()));
+        }
     }
 
     @Override
     protected String getURI() {
-        return OfficeWelcome.constants.root_url() + "client/acct-pay-contact/" + TreeClientPanel.instance().getEntityId();
+        if (clientId != null) {
+            return OfficeWelcome.constants.root_url() + "client/acct-pay-contact/" + clientId;
+        } else {
+            return OfficeWelcome.constants.root_url() + "client/acct-pay-contact/" + TreeClientPanel.instance().getEntityId();
+        }
     }
 }
