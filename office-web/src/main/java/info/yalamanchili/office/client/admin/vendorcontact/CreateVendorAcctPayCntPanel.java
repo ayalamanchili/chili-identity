a8715693ca5bc9aabf4cb81382047b4c86851e7a
rohith vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2016-10-06 16:03:56 -0400

    add link for vendor account payable contact

--- office-web/src/main/java/info/yalamanchili/office/client/admin/vendorcontact/CreateVendorAcctPayCntPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/admin/vendorcontact/CreateVendorAcctPayCntPanel.java
@@ -9,6 +9,7 @@
 package info.yalamanchili.office.client.admin.vendorcontact;
 
 import info.chili.gwt.crud.CreateComposite;
+import info.chili.gwt.widgets.GenericPopup;
 import info.chili.gwt.widgets.ResponseStatusWidget;
 import info.yalamanchili.office.client.OfficeWelcome;
 import info.yalamanchili.office.client.TabPanel;
@@ -24,16 +25,32 @@ public class CreateVendorAcctPayCntPanel extends CreateContactPanel {
     public CreateVendorAcctPayCntPanel(CreateComposite.CreateCompositeType type) {
         super(type);
     }
+    
+    String vendorId;
+
+    public CreateVendorAcctPayCntPanel(String vendorId, CreateComposite.CreateCompositeType type) {
+        super(type);
+        this.vendorId = vendorId;
+    }
 
     @Override
     protected void postCreateSuccess(String result) {
         new ResponseStatusWidget().show("Successfully Added Vendor Account Payable Contact");
+        GenericPopup.hideIfOpen();
+        if (SelectVendorAcctPayContact.instance() != null) {
+            SelectVendorAcctPayContact.instance().fireEvent();
+        } else {
         TabPanel.instance().adminPanel.entityPanel.clear();
         TabPanel.instance().adminPanel.entityPanel.add(new ReadAllVendorAcctPayContacts(TreeVendorsPanel.instance().getEntityId()));
+        }
     }
 
     @Override
     protected String getURI() {
+        if (vendorId != null) {
+            return OfficeWelcome.constants.root_url() + "vendor/acct-pay-contact/" + vendorId;
+        } else {
         return OfficeWelcome.constants.root_url() + "vendor/acct-pay-contact/" + TreeVendorsPanel.instance().getEntityId();
+        }
     }
 }
