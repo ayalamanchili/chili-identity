3138714ab113168412a346f160b89c34ebe1f005
Sudharani bandaru <sudharani.bandaru@SSTECHLAPTOP362.sstech.internal>
2016-10-12 16:57:47 -0400

    Incorporated changes  for review comments on update vendor notification changes.

--- office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/vendor/UpdateVendorNotificationProcess.java
+++ office-master/office-bpm/src/main/java/info/yalamanchili/office/bpm/vendor/UpdateVendorNotificationProcess.java
@@ -13,15 +13,11 @@ import info.chili.audit.AuditService;
 import info.chili.email.Email;
 import info.chili.spring.SpringContext;
 import info.yalamanchili.office.OfficeRoles;
-import info.yalamanchili.office.bpm.BPMUtils;
 import info.yalamanchili.office.config.OfficeServiceConfiguration;
-import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.email.MailUtils;
 import info.yalamanchili.office.entity.client.Vendor;
 import info.yalamanchili.office.entity.profile.Employee;
-import info.yalamanchili.office.entity.selfserv.TicketComment;
 import info.yalamanchili.office.jms.MessagingService;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -44,7 +40,7 @@ public class UpdateVendorNotificationProcess implements TaskListener {
     }
 
     private void updateVendorNotificationCreated(DelegateTask dt) {
-        notify(dt, Boolean.FALSE);
+        notify(dt, Boolean.FALSE, OfficeRoles.OfficeRole.ROLE_CONTRACTS_ADMIN.name(), OfficeRoles.OfficeRole.ROLE_BILLING_ADMIN.name());
     }
 
     public void notify(DelegateTask delegateTask, Boolean notifyEmployee, String... notifyRoles) {
@@ -52,33 +48,24 @@ public class UpdateVendorNotificationProcess implements TaskListener {
         Employee currentEmployee = null;
         if (delegateTask.getExecution().getVariable("currentEmployee") != null) {
             currentEmployee = (Employee) delegateTask.getExecution().getVariable("currentEmployee");
-            //TODO is this needed?
-            delegateTask.setOwner(currentEmployee.getFirstName() + " " + currentEmployee.getLastName());
         }
         Email email = new Email();
         email.setRichText(Boolean.TRUE);
-        email.setTos(getEmails(delegateTask, notifyEmployee, notifyRoles));
+        email.setTos(getEmails(notifyRoles));
         email.setSubject("Task Created:" + delegateTask.getName());
 
-        //messageText = messageText.concat("<table><thead><tr><th><p>Updated Vendor Fields</p></th></tr></thead><tbody>");
         Vendor vendor = (Vendor) delegateTask.getExecution().getVariable("vendor");
         List<AuditChangeDto> changes = AuditService.instance().compareWithRecentVersion(vendor, vendor.getId());
         Map<String, Object> emailCtx = new HashMap<>();
-        //List<TicketComment> comments = new ArrayList<>();
-        //comments.addAll(comment.getTicket().getComments());
-        //emailCtx.put("comments", comments);
-        try{
+
         emailCtx.put("changes", changes);
-        if(currentEmployee != null)
-            emailCtx.put("updatedBy", currentEmployee.getFirstName()+" "+ currentEmployee.getLastName());
-        emailCtx.put("entity", "Vendor");
-        }catch(Exception e){
-            e.printStackTrace();
+        if (currentEmployee != null) {
+            emailCtx.put("updatedBy", currentEmployee.getFirstName() + " " + currentEmployee.getLastName());
         }
+        emailCtx.put("entity", "Vendor");
+
         email.setTemplateName("entity_change_template.html");
         email.setContext(emailCtx);
-        
-        //messageText = messageText.concat("<br/> <br/> &nbsp; <b>Please click on the below link to complete the task: </b> <br/> &nbsp; " + getTaskLink(delegateTask));
         email.setHtml(Boolean.TRUE);
         messagingService.sendEmail(email);
     }
@@ -89,17 +76,10 @@ public class UpdateVendorNotificationProcess implements TaskListener {
         return sb.toString();
     }
 
-    public Set<String> getEmails(DelegateTask delegateTask, Boolean notifyEmployeeAlso, String... notifyRoles) {
+    public Set<String> getEmails(String... notifyRoles) {
         Set<String> emails = new HashSet();
-        emails.addAll(BPMUtils.getCandidateEmails(delegateTask));
         if (notifyRoles.length > 0) {
             emails.addAll(MailUtils.instance().getEmailsAddressesForRoles(notifyRoles));
-        }
-        if (notifyEmployeeAlso) {
-            Employee employee = (Employee) delegateTask.getExecution().getVariable("currentEmployee");
-            if (employee != null) {
-                emails.add(EmployeeDao.instance().getPrimaryEmail(employee.getId()));
-            }
         }
         return emails;
     }
