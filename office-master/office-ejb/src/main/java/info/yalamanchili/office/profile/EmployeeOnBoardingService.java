c990f8a27dbdbe61a4ee932e7475c05655eb8557
radhika.mukkala@sstech.us <>
2016-11-07 16:48:58 +0530

    capture cpd for prospects before onboarding lb changes

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeOnBoardingService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/EmployeeOnBoardingService.java
@@ -196,21 +196,22 @@ public class EmployeeOnBoardingService {
 
         emp = EmployeeDao.instance().save(emp);
         emp = em.merge(emp);
-            //add cpd to emp, when cpd attach to contact in prospects
-//        if (ContactDao.instance().findByEmail(code.getEmail()) != null) {
-//            Contact cnt = ContactDao.instance().findByEmail(code.getEmail());
-//            List<ClientInformation> cpds = ClientInfoHandleEntityDao.instance().getAllCpds(cnt.getId());
-//            List<ClientInformation> empCpds = new ArrayList();
-//            if (cpds != null && cpds.size() > 0) {
-//                for (ClientInformation cpd : cpds) {
-//                    cpd.setEmployee(emp);
-//                    empCpds.add(ClientInformationDao.instance().getEntityManager().merge(cpd));
-//                }
-//            }
-//            if (empCpds.size() > 0) {
-//                emp.setClientInformations(empCpds);
-//            }
-//        }
+        
+        //add cpd to emp, when cpd attach to contact in prospects
+        Contact cnt = ContactDao.instance().findByEmail(code.getEmail());
+        if (cnt != null) {
+            List<ClientInformation> cpds = ClientInfoHandleEntityDao.instance().getAllCpds(cnt.getId());
+            List<ClientInformation> empCpds = new ArrayList();
+            if (cpds != null && cpds.size() > 0) {
+                for (ClientInformation cpd : cpds) {
+                    cpd.setEmployee(emp);
+                    empCpds.add(ClientInformationDao.instance().getEntityManager().merge(cpd));
+                }
+            }
+            if (empCpds.size() > 0) {
+                emp.setClientInformations(empCpds);
+            }
+        }
 
         //Update Emergency Contact for Employee
         for (EmergencyContactDto ec : dto.getEmergencyContact()) {
