4f0aea14df44a29e0152858679475c7f3375e300
rohith vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2016-11-11 14:24:45 -0500

    add OtherNamesInfo Dao & Resource

---
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/immigration/OtherNamesInfoDao.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.dao.profile.immigration;
+
+import info.chili.dao.AbstractHandleEntityDao;
+import info.chili.spring.SpringContext;
+import info.yalamanchili.office.entity.immigration.OtherNamesInfo;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Rohith.Vallabhaneni
+ */
+@Repository
+@Scope("prototype")
+public class OtherNamesInfoDao extends AbstractHandleEntityDao<OtherNamesInfo> {
+    
+    @PersistenceContext
+    protected EntityManager em;
+
+    public OtherNamesInfoDao() {
+        super(OtherNamesInfo.class);
+    }
+
+    public static OtherNamesInfoDao instance() {
+        return SpringContext.getBean(OtherNamesInfoDao.class);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
