13ca62cb07ebef236cbef91ac17ebe0d1bae3c85
radhika.mukkala@sstech.us <>
2016-11-11 16:42:25 +0530

    immigration case changes  -> remove h1b questionanire panel as it is not completed

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/invite/InviteCodeValidationService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/invite/InviteCodeValidationService.java
@@ -35,10 +35,18 @@ public class InviteCodeValidationService {
         if (inviteCode == null) {
             throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "SYSTEM", "invalid.invitationcode", "Invalid Invitation Code");
         }
-        Date todayDate = new Date();
-        if ((todayDate.compareTo(inviteCode.getValidFromDate()) >= 0)
-                && (todayDate.compareTo(inviteCode.getExpiryDate()) <= 0)) {
-            return code;
+        if (inviteCode.getInviteType() != null) {
+            Date todayDate = new Date();
+            if ((todayDate.compareTo(inviteCode.getValidFromDate()) >= 0)
+                    && (todayDate.compareTo(inviteCode.getExpiryDate()) <= 0)) {
+                return code + "?type=" +inviteCode.getInviteType().getInvitationType().name();
+            }
+        }else{
+            Date todayDate = new Date();
+            if ((todayDate.compareTo(inviteCode.getValidFromDate()) >= 0)
+                    && (todayDate.compareTo(inviteCode.getExpiryDate()) <= 0)) {
+                return code;
+            }
         }
         throw new ServiceException(ServiceException.StatusCode.INVALID_REQUEST, "SYSTEM", "invalid.invitationcode", "Invalid Invitation Code");
     }
