17c438fa6593870f7b71221a61a99efbac0e15c6
ramana lukalapu <ramana.lukalapu@sstech.us>
2016-11-16 19:22:26 +0530

    Immigration module- OtherNamesInfoDao

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/immigration/OtherNamesInfoDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/immigration/OtherNamesInfoDao.java
@@ -1,3 +1,6 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,7 +23,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Scope("prototype")
 public class OtherNamesInfoDao extends AbstractHandleEntityDao<OtherNamesInfo> {
-    
+
     @PersistenceContext
     protected EntityManager em;
 
@@ -36,5 +39,5 @@ public class OtherNamesInfoDao extends AbstractHandleEntityDao<OtherNamesInfo> {
     public EntityManager getEntityManager() {
         return em;
     }
-    
+
 }
