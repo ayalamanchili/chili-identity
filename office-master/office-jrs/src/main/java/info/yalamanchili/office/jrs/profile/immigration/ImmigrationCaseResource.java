78419c02337f394ecf25682903938b910a88663e
radhika.mukkala@sstech.us <>
2016-11-18 18:59:45 +0530

    design questionnaire UI page 1

--- office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/immigration/ImmigrationCaseResource.java
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/immigration/ImmigrationCaseResource.java
@@ -15,18 +15,30 @@ import info.chili.email.Email;
 import info.chili.jpa.validation.Validate;
 import info.yalamanchili.office.cache.OfficeCacheKeys;
 import info.yalamanchili.office.config.OfficeServiceConfiguration;
+import info.yalamanchili.office.dao.invite.InviteCodeDao;
 import info.yalamanchili.office.dao.profile.CompanyDao;
+import info.yalamanchili.office.dao.profile.ContactDao;
+import info.yalamanchili.office.dao.profile.EmailDao;
 import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.yalamanchili.office.dao.profile.ext.EmployeeAdditionalDetailsDao;
+import info.yalamanchili.office.dao.profile.immigration.EducationRecordDao;
 import info.yalamanchili.office.dao.profile.immigration.ImmigrationCaseDao;
 import info.yalamanchili.office.dao.security.OfficeSecurityService;
 import info.yalamanchili.office.entity.Company;
+import info.yalamanchili.office.entity.immigration.EducationRecord;
 import info.yalamanchili.office.entity.immigration.ImmigrationCase;
 import info.yalamanchili.office.entity.immigration.ImmigrationCaseStatus;
+import info.yalamanchili.office.entity.profile.EmailType;
 import info.yalamanchili.office.entity.profile.Employee;
+import info.yalamanchili.office.entity.profile.Sex;
+import info.yalamanchili.office.entity.profile.ext.EmployeeAdditionalDetails;
+import info.yalamanchili.office.entity.profile.ext.Ethnicity;
+import info.yalamanchili.office.entity.profile.ext.MaritalStatus;
 import info.yalamanchili.office.entity.profile.invite.InvitationType;
 import info.yalamanchili.office.entity.profile.invite.InviteCode;
 import info.yalamanchili.office.jms.MessagingService;
 import info.yalamanchili.office.jrs.CRUDResource;
+import info.yalamanchili.office.profile.EmployeeOnBoardingService;
 import info.yalamanchili.office.profile.invite.InviteCodeGeneratorService;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -38,6 +50,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
@@ -87,6 +100,7 @@ public class ImmigrationCaseResource extends CRUDResource<ImmigrationCase> {
             if (entity.getEmployee() != null) {
                 Employee emp = EmployeeDao.instance().findById(entity.getEmployee().getId());
                 entity.setEmployee(emp);
+                entity.setEmail(emp.getPrimaryEmail().getEmail());
                 if (emp.getCompany() != null) {
                     entity.setCompany(CompanyDao.instance().findById(emp.getCompany().getId()));
                 } else {
@@ -103,7 +117,9 @@ public class ImmigrationCaseResource extends CRUDResource<ImmigrationCase> {
         } else {
             ImmigrationCase newCase = ImmigrationCaseDao.instance().findById(entity.getId());
             if (entity.getEmployee() != null) {
-                newCase.setEmployee(EmployeeDao.instance().findById(entity.getEmployee().getId()));
+                Employee emp = EmployeeDao.instance().findById(entity.getEmployee().getId());
+                newCase.setEmployee(emp);
+                newCase.setEmail(entity.getEmail());
             } else {
                 newCase.setEmail(entity.getEmail());
             }
@@ -157,7 +173,7 @@ public class ImmigrationCaseResource extends CRUDResource<ImmigrationCase> {
         Email email = new Email();
         email.addTo(empEmail);
         Map<String, Object> emailCtx = new HashMap<>();
-        emailCtx.put("invitationCode", OfficeServiceConfiguration.instance().getPortalWebUrl() + "?h1b-questionnaire=" + code.getInvitationCode() + "&invitationType=" + code.getInviteType().getInvitationType().name());
+        emailCtx.put("invitationCode", OfficeServiceConfiguration.instance().getPortalWebUrl() + "?h1b-questionnaire=" + code.getInvitationCode()); //+ "&invitationType=" + code.getInviteType().getInvitationType().name());
         if (emp != null) {
             emailCtx.put("employeeName", emp.getFirstName() + " " + emp.getLastName());
             if (emp.getCompany() != null) {
@@ -173,50 +189,175 @@ public class ImmigrationCaseResource extends CRUDResource<ImmigrationCase> {
         emailCtx.put("validFrom", new SimpleDateFormat("MM/dd/yyyy").format(code.getValidFromDate()));
         emailCtx.put("validTo", new SimpleDateFormat("MM/dd/yyyy").format(code.getExpiryDate()));
         email.setTemplateName("questionnaire_invitation_email_template.html");
-        String messageText = "Questionnaire";
+        String messageText = "Processing of your " + immigrationCase.getImmigrationCaseType() + " Visa Petition - " + CompanyDao.instance().findById(immigrationCase.getCompany().getId()).getName();
         email.setContext(emailCtx);
         email.setSubject(messageText);
         email.setBody(code.getInvitationCode());
         MessagingService.instance().sendEmail(email);
     }
 
-//    @GET
-//    @Path("h1b-questionnaire/get-details/{invitationCode}")
-//    public EmployeeH1BDetailsDto getdetails(@PathParam("invitationCode") String invitationCode) {
-//        String invCde = invitationCode.trim();
-//        InviteCode code = InviteCodeDao.instance().find(invCde);
-//        Employee emp;
-//        EmployeeH1BDetailsDto res = new EmployeeH1BDetailsDto();
-//        info.yalamanchili.office.entity.profile.Email email;
-//        if (EmployeeDao.instance().findByEmail(code.getEmail()) != null) {
-//            emp = EmployeeDao.instance().findByEmail(code.getEmail());
-//            if (emp.getEmails().size() > 0 && emp.getEmails().get(0) != null) {
-//                email = emp.getEmails().get(0);
-//                res.setEmail(email.getEmail());
-//                if (email.getEmailType().getEmailType().equals("Work")) {
-//                    res.setWorkEmail(email.getEmail());
-//                }
-//            }
-//            res.setEmpFirstName(emp.getFirstName());
-//            res.setEmpLastName(emp.getLastName());
-//            res.setDateOfBirth(emp.getDateOfBirth());
-//            res.setSsn(emp.getSsn());
-//            EmployeeAdditionalDetails details = EmployeeAdditionalDetailsDao.instance().find(emp);
-//            if (details != null && details.getId() != null) {
-//                res.setMaritalStatus(details.getMaritalStatus().name());
-//            }
-//            res.setMaritalStatus(MaritalStatus.Unknown.name());
-//            if (emp.getSex() != null) {
-//                res.setGender(emp.getSex().name());
-//            }
-//        }
-//        return res;
-//    }
+    @GET
+    @Path("h1b-questionnaire/get-details")
+    @CacheEvict(value = OfficeCacheKeys.IMMIGRATION_CASE)
+    public EmployeeH1BDetailsDto getdetails(@QueryParam("invitationCode") String invitationCode) {
+        EmployeeH1BDetailsDto detailsDto = new EmployeeH1BDetailsDto();
+        PersonalInfoDto res = new PersonalInfoDto();
+        info.yalamanchili.office.entity.profile.Email email;
+        Employee emp = getEmployee(invitationCode);
+        if (emp != null) {
+            if (emp.getEmails().size() > 0) {
+                email = emp.getEmails().get(emp.getEmails().size() - 1);
+                res.setEmail(email.getEmail());
+                if (email.getEmailType().getEmailType().equals("Work")) {
+                    res.setWorkEmail(email.getEmail());
+                }
+            }
+            res.setEmpFirstName(emp.getFirstName());
+            res.setEmpLastName(emp.getLastName());
+            if (emp.getMiddleInitial() != null) {
+                res.setMiddleInitial(emp.getMiddleInitial());
+            }
+            res.setDateOfBirth(emp.getDateOfBirth());
+            res.setSsn(emp.getSsn());
+            EmployeeAdditionalDetails details = EmployeeAdditionalDetailsDao.instance().find(emp);
+            if (details != null && details.getId() != null) {
+                res.setMaritalStatus(details.getMaritalStatus().name());
+            } else {
+                res.setMaritalStatus(MaritalStatus.Unknown.name());
+            }
+            if (emp.getSex() != null) {
+                res.setGender(emp.getSex().name());
+            }
+        }
+        detailsDto.setEmpPersonalInfo(res);
+        return detailsDto;
+    }
+
+    @PUT
+    @Path("save-personal-info/{invitationCode}")
+    public EmployeeH1BDetailsDto savePersonalInfo(@PathParam("invitationCode") String invitationCode, EmployeeH1BDetailsDto dto) {
+        Employee emp = getEmployee(invitationCode);
+        if (dto.getEmpPersonalInfo() != null) {
+            PersonalInfoDto personalInfoDto = dto.getEmpPersonalInfo();
+            emp.setFirstName(personalInfoDto.getEmpFirstName());
+            emp.setLastName(personalInfoDto.getEmpLastName());
+            emp.setDateOfBirth(personalInfoDto.getDateOfBirth());
+            if (personalInfoDto.getMiddleInitial() != null) {
+                emp.setMiddleInitial(personalInfoDto.getMiddleInitial());
+            }
+            emp.setSsn(personalInfoDto.getSsn());
+            //Personal Email
+            info.yalamanchili.office.entity.profile.Email emailAdd = new info.yalamanchili.office.entity.profile.Email();
+            emailAdd.setEmail(personalInfoDto.getEmail());
+            emailAdd.setPrimaryEmail(Boolean.TRUE);
+            //emailAdd.setEmailType(new EmployeeOnBoardingService().getPersonalEmailType());
+            emailAdd.setContact(ContactDao.instance().findByEmail(InviteCodeDao.instance().find(invitationCode.trim()).getEmail()));
+            emp.addEmail(EmailDao.instance().save(emailAdd));
+            //Work Email
+            info.yalamanchili.office.entity.profile.Email workEmail = new info.yalamanchili.office.entity.profile.Email();
+            workEmail.setEmail(personalInfoDto.getEmail());
+            workEmail.setPrimaryEmail(Boolean.TRUE);
+            workEmail.setEmailType(getWorkEmailType());
+            workEmail.setContact(ContactDao.instance().findByEmail(InviteCodeDao.instance().find(invitationCode.trim()).getEmail()));
+            emp.addEmail(EmailDao.instance().save(workEmail));
+            if (personalInfoDto.getGender().equalsIgnoreCase("male")) {
+                emp.setSex(Sex.MALE);
+            } else {
+                emp.setSex(Sex.FEMALE);
+            }
+            if (personalInfoDto.getMaritalStatus() != null) {
+                EmployeeAdditionalDetails details = EmployeeAdditionalDetailsDao.instance().find(emp);
+                if (details != null) {
+                    details.setMaritalStatus(setMaritalStatus(personalInfoDto.getMaritalStatus()));
+                    EmployeeAdditionalDetailsDao.instance().getEntityManager().merge(details);
+                } else {
+                    EmployeeAdditionalDetails addtnDetails = new EmployeeAdditionalDetails();
+                    addtnDetails.setMaritalStatus(setMaritalStatus(personalInfoDto.getMaritalStatus()));
+                    addtnDetails.setEthnicity(Ethnicity.Unspecified);
+                    addtnDetails.setReferredBy("Unknown");
+                    addtnDetails.setTargetEntityId(emp.getId());
+                    addtnDetails.setTargetEntityName(Employee.class.getCanonicalName());
+                    EmployeeAdditionalDetailsDao.instance().save(addtnDetails);
+                }
+            }
+            EmployeeDao.instance().getEntityManager().merge(emp);
+        }
+        return dto;
+    }
 
 //    @PUT
-//    @Path("save-immigration-info/{invitationCode}")
-//    public EmployeeH1BDetailsDto savePersonalInfo(@PathParam("invitationCode") String invitationCode, EmployeeH1BDetailsDto dto) {
+//    @Path("save-edu-info2/{invitationCode}")
+//    public EmployeeH1BDetailsDto saveEducatnRecInfo(@PathParam("invitationCode") String invitationCode, EmployeeH1BDetailsDto dto) {
+//        ImmigrationCase immiCase = getCase(invitationCode);
+//        EducationRecord record = new EducationRecord();
+//        if (dto.getEduRecord() != null) {
+//            EducationRecord immiEduRec = dto.getEduRecord();
+//            record.setAddress(immiEduRec.getAddress());
+//            record.setDateDegreeAwarded(immiEduRec.getDateDegreeAwarded());
+//            record.setDegreeOfStudy(immiEduRec.getDegreeOfStudy());
+//            record.setFieldOfStudy(immiEduRec.getFieldOfStudy());
+//            record.setNameOfSchool(immiEduRec.getNameOfSchool());
+//            record.setTargetEntityId(immiCase.getId());
+//            record.setTargetEntityName(ImmigrationCase.class.getCanonicalName());
+//            EducationRecordDao.instance().save(record);
+//        }
 //        return dto;
+//    }
+
+    private MaritalStatus setMaritalStatus(String maritalStatus) {
+        MaritalStatus status = null;
+        switch (maritalStatus) {
+            case "Married":
+                status = MaritalStatus.Married;
+                break;
+            case "Unmarried":
+                status = MaritalStatus.Unmarried;
+                break;
+            case "Unknown":
+                status = MaritalStatus.Unknown;
+                break;
+        }
+        return status;
+    }
+
+    public EmailType getWorkEmailType() {
+        Query getEmailType = EmailDao.instance().getEntityManager().createQuery("from " + EmailType.class.getCanonicalName()
+                + " where emailType=:emailTypeParam");
+        getEmailType.setParameter("emailTypeParam", "Work");
+        if (getEmailType.getResultList().size() > 0) {
+            return (EmailType) getEmailType.getResultList().get(0);
+        } else {
+            EmailType workEmailType = new EmailType();
+            workEmailType.setEmailType("Work");
+            return EmailDao.instance().getEntityManager().merge(workEmailType);
+        }
+    }
+
+    private Employee getEmployee(String invitationCode) {
+        InviteCode code = getInviteCode(invitationCode);
+        Employee emp;
+        if (EmployeeDao.instance().findByEmail(code.getEmail()) != null) {
+            emp = EmployeeDao.instance().findByEmail(code.getEmail());
+            return emp;
+        }
+        return null;
+    }
+
+    private InviteCode getInviteCode(String invitationCode) {
+        String invCde = invitationCode.trim();
+        return InviteCodeDao.instance().find(invCde);
+    }
+
+//    private ImmigrationCase getCase(String invitationCode) {
+//        InviteCode code = getInviteCode(invitationCode);
+//        Employee emp = getEmployee(code.getInvitationCode());
+//        List<ImmigrationCase> immigrationCases = immigrationCaseDao.instance().getImmigrationCases(emp, 0, 10);
+//        for (ImmigrationCase iCase : immigrationCases) {
+//            if (iCase.getEmail().equals(code.getEmail())) {
+//                return iCase;
+//            }
+//        }
+//        return null;
 //    }
 
     @PUT
