2f8e332f635315321704a1739d4482131969e950
radhika.mukkala@sstech.us <>
2016-11-18 19:00:10 +0530

    design questionnaire UI page 1

---
+++ office-master/office-jrs/src/main/java/info/yalamanchili/office/jrs/profile/immigration/EmployeeH1BDetailsDto.java
@@ -0,0 +1,44 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.jrs.profile.immigration;
+
+import info.yalamanchili.office.entity.immigration.EducationRecord;
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *
+ * @author radhika.mukkala
+ */
+@XmlRootElement
+@XmlType
+public class EmployeeH1BDetailsDto implements Serializable{
+    
+    private PersonalInfoDto empPersonalInfo;
+    
+    private EducationRecord eduRecord;
+
+    public PersonalInfoDto getEmpPersonalInfo() {
+        return empPersonalInfo;
+    }
+
+    public void setEmpPersonalInfo(PersonalInfoDto empPersonalInfo) {
+        this.empPersonalInfo = empPersonalInfo;
+    }
+    
+     public EducationRecord getEduRecord() {
+        return eduRecord;
+    }
+
+    public void setEduRecord(EducationRecord eduRecord) {
+        this.eduRecord = eduRecord;
+    }
+
+}
