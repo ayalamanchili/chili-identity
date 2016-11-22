ffcfd244e4102cb64ed4c3ba4e0c54589be1b7b4
radhika.mukkala@sstech.us <>
2016-11-22 19:25:26 +0530

    option to save emp other names info

---
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateOtherNamesInfoPopupPanel.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.client.profile.employee;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
+import info.chili.gwt.callback.ALAsyncCallback;
+import info.chili.gwt.crud.UpdateComposite;
+import info.chili.gwt.fields.DataType;
+import info.chili.gwt.rpc.HttpService;
+import info.chili.gwt.utils.Alignment;
+import info.chili.gwt.widgets.GenericPopup;
+import info.chili.gwt.widgets.ResponseStatusWidget;
+import info.yalamanchili.office.client.OfficeWelcome;
+import info.yalamanchili.office.client.resources.OfficeImages;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author radhika.mukkala
+ */
+public class UpdateOtherNamesInfoPopupPanel extends UpdateComposite {
+
+    private static Logger logger = Logger.getLogger(UpdateOtherNamesInfoPopupPanel.class.getName());
+
+    public UpdateOtherNamesInfoPopupPanel(String entityId) {
+        initUpdateComposite(entityId, "OtherNamesInfo", OfficeWelcome.constants);
+    }
+
+    @Override
+    protected JSONObject populateEntityFromFields() {
+        JSONObject otherNamesInfoObj = new JSONObject();
+        assignEntityValueFromField("firstName", otherNamesInfoObj);
+        assignEntityValueFromField("lastName", otherNamesInfoObj);
+        assignEntityValueFromField("middleName", otherNamesInfoObj);
+        entity.put("otherNamesInfo", otherNamesInfoObj);
+        return entity;
+    }
+
+    @Override
+    protected void updateButtonClicked() {
+        HttpService.HttpServiceAsync.instance().doPut(updateOtherNamesInfo(), entity.toString(),
+                OfficeWelcome.instance().getHeaders(), true, new AsyncCallback<String>() {
+                    @Override
+                    public void onFailure(Throwable arg0) {
+                        handleErrorResponse(arg0);
+                    }
+
+                    @Override
+                    public void onSuccess(String arg0) {
+                        postUpdateSuccess(arg0);
+                    }
+                });
+    }
+
+    @Override
+    public void populateFieldsFromEntity(JSONObject entity) {
+        if (entity.containsKey("otherNamesInfo")) {
+            JSONObject empPersoanlInfo = entity.get("otherNamesInfo").isObject();
+            assignFieldValueFromEntity("firstName", empPersoanlInfo, DataType.STRING_FIELD);
+            assignFieldValueFromEntity("lastName", empPersoanlInfo, DataType.STRING_FIELD);
+            assignFieldValueFromEntity("middleName", empPersoanlInfo, DataType.STRING_FIELD);
+        }
+    }
+
+    @Override
+    protected void postUpdateSuccess(String result) {
+        GenericPopup.hideIfOpen();
+        RootPanel.get().clear();
+        RootPanel.get().add(new Image(OfficeImages.INSTANCE.logo()));
+        RootPanel.get().add(new H1bQuestionnaireWidget(entityId));
+        new ResponseStatusWidget().show("Successfully  Updated Other Names Info");
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Override
+    protected void addWidgets() {
+        addField("firstName", false, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("lastName", false, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+        addField("middleName", false, false, DataType.STRING_FIELD, Alignment.HORIZONTAL);
+    }
+
+    @Override
+    protected void addWidgetsBeforeCaptionPanel() {
+    }
+
+    @Override
+    protected String getURI() {
+        return OfficeWelcome.constants.root_url() + "immigrationcase/h1b-questionnaire/get-details/?invitationCode=" + entityId;
+    }
+
+    @Override
+    public void loadEntity(String entityId) {
+        HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
+                new ALAsyncCallback<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        entity = (JSONObject) JSONParser.parseLenient(response);
+                        populateFieldsFromEntity(entity);
+                    }
+                });
+    }
+
+    protected String updateOtherNamesInfo() {
+        return OfficeWelcome.constants.root_url() + "immigrationcase/save-other-names-info/" + entityId;
+    }
+}
