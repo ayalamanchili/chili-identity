af844f68f90100502545ff44575577adff99c2cf
radhika.mukkala@sstech.us <>
2016-12-01 18:36:14 +0530

    refactor Questionnaire UI page - I

--- office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateAlienNoPopupPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateAlienNoPopupPanel.java
@@ -5,12 +5,10 @@
  */
 package info.yalamanchili.office.client.profile.employee;
 
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
 import info.chili.gwt.callback.ALAsyncCallback;
@@ -29,16 +27,19 @@ import java.util.logging.Logger;
  * @author Ramana.Lukalapu
  */
 public class UpdateAlienNoPopupPanel extends UpdateComposite {
-
+    
     private static Logger logger = Logger.getLogger(UpdateAlienNoPopupPanel.class.getName());
-
-    Button saveNextAlien = new Button("Save and Next");
-    boolean saveNextalienValue = false;
-
+    boolean isReadPanel = false;
+    
     public UpdateAlienNoPopupPanel(String entityId) {
         initUpdateComposite(entityId, "AlienNo", OfficeWelcome.constants);
     }
-
+    
+    public UpdateAlienNoPopupPanel(String entityId, boolean isReadPanel) {
+        this.isReadPanel = isReadPanel;
+        initUpdateComposite(entityId, "AlienNo", OfficeWelcome.constants);
+    }
+    
     @Override
     protected JSONObject populateEntityFromFields() {
         JSONObject alienNoObj = new JSONObject();
@@ -46,7 +47,7 @@ public class UpdateAlienNoPopupPanel extends UpdateComposite {
         entity.put("alienNumber", alienNoObj);
         return entity;
     }
-
+    
     @Override
     protected void updateButtonClicked() {
         JSONObject entity = populateEntityFromFields();
@@ -56,14 +57,14 @@ public class UpdateAlienNoPopupPanel extends UpdateComposite {
                     public void onFailure(Throwable arg0) {
                         handleErrorResponse(arg0);
                     }
-
+                    
                     @Override
                     public void onSuccess(String arg0) {
                         postUpdateSuccess(arg0);
                     }
                 });
     }
-
+    
     @Override
     public void populateFieldsFromEntity(JSONObject entity) {
         if (entity.containsKey("alienNumber")) {
@@ -71,44 +72,44 @@ public class UpdateAlienNoPopupPanel extends UpdateComposite {
             assignFieldValueFromEntity("alienNumber", alienNumber, DataType.STRING_FIELD);
         }
     }
-
+    
     @Override
     protected void postUpdateSuccess(String result) {
         GenericPopup.hideIfOpen();
         RootPanel.get().clear();
         RootPanel.get().add(new Image(OfficeImages.INSTANCE.logo()));
         RootPanel.get().add(new H1bQuestionnaireWidget(entityId));
-        if (saveNextalienValue == true) {
-            new GenericPopup(new UpdateEducationRecord1PopupPanel(entityId), 200, 200).show();
-        }
         new ResponseStatusWidget().show("Successfully  Updated Alien Number");
     }
-
+    
     @Override
     protected void addListeners() {
-        saveNextAlien.addClickHandler(this);
     }
-
+    
     @Override
     protected void configure() {
-        update.setText("Save");
+        if (isReadPanel == false) {
+            update.setText("Save");
+            update.setVisible(true);
+        } else {
+            update.setVisible(false);
+        }
     }
-
+    
     @Override
     protected void addWidgets() {
-        addField("alienNumber", false, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
-        entityActionsPanel.add(saveNextAlien);
+        addField("alienNumber", isReadPanel, true, DataType.STRING_FIELD, Alignment.HORIZONTAL);
     }
-
+    
     @Override
     protected void addWidgetsBeforeCaptionPanel() {
     }
-
+    
     @Override
     protected String getURI() {
         return OfficeWelcome.constants.root_url() + "immigrationcase/h1b-questionnaire/get-details/page-1?invitationCode=" + entityId;
     }
-
+    
     @Override
     public void loadEntity(String entityId) {
         HttpService.HttpServiceAsync.instance().doGet(getURI(), OfficeWelcome.instance().getHeaders(), true,
@@ -124,17 +125,8 @@ public class UpdateAlienNoPopupPanel extends UpdateComposite {
                     }
                 });
     }
-
+    
     protected String updateAlienNumber() {
         return URL.encode(OfficeWelcome.constants.root_url() + "immigrationcase/save-alien-info/" + entityId);
-    }
-
-    @Override
-    public void onClick(ClickEvent event) {
-        if (event.getSource().equals(saveNextAlien)) {
-            saveNextalienValue = true;
-            updateButtonClicked();
-        }
-        super.onClick(event);
     }
 }
