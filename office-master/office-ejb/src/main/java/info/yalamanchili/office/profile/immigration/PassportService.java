45e83d1fb628e72d8fb504a7a9aefe3db75d3d75
radhika.mukkala@sstech.us <>
2016-12-04 17:39:03 +0530

    suport for passport info save

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/immigration/PassportService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/immigration/PassportService.java
@@ -13,9 +13,13 @@ package info.yalamanchili.office.profile.immigration;
  * @author Madhu.Badiginchala
  */
 import info.yalamanchili.office.dao.profile.EmployeeDao;
+import info.yalamanchili.office.dao.profile.immigration.ImmigrationCaseDao;
 import info.yalamanchili.office.dao.profile.immigration.PassportDao;
+import info.yalamanchili.office.entity.immigration.ImmigrationCase;
+import info.yalamanchili.office.entity.immigration.OtherNamesInfo;
 import info.yalamanchili.office.entity.immigration.Passport;
 import info.yalamanchili.office.entity.profile.Employee;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.dozer.Mapper;
@@ -30,7 +34,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("request")
 public class PassportService {
-    
+
     @PersistenceContext
     protected EntityManager em;
     @Autowired
@@ -39,12 +43,33 @@ public class PassportService {
     protected PassportDao passportDao;
     @Autowired
     protected EmployeeDao employeeDao;
-    
+    @Autowired
+    protected ImmigrationCaseDao caseDao;
+
     public Passport savePassport(Long empId, Passport dto) {
         Passport passport = mapper.map(dto, Passport.class);
         Employee emp = employeeDao.findById(empId);
         passportDao.save(passport, emp.getId(), emp.getClass().getCanonicalName());
         return passport;
     }
-    
+
+    public Passport savePassportForCase(Long caseId, Passport passport) {
+        List<Passport> passportRecs = PassportDao.instance().findAll(caseId, ImmigrationCase.class.getCanonicalName());
+        if (passportRecs != null && passportRecs.size() > 0) {
+            Passport passportinfo = passportRecs.get(0);
+            passportinfo.setDateOfBirth(passport.getDateOfBirth());
+            passportinfo.setCountryOfBirth(passport.getCountryOfBirth());
+            passportinfo.setStateOfBirth(passport.getStateOfBirth());
+            passportinfo.setPassportNumber(passport.getPassportNumber());
+            passportinfo.setPassportCountryOfIssuance(passport.getPassportCountryOfIssuance());
+            passportinfo.setPassportStateOfIssuance(passport.getPassportStateOfIssuance());
+            passportinfo.setPassportIssuedDate(passport.getPassportIssuedDate());
+            passportinfo.setPassportExpiryDate(passport.getPassportExpiryDate());
+            return passportDao.getEntityManager().merge(passportinfo);
+        } else {
+            Passport save = passportDao.save(passport);
+            return save;
+        }
+    }
+
 }
