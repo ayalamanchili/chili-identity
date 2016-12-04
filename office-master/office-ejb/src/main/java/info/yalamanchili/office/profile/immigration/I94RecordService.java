cb181ff4219d9c273fe4952429a7ea1fa16882d4
radhika.mukkala@sstech.us <>
2016-12-04 18:47:53 +0530

    support for i94 record save to case

--- office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/immigration/I94RecordService.java
+++ office-master/office-ejb/src/main/java/info/yalamanchili/office/profile/immigration/I94RecordService.java
@@ -10,8 +10,12 @@ package info.yalamanchili.office.profile.immigration;
 
 import info.yalamanchili.office.dao.profile.EmployeeDao;
 import info.yalamanchili.office.dao.profile.immigration.I94RecordDao;
+import info.yalamanchili.office.entity.immigration.ImmigrationCase;
+import info.yalamanchili.office.entity.immigration.Passport;
 import info.yalamanchili.office.entity.immigration.i94Record;
 import info.yalamanchili.office.entity.profile.Employee;
+import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.dozer.Mapper;
@@ -42,4 +46,22 @@ public class I94RecordService {
         return i94RecordS;
     }
     
+    public i94Record saveI94RecForCase(Long caseId, i94Record record) {
+        List<i94Record> i94Recs = i94RecordDao.findAll(caseId, ImmigrationCase.class.getCanonicalName());
+        if (i94Recs != null && i94Recs.size() > 0) {
+            i94Record rec = i94Recs.get(0);
+            rec.setI94RecordNumber(record.getI94RecordNumber());
+            rec.setAdmitUntilDate(record.getAdmitUntilDate());
+            rec.setI94ValidFromDate(record.getI94ValidFromDate());
+            rec.setPortOfEntry(record.getPortOfEntry());
+            rec.setDateofEntry(record.getDateofEntry());
+            rec.setClassOfAdmission(record.getClassOfAdmission());
+            return i94RecordDao.getEntityManager().merge(rec);
+        } else {
+            record.setPortOfEntry("Port Of Entry");
+            record.setDateofEntry(new Date("01/01/0001"));
+            i94Record save = i94RecordDao.save(record);
+            return save;
+        }
+    }
 }
