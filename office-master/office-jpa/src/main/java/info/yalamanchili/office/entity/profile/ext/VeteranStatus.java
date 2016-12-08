8a67e99322f4a47066fc2a4bb7fb6820ea49314e
Sudharani bandaru <sudharani.bandaru@SSTECHLAPTOP362.sstech.internal>
2016-12-08 16:00:20 -0500

    Self identification form changes-Disability and Veteran status.

---
+++ office-master/office-jpa/src/main/java/info/yalamanchili/office/entity/profile/ext/VeteranStatus.java
@@ -0,0 +1,22 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.yalamanchili.office.entity.profile.ext;
+
+/**
+ *
+ * @author sudharani.bandaru
+ */
+public enum VeteranStatus {
+ 
+    One_or_more_of_the_classifications,
+    Veteran_but_not_a_protected_veteran,
+    Not_a_veteran,
+    Do_not_wish;
+    
+}
