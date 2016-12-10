6a9d7aa8cc56f7e6b9f4f3690a56c19f36ffc277
radhika.mukkala@sstech.us <>
2016-12-10 20:02:25 +0530

    questionnaire page - 3

--- office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/AddressTypeDao.java
+++ office-master/office-dao/src/main/java/info/yalamanchili/office/dao/profile/AddressTypeDao.java
@@ -4,10 +4,12 @@
 package info.yalamanchili.office.dao.profile;
 
 import info.chili.dao.CRUDDao;
+import info.chili.spring.SpringContext;
 import info.yalamanchili.office.entity.profile.AddressType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import org.springframework.context.annotation.Scope;
 
 import org.springframework.stereotype.Repository;
@@ -26,5 +28,22 @@ public class AddressTypeDao extends CRUDDao<AddressType> {
     @Override
     public EntityManager getEntityManager() {
         return em;
+    }
+    
+    public static AddressTypeDao instance() {
+        return SpringContext.getBean(AddressTypeDao.class);
+    }
+
+    public AddressType getAddressType(String addrressType) {
+        Query getAddrType = em.createQuery("from " + AddressType.class.getCanonicalName()
+                + " where addressType=:addressTypeParam");
+        getAddrType.setParameter("addressTypeParam", addrressType);
+        if (getAddrType.getResultList().size() > 0) {
+            return (AddressType) getAddrType.getResultList().get(0);
+        } else {
+            AddressType addressType = new AddressType();
+            addressType.setAddressType(addrressType);
+            return em.merge(addressType);
+        }
     }
 }
