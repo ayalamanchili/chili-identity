926c77d99a8420c5431dca33a082680273651865
radhika.mukkala@sstech.us <>
2016-12-12 17:20:03 +0530

    questionnaire page - 3 us address save issue

--- office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateUSAddressPopupPanel.java
+++ office-web/src/main/java/info/yalamanchili/office/client/profile/employee/UpdateUSAddressPopupPanel.java
@@ -7,14 +7,11 @@ package info.yalamanchili.office.client.profile.employee;
 
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
-import com.google.gwt.json.client.JSONString;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -49,10 +46,7 @@ public class UpdateUSAddressPopupPanel extends TUpdateComposite implements Chang
 
     private static Logger logger = Logger.getLogger(UpdateUSAddressPopupPanel.class.getName());
     boolean isReadPanel = false;
-
-    protected static HTML homeAddr = new HTML("\n <strong style=\"color:#F31212\">Home Address</strong>\n");
-    BooleanField isHomeAddrF;
-
+    
     public UpdateUSAddressPopupPanel(String entityId) {
         initUpdateComposite(entityId, "Address", OfficeWelcome.constants2);
     }
@@ -77,35 +71,6 @@ public class UpdateUSAddressPopupPanel extends TUpdateComposite implements Chang
         assignEntityValueFromField("workPhone", usAddrObj);
         assignEntityValueFromField("isHomeAddress", usAddrObj);
         addrArray.set(0, usAddrObj);
-        if (isHomeAddrF.getValue() == false) {
-            JSONObject homeAddrObj = new JSONObject();
-            StringField homeStreet1 = (StringField) fields.get("homeStreet1");
-            if (homeStreet1.getValue() != null && !homeStreet1.getValue().trim().isEmpty()) {
-                homeAddrObj.put("street1", new JSONString(homeStreet1.getValue()));
-            }
-            StringField homeStreet2 = (StringField) fields.get("homeStreet2");
-            if (homeStreet2.getValue() != null && !homeStreet2.getValue().trim().isEmpty()) {
-                homeAddrObj.put("street2", new JSONString(homeStreet2.getValue()));
-            }
-            StringField homeCity = (StringField) fields.get("homeCity");
-            if (homeCity.getValue() != null && !homeCity.getValue().trim().isEmpty()) {
-                homeAddrObj.put("city", new JSONString(homeCity.getValue()));
-            }
-            StringField homeZip = (StringField) fields.get("homeZip");
-            if (homeZip.getValue() != null && !homeZip.getValue().trim().isEmpty()) {
-                homeAddrObj.put("zip", new JSONString(homeZip.getValue()));
-            }
-            EnumField homeState = (EnumField) fields.get("homeState");
-            if (homeState.getValue() != null && !homeState.getValue().trim().isEmpty()) {
-                homeAddrObj.put("state", new JSONString(homeState.getValue()));
-            }
-            EnumField homeCountry = (EnumField) fields.get("homeCountry");
-            if (homeCountry.getValue() != null && !homeCountry.getValue().trim().isEmpty()) {
-                homeAddrObj.put("country", new JSONString(homeCountry.getValue()));
-            }
-            homeAddrObj.put("isHomeAddress", new JSONString("true"));
-            addrArray.set(1, homeAddrObj);
-        }
         entity.put("usForeignAddrInfo", addrArray);
         return entity;
     }
@@ -139,27 +104,21 @@ public class UpdateUSAddressPopupPanel extends TUpdateComposite implements Chang
                     if (getAddressType(usAddrObj).equals("US")) {
                         populateFields(usAddrObj);
                     }
-                    if (getAddressType(usAddrObj).equals("Home")) {
-                        populateHomeFields(usAddrObj);
-                    }
                 }
             } else if (entity.get("usForeignAddrInfo") instanceof JSONObject) {
                 usAddrObj = entity.get("usForeignAddrInfo").isObject();
                 if (getAddressType(usAddrObj).equals("US")) {
                     populateFields(usAddrObj);
                 }
-                if (getAddressType(usAddrObj).equals("Home")) {
-                    populateHomeFields(usAddrObj);
-                }
             }
         }
     }
 
     private String getAddressType(JSONObject address) {
-        if (address != null && !address.get("addressType").isObject().get("addressType").isString().stringValue().equalsIgnoreCase("Foreign") && (address.containsKey("isHomeAddress") && address.get("isHomeAddress").isString().stringValue().equalsIgnoreCase("true"))) {
-            return "US";
+        if (address != null) {
+            return address.get("addressType").isObject().get("addressType").isString().stringValue();
         } else {
-            return "Home";
+            return null;
         }
     }
 
@@ -172,24 +131,14 @@ public class UpdateUSAddressPopupPanel extends TUpdateComposite implements Chang
         assignFieldValueFromEntity("country", address, DataType.ENUM_FIELD);
         assignFieldValueFromEntity("phoneNumber", address, DataType.LONG_FIELD);
         assignFieldValueFromEntity("workPhone", address, DataType.LONG_FIELD);
-        assignFieldValueFromEntity("isHomeAddress", address, DataType.BOOLEAN_FIELD);
+        BooleanField isHomeAddress = (BooleanField) fields.get("isHomeAddress");
+        if ("true".equalsIgnoreCase(JSONUtils.toString(address, "isHomeAddress"))) {
+            isHomeAddress.setValue(true);
+        } else if ("false".equalsIgnoreCase(JSONUtils.toString(address, "isHomeAddress"))) {
+            isHomeAddress.setValid(false);
+        }
     }
 
-    private void populateHomeFields(JSONObject address) {
-        addHomeAddrWidget();
-        StringField street1 = (StringField) fields.get("homeStreet1");
-        street1.setValue(JSONUtils.toString(address, "street1"));
-        StringField street2 = (StringField) fields.get("homeStreet2");
-        street2.setValue(JSONUtils.toString(address, "street2"));
-        StringField city = (StringField) fields.get("homeCity");
-        city.setValue(JSONUtils.toString(address, "street1"));
-        StringField zip = (StringField) fields.get("homeZip");
-        zip.setValue(JSONUtils.toString(address, "zip"));
-        EnumField state = (EnumField) fields.get("state");
-        state.selectValue(JSONUtils.toString(address, "homeState"));
-        EnumField country = (EnumField) fields.get("country");
-        country.selectValue(JSONUtils.toString(address, "homeCountry"));
-    }
 
     @Override
     protected void postUpdateSuccess(String result) {
@@ -205,8 +154,6 @@ public class UpdateUSAddressPopupPanel extends TUpdateComposite implements Chang
         if (usCountriesF != null) {
             usCountriesF.listBox.addChangeHandler(this);
         }
-        isHomeAddrF = (BooleanField) fields.get("isHomeAddress");
-        isHomeAddrF.getBox().addClickHandler(this);
     }
 
     @Override
@@ -223,8 +170,6 @@ public class UpdateUSAddressPopupPanel extends TUpdateComposite implements Chang
                 setVisibleLengthSize(e.getKey(), 25);
             }
         }
-        isHomeAddrF = (BooleanField) fields.get("isHomeAddress");
-        isHomeAddrF.setValue(Boolean.TRUE);
     }
 
     protected void configureLabel(Label l) {
@@ -319,71 +264,6 @@ public class UpdateUSAddressPopupPanel extends TUpdateComposite implements Chang
 
     protected String updatePersonalInfo() {
         return URL.encode(OfficeWelcome.constants.root_url() + "address-info/save-us-address-info/" + entityId);
-    }
-
-    @Override
-    public void onClick(ClickEvent event) {
-        isHomeAddrF = (BooleanField) fields.get("isHomeAddress");
-        if (event.getSource().equals(isHomeAddrF.getBox())) {
-            if (isHomeAddrF.getValue() == false) {
-                addHomeAddrWidget();
-            } else {
-                entityFieldsPanel.removeCell(6, 3);
-                entityFieldsPanel.removeCell(6, 2);
-                entityFieldsPanel.removeCell(6, 1);
-                entityFieldsPanel.removeCell(5, 3);
-                entityFieldsPanel.removeCell(5, 2);
-                entityFieldsPanel.removeCell(5, 1);
-                entityFieldsPanel.removeCell(4, 1);
-            }
-        }
-        super.onClick(event); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void addHomeAddrWidget() {
-        entityFieldsPanel.setWidget(4, 1, homeAddr);
-        addField("homeStreet1", isReadPanel, true, DataType.STRING_FIELD, Alignment.HORIZONTAL, 5, 1);
-        addField("homeStreet2", isReadPanel, false, DataType.STRING_FIELD, Alignment.HORIZONTAL, 5, 2);
-        addField("homeCity", isReadPanel, true, DataType.STRING_FIELD, Alignment.HORIZONTAL, 5, 3);
-        addEnumField("homeState", isReadPanel, true, USAStatesFactory.getStates().toArray(new String[0]), Alignment.HORIZONTAL, 6, 2);
-        addEnumField("homeCountry", isReadPanel, true, CountryFactory.getCountries().toArray(new String[0]), Alignment.HORIZONTAL, 6, 1);
-        //addEnumField("homeState", isReadPanel, true, USAStatesFactory.getStates().toArray(new String[0]), Alignment.HORIZONTAL, 6, 2);
-        addField("homeZip", isReadPanel, true, DataType.STRING_FIELD, Alignment.HORIZONTAL, 6, 3);
-        usStatesF = (EnumField) fields.get("homeState");
-        if (isReadPanel == false) {
-            usCountriesF = (EnumField) fields.get("homeCountry");
-            usCountriesF.listBox.addChangeHandler(this);
-            String service = usCountriesF.getValue();
-            switch (service) {
-                case "USA":
-                    addEnumField("homeState", isReadPanel, true, USAStatesFactory.getStates().toArray(new String[0]), Alignment.HORIZONTAL, 6, 2);
-                    break;
-                case "INDIA":
-                    addEnumField("homeState", isReadPanel, true, IndiaStatesFactory.getStates().toArray(new String[0]), Alignment.HORIZONTAL, 6, 2);
-                    break;
-                case "CANADA":
-                    addEnumField("homeState", isReadPanel, true, CanadaStatesFactory.getStates().toArray(new String[0]), Alignment.HORIZONTAL, 6, 2);
-                    break;
-            }
-        }
-        doConfigureAddress();
-        doAlignFields(200);
-
-    }
-
-    private void doConfigureAddress() {
-        StringField homeStreet1 = (StringField) fields.get("homeStreet1");
-        configureLabel(homeStreet1.getLabel());
-        StringField homeStreet2 = (StringField) fields.get("homeStreet2");
-        configureLabel(homeStreet2.getLabel());
-        StringField homeCity = (StringField) fields.get("homeCity");
-        configureLabel(homeCity.getLabel());
-        EnumField homeState = (EnumField) fields.get("homeState");
-        configureLabel(homeState.getLabel());
-        EnumField homeCountry = (EnumField) fields.get("homeCountry");
-        configureLabel(homeCountry.getLabel());
-        StringField homeZip = (StringField) fields.get("homeZip");
-        configureLabel(homeZip.getLabel());
     }
 
     @Override
