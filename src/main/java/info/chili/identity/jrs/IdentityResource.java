2340925c121e5996920aa136e06e9e86d4e0256f
phani.y@sstech.us <phani.y@SSTECHLAPTOP363.sstech.internal>
2017-02-25 21:41:57 -0500

    login

---
+++ src/main/java/info/chili/identity/jrs/IdentityResource.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.chili.identity.jrs;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author phani.y
+ */
+@RestController
+public class IdentityResource {
+
+    
+}
--- src/main/java/info/chili/identity/security/jwt/TokenAuthenticationService.java
+++ src/main/java/info/chili/identity/security/jwt/TokenAuthenticationService.java
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 package info.chili.identity.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import java.io.IOException;
 import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,34 +21,45 @@ import java.util.Date;
  */
 public class TokenAuthenticationService {
 
-     private long EXPIRATIONTIME = 1000 * 60 * 60 * 24 * 10; // 10 days
-     private String secret = "ThisIsASecret";
-     private String tokenPrefix = "Bearer";
-     private String headerString = "Authorization";
-     public void addAuthentication(HttpServletResponse response, String username) {
-         // We generate a token now.
-         String JWT = Jwts.builder()
-             .setSubject(username)
-             .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
-             .signWith(SignatureAlgorithm.HS512, secret)
-             .compact();
-         response.addHeader(headerString, tokenPrefix + " " + JWT);
-     }
+    private long EXPIRATIONTIME = 1000 * 60 * 60 * 24 * 10; // 10 days
+    private String secret = "ThisIsASecret";
+    private String tokenPrefix = "Bearer";
+    private String headerString = "Authorization";
 
-     public Authentication getAuthentication(HttpServletRequest request) {
-         String token = request.getHeader(headerString);
-         if (token != null) {
-             // parse the token.
-             String username = Jwts.parser()
-                 .setSigningKey(secret)
-                 .parseClaimsJws(token)
-                 .getBody()
-                 .getSubject();
-             if (username != null) // we managed to retrieve a user
-             {
-                 return new AuthenticatedUser(username);
-             }
-         }
-         return null;
-     }
- }
+    public void addAuthentication(HttpServletResponse response, Authentication user) {
+        // We generate a token now.
+        String JWT = Jwts.builder()
+                .setSubject(user.getName())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+        response.addHeader(headerString, tokenPrefix + " " + JWT);
+        response.addHeader("Access-Control-Allow-Origin", "**");
+        addUser(response, user);
+    }
+
+    protected void addUser(HttpServletResponse response, Authentication user) {
+        try {
+            response.getWriter().print(new ObjectMapper().writeValueAsString(user));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public Authentication getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(headerString);
+        if (token != null) {
+            // parse the token.
+            String username = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+            if (username != null) // we managed to retrieve a user
+            {
+                return new AuthenticatedUser(username);
+            }
+        }
+        return null;
+    }
+}
